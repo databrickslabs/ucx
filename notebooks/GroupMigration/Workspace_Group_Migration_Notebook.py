6e130706e547b9a23b8e92af874ea3518031cb81
renardeinside <polarpersonal@gmail.com>
2023-06-02 19:09:36 +0200

    apply fixes

--- notebooks/GroupMigration/Workspace_Group_Migration_Notebook.py
+++ notebooks/GroupMigration/Workspace_Group_Migration_Notebook.py
@@ -75,22 +75,21 @@
 # COMMAND ----------
 
 import sys
-
-from tempfile import NamedTemporaryFile
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 ipython = get_ipython()
 
 print("Installing the uc-upgrade package and it's dependencies")
 
 with NamedTemporaryFile(suffix="-uc-upgrade-requirements.txt") as requirements_file:
-  print(f"Writing requirements to file {requirements_file.name}")
-  ipython.run_cell_magic("sh", "", f"poetry export --output={requirements_file.name} --without-hashes")
-  print("Saved the requirements to a provided file, installing them with pip")
-  ipython.run_line_magic("pip", f"install -r {requirements_file.name}")
-  print("Requirements installed successfully, restarting Python interpreter")
-  dbutils.library.restartPython()
-  print("Python interpreter restarted successfully")
+    print(f"Writing requirements to file {requirements_file.name}")
+    ipython.run_cell_magic("sh", "", f"poetry export --output={requirements_file.name} --without-hashes")
+    print("Saved the requirements to a provided file, installing them with pip")
+    ipython.run_line_magic("pip", f"install -r {requirements_file.name}")
+    print("Requirements installed successfully, restarting Python interpreter")
+    dbutils.library.restartPython()
+    print("Python interpreter restarted successfully")
 
 print("Reloading the path-based modules")
 ipython.run_line_magic("load_ext", "autoreload")
@@ -103,13 +102,16 @@ sys.path.append(project_root)
 
 print("Verifying that package can be properly loaded")
 try:
-  from uc_upgrade.group_migration import GroupMigration
-  print(f"Successfully loaded the uc-upgrade package")
+    from uc_upgrade.group_migration import GroupMigration
+
+    print(f"Successfully loaded the uc-upgrade package")
 except Exception as e:
-  print("Unable to import the UC migration utilities package from source. Please check that you've imported the whole repository and not just copied one file.")
-  print("Also check that you have the Files in Repos activated, e.g. use DBR 11.X+")
-  print("Original exception:")
-  print(e)
+    print(
+        "Unable to import the UC migration utilities package from source. Please check that you've imported the whole repository and not just copied one file."
+    )
+    print("Also check that you have the Files in Repos activated, e.g. use DBR 11.X+")
+    print("Original exception:")
+    print(e)
 
 # COMMAND ----------
 
