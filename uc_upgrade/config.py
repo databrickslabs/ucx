5532df34a8015edba9fa563ae4af247349311e90
renardeinside <polarpersonal@gmail.com>
2023-07-17 09:38:38 +0200

    remove scratches from repo

--- uc_upgrade/config.py
+++ uc_upgrade/config.py
@@ -2,9 +2,6 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any
-
-from databricks.sdk.runtime import *  # noqa: F403
 
 
 @dataclass
@@ -13,15 +10,9 @@ class AuthConfig:
     workspace_host: str
     account_console_host: str
 
-    @property
-    def _notebook_context(self) -> Any:
-        return dbutils.notebook.entry_point.getDbutils().notebook().getContext()  # noqa: F405
-
     @classmethod
-    def from_runtime(cls) -> AuthConfig:
-        _token = cls._notebook_context.apiUrl().getOrElse(None)
-        _host = cls._notebook_context.apiToken().getOrElse(None)
-        return AuthConfig(token=_token, workspace_host=_host, account_console_host="")
+    def from_string(cls, token: str):
+        pass
 
 
 @dataclass
