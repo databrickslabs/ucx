2794849fdbba285896628f01f66ba410faad92f1
renardeinside <polarpersonal@gmail.com>
2023-07-19 15:21:56 +0200

    add builder and installator

--- notebooks/common.py
+++ notebooks/common.py
@@ -1,32 +1,47 @@
+import os
+import shutil
+import subprocess
+import tempfile
 from pathlib import Path
-import importlib.util
-import sys
 
 
-def install_uc_migration_toolkit():
-    # these libraries are preinstalled on DBR
-    import tomli  # noqa: F401
-    from databricks.sdk.runtime import dbutils  # noqa: F401
-
-    # this function is provided in Databricks runtime
+def install_uc_migration_toolkit(parent_folder_path):
+    # Save the original working directory
+    original_cwd = Path.cwd()
     ipython = get_ipython()  # noqa: F821
+    from databricks.sdk.runtime import dbutils  # noqa: F821, E402
+    # Create a temporary directory with the prefix 'hatch-build'
+    with tempfile.TemporaryDirectory(prefix='hatch-build') as temp_dir:
+        temp_path = Path(temp_dir)
 
-    project_file = Path("../pyproject.toml").absolute()
-    dependency_string = " ".join(f"'{d}'" for d in tomli.loads(project_file.read_text())['project']['dependencies'])
-    ipython.run_line_magic("pip", f"install {dependency_string}")
-    dbutils.library.restartPython()
+        # Copy the content of the parent folder into the temporary directory
+        parent_folder_path = Path(parent_folder_path)
+        for item in parent_folder_path.iterdir():
+            if os.access(item, os.R_OK):
+                if item.is_file():
+                    shutil.copy2(item, temp_path / item.name)
+                elif item.is_dir():
+                    shutil.copytree(item, temp_path / item.name)
 
-    print("adding module to the system path")
-    module_src_path = Path(f"../src")
-    module_root_path = (module_src_path / "uc_migration_toolkit")
-    module_init_path = (module_src_path / "uc_migration_toolkit/__init__.py")
+        # Change the working directory to the temporary directory
+        os.chdir(temp_dir)
 
-    for _path in [module_src_path, module_root_path, module_init_path]:
-        sys.path.append(str(_path))
+        # Run the 'hatch' command with the specified arguments
+        try:
+            result = subprocess.run(["hatch", "-c", "-t", "wheel"], capture_output=True, text=True)
 
-    try:
-        import uc_migration_toolkit
-        from uc_migration_toolkit.config import MigrationConfig
-    except ImportError as e:
-        print("Failed to import uc_migration_toolkit")
-        raise e
+            if result.returncode != 0:
+                # Raise an exception with the error message from the shell
+                raise Exception(result.stderr.strip())
+
+            wheel_file = list(temp_path.glob("dist/*.whl"))[0]
+            ipython.run_cell_magic("pip", f"install {str(wheel_file.absolute())}")
+            dbutils.library.restartPython()
+
+        except Exception as e:
+            # Return to the original working directory before raising the exception
+            os.chdir(original_cwd)
+            raise e
+
+    # Return to the original working directory
+    os.chdir(original_cwd)
