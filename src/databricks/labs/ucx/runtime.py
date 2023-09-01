19dc74f156d8029c1749289503c0d52c71df1a04
Serge Smertin <serge.smertin@databricks.com>
2023-09-01 21:33:35 +0200

    skip tests

--- src/databricks/labs/ucx/runtime.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from databricks.sdk import WorkspaceClient
-
-
-def main():
-    ws = WorkspaceClient(auth_type="runtime")
-    print(f"run as {ws.current_user.me()}")
-    print(f"Invoked from job: {sys.argv}")
