1fa12c6de0acb981b3c104a23a8559ee2b2dfa80
Lars George <lars.george@databricks.com>
2023-09-05 10:42:41 -0700

    Externalize Scala script.

--- src/databricks/labs/ucx/toolkits/assessment.py
+++ src/databricks/labs/ucx/toolkits/assessment.py
@@ -1,4 +1,3 @@
-
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.compute import Language
 
@@ -29,38 +28,14 @@ class Assessment:
     def table_inventory(self):
         commands = CommandExecutor(self._ws, language=Language.SCALA)
 
-        command_output = commands.run(
-            f"""
-             import org.apache.spark.sql.catalyst.catalog.{{CatalogTable, CatalogTableType}};
-             import org.apache.spark.sql.catalyst.TableIdentifier;
-             import java.io.{{FileWriter, BufferedWriter, File}};
-             val bw = new BufferedWriter(new FileWriter(new File("/tmp/metastore_schema.csv"), true));
+        from importlib import resources as impresources
+        from databricks.labs.ucx.assessment import scala
 
-             bw.write("db,table,format,type,table_location,created_version,created_time,last_access,lib,inputformat,outputformat\\n");
-             val dbs = spark.sharedState.externalCatalog.listDatabases();
-             for( db <- dbs) {{
-               //println(s"database: ${{db}}")
-               val tables = spark.sharedState.externalCatalog.listTables(db);
-               for (t <- tables) {{
-                 try {{
-                   //println(s"table: ${{t}}")
-                   val table: CatalogTable = spark.sharedState.externalCatalog.getTable(db = db, table = t);
-                   val row = s"${{db}},${{t}},${{table.provider.getOrElse("Unknown")}},${{table.tableType.name}},${{table.storage.locationUri.getOrElse("None")}},${{table.createVersion}},${{table.createTime}},${{table.lastAccessTime}},${{table.storage.serde.getOrElse("Unknown")}},${{table.storage.inputFormat.getOrElse("Unknown")}},${{table.storage.outputFormat.getOrElse("Unknown")}}\\n";
-                   bw.write(row);
-                 }} catch {{
-                   case e: Exception => bw.write(s"${{db}},${{t}},Unknown,Unknown,NONE,,,,,,,\\n");
-                 }}
-               }}
+        inp_file = (impresources.files(scala) / 'assessment.scala')
+        with inp_file.open("rt") as f:
+            template = f.read()
 
-             }}
-
-             bw.close;
-             spark.sql("create catalog if not exists {self._inventory_catalog}");
-             spark.sql("create database if not exists {self._inventory_catalog}.{self._inventory_schema}");
-             val tables_df = spark.read.option("header","true").option("inferSchema","true").csv("/tmp/metastore_schema.csv");
-             tables_df.write.mode("overwrite").saveAsTable("{self._inventory_catalog}.{self._inventory_schema}.hms_tables");
-            """
-        )
+        command_output = commands.run(template)
         print(command_output)
 
     def external_locations(self):
