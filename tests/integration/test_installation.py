df23fe70d1874cc3acb73ef133aac707ee8e7886
Sara Ivanyos <sara.ivanyos@datapao.com>
2023-09-06 22:42:44 +0200

    draft commit

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -1,12 +1,17 @@
+import logging
 import shutil
 import subprocess
 import sys
+from io import BytesIO
 from pathlib import Path
 
 import pytest
+from databricks.sdk.service import compute, jobs
 from databricks.sdk.service.workspace import ImportFormat
 
 from databricks.labs.ucx.providers.mixins.compute import CommandExecutor
+
+logging.getLogger("databricks.sdk").setLevel("DEBUG")
 
 
 @pytest.fixture
@@ -85,3 +90,153 @@ def test_sql_backend_works(ws, wsfs_wheel):
     )
 
     assert len(database_names) > 0
+
+
+def test_toolkit_notebook(
+    acc,
+    ws,
+    sql_exec,
+    wsfs_wheel,
+    make_acc_group,
+    make_catalog,
+    make_cluster,
+    make_cluster_policy,
+    make_directory,
+    make_group,
+    make_instance_pool,
+    make_job,
+    make_notebook,
+    make_pipeline,
+    make_random,
+    make_repo,
+    make_secret_scope,
+    make_schema,
+    make_table,
+    make_user,
+):
+    logging.info("setting up fixtures")
+
+    user_a = make_user()
+    user_b = make_user()
+    user_c = make_user()
+
+    logging.info(f"user_a={user_a}, user_b={user_b}, user_c={user_c}, ")
+
+    # TODO acc_group
+    # TODO add users to groups
+    group_a = make_group()
+    group_b = make_group()
+    group_c = make_group()
+
+    selected_groups = ",".join([group_a.display_name, group_b.display_name, group_c.display_name])
+
+    logging.info(
+        f"group_a={group_a}, " 
+        f"group_b={group_b}, "
+        f"group_c={group_c}, ")
+
+    cluster = make_cluster(num_workers=1)
+    cluster_policy = make_cluster_policy()
+    directory = make_directory()
+    instance_pool = make_instance_pool()
+    job = make_job()
+    notebook = make_notebook()
+    pipeline = make_pipeline()
+    repo = make_repo()
+    secret_scope = make_secret_scope()
+
+    logging.info(
+        f"cluster={cluster}, "
+        f"cluster_policy={cluster_policy}, "
+        f"directory={directory}, "
+        f"instance_pool={instance_pool}, "
+        f"job={job}, "
+        f"notebook={notebook}, "
+        f"pipeline={pipeline}"
+        f"repo={repo}, "
+        f"secret_scope={secret_scope}, "
+    )
+
+    # TODO create fixtures for DBSQL assets
+    # TODO set permissions
+
+    schema_a = make_schema()
+    schema_b = make_schema()
+    schema_c = make_schema()
+    table_a = make_table(schema=schema_a)
+    table_b = make_table(schema=schema_b)
+
+    logging.info(
+        f"schema_a={schema_a}, "
+        f"schema_b={schema_b}, "
+        f"schema_c={schema_c}, "
+        f"table_a={table_a}, "
+        f"table_b={table_b}, "
+    )
+
+    databases = ",".join([schema_a.split(".")[1], schema_b.split(".")[1], schema_c.split(".")[1]])
+
+    sql_exec(f"GRANT USAGE ON SCHEMA default TO `{group_a.display_name}`")
+    sql_exec(f"GRANT USAGE ON SCHEMA default TO `{group_b.display_name}`")
+    sql_exec(f"GRANT SELECT ON TABLE {table_a} TO `{group_a.display_name}`")
+    sql_exec(f"GRANT SELECT ON TABLE {table_b} TO `{group_b.display_name}`")
+    sql_exec(f"GRANT MODIFY ON SCHEMA {schema_b} TO `{group_b.display_name}`")
+
+    inventory_table = make_table(schema=make_schema(catalog=make_catalog()))
+    inventory_catalog, inventory_schema, inventory_table = inventory_table.split(".")
+
+    logging.info(
+        f"inventory_catalog={inventory_catalog}, "
+        f"inventory_schema={inventory_schema}, "
+        f"inventory_table={inventory_table}, "
+    )
+
+    logging.info("uploading notebook")
+
+    ucx_notebook_path = Path("./test_toolkit_notebook.py").absolute()
+    my_user = ws.current_user.me().user_name
+    remote_ucx_notebook_location = f"/Users/{my_user}/notebooks/{make_random(10)}"
+    ws.workspace.mkdirs(remote_ucx_notebook_location)
+    ws_notebook = f"{remote_ucx_notebook_location}/test_notebook"
+
+    with open(ucx_notebook_path, "rb") as fh:
+        buf_notebook = BytesIO(fh.read())
+    ws.workspace.upload(ws_notebook, buf_notebook, format=ImportFormat.AUTO)
+
+    logging.info("creating job")
+
+    created_job = ws.jobs.create(
+        tasks=[
+            jobs.Task(
+                task_key="uc-migrate",
+                notebook_task=jobs.NotebookTask(
+                    notebook_path=ws_notebook,
+                    base_parameters={
+                        "inventory_catalog": inventory_catalog,
+                        "inventory_schema": inventory_schema,
+                        "selected_groups": selected_groups,
+                        "databases": databases,
+                    },
+                ),
+                libraries=[compute.Library(whl=f"/Workspace{wsfs_wheel}")],
+                new_cluster=compute.ClusterSpec(
+                    node_type_id=ws.clusters.select_node_type(local_disk=True),
+                    spark_version=ws.clusters.select_spark_version(latest=True),
+                    num_workers=1,
+                    spark_conf={"spark.databricks.acl.sqlOnly": True},
+                ),
+            )
+        ],
+        name="[UCX] Run Migration",
+    )
+    ws.jobs.run_now(created_job.job_id).result()
+
+    # TODO Validate migration, tacl
+
+    logging.info("deleting workbook")
+
+    ws.workspace.delete(remote_ucx_notebook_location, recursive=True)
+
+    logging.info("deleting job")
+
+    ws.jobs.delete(created_job.job_id)
