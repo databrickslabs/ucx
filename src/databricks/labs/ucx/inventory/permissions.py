042431ab2bc27dd913675a64ef26fd40d8a93867
renardeinside <polarpersonal@gmail.com>
2023-09-11 17:08:35 +0200

    refactor supports logic in permissions manager

--- src/databricks/labs/ucx/inventory/permissions.py
+++ src/databricks/labs/ucx/inventory/permissions.py
@@ -1,4 +1,5 @@
 import logging
+from itertools import groupby
 from typing import Literal
 
 from databricks.sdk import WorkspaceClient
@@ -37,20 +38,31 @@ class PermissionManager:
     def apply_group_permissions(self, migration_state: GroupMigrationState, destination: Literal["backup", "account"]):
         logger.info(f"Applying the permissions to {destination} groups")
         logger.info(f"Total groups to apply permissions: {len(migration_state.groups)}")
-        items = self._permissions_inventory.load_all()
+        # list shall be sorted prior to using group by
+        items = sorted(self._permissions_inventory.load_all(), key=lambda i: i.support)
         logger.info(f"Total inventorized items: {len(items)}")
         applier_tasks = []
-        for name, _support in self._supports_provider.supports.items():
-            logger.info(f"Adding applier tasks for {name}")
-            applier_tasks.extend(
-                [
-                    self._supports_provider.supports.get(item.support).get_apply_task(
-                        item, migration_state, destination
-                    )
-                    for item in items
-                ]
-            )
-            logger.info(f"Added applier tasks for {name}")
+        supports_to_items = {
+            support: list(items_subset) for support, items_subset in groupby(items, key=lambda i: i.support)
+        }
+
+        # we first check that all supports are valid.
+        for support in supports_to_items:
+            if support not in self._supports_provider.supports:
+                msg = f"Could not find support for {support}. Please check the inventory table."
+                raise ValueError(msg)
+
+        for support, items_subset in supports_to_items.items():
+            relevant_support = self._supports_provider.supports.get(support)
+            if not relevant_support:
+                msg = f"Could not find support for {support}. Total items for this support: {len(list(items_subset))}"
+                raise ValueError(msg)
+
+            tasks_for_support = [
+                relevant_support.get_apply_task(item, migration_state, destination) for item in items_subset
+            ]
+            logger.info(f"Total tasks for {support}: {len(tasks_for_support)}")
+            applier_tasks.extend(tasks_for_support)
 
         logger.info(f"Total applier tasks: {len(applier_tasks)}")
         logger.info("Starting the permissions application")
