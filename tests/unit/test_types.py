8c181dca02c8bcb2d7fc2a6043999fb2e862ee01
renardeinside <polarpersonal@gmail.com>
2023-09-11 10:04:04 +0200

    add tests for types

---
+++ tests/unit/test_types.py
@@ -0,0 +1,26 @@
+from databricks.labs.ucx.inventory.types import PermissionsInventoryItem, RequestObjectType
+
+
+def test_item_without_extras():
+    sample_item = PermissionsInventoryItem(
+        object_id="object1",
+        crawler="clusters",
+        raw_object_permissions="test acl",
+    )
+    assert sample_item.extras() == {}
+
+
+def test_item_with_extras():
+    sample_item = PermissionsInventoryItem(
+        object_id="object1",
+        crawler="clusters",
+        raw_object_permissions="test acl",
+        raw_extras='{"test": "test"}'
+    )
+    assert sample_item.extras() == {"test": "test"}
+
+
+def test_request_object_type():
+    typed = RequestObjectType.AUTHORIZATION
+    assert typed == "authorization"
+    assert typed.__repr__() == "authorization"
