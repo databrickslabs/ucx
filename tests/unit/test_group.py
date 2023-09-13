1860e65b9d2139f938fb53dfddbec243f8d48cc4
Ivan Trusov <polarpersonal@gmail.com>
2023-09-13 21:32:16 +0200

    Prefetch all account-level and workspace-level groups (#192)
    Addresses #190 and other SCIM-related issues. 
    
    Conceptually, the following happens:
    
    1. Existing method to list the workspace groups is inconsistent, and the
    root cause of these issues is somewhere on SCIM API side. We've raised a
    ticket, but the resolution timing is unclear.
    2. To avoid being time-dependent on SCIM API, we re-introduce the
    listing methods, but this time we intentionally don't use `filter`-based
    conditions and explicitly split the API methods into two.
    3. To avoid reaching out to the SCIM API too frequently, we list the
    whole API only once and then work with an in-memory data.
    
    This PR also adds 100% coverage for `GroupManager` and adds integration
    tests for groups to verify it works as expected.

--- tests/unit/test_group.py
+++ tests/unit/test_group.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, call
+from unittest.mock import Mock
 
 import pytest
 from databricks.sdk.service.iam import Group, ResourceMeta
@@ -21,23 +21,30 @@ def compare(s, t):
 def test_account_groups_should_not_be_considered():
     client = Mock()
     users_group = Group(display_name="analysts", meta=ResourceMeta(resource_type="WorkspaceGroup"))
-    account_admins_group = Group(display_name="account admins", meta=ResourceMeta(resource_type="AccountGroup"))
+    account_admins_group = Group(display_name="account admins", meta=ResourceMeta(resource_type="Group"))
     client.groups.list.return_value = [users_group, account_admins_group]
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [account_admins_group]],
+    }
 
     group_conf = GroupsConfig(selected=[""])
-
-    assert GroupManager(client, group_conf)._find_eligible_groups() == [users_group.display_name]
+    gm = GroupManager(client, group_conf)
+    assert gm._list_workspace_groups() == [users_group]
+    assert gm._list_account_groups() == [account_admins_group]
 
 
 def test_if_only_account_groups_it_should_return_empty_value():
     client = Mock()
-    users_group = Group(display_name="analysts", meta=ResourceMeta(resource_type="AccountGroup"))
-    account_admins_group = Group(display_name="account admins", meta=ResourceMeta(resource_type="AccountGroup"))
-    client.groups.list.return_value = [users_group, account_admins_group]
-
-    group_conf = GroupsConfig(selected=[""])
-
-    assert GroupManager(client, group_conf)._find_eligible_groups() == []
+    Group(display_name="analysts", meta=ResourceMeta(resource_type="Group"))
+    account_admins_group = Group(display_name="account admins", meta=ResourceMeta(resource_type="Group"))
+    client.groups.list.return_value = []
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [account_admins_group]],
+    }
+    group_conf = GroupsConfig(auto=True)
+    gm = GroupManager(client, group_conf)
+    assert gm._list_workspace_groups() == []
+    assert gm._list_account_groups() == [account_admins_group]
 
 
 def test_backup_group_should_be_created_with_name_defined_in_conf():
@@ -50,9 +57,9 @@ def test_backup_group_should_be_created_with_name_defined_in_conf():
         display_name="dbr_backup_analysts_group_backup", meta=ResourceMeta(resource_type="WorkspaceGroup")
     )
     client.groups.create.return_value = analysts_group_backup
+    client.api_client.do.return_value = {}
 
     group_conf = GroupsConfig(selected=[""], backup_group_prefix="dbr_backup_")
-
     assert (
         GroupManager(client, group_conf)._get_or_create_backup_group("analysts", analysts_group)
         == analysts_group_backup
@@ -66,82 +73,58 @@ def test_backup_group_should_not_be_created_if_already_exists():
         display_name="dbr_backup_analysts_group_backup", meta=ResourceMeta(resource_type="WorkspaceGroup")
     )
     client.groups.list.return_value = [analysts_group_backup]
+    client.api_client.do.return_value = {}
 
-    group_conf = GroupsConfig(selected=[""], backup_group_prefix="dbr_backup_")
-
+    group_conf = GroupsConfig(auto=True, backup_group_prefix="dbr_backup_")
     assert (
-        GroupManager(client, group_conf)._get_or_create_backup_group(
-            "dbr_backup_analysts_group_backup", analysts_group_backup
-        )
+        GroupManager(client, group_conf)._get_or_create_backup_group("analysts_group_backup", analysts_group_backup)
         == analysts_group_backup
     )
 
 
 def test_prepare_groups_in_environment_with_one_group_in_conf_should_return_migrationgroupinfo_object():
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    ws_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    acc_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
     backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
 
-    def my_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return [backup_de_group]
-
     client = Mock()
-    client.groups.list.side_effect = my_side_effect
-    client.api_client.do.return_value = {"Resources": [de_group.as_dict()]}
+    client.groups.list.return_value = [ws_de_group]
+    client.groups.create.return_value = backup_de_group
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [acc_de_group]],
+    }
 
     group_conf = GroupsConfig(selected=["de"], backup_group_prefix="dbr_backup_")
     manager = GroupManager(client, group_conf)
     manager.prepare_groups_in_environment()
 
-    group_info = MigrationGroupInfo(workspace=de_group, account=de_group, backup=backup_de_group)
+    group_info = MigrationGroupInfo(workspace=ws_de_group, account=acc_de_group, backup=backup_de_group)
     assert manager._migration_state.groups == [group_info]
+    assert len(manager._workspace_groups) == 2  # created backup group should be added to the list
 
 
 def test_prepare_groups_in_environment_with_multiple_groups_in_conf_should_return_two_migrationgroupinfo_object():
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
-    ds_group = Group(display_name="ds", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    ws_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    ws_ds_group = Group(display_name="ds", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+
+    acc_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
+    acc_ds_group = Group(display_name="ds", meta=ResourceMeta(resource_type="Group"))
 
     backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
     backup_ds_group = Group(display_name="dbr_backup_ds", meta=ResourceMeta(resource_type="WorkspaceGroup"))
 
     client = Mock()
-
-    def api_client_side_effect(method, path, query, **kwargs):
-        if query == {
-            "filter": "displayName eq 'ds'",
-            "attributes": "id,displayName,meta,entitlements,roles,members",
-            "excludedAttributes": None,
-        }:
-            return {"Resources": [ds_group.as_dict()]}
-        elif query == {
-            "filter": "displayName eq 'de'",
-            "attributes": "id,displayName,meta,entitlements,roles,members",
-            "excludedAttributes": None,
-        }:
-            return {"Resources": [de_group.as_dict()]}
-
-    client.api_client.do.side_effect = api_client_side_effect
-
-    def list_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'ds'":
-            return [ds_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return [backup_de_group]
-        elif filter == "displayName eq 'dbr_backup_ds'":
-            return [backup_ds_group]
-
-    client.groups.list.side_effect = list_side_effect
-
+    client.groups.list.return_value = iter([ws_de_group, ws_ds_group])
+    client.groups.create.side_effect = [backup_de_group, backup_ds_group]
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [acc_de_group, acc_ds_group]],
+    }
     group_conf = GroupsConfig(selected=["de", "ds"], backup_group_prefix="dbr_backup_")
     manager = GroupManager(client, group_conf)
     manager.prepare_groups_in_environment()
 
-    de_group_info = MigrationGroupInfo(workspace=de_group, account=de_group, backup=backup_de_group)
-    ds_group_info = MigrationGroupInfo(workspace=ds_group, account=ds_group, backup=backup_ds_group)
+    de_group_info = MigrationGroupInfo(workspace=ws_de_group, account=acc_de_group, backup=backup_de_group)
+    ds_group_info = MigrationGroupInfo(workspace=ws_ds_group, account=acc_ds_group, backup=backup_ds_group)
 
     assert compare(manager._migration_state.groups, [ds_group_info, de_group_info])
 
@@ -152,6 +135,7 @@ def test_prepare_groups_in_environment_should_throw_when_account_group_doesnt_ex
     client = Mock()
     client.api_client.do.return_value = {}
     client.groups.list.return_value = [de_group]
+    client.api_client.do.return_value = {}
 
     group_conf = GroupsConfig(selected=["de"], backup_group_prefix="dbr_backup_")
     manager = GroupManager(client, group_conf)
@@ -163,176 +147,107 @@ def test_prepare_groups_in_environment_should_throw_when_account_group_doesnt_ex
 
 def test_prepare_groups_in_environment_should_throw_when_workspace_group_doesnt_exist():
     client = Mock()
-    client.api_client.do.return_value = {}
+    client.api_client.do.side_effect = [{}, {}]
     client.groups.list.return_value = []
 
     group_conf = GroupsConfig(selected=["de"], backup_group_prefix="dbr_backup_")
     manager = GroupManager(client, group_conf)
-
     with pytest.raises(AssertionError) as e_info:
         manager.prepare_groups_in_environment()
-    assert str(e_info.value) == "Group de not found on the workspace level"
+        assert str(e_info.value) == "Group de not found on the workspace level"
 
 
 def test_prepare_groups_in_environment_with_backup_group_not_created_should_create_it():
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    ws_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    acc_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
+
     backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
 
-    def groups_list_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return []
-        elif filter == 'displayName ne "users" and displayName ne "admins" and displayName ne "account users"':
-            return [de_group]
-
     client = Mock()
-    client.groups.list.side_effect = groups_list_side_effect
-    client.api_client.do.return_value = {"Resources": [de_group.as_dict()]}
+    client.groups.list.return_value = iter([ws_de_group])
     client.groups.create.return_value = backup_de_group
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [acc_de_group]],
+    }
 
     group_conf = GroupsConfig(selected=["de"], backup_group_prefix="dbr_backup_")
     manager = GroupManager(client, group_conf)
     manager.prepare_groups_in_environment()
-
-    group_info = MigrationGroupInfo(workspace=de_group, account=de_group, backup=backup_de_group)
+    group_info = MigrationGroupInfo(workspace=ws_de_group, account=acc_de_group, backup=backup_de_group)
     assert manager._migration_state.groups == [group_info]
 
 
 def test_prepare_groups_in_environment_with_conf_in_auto_mode_should_populate_migrationgroupinfo_object():
     client = Mock()
 
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    ws_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    acc_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
+
     backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
 
-    def my_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return [backup_de_group]
-        elif filter == 'displayName ne "users" and displayName ne "admins" and displayName ne "account users"':
-            return [de_group]
-
-    client.groups.list.side_effect = my_side_effect
-    client.api_client.do.return_value = {"Resources": [de_group.as_dict()]}
+    client.groups.list.return_value = iter([ws_de_group])
+    client.groups.create.return_value = backup_de_group
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [acc_de_group]],
+    }
 
     group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", auto=True)
     manager = GroupManager(client, group_conf)
     manager.prepare_groups_in_environment()
 
-    group_info = MigrationGroupInfo(workspace=de_group, account=de_group, backup=backup_de_group)
+    group_info = MigrationGroupInfo(workspace=ws_de_group, account=acc_de_group, backup=backup_de_group)
     assert manager._migration_state.groups == [group_info]
-
-
-def test_prepare_groups_in_environment_with_conf_in_auto_mode_and_backup_group_exists_should_call_list_and_do():
-    client = Mock()
-
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
-    backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
-
-    def my_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return [backup_de_group]
-        elif filter == 'displayName ne "users" and displayName ne "admins" and displayName ne "account users"':
-            return [de_group]
-
-    client.groups.list.side_effect = my_side_effect
-    client.api_client.do.return_value = {"Resources": [de_group.as_dict()]}
-
-    group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", auto=True)
-    manager = GroupManager(client, group_conf)
-    manager.prepare_groups_in_environment()
-
-    assert client.mock_calls == [
-        call.groups.list(
-            attributes="displayName,meta",
-            filter='displayName ne "users" and displayName ne "admins" and displayName ne "account users"',
-        ),
-        call.groups.list(attributes="id,displayName,meta,entitlements,roles,members", filter="displayName eq 'de'"),
-        call.api_client.do(
-            "GET",
-            "/api/2.0/account/scim/v2/Groups",
-            query={
-                "filter": "displayName eq 'de'",
-                "attributes": "id,displayName,meta,entitlements,roles,members",
-                "excludedAttributes": None,
-            },
-        ),
-        call.groups.list(
-            filter="displayName eq 'dbr_backup_de'", attributes="id,displayName,meta,entitlements,roles,members"
-        ),
-    ]
-
-
-def test_prepare_groups_in_environment_with_no_groups_in_conf_and_backup_group_exists_should_make_api_calls():
-    client = Mock()
-
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
-
-    def my_side_effect(filter, **kwargs):  # noqa: A002
-        if filter == "displayName eq 'de'":
-            return [de_group]
-        elif filter == "displayName eq 'dbr_backup_de'":
-            return []
-        elif filter == 'displayName ne "users" and displayName ne "admins" and displayName ne "account users"':
-            return [de_group]
-
-    client.groups.list.side_effect = my_side_effect
-    client.api_client.do.return_value = {"Resources": [de_group.as_dict()]}
-
-    group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", auto=True)
-    manager = GroupManager(client, group_conf)
-    manager.prepare_groups_in_environment()
-
-    assert client.mock_calls == [
-        call.groups.list(
-            attributes="displayName,meta",
-            filter='displayName ne "users" and displayName ne "admins" and displayName ne "account users"',
-        ),
-        call.groups.list(attributes="id,displayName,meta,entitlements,roles,members", filter="displayName eq 'de'"),
-        call.api_client.do(
-            "GET",
-            "/api/2.0/account/scim/v2/Groups",
-            query={
-                "filter": "displayName eq 'de'",
-                "attributes": "id,displayName,meta,entitlements,roles,members",
-                "excludedAttributes": None,
-            },
-        ),
-        call.groups.list(
-            filter="displayName eq 'dbr_backup_de'", attributes="id,displayName,meta,entitlements,roles,members"
-        ),
-        call.groups.create(
-            display_name="dbr_backup_de",
-            meta=ResourceMeta(resource_type="WorkspaceGroup"),
-            entitlements=None,
-            roles=None,
-            members=None,
-        ),
-    ]
 
 
 def test_replace_workspace_groups_with_account_groups_should_call_delete_and_do():
     client = Mock()
 
-    test_workspace_id = 100
-    de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"), id=test_workspace_id)
+    test_ws_group_id = "100"
+    test_acc_group_id = "200"
+    ws_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"), id=test_ws_group_id)
+    acc_de_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"), id=test_acc_group_id)
+
     backup_de_group = Group(display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
 
-    client.groups.list.return_value = [de_group]
+    client.groups.list.return_value = [ws_de_group]
+    client.api_client.do.side_effect = [
+        {
+            "Resources": [g.as_dict() for g in [acc_de_group]],
+        },
+        {},
+    ]
 
     group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", auto=True)
     manager = GroupManager(client, group_conf)
 
-    group_info = MigrationGroupInfo(workspace=de_group, account=de_group, backup=backup_de_group)
+    group_info = MigrationGroupInfo(workspace=ws_de_group, account=acc_de_group, backup=backup_de_group)
     manager._migration_state.groups = [group_info]
     manager.replace_workspace_groups_with_account_groups()
 
-    client.groups.delete.assert_called_with(test_workspace_id)
+    client.groups.delete.assert_called_with(test_ws_group_id)
     client.api_client.do.assert_called_with(
         "PUT",
-        f"/api/2.0/preview/permissionassignments/principals/{test_workspace_id}",
+        f"/api/2.0/preview/permissionassignments/principals/{test_acc_group_id}",
         data='{"permissions": ["USER"]}',
     )
+
+
+def test_delete_backup_groups():
+    client = Mock()
+
+    test_ws_group = Group(display_name="de", meta=ResourceMeta(resource_type="WorkspaceGroup"))
+    test_acc_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
+    backup_group_id = "100"
+    client.groups.list.return_value = [test_ws_group]
+    client.groups.create.return_value = Group(
+        display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"), id=backup_group_id
+    )
+    client.api_client.do.return_value = {
+        "Resources": [g.as_dict() for g in [test_acc_group]],
+    }
+
+    group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", selected=["de"])
+    manager = GroupManager(client, group_conf)
+    manager.prepare_groups_in_environment()
+    manager.delete_backup_groups()
+    client.groups.delete.assert_called_with(id=backup_group_id)
