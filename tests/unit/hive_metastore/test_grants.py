50d4ea4fe3b09adb38755e1e1d5e69d6e6d97c96
William Conti <william.conti@databricks.com>
2023-09-20 18:44:31 +0200

    fix tests

--- tests/unit/hive_metastore/test_grants.py
+++ tests/unit/hive_metastore/test_grants.py
@@ -168,14 +168,14 @@ def test_crawler_snapshot():
 
 
 def test_grants_returning_error_when_describing():
-    errors = {"DESCRIBE TABLE EXTENDED hive_metastore.test_database.table1": "error"}
+    errors = {"SHOW GRANTS ON TABLE hive_metastore.test_database.table1": "error"}
     rows = {
+        "SHOW TABLES FROM hive_metastore.test": [("dummy", "table1", False), ("dummy", "table2", False)],
         "SHOW GRANTS ON TABLE hive_metastore.test_database.table2": [("principal1", "OWNER", "TABLE", "")],
-        "DESCRIBE TABLE EXTENDED hive_metastore.test_database.table2": [
+        "DESCRIBE *": [
             ("Catalog", "catalog", ""),
             ("Type", "delta", ""),
         ],
-        "SHOW TABLES FROM hive_metastore.test": [("dummy", "table1", False), ("dummy", "table2", False)],
     }
 
     tc = TablesCrawler(MockBackend(fails_on_first=errors, rows=rows), "main", "default")
