2077fdb5bfc956a7d3043b841c34e09fd536fcb9
FastLee <lbareket@gmail.com>
2023-09-25 10:21:52 -0400

    Fixed a couple of report issues.

---
+++ tests/unit/assessment/test_dashboard.py
@@ -0,0 +1,37 @@
+import io
+
+import yaml
+from databricks.sdk.service import iam
+from databricks.sdk.service.sql import DataSource, Dashboard, Query, Visualization, Widget
+
+from databricks.labs.ucx.config import MigrationConfig, GroupsConfig
+from databricks.labs.ucx.framework.dashboards import DashboardFromFiles
+from databricks.labs.ucx.install import Installer
+
+
+def test_dashboard(mocker):
+    ws = mocker.Mock()
+    ws.current_user.me = lambda: iam.User(user_name="me@example.com", groups=[iam.ComplexValue(display="admins")])
+    ws.config.host = "https://foo"
+    ws.config.is_aws = True
+    config_bytes = yaml.dump(MigrationConfig(inventory_database="a", groups=GroupsConfig(auto=True)).as_dict()).encode(
+        "utf8"
+    )
+    ws.workspace.download = lambda _: io.BytesIO(config_bytes)
+    ws.data_sources.list = lambda: [DataSource(id="bcd", warehouse_id="000000")]
+    ws.dashboards.create.return_value = Dashboard(id="abc")
+    ws.queries.create.return_value = Query(id="abc")
+    ws.query_visualizations.create.return_value = Visualization(id="abc")
+    ws.dashboard_widgets.create.return_value = Widget(id="abc")
+    installer = Installer(ws)
+    local_query_files = installer._find_project_root() / "src/databricks/labs/ucx/assessment/queries"
+    dash = DashboardFromFiles(
+        ws,
+        local_folder=local_query_files,
+        remote_folder=f"/users/not_a_real_user/queries",
+        name="Assessment",
+        warehouse_id="000000",
+        query_text_callback=installer._replace_inventory_variable,
+    )
+    dashboard = dash.create_dashboard()
+    assert dashboard is not None
