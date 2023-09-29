f1db173d8c0fdccbd190bf6742adbab163c430a8
william-conti <107417584+william-conti@users.noreply.github.com>
2023-09-29 16:17:52 +0200

    Added a migration state to skip already migrated tables (#325)
    Add a migration state to skip already migrated tables using an in-memory
    state
    
    ---------
    
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -90,7 +90,7 @@ def make_schema(sql_exec, make_random):
         return schema
 
     yield from factory(  # noqa: F405
-        "schema", create, lambda schema_name: sql_exec(f"DROP SCHEMA IF EXISTS {schema_name} CASCADE")
+        "schema", create, lambda schema: sql_exec(f"DROP SCHEMA IF EXISTS {schema} CASCADE")
     )
 
 
@@ -104,15 +104,18 @@ def make_table(sql_exec, make_schema, make_random):
     def create(
         *,
         catalog="hive_metastore",
+        name: str | None = None,
         schema: str | None = None,
         ctas: str | None = None,
         non_delta: bool = False,
         external: bool = False,
         view: bool = False,
+        tbl_properties: dict[str, str] | None = None,
     ):
         if schema is None:
             schema = make_schema(catalog=catalog)
-        name = f"{schema}.ucx_T{make_random(4)}".lower()
+        if name is None:
+            name = f"{schema}.ucx_T{make_random(4)}".lower()
         ddl = f'CREATE {"VIEW" if view else "TABLE"} {name}'
         if ctas is not None:
             # temporary (if not view)
@@ -128,6 +131,10 @@ def make_table(sql_exec, make_schema, make_random):
         else:
             # managed table
             ddl = f"{ddl} (id INT, value STRING)"
+        if tbl_properties:
+            tbl_properties = ",".join([f" '{k}' = '{v}' " for k, v in tbl_properties.items()])
+            ddl = f"{ddl} TBLPROPERTIES ({tbl_properties})"
+
         sql_exec(ddl)
         return name
 
@@ -150,6 +157,7 @@ def test_table_fixture(make_table):
     logger.info(f"Created new external JSON table in new schema: {make_table(non_delta=True)}")
     logger.info(f'Created new tmp table in new schema: {make_table(ctas="SELECT 2+2 AS four")}')
     logger.info(f'Created new view in new schema: {make_table(view=True, ctas="SELECT 2+2 AS four")}')
+    logger.info(f'Created table with properties: {make_table(tbl_properties={"test": "tableproperty"})}')
 
 
 @pytest.fixture
--- tests/unit/hive_metastore/test_migrate.py
+++ tests/unit/hive_metastore/test_migrate.py
@@ -1,7 +1,7 @@
 import logging
 from unittest.mock import MagicMock
 
-import pytest
+from databricks.sdk.service.catalog import CatalogInfo, SchemaInfo, TableInfo
 
 from databricks.labs.ucx.hive_metastore.tables import TablesCrawler, TablesMigrate
 
@@ -28,7 +28,7 @@ def test_migrate_managed_tables_should_produce_proper_queries():
     backend = MockBackend(fails_on_first=errors, rows=rows)
     tc = TablesCrawler(backend, "inventory_database")
     client = MagicMock()
-    tm = TablesMigrate(tc, client, backend, "")
+    tm = TablesMigrate(tc, client, backend)
     tm.migrate_tables()
 
     assert (list(backend.queries)) == [
@@ -39,18 +39,22 @@ def test_migrate_managed_tables_should_produce_proper_queries():
     ]
 
 
-@pytest.mark.skip(reason="Not implemented yet")
 def test_migrate_managed_tables_should_do_nothing_if_upgrade_tag_is_present():
     errors = {}
     rows = {
         "SELECT": [
-            ("hive_metastore", "db1", "managed", "MANAGED", "DELTA", None, None, "[upgraded_to=target]"),
+            ("hive_metastore", "db1", "managed", "MANAGED", "DELTA", None, None),
         ]
     }
     backend = MockBackend(fails_on_first=errors, rows=rows)
     tc = TablesCrawler(backend, "inventory_database")
     client = MagicMock()
-    tm = TablesMigrate(tc, client, backend, "")
+    client.catalogs.list.return_value = [CatalogInfo(name="catalog_1")]
+    client.schemas.list.return_value = [SchemaInfo(name="db1")]
+    client.tables.list.return_value = [
+        TableInfo(full_name="catalog_1.db1.managed", properties={"upgraded_from": "hive_metastore.db1.managed"})
+    ]
+    tm = TablesMigrate(tc, client, backend, default_catalog="catalog_1")
     tm.migrate_tables()
 
     assert (list(backend.queries)) == ["SELECT * FROM hive_metastore.inventory_database.tables"]
@@ -67,7 +71,7 @@ def test_migrate_tables_should_migrate_tables_to_default_catalog_if_not_found_in
     tc = TablesCrawler(backend, "inventory_database")
     client = MagicMock()
     database_to_catalog_mapping = {"db1": "catalog_1", "db2": "catalog_2"}
-    tm = TablesMigrate(tc, client, backend, "", database_to_catalog_mapping=database_to_catalog_mapping)
+    tm = TablesMigrate(tc, client, backend, database_to_catalog_mapping=database_to_catalog_mapping)
     tm.migrate_tables()
 
     assert (list(backend.queries)) == [
@@ -88,7 +92,7 @@ def test_migrate_tables_should_migrate_tables_to_default_catalog_if_specified():
     backend = MockBackend(fails_on_first=errors, rows=rows)
     tc = TablesCrawler(backend, "inventory_database")
     client = MagicMock()
-    tm = TablesMigrate(tc, client, backend, "", default_catalog="test_catalog")
+    tm = TablesMigrate(tc, client, backend, default_catalog="test_catalog")
     tm.migrate_tables()
 
     assert (list(backend.queries)) == [
@@ -97,3 +101,19 @@ def test_migrate_tables_should_migrate_tables_to_default_catalog_if_specified():
         "ALTER TABLE hive_metastore.db1.managed SET TBLPROPERTIES ('upgraded_to' = 'test_catalog.db1.managed');",
         "ALTER TABLE test_catalog.db1.managed SET TBLPROPERTIES ('upgraded_from' = 'hive_metastore.db1.managed');",
     ]
+
+
+def test_migrate_tables_should_add_table_to_cache_when_migrated():
+    errors = {}
+    rows = {
+        "SELECT": [
+            ("hive_metastore", "db1", "managed", "MANAGED", "DELTA", None, None),
+        ]
+    }
+    backend = MockBackend(fails_on_first=errors, rows=rows)
+    tc = TablesCrawler(backend, "inventory_database")
+    client = MagicMock()
+    tm = TablesMigrate(tc, client, backend, default_catalog="test_catalog")
+    tm.migrate_tables()
+
+    assert tm._seen_tables == {"test_catalog.db1.managed": "hive_metastore.db1.managed"}
