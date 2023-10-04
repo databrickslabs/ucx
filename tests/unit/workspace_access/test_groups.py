ea89992471bffb05d378c3add57ba4a5ad19064c
mwojtyczka <wojtyczka.marcin@gmail.com>
2023-10-04 12:26:17 +0200

    updated unit test to check if groups are skipped when selecteed

--- tests/unit/workspace_access/test_groups.py
+++ tests/unit/workspace_access/test_groups.py
@@ -400,11 +400,17 @@ def test_delete_selected_backup_groups():
         display_name="dbr_backup_de", meta=ResourceMeta(resource_type="WorkspaceGroup"), id=backup_group_id
     )
 
-    client.groups.list.return_value = [ws_group, test_ws_backup_group]
+    ws_group_to_skip = Group(display_name="de2", meta=ResourceMeta(resource_type="Group"))
+    test_ws_backup_group_to_skip = Group(
+        display_name="dbr_backup_de2", meta=ResourceMeta(resource_type="WorkspaceGroup"), id="1"
+    )
+
+    client.groups.list.return_value = [ws_group, test_ws_backup_group, ws_group_to_skip, test_ws_backup_group_to_skip]
 
     test_acc_group = Group(display_name="de", meta=ResourceMeta(resource_type="Group"))
+    test_acc_group_to_skip = Group(display_name="de2", meta=ResourceMeta(resource_type="Group"))
     client.api_client.do.return_value = {
-        "Resources": [g.as_dict() for g in [test_acc_group]],
+        "Resources": [g.as_dict() for g in [test_acc_group, test_acc_group_to_skip]],
     }
 
     group_conf = GroupsConfig(backup_group_prefix="dbr_backup_", selected=["de"])
