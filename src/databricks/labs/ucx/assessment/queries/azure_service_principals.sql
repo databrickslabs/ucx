f306aeb7ff2bfec54f8bfba23a35f87035a837d4
dipankarkusha-db <dipankar.kushari@databricks.com>
2023-10-06 15:33:02 -0400

    Added changes for supporting Klass.
    Fixes #249

--- src/databricks/labs/ucx/assessment/queries/azure_service_principals.sql
+++ src/databricks/labs/ucx/assessment/queries/azure_service_principals.sql
@@ -1,3 +1,5 @@
+-- viz type=table, name=Azure SPNs, columns=application_id,secret_scope,secret_key,tenant_id,storage_account
+-- widget title=Azure SPNs, col=0, row=41, size_x=6, size_y=8
 select
   application_id,
   secret_scope,
