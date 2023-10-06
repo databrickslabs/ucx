8df6153781b6806675cb9f9bf12a503162da39c1
Lars George <136075433+larsgeorge-db@users.noreply.github.com>
2023-10-06 22:07:44 +0200

    Improved dashboard queries to show unsupported storage types. (#398)
    Fixes #355 
    
    <img width="851" alt="image"
    src="https://github.com/databrickslabs/ucx/assets/136075433/b6e6c937-95fa-45c1-8cfb-3318c6ae5eb6">
    
    <img width="850" alt="image"
    src="https://github.com/databrickslabs/ucx/assets/136075433/1b251bbd-26ca-41e0-8b4a-9729513e757f">

--- src/databricks/labs/ucx/assessment/queries/database_summary.sql
+++ src/databricks/labs/ucx/assessment/queries/database_summary.sql
@@ -3,22 +3,34 @@
 SELECT `database`,
        SUM(is_table) AS tables,
        SUM(is_view) AS views,
+       SUM(is_unsupported) AS unsupported,
        SUM(is_dbfs_root) AS dbfs_root,
        SUM(is_delta) AS delta_tables,
        CASE
-           WHEN (SUM(is_dbfs_root)/SUM(is_table) > .3) THEN "Asset Replication Required"
-           WHEN (SUM(is_delta)/SUM(is_table) < .7) THEN "Some Non Delta Assets"
+           WHEN SUM(is_table) = 0 AND SUM(is_view) > 0 THEN "View Migration Required"
+           WHEN SUM(is_dbfs_root)/SUM(is_table) > .3 THEN "Asset Replication Required"
+           WHEN SUM(is_delta)/SUM(is_table) < .7 THEN "Some Non Delta Assets"
+           WHEN SUM(is_unsupported)/SUM(is_table) > .7 THEN "Storage Migration Required"
            ELSE "In Place Sync"
        END AS upgrade
 FROM
-  (SELECT DATABASE,
+  (SELECT `database`,
           name,
           object_type,
-          UPPER(table_format) AS format,
-          LOCATION,
+          UPPER(table_format) AS `format`,
+          `location`,
           IF(object_type IN ("MANAGED", "EXTERNAL"), 1, 0) AS is_table,
           IF(object_type = "VIEW", 1, 0) AS is_view,
-          IF(STARTSWITH(location, "/dbfs/") AND NOT STARTSWITH(location, "/dbfs/mnt"), 1, 0) AS is_dbfs_root,
+          CASE
+              WHEN STARTSWITH(location, "dbfs:/") AND NOT STARTSWITH(location, "dbfs:/mnt") THEN 1
+              WHEN STARTSWITH(location, "/dbfs/") AND NOT STARTSWITH(location, "/dbfs/mnt") THEN 1
+              ELSE 0
+          END AS is_dbfs_root,
+          CASE
+              WHEN STARTSWITH(location, "wasb") THEN 1
+              WHEN STARTSWITH(location, "adl") THEN 1
+              ELSE 0
+          END AS is_unsupported,
           IF(UPPER(format) = "DELTA", 1, 0) AS is_delta
    FROM $inventory.tables)
 GROUP BY `database`
