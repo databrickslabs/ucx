0031b5da39887efa3a128723064f8dcffcd0ca8f
Serge Smertin <serge.smertin@databricks.com>
2023-10-07 21:52:02 +0200

    ..

--- tests/integration/workspace_access/test_permissions_manager.py
+++ tests/integration/workspace_access/test_permissions_manager.py
@@ -3,10 +3,8 @@ from databricks.labs.ucx.workspace_access.base import Permissions
 from databricks.labs.ucx.workspace_access.manager import PermissionManager
 
 
-def test_permissions_save_and_load(ws, sql_backend, make_schema, env_or_skip):
-    schema = make_schema().split(".")[-1]
-    backend = StatementExecutionBackend(ws, env_or_skip("TEST_DEFAULT_WAREHOUSE_ID"))
-    pi = PermissionManager(backend, schema, [], {})
+def test_permissions_save_and_load(ws, sql_backend, inventory_schema, env_or_skip):
+    pi = PermissionManager(sql_backend, inventory_schema, [], {})
 
     saved = [
         Permissions(object_id="abc", object_type="bcd", raw="def"),
