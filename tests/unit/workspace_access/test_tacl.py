d13eff364102e0b3722a431eca73a48bb53086cb
Serge Smertin <serge.smertin@databricks.com>
2023-10-08 16:15:12 +0200

    ..

--- tests/unit/workspace_access/test_tacl.py
+++ tests/unit/workspace_access/test_tacl.py
@@ -1,7 +1,6 @@
 import json
 
 from databricks.sdk.service.iam import Group
-from unit.framework.mocks import MockBackend
 
 from databricks.labs.ucx.hive_metastore import GrantsCrawler, TablesCrawler
 from databricks.labs.ucx.workspace_access.base import Permissions
@@ -10,6 +9,7 @@ from databricks.labs.ucx.workspace_access.groups import (
     MigrationGroupInfo,
 )
 from databricks.labs.ucx.workspace_access.tacl import TableAclSupport
+from ..framework.mocks import MockBackend
 
 
 def test_tacl_crawler():
@@ -61,3 +61,34 @@ def test_tacl_applier(mocker):
     task()
 
     assert ["GRANT SELECT ON TABLE catalog_a.database_b.table_c TO `tmp-backup-abc`"] == sql_backend.queries
+
+
+def test_tacl_applier_no_target_principal(mocker):
+    sql_backend = MockBackend()
+    table_acl_support = TableAclSupport(mocker.Mock(), sql_backend)
+
+    permissions = Permissions(
+        object_type="TABLE",
+        object_id="catalog_a.database_b.table_c",
+        raw=json.dumps(
+            {
+                "principal": "foo@example.com",
+                "action_type": "SELECT",
+                "catalog": "catalog_a",
+                "database": "database_b",
+                "table": "table_c",
+            }
+        ),
+    )
+    migration_state = GroupMigrationState()
+    migration_state.add(
+        MigrationGroupInfo(
+            workspace=Group(display_name="abc"),
+            backup=Group(display_name="tmp-backup-abc"),
+            account=Group(display_name="account-abc"),
+        )
+    )
+    task = table_acl_support.get_apply_task(permissions, migration_state, "backup")
+    task()
+
+    assert [] == sql_backend.queries
