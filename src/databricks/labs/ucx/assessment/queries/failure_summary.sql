a53f758095d4c449fe4fb7c585a602958594d43d
dipankarkusha-db <dipankar.kushari@databricks.com>
2023-10-14 15:22:42 -0400

    Added Failure Summary in the dashboard.
    Fixes #309

--- src/databricks/labs/ucx/assessment/queries/failure_summary.sql
+++ src/databricks/labs/ucx/assessment/queries/failure_summary.sql
@@ -1,12 +1,8 @@
--- viz type=table, name=Failure Summary, columns=issue,object_type,issue_count,jobs_issue_percentage,clusters_issue_percentage,gis_issue_percentage,pipelines_issue_percentage
--- widget title = Failure Summary, col=0, row=65, size_x=6, size_y=8
+-- viz type=table, name=Failure Summary, columns=issue,object_type,issue_count
+-- widget title = Failure Summary, col=3, row=0, size_x=6, size_y=3
 SELECT
   issue,
   object_type,
-  issue_count,
-  jobs_issue_percentage,
-  clusters_issue_percentage,
-  gis_issue_percentage,
-  pipelines_issue_percentage
+  issue_count
 FROM
   $inventory.vw_failure_summary;
