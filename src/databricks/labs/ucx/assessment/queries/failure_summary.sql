41db3058b98fb95126eae38d89b145aba8ca280c
dipankarkusha-db <dipankar.kushari@databricks.com>
2023-10-14 20:36:50 -0400

    Added Failure Summary in the dashboard.
    Fixes #309

--- src/databricks/labs/ucx/assessment/queries/failure_summary.sql
+++ src/databricks/labs/ucx/assessment/queries/failure_summary.sql
@@ -1,5 +1,5 @@
 -- viz type=table, name=Failure Summary, columns=issue,object_type,issue_count
--- widget title = Failure Summary, col=0, row=65, size_x=6, size_y=8
+-- widget title=Failure Summary, col=0, row=65, size_x=6, size_y=8
 SELECT
   issue,
   object_type,
