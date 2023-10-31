2b1ba7a78d19fc21fd77fda13814486d326ddb92
dipankarkusha-db <dipankar.kushari@databricks.com>
2023-10-31 19:47:07 -0400

    Added spark config for hms lineage enablement.
    Fixes #324

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -8,7 +8,11 @@ import yaml
 from databricks.sdk.core import DatabricksError
 from databricks.sdk.errors import OperationFailed
 from databricks.sdk.service import iam, jobs
-from databricks.sdk.service.compute import Policy
+from databricks.sdk.service.compute import (
+    GlobalInitScriptDetails,
+    GlobalInitScriptDetailsWithContent,
+    Policy,
+)
 from databricks.sdk.service.sql import (
     Dashboard,
     DataSource,
@@ -445,3 +449,153 @@ def test_replace_pydoc(mocker):
     Test2
 Test3"""
     )
+
+
+def test_global_init_script_already_exists_enabled(mocker):
+    ws = mocker.Mock()
+    ginit_scripts = [
+        GlobalInitScriptDetails(
+            created_at=1695045723722,
+            created_by="test@abc.com",
+            enabled=True,
+            name="test123",
+            position=0,
+            script_id="12345",
+            updated_at=1695046359612,
+            updated_by="test@abc.com",
+        )
+    ]
+    ws.global_init_scripts.list.return_value = ginit_scripts
+    ws.global_init_scripts.get.return_value = GlobalInitScriptDetailsWithContent(
+        created_at=1695045723722,
+        created_by="test@abc.com",
+        enabled=True,
+        name="test123",
+        position=0,
+        script="aWYgW1sgJERCX0lTX0RSSVZFUiA9ICJUUlVFIiB"
+        "dXTsgdGhlbgogIGRyaXZlcl9jb25mPSR7REJfSE9NRX0"
+        "vZHJpdmVyL2NvbmYvc3BhcmstYnJhbmNoLmNvbmYKICBpZ"
+        "iBbICEgLWUgJGRyaXZlcl9jb25mIF0gOyB0aGVuCiAgICB0b"
+        "3VjaCAkZHJpdmVyX2NvbmYKICBmaQpjYXQgPDwgRU9GID4+ICAkZ"
+        "HJpdmVyX2NvbmYKICBbZHJpdmVyXSB7CiAgICJzcGFyay5kYXRhYnJpY2tzLm"
+        "RhdGFMaW5lYWdlLmVuYWJsZWQiID0gdHJ1ZQogICB9CkVPRgpmaQ==",
+        script_id="12C100F8BB38B002",
+        updated_at=1695046359612,
+        updated_by="test@abc.com",
+    )
+    install = WorkspaceInstaller(ws)
+    mocker.patch("builtins.input", return_value="yes")
+    install._install_spark_config_for_hms_lineage()
+
+
+def test_global_init_script_already_exists_disabled(mocker):
+    ws = mocker.Mock()
+    ginit_scripts = [
+        GlobalInitScriptDetails(
+            created_at=1695045723722,
+            created_by="test@abc.com",
+            enabled=False,
+            name="test123",
+            position=0,
+            script_id="12345",
+            updated_at=1695046359612,
+            updated_by="test@abc.com",
+        )
+    ]
+    ws.global_init_scripts.list.return_value = ginit_scripts
+    ws.global_init_scripts.get.return_value = GlobalInitScriptDetailsWithContent(
+        created_at=1695045723722,
+        created_by="test@abc.com",
+        enabled=False,
+        name="test123",
+        position=0,
+        script="aWYgW1sgJERCX0lTX0RSSVZFUiA9ICJUUlVFIiB"
+        "dXTsgdGhlbgogIGRyaXZlcl9jb25mPSR7REJfSE9NRX0"
+        "vZHJpdmVyL2NvbmYvc3BhcmstYnJhbmNoLmNvbmYKICBpZ"
+        "iBbICEgLWUgJGRyaXZlcl9jb25mIF0gOyB0aGVuCiAgICB0b"
+        "3VjaCAkZHJpdmVyX2NvbmYKICBmaQpjYXQgPDwgRU9GID4+ICAkZ"
+        "HJpdmVyX2NvbmYKICBbZHJpdmVyXSB7CiAgICJzcGFyay5kYXRhYnJpY2tzLm"
+        "RhdGFMaW5lYWdlLmVuYWJsZWQiID0gdHJ1ZQogICB9CkVPRgpmaQ==",
+        script_id="12C100F8BB38B002",
+        updated_at=1695046359612,
+        updated_by="test@abc.com",
+    )
+    install = WorkspaceInstaller(ws)
+    mocker.patch("builtins.input", return_value="yes")
+    install._install_spark_config_for_hms_lineage()
+
+
+def test_global_init_script_exists_disabled_not_enabled(mocker):
+    ws = mocker.Mock()
+    ginit_scripts = [
+        GlobalInitScriptDetails(
+            created_at=1695045723722,
+            created_by="test@abc.com",
+            enabled=False,
+            name="test123",
+            position=0,
+            script_id="12345",
+            updated_at=1695046359612,
+            updated_by="test@abc.com",
+        )
+    ]
+    ws.global_init_scripts.list.return_value = ginit_scripts
+    ws.global_init_scripts.get.return_value = GlobalInitScriptDetailsWithContent(
+        created_at=1695045723722,
+        created_by="test@abc.com",
+        enabled=False,
+        name="test123",
+        position=0,
+        script="aWYgW1sgJERCX0lTX0RSSVZFUiA9ICJUUlVFIiB"
+        "dXTsgdGhlbgogIGRyaXZlcl9jb25mPSR7REJfSE9NRX0"
+        "vZHJpdmVyL2NvbmYvc3BhcmstYnJhbmNoLmNvbmYKICBpZ"
+        "iBbICEgLWUgJGRyaXZlcl9jb25mIF0gOyB0aGVuCiAgICB0b"
+        "3VjaCAkZHJpdmVyX2NvbmYKICBmaQpjYXQgPDwgRU9GID4+ICAkZ"
+        "HJpdmVyX2NvbmYKICBbZHJpdmVyXSB7CiAgICJzcGFyay5kYXRhYnJpY2tzLm"
+        "RhdGFMaW5lYWdlLmVuYWJsZWQiID0gdHJ1ZQogICB9CkVPRgpmaQ==",
+        script_id="12C100F8BB38B002",
+        updated_at=1695046359612,
+        updated_by="test@abc.com",
+    )
+    install = WorkspaceInstaller(ws)
+    mocker.patch("builtins.input", return_value="no")
+    install._install_spark_config_for_hms_lineage()
+
+
+def test_global_init_script_create_new(mocker):
+    ws = mocker.Mock()
+    ginit_scripts = [
+        GlobalInitScriptDetails(
+            created_at=1695045723722,
+            created_by="test@abc.com",
+            enabled=True,
+            name="test123",
+            position=0,
+            script_id="12345",
+            updated_at=1695046359612,
+            updated_by="test@abc.com",
+        )
+    ]
+    ws.global_init_scripts.list.return_value = ginit_scripts
+    ws.global_init_scripts.get.return_value = GlobalInitScriptDetailsWithContent(
+        created_at=1695045723722,
+        created_by="test@abc.com",
+        enabled=True,
+        name="test123",
+        position=0,
+        script="JXNoCmVjaG8gIj09PT0gQmVnaW4gb2YgdGhlIGd"
+        "sb2JhbCBpbml0IHNjcmlwdCIKCmVjaG8gIj09PT0"
+        "gTGlzdCBWb2x1bWUsIGl0IHdvbid0IHdvcmsiCmxzIC9Wb2x1bWVzL3lhbmd3"
+        "YW5nX2RlbW8vZGVmYXVsdC92b2wxL21zb2RiY3NxbDE3LmRlYiAKc3VkbyB"
+        "jcCAvVm9sdW1lcy95YW5nd2FuZ19kZW1vL2RlZmF1bHQvdm9sMS9tc29kYmNzcWw"
+        "xNy5kZWIgLi8KCmVjaG8gIj09PT0gTGlzdCBEQkZTIgpscyAvZGJmcy9GaWxlU3R"
+        "vcmUvbXNvZGJjc3FsMTcuZGViCnN1ZG8gY3AgL2RiZnMvRmlsZVN0b3JlL21zb2RiY3N"
+        "xbDE3LmRlYiAuLwoKc3VkbyBBQ0NFUFRfRVVMQT1ZIGFwdCBpbnN0YWxsIC15IC4vbXNvZ"
+        "GJjc3FsMTcuZGViCgplY2hvICI9PT09IEZpbmlzaGVkIEluc3RhbGwgLmRlYiI=",
+        script_id="12C100F8BB38B002",
+        updated_at=1695046359612,
+        updated_by="test@abc.com",
+    )
+    install = WorkspaceInstaller(ws)
+    mocker.patch("builtins.input", return_value="yes")
+    install._install_spark_config_for_hms_lineage()
