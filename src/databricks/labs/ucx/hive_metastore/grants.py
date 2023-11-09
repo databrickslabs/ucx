b7b813b06ec7c63dab6931ad95406a9f6e4f4da7
FastLee <lbareket@gmail.com>
2023-11-09 13:41:40 -0500

    Created a test to reproduce the error.

--- src/databricks/labs/ucx/hive_metastore/grants.py
+++ src/databricks/labs/ucx/hive_metastore/grants.py
@@ -80,6 +80,11 @@ class Grant:
         else:
             return f"GRANT {self.action_type} ON {object_type} {object_key} TO `{self.principal}`"
 
+    def hive_chown_sql(self) -> str:
+        object_type, object_key = self.this_type_and_key()
+        # See https://docs.databricks.com/en/sql/language-manual/security-grant.html
+        return f"ALTER {object_key} OWNER TO `{self.principal}`"
+
     def hive_revoke_sql(self) -> str:
         object_type, object_key = self.this_type_and_key()
         return f"REVOKE {self.action_type} ON {object_type} {object_key} FROM `{self.principal}`"
