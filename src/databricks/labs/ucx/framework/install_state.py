3b729d1b6717916a583f3ec1b4c85d943f664017
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-11-09 15:51:41 +0100

    Use `~/.ucx/state.json` to store the state of both dashboards and jobs (#561)
    This speeds up (re-)installation on large workspaces with thousands of
    configured jobs.

---
+++ src/databricks/labs/ucx/framework/install_state.py
@@ -0,0 +1,45 @@
+import json
+import logging
+from json import JSONDecodeError
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.core import DatabricksError
+from databricks.sdk.service.workspace import ImportFormat
+
+logger = logging.getLogger(__name__)
+
+
+class InstallState:
+    def __init__(self, ws: WorkspaceClient, install_folder: str, version: int = 1):
+        self._ws = ws
+        self._state_file = f"{install_folder}/state.json"
+        self._version = version
+        self._state = {}
+
+    def __getattr__(self, item):
+        if not self._state:
+            self._state = self._load()
+        if item not in self._state["resources"]:
+            self._state["resources"][item] = {}
+        return self._state["resources"][item]
+
+    def _load(self):
+        default_state = {"$version": self._version, "resources": {}}
+        try:
+            raw = json.load(self._ws.workspace.download(self._state_file))
+            version = raw.get("$version", None)
+            if version != self._version:
+                msg = f"expected state $version={self._version}, got={version}"
+                raise ValueError(msg)
+            return raw
+        except DatabricksError as err:
+            if err.error_code == "RESOURCE_DOES_NOT_EXIST":
+                return default_state
+            raise err
+        except JSONDecodeError:
+            logger.warning(f"JSON state file corrupt: {self._state_file}")
+            return default_state
+
+    def save(self):
+        state_dump = json.dumps(self._state, indent=2).encode("utf8")
+        self._ws.workspace.upload(self._state_file, state_dump, format=ImportFormat.AUTO, overwrite=True)
