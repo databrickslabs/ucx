ee54e2be55a3b9302ede1995261085a69ddee183
FastLee <lbareket@gmail.com>
2023-11-13 11:36:27 -0500

    Added unit test.

--- src/databricks/labs/ucx/hive_metastore/grants.py
+++ src/databricks/labs/ucx/hive_metastore/grants.py
@@ -76,11 +76,10 @@ class Grant:
     def hive_grant_sql(self) -> str:
         object_type, object_key = self.this_type_and_key()
         # See https://docs.databricks.com/en/sql/language-manual/security-grant.html
-        if self.action_type.upper() == 'OWN':
+        if self.action_type.upper() == "OWN":
             return f"ALTER {object_key} OWNER TO `{self.principal}`"
         else:
             return f"GRANT {self.action_type} ON {object_type} {object_key} TO `{self.principal}`"
-
 
     def hive_revoke_sql(self) -> str:
         object_type, object_key = self.this_type_and_key()
