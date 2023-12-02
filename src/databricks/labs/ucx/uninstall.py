37757349d6e5cd39be60c9d27abcbc2bbffbad3b
Hari Selvarajan <hari.selvarajan@databricks.com>
2023-12-02 18:57:13 +0000

    change log name

--- src/databricks/labs/ucx/uninstall.py
+++ src/databricks/labs/ucx/uninstall.py
@@ -5,7 +5,7 @@ from databricks.sdk import WorkspaceClient
 from databricks.labs.ucx.__about__ import __version__
 from databricks.labs.ucx.install import WorkspaceInstaller
 
-logger = logging.getLogger("databricks.labs.ucx.uninstall")
+logger = logging.getLogger("databricks.labs.ucx.install")
 
 if __name__ == "__main__":
     logger.setLevel("INFO")
