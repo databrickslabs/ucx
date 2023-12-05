0a50d63e923cbf593d6196c3fb26346c8670c56b
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-12-05 18:05:01 -0500

    Enable integration tests for `EXTERNAL` table migrations (#677)
    This PR adds integration tests for EXTERNAL and MANAGED table migrations
    on Azure

--- tests/integration/hive_metastore/test_migrate.py
+++ tests/integration/hive_metastore/test_migrate.py
@@ -2,14 +2,16 @@ import logging
 
 import pytest
 
-from databricks.labs.ucx.hive_metastore import TablesCrawler
 from databricks.labs.ucx.hive_metastore.tables import TablesMigrate
+
+from ..conftest import StaticTablesCrawler
 
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.skip("integration testing environments on AWS do not yet have UC IAM properly setup")
-def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog, make_schema, make_table, env_or_skip):
+def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog, make_schema, make_table):
+    if not ws.config.is_azure:
+        pytest.skip("temporary: only works in azure test env")
     src_schema = make_schema(catalog_name="hive_metastore")
     src_managed_table = make_table(catalog_name=src_schema.catalog_name, schema_name=src_schema.name)
 
@@ -18,7 +20,8 @@ def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog,
 
     logger.info(f"dst_catalog={dst_catalog.name}, managed_table={src_managed_table.full_name}")
 
-    crawler = TablesCrawler(sql_backend, inventory_schema)
+    # crawler = TablesCrawler(sql_backend, inventory_schema)
+    crawler = StaticTablesCrawler(sql_backend, inventory_schema, [src_managed_table])
     tm = TablesMigrate(crawler, ws, sql_backend, dst_catalog.name)
     tm.migrate_tables()
 
@@ -26,13 +29,14 @@ def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog,
     assert len(target_tables) == 1
 
     target_table_properties = ws.tables.get(f"{dst_schema.full_name}.{src_managed_table.name}").properties
-    assert target_table_properties["upgraded_from"] == src_managed_table
+    assert target_table_properties["upgraded_from"] == src_managed_table.full_name
 
 
-@pytest.mark.skip("integration testing environments on AWS do not yet have UC IAM properly setup")
 def test_migrate_tables_with_cache_should_not_create_table(
-    ws, sql_backend, inventory_schema, make_random, make_catalog, make_schema, make_table, env_or_skip
+    ws, sql_backend, inventory_schema, make_random, make_catalog, make_schema, make_table
 ):
+    if not ws.config.is_azure:
+        pytest.skip("temporary: only works in azure test env")
     src_schema = make_schema(catalog_name="hive_metastore")
 
     dst_catalog = make_catalog()
@@ -56,10 +60,10 @@ def test_migrate_tables_with_cache_should_not_create_table(
         f"target_catalog={dst_catalog.name}, "
         f"source_managed_table={src_managed_table}"
         f"target_managed_table={dst_managed_table}"
-        f""
     )
 
-    crawler = TablesCrawler(sql_backend, inventory_schema)
+    # crawler = TablesCrawler(sql_backend, inventory_schema)
+    crawler = StaticTablesCrawler(sql_backend, inventory_schema, [src_managed_table])
     tm = TablesMigrate(crawler, ws, sql_backend, dst_catalog.name)
     tm.migrate_tables()
 
@@ -69,17 +73,21 @@ def test_migrate_tables_with_cache_should_not_create_table(
     assert target_tables[0]["tableName"] == table_name
 
 
-@pytest.mark.skip(reason="Needs Storage credential + External Location in place")
 def test_migrate_external_table(ws, sql_backend, inventory_schema, make_catalog, make_schema, make_table, env_or_skip):
+    if not ws.config.is_azure:
+        pytest.skip("temporary: only works in azure test env")
     src_schema = make_schema(catalog_name="hive_metastore")
-    src_external_table = make_table(schema_name=src_schema.name, external=True)
+
+    mounted_location = f'dbfs:/mnt/{env_or_skip("TEST_MOUNT_NAME")}/a/b/c'
+    src_external_table = make_table(schema_name=src_schema.name, external_csv=mounted_location)
 
     dst_catalog = make_catalog()
-    dst_schema = make_schema(catalog_name=dst_catalog.name, schema_name=src_schema.name)
+    dst_schema = make_schema(catalog_name=dst_catalog.name, name=src_schema.name)
 
     logger.info(f"dst_catalog={dst_catalog.name}, external_table={src_external_table.full_name}")
 
-    crawler = TablesCrawler(sql_backend, inventory_schema)
+    # crawler = TablesCrawler(sql_backend, inventory_schema)
+    crawler = StaticTablesCrawler(sql_backend, inventory_schema, [src_external_table])
     tm = TablesMigrate(crawler, ws, sql_backend, dst_catalog.name)
     tm.migrate_tables()
 
