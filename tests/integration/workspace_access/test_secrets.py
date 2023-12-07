58e6121f5d7fa478e97fef17e986d2f66494a4b6
andrascsillag-db <andras.csillag@databricks.com>
2023-12-07 16:57:54 +0100

    remove iteration from integration test

--- tests/integration/workspace_access/test_secrets.py
+++ tests/integration/workspace_access/test_secrets.py
@@ -24,32 +24,29 @@ def test_secrets(
 ):
     """
     This test does the following:
-    * creates 2 ws groups and secret scopes
-    * assigns ACL permissions to the groups on the secrets
+    * creates a ws group and secret scope
+    * assigns ACL permissions to the group on the secret
     * run assessment
-    * migrate groups
-    * verify that the migrated groups has the same permissions on the scopes
+    * migrate group
+    * verify that the migrated group has the same permissions on the scope
     :return:
     """
 
-    created_items = []
-    for _ in range(2):
-        ws_group, acc_group = make_ucx_group()
+    ws_group, acc_group = make_ucx_group()
 
-        scope = make_secret_scope()
-        make_secret_scope_acl(scope=scope, principal=ws_group.display_name, permission=AclPermission.WRITE)
+    scope = make_secret_scope()
+    make_secret_scope_acl(scope=scope, principal=ws_group.display_name, permission=AclPermission.WRITE)
 
-        scope_acl = ws.secrets.get_acl(scope, ws_group.display_name)
-        item = {
-            "ws_group": ws_group,
-            "acc_group": acc_group,
-            "scope": scope,
-            "scope_acl": scope_acl,
-        }
-        created_items.append(item)
-        logger.debug(f"secret scope acls: {scope_acl}")
+    scope_acl = ws.secrets.get_acl(scope, ws_group.display_name)
+    item = {
+        "ws_group": ws_group,
+        "acc_group": acc_group,
+        "scope": scope,
+        "scope_acl": scope_acl,
+    }
+    logger.debug(f"secret scope acls: {scope_acl}")
 
-    created_groups_names = [i["ws_group"].display_name for i in created_items]
+    created_group_name = ws_group.display_name
 
     # Task 1 - crawl_permissions
     secret_support = SecretScopesSupport(ws)
@@ -58,33 +55,32 @@ def test_secrets(
     pi.inventorize_permissions()
 
     # Task 2 - crawl_groups
-    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=created_groups_names)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=[created_group_name])
     group_manager.snapshot()
 
     # Task 2 - apply_permissions_to_backup_groups
-    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=created_groups_names)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=[created_group_name])
     group_manager.rename_groups()
 
     # Task 3 - reflect_account_groups_on_workspace
-    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=created_groups_names)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=[created_group_name])
     group_manager.reflect_account_groups_on_workspace()
 
     # Task 4 - apply_permissions_to_account_groups
-    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=created_groups_names)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema, include_group_names=[created_group_name])
     migration_state = group_manager.get_migration_state()
     permission_manager = PermissionManager.factory(ws, sql_backend, inventory_database=inventory_schema)
     permission_manager.apply_group_permissions(migration_state)
 
-    for item in created_items:
-        ws_group = item["ws_group"]
-        acc_group = item["acc_group"]
-        scope = item["scope"]
-        old_scope_acl = item["scope_acl"]
-        old_group = ws.groups.get(ws_group.id)
-        reflected_group = ws.groups.get(acc_group.id)
-        reflected_scope_acls = ws.secrets.get_acl(scope, reflected_group.display_name)
+    ws_group = item["ws_group"]
+    acc_group = item["acc_group"]
+    scope = item["scope"]
+    old_scope_acl = item["scope_acl"]
+    old_group = ws.groups.get(ws_group.id)
+    reflected_group = ws.groups.get(acc_group.id)
+    reflected_scope_acls = ws.secrets.get_acl(scope, reflected_group.display_name)
 
-        assert reflected_group.display_name == ws_group.display_name
-        assert reflected_group.id == acc_group.id
-        assert old_scope_acl == reflected_scope_acls
-        assert len(reflected_group.members) == len(old_group.members)
+    assert reflected_group.display_name == ws_group.display_name
+    assert reflected_group.id == acc_group.id
+    assert old_scope_acl == reflected_scope_acls
+    assert len(reflected_group.members) == len(old_group.members)
