cebf703f37f869b90f5ae562f5d2e8b18d7874ab
Serge Smertin <serge.smertin@databricks.com>
2023-12-20 16:28:18 +0100

    Decouple group configuration from `install.py`
    This PR reduces the size of `install.py` file

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -276,6 +276,7 @@ def test_save_config(ws):
         promtps=MockPrompts(
             {
                 r".*PRO or SERVERLESS SQL warehouse.*": "1",
+                r'Choose how to map the workspace groups.*': "2",
                 r".*": "",
             }
         ),
@@ -411,6 +412,7 @@ def test_save_config_auto_groups(ws):
         promtps=MockPrompts(
             {
                 r".*PRO or SERVERLESS SQL warehouse.*": "1",
+                r'Choose how to map the workspace groups.*': "2",
                 r".*": "",
             }
         ),
@@ -445,6 +447,7 @@ def test_save_config_strip_group_names(ws):
         promtps=MockPrompts(
             {
                 r".*PRO or SERVERLESS SQL warehouse.*": "1",
+                r'Choose how to map the workspace groups.*': '2',  # specify names
                 r".*workspace group names.*": "g1, g2, g99",
                 r".*": "",
             }
@@ -506,6 +509,7 @@ def test_save_config_with_custom_policy(ws):
             {
                 r".*PRO or SERVERLESS SQL warehouse.*": "1",
                 r".*follow a policy.*": "yes",
+                r'Choose how to map the workspace groups.*': "2",
                 r".*Choose a cluster policy.*": "0",
                 r".*": "",
             }
@@ -561,6 +565,7 @@ def test_save_config_with_glue(ws):
         promtps=MockPrompts(
             {
                 r".*PRO or SERVERLESS SQL warehouse.*": "1",
+                r'Choose how to map the workspace groups.*': "2",
                 r".*connect to the external metastore?.*": "yes",
                 r".*Choose a cluster policy.*": "0",
                 r".*": "",
