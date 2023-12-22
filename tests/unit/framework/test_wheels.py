3537abde3f0ef75425d193bb6ce17f7d3d1b2c1b
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-12-22 13:53:20 +0100

    Speedup installer execution (#727)
    This PR adds a level of parallelism to installer component and improves
    the speed of execution for installer-related integration test

--- tests/unit/framework/test_wheels.py
+++ tests/unit/framework/test_wheels.py
@@ -1,9 +1,12 @@
 import os
 from unittest.mock import MagicMock
 
+import pytest
+
 from databricks.labs.ucx.framework.wheels import Wheels
 
 
+@pytest.mark.skip  # move this test to a separate package, as it slows down the overall execution
 def test_build_wheel():
     ws = MagicMock()
     wheels = Wheels(ws, "/Shared/.ucx", "0.0.1")
