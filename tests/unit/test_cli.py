f20b252ae53c8d4c138b1fad322d381aa6772f2b
FastLee <lbareket@gmail.com>
2023-12-22 14:00:19 -0500

    Added unit test for CLI.

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -5,7 +5,7 @@ from databricks.sdk.errors import NotFound
 from databricks.sdk.service import iam
 from databricks.sdk.service.iam import ComplexValue, User
 
-from databricks.labs.ucx.cli import skip
+from databricks.labs.ucx.cli import revert_migrated_tables, skip
 
 
 @pytest.fixture
@@ -33,3 +33,22 @@ def test_skip_no_ucx(caplog, mocker):
     mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=None)
     skip(schema="schema", table="table")
     assert [rec.message for rec in caplog.records if "UCX configuration" in rec.message]
+
+
+def test_revert(mocker):
+    mocker.patch("databricks.sdk.WorkspaceClient.__init__", return_value=None)
+    current_user = MagicMock()
+    current_user.me.return_value = User(user_name="foo", groups=[ComplexValue(display="admins")])
+    current_user.return_value = None
+    mocker.patch("databricks.sdk.WorkspaceClient.current_user", return_value=current_user)
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.__init__", return_value=None)
+    installation = MagicMock()
+    installation.config.warehouse_id = 123
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=installation)
+    mocker.patch("databricks.labs.ucx.framework.crawlers.StatementExecutionBackend.__init__", return_value=None)
+    mocker.patch("databricks.labs.ucx.hive_metastore.tables.TablesMigrate.__init__", return_value=None)
+    rmt = mocker.patch(
+        "databricks.labs.ucx.hive_metastore.tables.TablesMigrate.revert_migrated_tables", return_value=None
+    )
+    revert_migrated_tables(schema="schema", table="table")
+    rmt.assert_called_with(schema="schema", table="table")
