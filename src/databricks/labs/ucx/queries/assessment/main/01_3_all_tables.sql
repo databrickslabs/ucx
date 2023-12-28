b53c49039ca81558dcab69bd4a9a2b31f85edb5c
FastLee <lbareket@gmail.com>
2023-12-28 15:32:31 -0500

    Added dashboard query

--- src/databricks/labs/ucx/queries/assessment/main/01_3_all_tables.sql
+++ src/databricks/labs/ucx/queries/assessment/main/01_3_all_tables.sql
@@ -1,6 +1,6 @@
 -- viz type=table, name=Table Types, search_by=name, columns=name,type,format,storage,is_delta,location
 -- widget title=Table Types, row=1, col=3, size_x=3, size_y=8
-SELECT CONCAT(`database`, '.', name) AS name,
+SELECT CONCAT(tables.`database`, '.', tables.name) AS name,
        object_type AS type,
        table_format AS format,
        CASE
@@ -13,5 +13,9 @@ SELECT CONCAT(`database`, '.', name) AS name,
            ELSE "EXTERNAL"
        END AS storage,
        IF(format = "DELTA", "Yes", "No") AS is_delta,
-       location
-FROM $inventory.tables
+       location,
+       size_in_bytes
+FROM $inventory.tables left outer join $inventory.table_size on
+$inventory.tables.catalog = $inventory.table_size.catalog and
+$inventory.tables.database = $inventory.table_size.database and
+$inventory.tables.name = $inventory.table_size.name
