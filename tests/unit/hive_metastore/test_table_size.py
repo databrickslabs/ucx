0f7865feb684cb28922a2d5c7457e2a83c92a1cc
Liran Bareket <lbareket@gmail.com>
2023-12-29 16:06:29 -0500

    Added Assessment step to estimate the size of DBFS Root Tables. (#741)
    Closes #330
    
    - Added assessment step estimate dbfs root table size.
    - This should help deciding whether the table is a good candidate for
    cloning.

---
+++ tests/unit/hive_metastore/test_table_size.py
@@ -0,0 +1,36 @@
+import sys
+
+from databricks.labs.ucx.hive_metastore.table_size import TableSize, TableSizeCrawler
+from tests.unit.framework.mocks import MockBackend
+
+
+class SparkSession:
+    pass
+
+
+def test_table_size_crawler(mocker):
+    errors = {}
+    rows = {
+        "table_size": [],
+        "hive_metastore.inventory_database.tables": [
+            ("hive_metastore", "db1", "table1", "MANAGED", "DELTA", "dbfs:/location/table", None),
+            ("hive_metastore", "db1", "table2", "MANAGED", "DELTA", "/dbfs/location/table", None),
+            ("hive_metastore", "db1", "table3", "MANAGED", "DELTA", "dbfs:/mnt/location/table", None),
+            ("hive_metastore", "db1", "table4", "MANAGED", "DELTA", "s3:/location/table", None),
+            ("hive_metastore", "db1", "table5", "MANAGED", "DELTA", "/dbfs/mnt/location/table", None),
+            ("hive_metastore", "db1", "table6", "MANAGED", "DELTA", "/dbfs/databricks-datasets/location/table", None),
+            ("hive_metastore", "db1", "table7", "MANAGED", "DELTA", "dbfs:/databricks-datasets/location/table", None),
+            ("hive_metastore", "db1", "table8", "MANAGED", "DELTA", "/databricks-datasets/location/table", None),
+            ("hive_metastore", "db1", "view", "VIEW", "DELTA", None, "SELECT * FROM TABLE"),
+        ],
+        "SHOW DATABASES": [("db1",)],
+    }
+    backend = MockBackend(fails_on_first=errors, rows=rows)
+    pyspark_sql_session = mocker.Mock()
+    sys.modules["pyspark.sql.session"] = pyspark_sql_session
+    tsc = TableSizeCrawler(backend, "inventory_database")
+    tsc._spark._jsparkSession.table().queryExecution().analyzed().stats().sizeInBytes.side_effect = [100, 200, 300]
+    results = tsc.snapshot()
+    assert len(results) == 2
+    assert TableSize("hive_metastore", "db1", "table1", 100) in results
+    assert TableSize("hive_metastore", "db1", "table2", 200) in results
