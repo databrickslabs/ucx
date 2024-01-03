06c666c574f08f218c3982d3c2a201dc3437cfd4
Douglas Moore <dmoore247@users.noreply.github.com>
2024-01-03 11:00:19 -0500

    Reorganize assessment dashboard to increase readability (#738)
    Closes #734 
    <img width="1673" alt="image"
    src="https://github.com/databrickslabs/ucx/assets/1122251/42e8eba8-9a89-4a20-958d-1209f51e542d">

--- src/databricks/labs/ucx/queries/assessment/main/03_0_all_tables.sql
+++ src/databricks/labs/ucx/queries/assessment/main/03_0_all_tables.sql
@@ -1,5 +1,5 @@
 -- viz type=table, name=Table Types, search_by=name, columns=name,type,format,storage,is_delta,location
--- widget title=Table Types, row=1, col=3, size_x=3, size_y=8
+-- widget title=Table Types, row=4, col=0, size_x=6, size_y=8
 SELECT CONCAT(tables.`database`, '.', tables.name) AS name,
        object_type AS type,
        table_format AS format,
