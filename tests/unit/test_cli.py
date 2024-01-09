ff10e67a0efadb433787df0bd5cec357efd201b6
Hari Selvarajan <105197202+HariGS-DB@users.noreply.github.com>
2024-01-09 20:19:13 +0000

    Added `databricks labs ucx move` command  (#756)
    closes https://github.com/databrickslabs/ucx/issues/675
    
    ---------
    
    Co-authored-by: Liran Bareket <lbareket@gmail.com>
    Co-authored-by: Marcin Wojtyczka <wojtyczka.marcin@gmail.com>
    Co-authored-by: prajin-29 <127273819+prajin-29@users.noreply.github.com>
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,11 +1,11 @@
-from unittest.mock import create_autospec
+from unittest.mock import create_autospec, patch
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import NotFound
 from databricks.sdk.service import iam
 from databricks.sdk.service.iam import User
 
-from databricks.labs.ucx.cli import repair_run, skip
+from databricks.labs.ucx.cli import move, repair_run, skip
 
 
 def test_skip_no_schema(caplog):
@@ -42,3 +42,48 @@ def test_no_step_in_repair_run(mocker, caplog):
         repair_run(w, "")
     except KeyError as e:
         assert e.args[0] == "You did not specify --step"
+
+
+def test_move_no_ucx(mocker, caplog):
+    w = create_autospec(WorkspaceClient)
+    w.current_user.me = lambda: iam.User(user_name="foo", groups=[iam.ComplexValue(display="admins")])
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=None)
+    move(w, "", "", "", "", "")
+    assert [rec.message for rec in caplog.records if "UCX configuration" in rec.message]
+
+
+def test_move_no_catalog(mocker, caplog):
+    w = create_autospec(WorkspaceClient)
+    w.current_user.me = lambda: iam.User(user_name="foo", groups=[iam.ComplexValue(display="admins")])
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=w.current_user)
+    move(w, "", "", "", "", "")
+    assert (
+        len([rec.message for rec in caplog.records if "Please enter from_catalog and to_catalog" in rec.message]) == 1
+    )
+
+
+def test_move_same_schema(mocker, caplog):
+    w = create_autospec(WorkspaceClient)
+    w.current_user.me = lambda: iam.User(user_name="foo", groups=[iam.ComplexValue(display="admins")])
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=w.current_user)
+    move(w, "SrcCat", "SrcS", "*", "SrcCat", "SrcS")
+    assert len([rec.message for rec in caplog.records if "please select a different schema" in rec.message]) == 1
+
+
+def test_move_no_schema(mocker, caplog):
+    w = create_autospec(WorkspaceClient)
+    w.current_user.me = lambda: iam.User(user_name="foo", groups=[iam.ComplexValue(display="admins")])
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=w.current_user)
+    move(w, "SrcCat", "", "*", "TgtCat", "")
+    assert len([rec.message for rec in caplog.records if "Please enter from_schema, to_schema" in rec.message]) == 1
+
+
+def test_move(mocker, caplog, monkeypatch):
+    w = create_autospec(WorkspaceClient)
+    w.current_user.me = lambda: iam.User(user_name="foo", groups=[iam.ComplexValue(display="admins")])
+    mocker.patch("databricks.labs.ucx.installer.InstallationManager.for_user", return_value=w.current_user)
+    monkeypatch.setattr("builtins.input", lambda _: "yes")
+
+    with patch("databricks.labs.ucx.hive_metastore.table_migrate.TableMove.move_tables", return_value=None) as m:
+        move(w, "SrcC", "SrcS", "*", "TgtC", "ToS")
+        m.assert_called_once()
