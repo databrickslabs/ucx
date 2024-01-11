1ea3c6a3e9c2ca0cda6481dd6a3aeb40f64c080b
mwojtyczka <marcin.wojtyczka@databricks.com>
2024-01-11 14:59:28 +0100

    implemented code review feedback

--- tests/unit/workspace_access/test_groups.py
+++ tests/unit/workspace_access/test_groups.py
@@ -12,7 +12,7 @@ from databricks.sdk.service.iam import ComplexValue, Group, ResourceMeta
 from databricks.labs.ucx.workspace_access.groups import (
     ConfigureGroups,
     GroupManager,
-    MigratedGroup,
+    MigratedGroup, MigrationState,
 )
 from tests.unit.framework.mocks import MockBackend
 
@@ -805,3 +805,21 @@ def test_configure_match():
     cg.run()
     assert r"\[(#+)\]" == cg.workspace_group_regex
     assert r"\((#+)\)" == cg.account_group_regex
+
+
+def test_state():
+    groups = [
+        MigratedGroup(
+            id_in_workspace="1", name_in_workspace="test1", name_in_account="acc_test1", temporary_name="db-temp-test1"
+        )
+    ]
+
+    state = MigrationState(groups)
+
+    assert state.get_target_principal("test1") == "acc_test1"
+    assert state.get_temp_principal("test1") == "db-temp-test1"
+    assert state.is_in_scope("test1")
+
+    assert not state.get_target_principal("invalid_group_name")
+    assert not state.get_temp_principal("invalid_group_name")
+    assert not state.is_in_scope("invalid_group_name")
