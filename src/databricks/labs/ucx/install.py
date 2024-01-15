11232f6b607ee73d2ab9dd06e11e0274eb0cb1f0
Hari Selvarajan <hari.selvarajan@databricks.com>
2024-01-15 14:43:47 +0000

    adding table info

--- src/databricks/labs/ucx/install.py
+++ src/databricks/labs/ucx/install.py
@@ -155,6 +155,7 @@ def deploy_schema(sql_backend: SqlBackend, inventory_schema: str):
             functools.partial(table, "table_failures", TableError),
             functools.partial(table, "workspace_objects", WorkspaceObjectInfo),
             functools.partial(table, "permissions", Permissions),
+            functools.partial(table, "azure_storage_accounts", AzureServicePrincipalInfo)
         ],
     )
     deployer.deploy_view("objects", "queries/views/objects.sql")
