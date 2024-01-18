56e0185716a893ab125d7652a2ae878766b0beba
FastLee <liran.bareket@databricks.com>
2024-01-18 15:41:09 -0500

    Added crawler and some tests.

--- src/databricks/labs/ucx/assessment/aws.py
+++ src/databricks/labs/ucx/assessment/aws.py
@@ -1,7 +1,14 @@
 import logging
+import re
 import subprocess
 import json
 from dataclasses import dataclass
+from typing import Callable, Iterable, List
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.compute import InstanceProfile
+
+from databricks.labs.ucx.framework.crawlers import CrawlerBase, SqlBackend
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +19,27 @@ class AWSRole:
     role_name: str
     role_id: str
     arn: str
+
+
+@dataclass
+class AWSPolicyAction:
+    resource_type: str
+    Action: set[str]
+    resource_path: str
+
+
+@dataclass
+class AWSInstanceProfileAccess:
+    instance_profile_arn: str
+    iam_role_arn: str
+
+
+@dataclass
+class AWSInstanceProfile:
+    instance_profile: str
+    resource_type: str
+    resource_path: str
+    actions: str
 
 
 def iam_profiles():
@@ -84,58 +112,116 @@ def run_command(command):
     return process.returncode, output.decode("utf-8"), error.decode("utf-8")
 
 
-class AwsResources:
-    def __init__(self):
-        pass
+class AWSResources:
+
+    S3_ACTIONS = {
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject",
+        "s3:PutObjectAcl"
+    }
+    S3_REGEX = "arn:aws:s3:::([a-zA-Z0-9+=,.@_-]*)\/\*"
+
+    def __init__(self, profile: str, command_runner: Callable[[str],str] = run_command):
+        self._profile = profile
+        self._command_runner = command_runner
+
+    def validate_connection(self):
+        validate_command = f"aws sts get-caller-identity --profile {self._profile}"
+        code, output, error = run_command(validate_command)
+        if code == 0:
+            logger.info(output)
+            return True
+        logger.error(error)
+        return False
+
+    def get_roles_in_instance_profile(self, instance_profile_name: str):
+        get_instance_profile_cmd = f"aws iam get-instance-profile --profile {self._profile} --instance-profile-name {instance_profile_name}"
+        code, output, error = run_command(get_instance_profile_cmd)
+        if code == 0:
+            ip_details = json.loads(output)
+            try:
+                for role in ip_details["InstanceProfile"]["Roles"]:
+                    yield AWSRole(
+                        path=role["Path"],
+                        role_name=role["RoleName"],
+                        role_id=role["RoleId"],
+                        arn=role["Arn"]
+                    )
+            except KeyError:
+                logger.error(f"Malformed response from AWS CLI {ip_details}")
+        logger.error(error)
+
+    def list_policies_in_role(self, role_name: str):
+        list_policies_cmd = f"aws iam list-role-policies --profile {self._profile} --role-name {role_name} --no-paginate"
+        code, output, error = run_command(list_policies_cmd)
+        if code == 0:
+            policies = json.loads(output)
+            for policy in policies.get("PolicyNames", []):
+                yield policy
+        logger.error(error)
+
+    def list_attached_policies_in_role(self, role_name: str):
+        list_attached_policies_cmd = f"aws iam list-attached-role-policies --profile {self._profile} --role-name {role_name}"
+        code, output, error = run_command(list_attached_policies_cmd)
+        if code == 0:
+            policies = json.loads(output)
+            for policy in policies.get("AttachedPolicies", []):
+                yield policy.get("PolicyName")
+        logger.error(error)
+
+    def get_role_policy(self, role_name, policy_name: str):
+        get_policy = f"aws iam get-role-policy --profile {self._profile} --role-name {role_name} --policy-name {policy_name}"
+        code, output, error = run_command(get_policy)
+        if code == 0:
+            policy = json.loads(output)
+            for action in policy["PolicyDocument"].get("Statement", []):
+                actions = action["Action"]
+                s3_action = False
+                if isinstance(actions, List):
+                    for single_action in actions:
+                        if single_action in self.S3_ACTIONS:
+                            s3_action = True
+                            continue
+                else:
+                    if actions in self.S3_ACTIONS:
+                        s3_action = True
+
+                if not s3_action:
+                    continue
+                for resource in action.get("Resource", []):
+                    match = re.match(self.S3_REGEX, resource)
+                    if match:
+                        yield AWSPolicyAction("s3", set(actions,), match.group(1))
+        logger.error(error)
+
+
+class AWSInstanceProfileCrawler(CrawlerBase[InstanceProfile]):
+    def __init__(self, ws: WorkspaceClient, sbe: SqlBackend, schema):
+        super().__init__(sbe, "hive_metastore", schema, "aws_instance_profile", InstanceProfile)
+        self._ws = ws
+        self._schema = schema
+
+    def _crawl(self) -> Iterable[InstanceProfile]:
+        return list(self._ws.instance_profiles.list())
+
+    def snapshot(self) -> Iterable[InstanceProfile]:
+        return self._snapshot(self._try_fetch, self._crawl)
+
+    def _try_fetch(self) -> Iterable[InstanceProfile]:
+        for row in self._fetch(f"SELECT * FROM {self._schema}.{self._table}"):
+            yield InstanceProfile(*row)
+
+class AWSResourcePermissions:
+    def __init__(self, ws: WorkspaceClient, awsrm: AWSResources,
+                 instance_profile_crawler: AWSInstanceProfileCrawler,
+                 folder: str | None = None):
+        self._folder = folder
+        self._awsrm = awsrm
+        self._ws = ws
+
+    def _get_instance_profiles(self) -> list[str]:
+        instance_profiles = self._awsrm.snapshot()
 
 
 
-
-
-
-def validate_connection(profile: str):
-    validate_command = f"aws sts get-caller-identity --profile {profile}"
-    code, output, error = run_command(validate_command)
-    if code == 0:
-        logger.info(output)
-        return True
-    logger.error(error)
-    return False
-
-
-def get_roles_in_instance_profile(instance_profile_name: str):
-    get_instance_profile_cmd = f"aws iam get-instance-profile --instance-profile-name {instance_profile_name}"
-    code, output, error = run_command(get_instance_profile_cmd)
-    if code == 0:
-        ip_details = json.loads(output)
-        try:
-            for role in ip_details["InstanceProfile"]["Roles"]:
-                yield AWSRole(
-                    path=role["Path"],
-                    role_name=role["RoleName"],
-                    role_id=role["RoleId"],
-                    arn=role["Arn"]
-                )
-        except KeyError:
-            logger.error(f"Malformed response from AWS CLI {ip_details}")
-    logger.error(error)
-
-
-def get_policies_in_role(role_name: str):
-    get_policies_in_role_cmd = f"aws iam list-role-policies --role-name {role_name}"
-    code, output, error = run_command(get_policies_in_role_cmd)
-    if code == 0:
-        policies = json.loads(output)
-        for policy in policies.get("PolicyNames", []):
-            yield policy
-    logger.error(error)
-
-
-def get_attached_policies_in_role(role_name: str):
-    get_policies_in_role_cmd = f"aws iam list-attached-role-policies --role-name {role_name}"
-    code, output, error = run_command(get_policies_in_role_cmd)
-    if code == 0:
-        policies = json.loads(output)
-        for policy in policies.get("AttachedPolicies", []):
-            yield policy.get("PolicyName")
-    logger.error(error)
