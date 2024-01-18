36fcef0e0c5907d8fa86c12ba06ccb3316fac969
Serge Smertin <serge.smertin@databricks.com>
2024-01-18 11:50:47 +0100

    add lakeview

--- tests/sql_formatter.py
+++ tests/sql_formatter.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 import sqlglot
 
+
 def test_sql():
-    query_folder = Path(__file__).parent / '../src/databricks/labs/ucx/assessment/queries'
-    for query_file in query_folder.glob('*.sql'):
-        with query_file.open('r') as f:
+    query_folder = Path(__file__).parent / "../src/databricks/labs/ucx/queries/assessment/main"
+    for query_file in query_folder.glob("*.sql"):
+        with query_file.open("r") as f:
             sql_query = f.read()
-            sql_ast = sqlglot.parse_one(sql_query, read='spark')
-            formatted = sql_ast.sql(dialect='spark', pretty=True)
+            sql_ast = sqlglot.parse_one(sql_query, read="spark")
+            formatted = sql_ast.sql(dialect="spark", pretty=True)
             print(formatted)
             print()
