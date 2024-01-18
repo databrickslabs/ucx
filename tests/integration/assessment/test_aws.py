1c8871b53d69f737190fd28743d66b4dcc907a12
FastLee <liran.bareket@databricks.com>
2024-01-18 17:49:44 -0500

    Tied up development. First Cut.

--- tests/integration/assessment/test_aws.py
+++ tests/integration/assessment/test_aws.py
@@ -2,7 +2,7 @@ import logging
 
 import pytest
 
-from databricks.labs.ucx.assessment.aws import AWSResources
+from databricks.labs.ucx.assessment.aws import AWSResources, AWSInstanceProfileCrawler
 
 
 def test_aws_validate(env_or_skip):
@@ -34,6 +34,16 @@ def test_role_policy(env_or_skip):
     role_policy_actions = list(aws.get_role_policy("databricks-s3-access","databricks-s3-access"))
     print(role_policy_actions)
     assert len(role_policy_actions) == 15
+
+
+def test_instance_profile_crawler(env_or_skip, ws, sql_backend, inventory_schema):
+    profile = env_or_skip("AWS_DEFAULT_PROFILE")
+    instance_profile_crawler = AWSInstanceProfileCrawler(ws,sql_backend,inventory_schema)
+    instance_profiles = instance_profile_crawler.snapshot()
+    print(instance_profiles)
+    assert instance_profiles
+
+
 
 
 
