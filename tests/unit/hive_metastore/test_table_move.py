066e1b9564fe5c173c542de33e1018ae74f6938b
FastLee <liran.bareket@databricks.com>
2024-01-24 15:52:07 -0500

    Added unit tests

--- tests/unit/hive_metastore/test_table_move.py
+++ tests/unit/hive_metastore/test_table_move.py
@@ -1,5 +1,5 @@
 import logging
-from unittest.mock import create_autospec
+from unittest.mock import MagicMock, create_autospec
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import NotFound
@@ -12,6 +12,7 @@ from databricks.sdk.service.catalog import (
     TableType,
 )
 
+from databricks.labs.ucx.framework.crawlers import StatementExecutionBackend
 from databricks.labs.ucx.hive_metastore.table_migrate import TableMove
 from databricks.labs.ucx.mixins.sql import Row
 
@@ -64,10 +65,10 @@ def test_move_tables_not_found_table_error(mocker, caplog):
     assert len([rec.message for rec in caplog.records if "Could not find table SrcC.SrcS.table1" in rec.message]) == 1
 
 
-def test_move_tables_not_found_table_unknown_error(mocker, caplog):
+def test_move_tables_not_found_table_unknown_error(caplog):
     client = create_autospec(WorkspaceClient)
     client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
-    backend = mocker.patch("databricks.labs.ucx.framework.crawlers.StatementExecutionBackend.__init__")
+    backend = backend = create_autospec(StatementExecutionBackend)
     backend.execute.side_effect = NotFound("unknown error")
 
     client.tables.list.return_value = [
@@ -86,10 +87,33 @@ def test_move_tables_not_found_table_unknown_error(mocker, caplog):
     assert len([rec.message for rec in caplog.records if "unknown error" in rec.message]) == 1
 
 
-def test_move_tables_not_found_view_error(mocker, caplog):
+def test_alias_tables_not_found_table_unknown_error(caplog):
     client = create_autospec(WorkspaceClient)
     client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
-    backend = mocker.patch("databricks.labs.ucx.framework.crawlers.StatementExecutionBackend.__init__")
+    backend = create_autospec(StatementExecutionBackend)
+    backend.execute = MagicMock()
+    backend.execute.side_effect = NotFound("unknown error")
+
+    client.tables.list.return_value = [
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="table1",
+            full_name="SrcC.SrcS.table1",
+            table_type=TableType.EXTERNAL,
+        ),
+    ]
+    client.tables.get.side_effect = [NotFound()]
+
+    tm = TableMove(client, backend)
+    tm.alias_tables("SrcC", "SrcS", "table1", "TgtC", "TgtS")
+    assert len([rec.message for rec in caplog.records if "unknown error" in rec.message]) == 1
+
+
+def test_move_tables_not_found_view_error(caplog):
+    client = create_autospec(WorkspaceClient)
+    client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
+    backend = create_autospec(StatementExecutionBackend)
     backend.execute.side_effect = NotFound("[TABLE_OR_VIEW_NOT_FOUND]")
 
     client.tables.list.return_value = [
@@ -109,10 +133,56 @@ def test_move_tables_not_found_view_error(mocker, caplog):
     assert len([rec.message for rec in caplog.records if "Could not find view SrcC.SrcS.view1" in rec.message]) == 1
 
 
-def test_move_tables_not_found_view_unknown_error(mocker, caplog):
+def test_alias_tables_not_found_view_error(caplog):
     client = create_autospec(WorkspaceClient)
     client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
-    backend = mocker.patch("databricks.labs.ucx.framework.crawlers.StatementExecutionBackend.__init__")
+    backend = create_autospec(StatementExecutionBackend)
+    backend.execute.side_effect = NotFound("[TABLE_OR_VIEW_NOT_FOUND]")
+
+    client.tables.list.return_value = [
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="view1",
+            full_name="SrcC.SrcS.view1",
+            table_type=TableType.VIEW,
+            view_definition="SELECT * FROM SrcC.SrcS.table1",
+        ),
+    ]
+    client.tables.get.side_effect = [NotFound()]
+
+    tm = TableMove(client, backend)
+    tm.alias_tables("SrcC", "SrcS", "view1", "TgtC", "TgtS")
+    assert len([rec.message for rec in caplog.records if "Could not find view SrcC.SrcS.view1" in rec.message]) == 1
+
+
+def test_move_tables_not_found_view_unknown_error(caplog):
+    client = create_autospec(WorkspaceClient)
+    client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
+    backend = create_autospec(StatementExecutionBackend)
+    backend.execute.side_effect = NotFound("unknown error")
+
+    client.tables.list.return_value = [
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="view1",
+            full_name="SrcC.SrcS.view1",
+            table_type=TableType.VIEW,
+            view_definition="SELECT * FROM SrcC.SrcS.table1",
+        ),
+    ]
+    client.tables.get.side_effect = [NotFound()]
+
+    tm = TableMove(client, backend)
+    tm.move_tables("SrcC", "SrcS", "view1", "TgtC", "TgtS", False)
+    assert len([rec.message for rec in caplog.records if "unknown error" in rec.message]) == 1
+
+
+def test_alias_tables_not_found_view_unknown_error(caplog):
+    client = create_autospec(WorkspaceClient)
+    client.schemas.get.side_effect = [SchemaInfo(), NotFound()]
+    backend = create_autospec(StatementExecutionBackend)
     backend.execute.side_effect = NotFound("unknown error")
 
     client.tables.list.return_value = [
@@ -231,6 +301,93 @@ def test_move_all_tables_and_drop_source():
         "DROP VIEW SrcC.SrcS.view2",
         "SHOW CREATE TABLE SrcC.SrcS.table1",
         "SHOW CREATE TABLE SrcC.SrcS.table2",
+    ] == sorted(backend.queries)
+
+
+def test_alias_all_tables():
+    client = create_autospec(WorkspaceClient)
+
+    client.tables.list.return_value = [
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="table1",
+            full_name="SrcC.SrcS.table1",
+            table_type=TableType.EXTERNAL,
+        ),
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="table2",
+            full_name="SrcC.SrcS.table2",
+            table_type=TableType.EXTERNAL,
+        ),
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="table3",
+            full_name="SrcC.SrcS.table3",
+            table_type=TableType.MANAGED,
+        ),
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="view1",
+            full_name="SrcC.SrcS.view1",
+            table_type=TableType.VIEW,
+            view_definition="SELECT * FROM SrcC.SrcS.another_table1 WHERE field1=value",
+        ),
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="view2",
+            full_name="SrcC.SrcS.view2",
+            table_type=TableType.VIEW,
+            view_definition="SELECT * FROM SrcC.SrcS.another_table2 WHERE field2=value",
+        ),
+        TableInfo(
+            catalog_name="SrcC",
+            schema_name="SrcS",
+            name="view3",
+            full_name="SrcC.SrcS.view3",
+            table_type=TableType.VIEW,
+            view_definition="SELECT * FROM SrcC.SrcS.table1",
+        ),
+    ]
+
+    perm_list = PermissionsList([PrivilegeAssignment("foo", [Privilege.SELECT])])
+    perm_none = PermissionsList(None)
+
+    grants_mapping = {
+        "SrcC.SrcS.table1": perm_list,
+        "SrcC.SrcS.table2": perm_none,
+        "SrcC.SrcS.table3": perm_none,
+        "SrcC.SrcS.view1": perm_list,
+        "SrcC.SrcS.view2": perm_none,
+        "SrcC.SrcS.view3": perm_none,
+    }
+
+    def target_tables_mapping(full_name):
+        to_migrate = ["TgtC.TgtS.table1", "TgtC.TgtS.table2", "TgtC.TgtS.view1", "TgtC.TgtS.view2"]
+        if full_name in to_migrate:
+            raise NotFound()
+
+        not_to_migrate = ["TgtC.TgtS.table3", "TgtC.TgtS.view3"]
+        if full_name in not_to_migrate:
+            return TableInfo()
+
+    client.grants.get.side_effect = lambda _, full_name: grants_mapping[full_name]
+    client.schemas.get.side_effect = [SchemaInfo(), SchemaInfo()]
+    client.tables.get.side_effect = target_tables_mapping
+    backend = MockBackend()
+    tm = TableMove(client, backend)
+    tm.alias_tables("SrcC", "SrcS", "*", "TgtC", "TgtS")
+
+    assert [
+        'CREATE VIEW TgtC.TgtS.table1 AS SELECT * FROM SrcC.SrcS.table1',
+        'CREATE VIEW TgtC.TgtS.table2 AS SELECT * FROM SrcC.SrcS.table2',
+        'CREATE VIEW TgtC.TgtS.view1 AS SELECT * FROM SrcC.SrcS.another_table1 WHERE ' 'field1=value',
+        'CREATE VIEW TgtC.TgtS.view2 AS SELECT * FROM SrcC.SrcS.another_table2 WHERE ' 'field2=value',
     ] == sorted(backend.queries)
 
 
