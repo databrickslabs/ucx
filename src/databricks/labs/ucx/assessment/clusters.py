databrickslabs/ucx

commit c03596eb2538ebc3754641bd50abafcf11b60d4a

Cleaned up Job assessment and Cluster assessment to improve testing and reduce redundancy. (#825)

src/databricks/labs/ucx/assessment/clusters.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import NotFound
-from databricks.sdk.service.compute import ClusterSource, Policy
+from databricks.sdk.service.compute import ClusterDetails, ClusterSource, Policy
 
 from databricks.labs.ucx.assessment.crawlers import (
     _AZURE_SP_CONF_FAILURE_MSG,
@@ -67,6 +67,35 @@ class ClustersMixin:
                 continue
             failures.append(f"{_AZURE_SP_CONF_FAILURE_MSG} cluster.")
 
+    def _check_cluster_failures(self, cluster: ClusterDetails):
+        failures = []
+        if not cluster.creator_user_name:
+            logger.warning(
+                f"Cluster {cluster.cluster_id} have Unknown creator, it means that the original creator "
+                f"has been deleted and should be re-created"
+            )
+        cluster_info = ClusterInfo(
+            cluster_id=cluster.cluster_id if cluster.cluster_id else "",
+            cluster_name=cluster.cluster_name,
+            creator=cluster.creator_user_name,
+            success=1,
+            failures="[]",
+        )
+        support_status = spark_version_compatibility(cluster.spark_version)
+        if support_status != "supported":
+            failures.append(f"not supported DBR: {cluster.spark_version}")
+        if cluster.spark_conf is not None:
+            self._check_spark_conf(cluster, failures)
+        # Checking if Azure cluster config is present in cluster policies
+        if cluster.policy_id:
+            self._check_cluster_policy(cluster, failures)
+        if cluster.init_scripts:
+            self._check_init_scripts(cluster, failures)
+        cluster_info.failures = json.dumps(failures)
+        if len(failures) > 0:
+            cluster_info.success = 0
+        return cluster_info
+
 
 class ClustersCrawler(CrawlerBase[ClusterInfo], ClustersMixin):
     def __init__(self, ws: WorkspaceClient, sbe: SqlBackend, schema):
@@ -81,37 +110,7 @@ class ClustersCrawler(CrawlerBase[ClusterInfo], ClustersMixin):
         for cluster in all_clusters:
             if cluster.cluster_source == ClusterSource.JOB:
                 continue
-            if not cluster.creator_user_name:
-                logger.warning(
-                    f"Cluster {cluster.cluster_id} have Unknown creator, it means that the original creator "
-                    f"has been deleted and should be re-created"
-                )
-            cluster_info = ClusterInfo(
-                cluster_id=cluster.cluster_id,
-                cluster_name=cluster.cluster_name,
-                creator=cluster.creator_user_name,
-                success=1,
-                failures="[]",
-            )
-            support_status = spark_version_compatibility(cluster.spark_version)
-            failures = []
-            if support_status != "supported":
-                failures.append(f"not supported DBR: {cluster.spark_version}")
-
-            if cluster.spark_conf is not None:
-                self._check_spark_conf(cluster, failures)
-
-            # Checking if Azure cluster config is present in cluster policies
-            if cluster.policy_id:
-                self._check_cluster_policy(cluster, failures)
-
-            if cluster.init_scripts:
-                self._check_init_scripts(cluster, failures)
-
-            cluster_info.failures = json.dumps(failures)
-            if len(failures) > 0:
-                cluster_info.success = 0
-            yield cluster_info
+            yield self._check_cluster_failures(cluster)
 
     def snapshot(self) -> Iterable[ClusterInfo]:
         return self._snapshot(self._try_fetch, self._crawl)

src/databricks/labs/ucx/assessment/crawlers.py
@@ -63,7 +63,9 @@ def _azure_sp_conf_present_check(config: dict) -> bool:
     return False
 
 
-def spark_version_compatibility(spark_version: str) -> str:
+def spark_version_compatibility(spark_version: str | None) -> str:
+    if not spark_version:
+        return "unreported version"
     first_comp_custom_rt = 3
     first_comp_custom_x = 2
     dbr_version_components = spark_version.split("-")

src/databricks/labs/ucx/assessment/jobs.py
@@ -3,19 +3,10 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.errors import NotFound
-from databricks.sdk.service.compute import Policy
 from databricks.sdk.service.jobs import BaseJob
 
-from databricks.labs.ucx.assessment.crawlers import (
-    _AZURE_SP_CONF_FAILURE_MSG,
-    INCOMPATIBLE_SPARK_CONFIG_KEYS,
-    _azure_sp_conf_in_init_scripts,
-    _azure_sp_conf_present_check,
-    _get_init_script_data,
-    logger,
-    spark_version_compatibility,
-)
+from databricks.labs.ucx.assessment.clusters import ClustersMixin
+from databricks.labs.ucx.assessment.crawlers import logger
 from databricks.labs.ucx.framework.crawlers import CrawlerBase, SqlBackend
 
 
@@ -28,7 +19,7 @@ class JobInfo:
     creator: str | None = None
 
 
-class JobsMixin:
+class JobsMixin(ClustersMixin):
     @staticmethod
     def _get_cluster_configs_from_all_jobs(all_jobs, all_clusters_by_id):
         for j in all_jobs:
@@ -90,30 +81,12 @@ class JobsCrawler(CrawlerBase[JobInfo], JobsMixin):
             )
 
         for job, cluster_config in self._get_cluster_configs_from_all_jobs(all_jobs, all_clusters_by_id):
-            support_status = spark_version_compatibility(cluster_config.spark_version)
             job_id = job.job_id
             if not job_id:
                 continue
-            if support_status != "supported":
-                job_assessment[job_id].add(f"not supported DBR: {cluster_config.spark_version}")
-
-            if cluster_config.spark_conf is not None:
-                self._job_spark_conf(cluster_config, job_assessment, job_id)
-
-            # Checking if Azure cluster config is present in cluster policies
-            if cluster_config.policy_id:
-                policy = self._safe_get_cluster_policy(cluster_config.policy_id)
-                if policy is None:
-                    continue
-                if policy.definition:
-                    if _azure_sp_conf_present_check(json.loads(policy.definition)):
-                        job_assessment[job_id].add(f"{_AZURE_SP_CONF_FAILURE_MSG} Job cluster.")
-                if policy.policy_family_definition_overrides:
-                    if _azure_sp_conf_present_check(json.loads(policy.policy_family_definition_overrides)):
-                        job_assessment[job_id].add(f"{_AZURE_SP_CONF_FAILURE_MSG} Job cluster.")
-
-            if cluster_config.init_scripts:
-                self._init_scripts(cluster_config, job_assessment, job_id)
+            cluster_failures = self._check_cluster_failures(cluster_config)
+            for failure in json.loads(cluster_failures.failures):
+                job_assessment[job_id].add(failure)
 
         # TODO: next person looking at this - rewrite, as this code makes no sense
         for job_key in job_details.keys():  # pylint: disable=consider-using-dict-items,consider-iterating-dictionary
@@ -121,33 +94,6 @@ class JobsCrawler(CrawlerBase[JobInfo], JobsMixin):
             if len(job_assessment[job_key]) > 0:
                 job_details[job_key].success = 0
         return list(job_details.values())
-
-    def _init_scripts(self, cluster_config, job_assessment, job_id):
-        for init_script_info in cluster_config.init_scripts:
-            init_script_data = _get_init_script_data(self._ws, init_script_info)
-            if not init_script_data:
-                continue
-            if not _azure_sp_conf_in_init_scripts(init_script_data):
-                continue
-            job_assessment[job_id].add(f"{_AZURE_SP_CONF_FAILURE_MSG} Job cluster.")
-
-    def _job_spark_conf(self, cluster_config, job_assessment, job_id):
-        for k in INCOMPATIBLE_SPARK_CONFIG_KEYS:
-            if k in cluster_config.spark_conf:
-                job_assessment[job_id].add(f"unsupported config: {k}")
-        for value in cluster_config.spark_conf.values():
-            if "dbfs:/mnt" in value or "/dbfs/mnt" in value:
-                job_assessment[job_id].add(f"using DBFS mount in configuration: {value}")
-        # Checking if Azure cluster config is present in spark config
-        if _azure_sp_conf_present_check(cluster_config.spark_conf):
-            job_assessment[job_id].add(f"{_AZURE_SP_CONF_FAILURE_MSG} Job cluster.")
-
-    def _safe_get_cluster_policy(self, policy_id: str) -> Policy | None:
-        try:
-            return self._ws.cluster_policies.get(policy_id)
-        except NotFound:
-            logger.warning(f"The cluster policy was deleted: {policy_id}")
-            return None
 
     def snapshot(self) -> Iterable[JobInfo]:
         return self._snapshot(self._try_fetch, self._crawl)

tests/unit/assessment/test_clusters.py
@@ -1,7 +1,7 @@
-from unittest.mock import Mock, create_autospec
+import json
+from unittest.mock import MagicMock
 
 import pytest
-from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import DatabricksError, InternalError, NotFound
 from databricks.sdk.service.compute import (
     AutoScale,
@@ -19,87 +19,10 @@ from ..framework.mocks import MockBackend
 from . import workspace_client_mock
 
 
-def test_cluster_assessment(mocker):
-    sample_clusters = [
-        ClusterDetails(
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_conf={"spark.databricks.delta.preview.enabled": "true"},
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="13.3.x-cpu-ml-scala2.12",
-            cluster_id="0807-225846-motto493",
-        ),
-        ClusterDetails(
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_conf={"spark.databricks.delta.preview.enabled": "true"},
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="9.3.x-cpu-ml-scala2.12",
-            cluster_id="0810-225833-atlanta69",
-        ),
-        ClusterDetails(
-            cluster_name="Tech Summit FY24 Cluster",
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_conf={
-                "spark.hadoop.fs.azure.account."
-                "oauth2.client.id.abcde.dfs.core.windows.net": "{{secrets/abcff/sp_app_client_id}}",
-                "spark.hadoop.fs.azure.account."
-                "oauth2.client.endpoint.abcde.dfs.core.windows.net": "https://login.microsoftonline.com/dedededede"
-                "/token",
-                "spark.hadoop.fs.azure.account."
-                "oauth2.client.secret.abcde.dfs.core.windows.net": "{{secrets/abcff/sp_secret}}",
-            },
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="13.3.x-cpu-ml-scala2.12",
-            cluster_id="0915-190044-3dqy6751",
-        ),
-        ClusterDetails(
-            cluster_name="Tech Summit FY24 Cluster-1",
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            policy_id="D96308F1BF0003A7",
-            spark_version="13.3.x-cpu-ml-scala2.12",
-            cluster_id="0915-190044-3dqy6751",
-        ),
-    ]
-
-    ws = Mock()
-
-    ws.cluster_policies.get().definition = (
-        '{\n  "spark_conf.fs.azure.account.auth.type": {\n    '
-        '"type": "fixed",\n    "value": "OAuth",\n   '
-        ' "hidden": true\n  },\n  "spark_conf.fs.azure.account.oauth.provider.type": {\n   '
-        ' "type": "fixed",\n    "value": '
-        '"org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",\n    '
-        '"hidden": true\n  },\n  "spark_conf.fs.azure.account.oauth2.client.id": {\n    '
-        '"type": "fixed",\n    "value": "fsfsfsfsffsfsf",\n    "hidden": true\n  },\n  '
-        '"spark_conf.fs.azure.account.oauth2.client.secret": {\n    "type": "fixed",\n    '
-        '"value": "gfgfgfgfggfggfgfdds",\n    "hidden": true\n  },\n  '
-        '"spark_conf.fs.azure.account.oauth2.client.endpoint": {\n    '
-        '"type": "fixed",\n    '
-        '"value": "https://login.microsoftonline.com/1234ededed/oauth2/token",\n    '
-        '"hidden": true\n  }\n}'
-    )
-    ws.cluster_policies.get().policy_family_definition_overrides = (
-        '{\n  "spark_conf.fs.azure.account.auth.type": {\n    '
-        '"type": "fixed",\n    "value": "OAuth",\n   '
-        ' "hidden": true\n  },\n  "spark_conf.fs.azure.account.oauth.provider.type": {\n   '
-        ' "type": "fixed",\n    "value": '
-        '"org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",\n    '
-        '"hidden": true\n  },\n  "spark_conf.fs.azure.account.oauth2.client.id": {\n    '
-        '"type": "fixed",\n    "value": "fsfsfsfsffsfsf",\n    "hidden": true\n  },\n  '
-        '"spark_conf.fs.azure.account.oauth2.client.secret": {\n    "type": "fixed",\n    '
-        '"value": "gfgfgfgfggfggfgfdds",\n    "hidden": true\n  },\n  '
-        '"spark_conf.fs.azure.account.oauth2.client.endpoint": {\n    '
-        '"type": "fixed",\n    '
-        '"value": "https://login.microsoftonline.com/1234ededed/oauth2/token",\n    '
-        '"hidden": true\n  }\n}'
-    )
-
-    crawler = ClustersCrawler(ws, MockBackend(), "ucx")._assess_clusters(sample_clusters)
-    result_set = list(crawler)
+def test_cluster_assessment():
+    ws = workspace_client_mock(clusters="assortment-conf.json")
+    crawler = ClustersCrawler(ws, MockBackend(), "ucx")
+    result_set = list(crawler.snapshot())
 
     assert len(result_set) == 4
     assert result_set[0].success == 1
@@ -116,44 +39,23 @@ def test_cluster_assessment_cluster_policy_no_spark_conf():
     assert result_set1[0].success == 1
 
 
-def test_cluster_assessment_cluster_policy_not_found(mocker):
-    sample_clusters1 = [
-        ClusterDetails(
-            cluster_name="cluster1",
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            policy_id="D96308F1BF0003A8",
-            spark_version="13.3.x-cpu-ml-scala2.12",
-            cluster_id="0915-190044-3dqy6751",
-        )
-    ]
-    ws = Mock()
+def test_cluster_assessment_cluster_policy_not_found(caplog):
+    ws = workspace_client_mock(clusters="assortment-conf.json")
+    ws.cluster_policies.get = MagicMock()
     ws.cluster_policies.get.side_effect = NotFound("NO_POLICY")
-    clusters_crawler = ClustersCrawler(ws, MockBackend(), "ucx")
-    crawler = clusters_crawler._assess_clusters(sample_clusters1)
-    results = list(crawler)
-    assert len(results) == 1
+    crawler = ClustersCrawler(ws, MockBackend(), "ucx")
+    list(crawler.snapshot())
+    assert "The cluster policy was deleted" in caplog.messages[len(caplog.messages) - 1]
 
 
-def test_cluster_assessment_cluster_policy_exception(mocker):
-    sample_clusters1 = [
-        ClusterDetails(
-            cluster_name="cluster1",
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            policy_id="D96308F1BF0003A8",
-            spark_version="13.3.x-cpu-ml-scala2.12",
-            cluster_id="0915-190044-3dqy6751",
-        )
-    ]
-    ws = Mock()
+def test_cluster_assessment_cluster_policy_exception():
+    ws = workspace_client_mock(clusters="assortment-conf.json")
+    ws.cluster_policies.get = MagicMock()
     ws.cluster_policies.get.side_effect = InternalError(...)
-    crawler = ClustersCrawler(ws, MockBackend(), "ucx")._assess_clusters(sample_clusters1)
+    crawler = ClustersCrawler(ws, MockBackend(), "ucx")
 
     with pytest.raises(DatabricksError):
-        list(crawler)
+        list(crawler.snapshot())
 
 
 def test_cluster_assessment_with_spn_cluster_policy_not_found(mocker):
@@ -308,78 +210,36 @@ def test_cluster_init_script(mocker):
     assert len(init_crawler) == 1
 
 
-def test_cluster_init_script_check_dbfs(mocker):
-    sample_clusters = [
-        ClusterDetails(
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            cluster_source=ClusterSource.UI,
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="12.3.x-cpu-ml-scala2.12",
-            cluster_id="0810-225833-atlanta69",
-            cluster_name="Tech Summit FY24 Cluster-1",
-            init_scripts=[
-                InitScriptInfo(
-                    dbfs=DbfsStorageInfo(destination="dbfs:"),
-                    s3=None,
-                    volumes=None,
-                    workspace=None,
-                ),
-                InitScriptInfo(
-                    dbfs=DbfsStorageInfo(destination="dbfs"),
-                    s3=None,
-                    volumes=None,
-                    workspace=None,
-                ),
-                InitScriptInfo(
-                    dbfs=DbfsStorageInfo(destination=":/users/test@test.com/init_scripts/test.sh"),
-                    s3=None,
-                    volumes=None,
-                    workspace=None,
-                ),
-                InitScriptInfo(
-                    dbfs=None,
-                    s3=None,
-                    volumes=None,
-                    workspace=WorkspaceStorageInfo(
-                        destination="/Users/dipankar.kushari@databricks.com/init_script_1.sh"
-                    ),
-                ),
-            ],
-        )
-    ]
-    ws = create_autospec(WorkspaceClient)
-    ws.clusters.list.return_value = sample_clusters
+def test_cluster_init_script_check_dbfs():
+    ws = workspace_client_mock(clusters="dbfs-init-scripts.json")
     ws.dbfs.read().data = "JXNoCmVjaG8gIj0="
     ws.workspace.export().content = "JXNoCmVjaG8gIj0="
     init_crawler = ClustersCrawler(ws, MockBackend(), "ucx").snapshot()
     assert len(init_crawler) == 1
 
 
-def test_cluster_without_owner_should_have_empty_creator_name(mocker):
-    sample_clusters = [
-        ClusterDetails(
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            cluster_source=ClusterSource.UI,
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="12.3.x-cpu-ml-scala2.12",
-            cluster_id="0810-225833-atlanta69",
-            cluster_name="Tech Summit FY24 Cluster-1",
-        )
-    ]
-    ws = mocker.Mock()
+def test_cluster_without_owner_should_have_empty_creator_name():
+    ws = workspace_client_mock(clusters="no-spark-conf.json")
     mockbackend = MockBackend()
-
-    ws.clusters.list.return_value = sample_clusters
     ClustersCrawler(ws, mockbackend, "ucx").snapshot()
     result = mockbackend.rows_written_for("hive_metastore.ucx.clusters", "append")
     assert result == [
         ClusterInfo(
-            cluster_id="0810-225833-atlanta69",
-            cluster_name="Tech Summit FY24 Cluster-1",
+            cluster_id="0915-190044-3dqy6751",
+            cluster_name="Tech Summit FY24 Cluster-2",
             creator=None,
             success=1,
-            failures="[]",
+            failures='[]',
         )
     ]
+
+
+def test_cluster_with_multiple_failures():
+    ws = workspace_client_mock(clusters="multiple-failures-conf.json")
+    crawler = ClustersCrawler(ws, MockBackend(), "ucx")
+    result_set = list(crawler.snapshot())
+    assert len(result_set) == 1
+    assert result_set[0].success == 0
+    failures = json.loads(result_set[0].failures)
+    assert 'unsupported config: spark.databricks.passthrough.enabled' in failures
+    assert 'not supported DBR: 9.3.x-cpu-ml-scala2.12' in failures

tests/unit/assessment/test_jobs.py
@@ -68,6 +68,7 @@ def test_job_assessment():
         ClusterDetails(
             autoscale=AutoScale(min_workers=1, max_workers=6),
             spark_conf={"spark.databricks.delta.preview.enabled": "true"},
+            creator_user_name="anonymous@databricks.com",
             spark_context_id=5134472582179565315,
             spark_env_vars=None,
             spark_version="13.3.x-cpu-ml-scala2.12",
@@ -77,6 +78,7 @@ def test_job_assessment():
         ClusterDetails(
             autoscale=AutoScale(min_workers=1, max_workers=6),
             spark_conf={"spark.databricks.delta.preview.enabled": "true"},
+            creator_user_name="anonymous@databricks.com",
             spark_context_id=5134472582179565315,
             spark_env_vars=None,
             spark_version="9.3.x-cpu-ml-scala2.12",

commit b4128cc65cf68f93aba36e6e830c466f73d2df62

Addressed integration test issue (#839)
Fixed and issue introduced with PR #825

src/databricks/labs/ucx/assessment/clusters.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import NotFound
+from databricks.sdk.service import compute
 from databricks.sdk.service.compute import ClusterDetails, ClusterSource, Policy
 
 from databricks.labs.ucx.assessment.crawlers import (
@@ -67,17 +68,21 @@ class ClustersMixin:
                 continue
             failures.append(f"{_AZURE_SP_CONF_FAILURE_MSG} cluster.")
 
-    def _check_cluster_failures(self, cluster: ClusterDetails):
+    def _check_cluster_failures(self, cluster: ClusterDetails | compute.ClusterSpec):
         failures = []
-        if not cluster.creator_user_name:
+        if isinstance(cluster, ClusterDetails) and not cluster.creator_user_name:
             logger.warning(
                 f"Cluster {cluster.cluster_id} have Unknown creator, it means that the original creator "
                 f"has been deleted and should be re-created"
             )
+        cluster_id = cluster.cluster_id if isinstance(cluster, ClusterDetails) and cluster.cluster_id else ""
+        creator_user_name = (
+            cluster.creator_user_name if isinstance(cluster, ClusterDetails) and cluster.creator_user_name else None
+        )
         cluster_info = ClusterInfo(
-            cluster_id=cluster.cluster_id if cluster.cluster_id else "",
+            cluster_id=cluster_id,
             cluster_name=cluster.cluster_name,
-            creator=cluster.creator_user_name,
+            creator=creator_user_name,
             success=1,
             failures="[]",
         )