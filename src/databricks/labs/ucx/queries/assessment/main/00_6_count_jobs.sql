33b36dce28f2f5c4d7dcaaba49b1c8db734dff1b
Prajin <prajin.sivaprasad@databricks.com>
2024-01-30 12:09:54 +0530

    Added Job Count in Top Tile

---
+++ src/databricks/labs/ucx/queries/assessment/main/00_6_count_jobs.sql
@@ -0,0 +1,3 @@
+-- viz type=counter, name=Total Job Count, counter_label=Total Jobs, value_column=count_total_jobs
+-- widget row=0, col=6, size_x=1, size_y=3
+SELECT count(*) AS count_total_jobs FROM $inventory.jobs WHERE job_name not like '[UCX]%'
