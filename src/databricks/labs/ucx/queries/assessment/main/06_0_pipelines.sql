5aa8db33c58a4ba71b0be754d8a00e2ce2811a0a
Prajin <prajin.sivaprasad@databricks.com>
2024-01-31 11:12:15 +0530

    Added Job Count in Top Tile

--- src/databricks/labs/ucx/queries/assessment/main/06_0_pipelines.sql
+++ src/databricks/labs/ucx/queries/assessment/main/06_0_pipelines.sql
@@ -1,5 +1,5 @@
 -- viz type=table, name=Pipelines, columns=failure,pipeline_name,creator_name
--- widget title=Incompatible Delta Live Tables, row=6, col=0, size_x=3, size_y=8
+-- widget title=Incompatible Delta Live Tables, row=7, col=0, size_x=3, size_y=8
 SELECT
     EXPLODE(FROM_JSON(failures, 'array<string>')) AS failure,
     pipeline_name,
--- src/databricks/labs/ucx/queries/assessment/main/06_3_global_init_scripts.sql
+++ src/databricks/labs/ucx/queries/assessment/main/06_3_global_init_scripts.sql
@@ -1,5 +1,5 @@
 -- viz type=table, name=Global Init Scripts, columns=failure,script_name,created_by
--- widget title=Incompatible Global Init Scripts, row=5, col=3, size_x=3, size_y=8
+-- widget title=Incompatible Global Init Scripts, row=6, col=3, size_x=3, size_y=8
 SELECT
     EXPLODE(FROM_JSON(failures, 'array<string>')) AS failure,
     script_name,
