d482bcf04582866fd22b50c653464a4095a9facf
Vuong <vuong.nguyen@databricks.com>
2024-02-07 17:39:25 +0000

    almost final refactoring

--- tests/unit/assessment/test_azure.py
+++ tests/unit/assessment/test_azure.py
@@ -241,7 +241,7 @@ def test_save_spn_permissions_valid_storage_accounts(caplog, mocker, az_token):
 
 
 def test_azure_spn_info_without_secret():
-    ws = workspace_client_mock(clusters="single-cluster-spn-config.json")
+    ws = workspace_client_mock(clusters="single-cluster-spn.json")
     sample_spns = [{"application_id": "test123456789", "secret_scope": "", "secret_key": ""}]
     AzureServicePrincipalCrawler(ws, MockBackend(), "ucx")._list_all_cluster_with_spn_in_spark_conf()
     crawler = AzureServicePrincipalCrawler(ws, MockBackend(), "ucx")._assess_service_principals(sample_spns)
@@ -286,115 +286,7 @@ def test_azure_service_principal_info_no_spark_conf_crawl():
 
 
 def test_azure_service_principal_info_policy_family_conf_crawl(mocker):
-    sample_clusters = [
-        ClusterDetails(
-            autoscale=AutoScale(min_workers=1, max_workers=6),
-            cluster_source=ClusterSource.UI,
-            spark_context_id=5134472582179565315,
-            spark_env_vars=None,
-            spark_version="9.3.x-cpu-ml-scala2.12",
-            cluster_id="0810-225833-atlanta69",
-            cluster_name="Tech Summit FY24 Cluster-1",
-            spark_conf={
-                "spark.hadoop.fs.azure.account.oauth2.client.id.abcde.dfs.core.windows.net": "",
-            },
-        )
-    ]
-    sample_pipelines = [
-        PipelineInfo(
-            creator_name="abcde.defgh@databricks.com",
-            pipeline_name="New DLT Pipeline",
-            pipeline_id="0112eae7-9d11-4b40-a2b8-6c83cb3c7497",
-            success=1,
-            failures="",
-        )
-    ]
-    sample_jobs = [
-        BaseJob(
-            created_time=1694536604319,
-            creator_user_name="anonymous@databricks.com",
-            job_id=536591785949415,
-            settings=JobSettings(
-                compute=None,
-                continuous=None,
-                job_clusters=[
-                    JobCluster(
-                        job_cluster_key="rrrrrrrr",
-                        new_cluster=ClusterSpec(autoscale=None, node_type_id="Standard_DS3_v2", num_workers=2),
-                    ),
-                ],
-                tasks=[
-                    Task(
-                        task_key="Ingest",
-                        existing_cluster_id="0810-225833-atlanta69",
-                        new_cluster=ClusterSpec(autoscale=None, node_type_id="Standard_DS3_v2", num_workers=2),
-                        notebook_task=NotebookTask(
-                            notebook_path="/Users/foo.bar@databricks.com/Customers/Example/Test/Load"
-                        ),
-                        timeout_seconds=0,
-                    )
-                ],
-                timeout_seconds=0,
-            ),
-        ),
-        BaseJob(
-            created_time=1694536604319,
-            creator_user_name="anonymous@databricks.com",
-            job_id=536591785949415,
-            settings=JobSettings(
-                compute=None,
-                continuous=None,
-                job_clusters=[
-                    JobCluster(
-                        job_cluster_key="rrrrrrrrr",
-                        new_cluster=ClusterSpec(autoscale=None, node_type_id="Standard_DS3_v2", num_workers=2),
-                    ),
-                ],
-                tasks=[
-                    Task(
-                        task_key="Ingest",
-                        new_cluster=ClusterSpec(autoscale=None, node_type_id="Standard_DS3_v2", num_workers=2),
-                        notebook_task=NotebookTask(
-                            notebook_path="/Users/foo.bar@databricks.com/Customers/Example/Test/Load"
-                        ),
-                        timeout_seconds=0,
-                    )
-                ],
-                timeout_seconds=0,
-            ),
-        ),
-    ]
-    ws = mocker.Mock()
-    ws.clusters.list.return_value = sample_clusters
-    ws.pipelines.list_pipelines.return_value = sample_pipelines
-    config_dict = {}
-    ws.pipelines.get().spec.configuration = config_dict
-    ws.jobs.list.return_value = sample_jobs
-    ws.cluster_policies.get().policy_family_definition_overrides = json.dumps(
-        {
-            "spark_conf.fs.azure1.account.auth.type": {"type": "fixed", "value": "OAuth", "hidden": "true"},
-            "spark_conf.fs.azure1.account.oauth.provider.type": {
-                "type": "fixed",
-                "value": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-                "hidden": "true",
-            },
-            "spark_conf.fs.azure.account.oauth2.client.id": {
-                "type": "fixed",
-                "value": "",
-                "hidden": "true",
-            },
-            "spark_conf.fs.azure1.account.oauth2.client.secret": {
-                "type": "fixed",
-                "value": "gfgfgfgfggfggfgfdds",
-                "hidden": "true",
-            },
-            "spark_conf.fs.azure1.account.oauth2.client.endpoint": {
-                "type": "fixed",
-                "value": "https://login.microsoftonline.com/1234ededed/oauth2/token",
-                "hidden": "true",
-            },
-        }
-    )
+    ws = workspace_client_mock()
     spn_crawler = AzureServicePrincipalCrawler(ws, MockBackend(), "ucx")._crawl()
 
     assert len(spn_crawler) == 0
