d5c28a04c20df6c30c1b41ac24efe8b94b4f12dc
Hari Selvarajan <hari.selvarajan@databricks.com>
2024-02-07 20:09:19 +0000

    increasing timeouts

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -147,7 +147,7 @@ def test_new_job_cluster_with_policy_assessment(
     assert before[ws_group_a.display_name] == PermissionLevel.CAN_USE
 
 
-@retried(on=[NotFound, Unknown, InvalidParameterValue], timeout=timedelta(minutes=15))
+@retried(on=[NotFound, Unknown, InvalidParameterValue], timeout=timedelta(minutes=20))
 def test_running_real_assessment_job(
     ws, new_installation, make_ucx_group, make_cluster_policy, make_cluster_policy_permissions
 ):
