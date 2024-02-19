6c44c2925c1db77891331292290b03ae35cae37f
David Leiva <david.leiva04@gmail.com>
2024-02-19 14:48:03 -0600

    Added colum `is_partitioned` inside ucx.tables

--- src/databricks/labs/ucx/hive_metastore/tables.py
+++ src/databricks/labs/ucx/hive_metastore/tables.py
@@ -38,6 +38,7 @@ class Table:
     upgraded_to: str | None = None
 
     storage_properties: str | None = None
+    is_partitioned: bool | None = None
 
     DBFS_ROOT_PREFIXES: typing.ClassVar[list[str]] = [
         "/dbfs/",
@@ -251,6 +252,7 @@ class TablesCrawler(CrawlerBase):
                     "upgraded_to", None
                 ),
                 storage_properties=self._parse_table_props(describe.get("Storage Properties", "").lower()),  # type: ignore[arg-type]
+                is_partitioned="#Partition Information" in describe
             )
         except Exception as e:  # pylint: disable=broad-exception-caught
             # TODO: https://github.com/databrickslabs/ucx/issues/406
