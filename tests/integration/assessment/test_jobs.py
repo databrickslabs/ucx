c33558bc4a5fbf62f35ab7a21b86cc0ff595ebe0
FastLee <liran.bareket@databricks.com>
2024-03-01 18:39:25 -0500

    Added integration test.

--- tests/integration/assessment/test_jobs.py
+++ tests/integration/assessment/test_jobs.py
@@ -1,7 +1,11 @@
+import json
+import time
 from datetime import timedelta
 
 from databricks.sdk.errors import NotFound
 from databricks.sdk.retries import retried
+from databricks.sdk.service.jobs import BaseRun, NotebookTask, RunTask
+from databricks.sdk.service.workspace import ImportFormat
 
 from databricks.labs.ucx.assessment.jobs import JobsCrawler, SubmitRunsCrawler
 
@@ -25,10 +29,33 @@ def test_job_crawler(ws, make_job, inventory_schema, sql_backend):
 
 
 @retried(on=[NotFound], timeout=timedelta(minutes=5))
-def test_job_run_crawler(ws, make_job, inventory_schema, sql_backend):
-    # new_job = make_job(spark_conf=_SPARK_CONF)
-    job_run_crawler = SubmitRunsCrawler(ws=ws, sbe=sql_backend, schema=inventory_schema, num_days_history=30)
+def test_job_run_crawler(ws, env_or_skip, inventory_schema, sql_backend):
+    cluster_id = env_or_skip("TEST_DEFAULT_CLUSTER_ID")
+    dummy_notebook = """# Databricks notebook source
+# MAGIC
+# COMMAND ----------
+pass
+"""
+    dir = "/tmp/ucx"
+    notebook = "dummy_notebook"
+    ws.workspace.mkdirs(dir)
+    ws.workspace.upload(f"{dir}/{notebook}.py", dummy_notebook.encode("utf8"), format=ImportFormat.AUTO, overwrite=True)
+    tasks = [RunTask(task_key="123",
+                     notebook_task=NotebookTask(notebook_path=f"{dir}/{notebook}"),
+                     existing_cluster_id=cluster_id
+                     )]
+    run = ws.jobs.submit(run_name=f'ucx-test-{time.time_ns()}',
+                   tasks=tasks).result()
+    assert run
+    run_id = run.run_id
+
+    job_run_crawler = SubmitRunsCrawler(ws=ws, sbe=sql_backend, schema=inventory_schema, num_days_history=1)
     job_runs = job_run_crawler.snapshot()
 
     assert len(job_runs) >= 1
-    # assert int(results[0].job_id) == new_job.job_id
+    failures = None
+    for job_run in job_runs:
+        if run_id in json.loads(job_run.run_ids):
+            failures = job_run.failures
+            continue
+    assert failures and failures == "[]"
