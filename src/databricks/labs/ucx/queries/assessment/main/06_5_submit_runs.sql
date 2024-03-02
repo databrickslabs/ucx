7418bbf3f823a92a514aa00d9405d916b151ede3
FastLee <liran.bareket@databricks.com>
2024-03-02 16:16:57 -0500

    Fixed Bad Queries

--- src/databricks/labs/ucx/queries/assessment/main/06_5_submit_runs.sql
+++ src/databricks/labs/ucx/queries/assessment/main/06_5_submit_runs.sql
@@ -2,7 +2,7 @@
 -- widget title=Incompatible Submit Runs Failures, row=6, col=5, size_x=3, size_y=8
 SELECT
     EXPLODE(FROM_JSON(failures, 'array<string>')) AS failure,
-    COUNT(DISTINCT hashed_id) AS submit_runs
-    COLLECT_LIST(DISTINCT hashed_id) AS run_ids
+    COUNT(DISTINCT hashed_id) AS submit_runs,
+    COLLECT_LIST(DISTINCT run_ids) AS run_ids
 FROM $inventory.submit_runs
 group by 1
