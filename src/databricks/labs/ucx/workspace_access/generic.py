1b5ba473b42947abd1dfa0637aaa3f6ff88d619d
Serge Smertin <serge.smertin@databricks.com>
2024-03-02 13:09:23 +0100

    Improved installation integration test flakiness
    - improved `_infer_error_from_job_run` and `_infer_error_from_task_run` to also catch `KeyError` and `ValueError`
    - removed retries for `Unknown` errors for installation tests

--- src/databricks/labs/ucx/workspace_access/generic.py
+++ src/databricks/labs/ucx/workspace_access/generic.py
@@ -418,6 +418,7 @@ def feature_store_listing(ws: WorkspaceClient):
             result = ws.api_client.do(
                 "GET", "/api/2.0/feature-store/feature-tables/search", query={"page_token": token, "max_results": 200}
             )
+            assert isinstance(result, dict)
             for table in result.get("feature_tables", []):
                 feature_tables.append(GenericPermissionsInfo(table["id"], "feature-tables"))
 
