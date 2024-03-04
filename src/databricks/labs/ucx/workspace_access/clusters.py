a84421eba1107ae51ea318bab57c01fa4d455ed5
Prajin <prajin.sivaprasad@databricks.com>
2024-03-04 12:21:09 +0530

    Adding command to Remap the cluster to UC

--- src/databricks/labs/ucx/workspace_access/clusters.py
+++ src/databricks/labs/ucx/workspace_access/clusters.py
@@ -4,7 +4,7 @@ import os
 from databricks.labs.blueprint.tui import Prompts
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import InvalidParameterValue
-from databricks.sdk.service.compute import DataSecurityMode
+from databricks.sdk.service.compute import DataSecurityMode, AutoScale
 logger = logging.getLogger(__name__)
 
 
@@ -24,14 +24,23 @@ class ClusterAccess:
     def map_cluster_to_uc(self, cluster_id):
         try:
             spark_version = self._ws.clusters.select_spark_version(latest=True)
+            node_type_id = self._ws.clusters.select_node_type(local_disk=True)
             security_modes = {"Single User": DataSecurityMode.SINGLE_USER, "Shared": DataSecurityMode.USER_ISOLATION}
             access_mode = self._prompts.choice_from_dict("Select cluster access mode", security_modes)
-            if access_mode == 'SINGLE_USER':
-                data_security_mode = DataSecurityMode.SINGLE_USER
-            else:
-                data_security_mode = DataSecurityMode.USER_ISOLATION
+            cluster_details = self._ws.clusters.get(cluster_id)
+            data_security_mode = access_mode
             self._ws.clusters.edit(
-                cluster_id=cluster_id, spark_version=spark_version, data_security_mode=data_security_mode
+                cluster_id=cluster_id,
+                cluster_name=cluster_details.cluster_name,
+                spark_version=spark_version,
+                spark_conf=cluster_details.spark_conf,
+                spark_env_vars=cluster_details.spark_env_vars,
+                data_security_mode=data_security_mode,
+                node_type_id=node_type_id,
+                autoscale=cluster_details.autoscale,
+                policy_id=cluster_details.policy_id,
+                autotermination_minutes=cluster_details.autotermination_minutes,
+
             )
         except InvalidParameterValue as e:
             logger.warning(f"skipping cluster remapping: {e}")
