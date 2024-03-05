9385cd560c99c6cd827b89d8f479df87d013fb9f
Prajin <prajin.sivaprasad@databricks.com>
2024-03-05 14:44:23 +0530

    Adding the terraform Prompt

--- src/databricks/labs/ucx/config.py
+++ src/databricks/labs/ucx/config.py
@@ -36,6 +36,7 @@ class WorkspaceConfig:  # pylint: disable=too-many-instance-attributes
     override_clusters: dict[str, str] | None = None
     policy_id: str | None = None
     num_days_submit_runs_history: int = 30
+    is_terraform_used: bool =False
 
     def replace_inventory_variable(self, text: str) -> str:
         return text.replace("$inventory", f"hive_metastore.{self.inventory_database}")
