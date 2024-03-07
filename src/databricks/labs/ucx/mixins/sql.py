f850692bec3afb8b175d05c7623662bbd740b1d6
David Leiva <david.leiva04@gmail.com>
2024-03-07 02:12:13 -0600

    update

--- src/databricks/labs/ucx/mixins/sql.py
+++ src/databricks/labs/ucx/mixins/sql.py
@@ -264,17 +264,10 @@ class StatementExecutionExt:
             type_name = col.type_name
             if not type_name:
                 type_name = ColumnInfoTypeName.NULL
-            if type_name == ColumnInfoTypeName.BOOLEAN:
-                conv = self._get_convert_boolean_type_function
-            else:
-                conv = self.type_converters.get(type_name, None)
+            conv = self.type_converters.get(type_name, None)
             if conv is None:
                 msg = f"{col.name} has no {type_name.value} converter"
                 raise ValueError(msg)
             col_conv.append(conv)
         row_factory = type("Row", (Row,), {"__columns__": col_names})
         return col_conv, row_factory
-
-    @staticmethod
-    def _get_convert_boolean_type_function(value):
-        return literal_eval(value.capitalize())
