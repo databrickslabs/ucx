63ce5de1a36fca943dca020a7e385e5c76df1a7c
Amin Movahed <amin.movahed@databricks.com>
2024-03-07 12:16:15 +1100

    Adding three unit test functions to test_verification.py
    test_verify
    test_verify_applied_scope_acls_fails
    test_verify_applied_scope_acls

--- tests/unit/workspace_access/test_verification.py
+++ tests/unit/workspace_access/test_verification.py
@@ -79,3 +79,114 @@ def test_fail_get_permissions_missing_object(caplog):
     manager.verify_applied_permissions("ot1", "oid1", state, "backup")
     manager.verify_roles_and_entitlements(state, "backup")
     assert caplog.messages == ['removed on backend: ot1.oid1', 'removed on backend: wid1']
+
+
+def test_verify(mocker):
+    ws = create_autospec(WorkspaceClient)
+    secret_scopes = SecretScopesSupport(ws)
+    manager = VerificationManager(ws, secret_scopes)
+
+    state = MigrationState(
+        [
+            MigratedGroup(
+                id_in_workspace="wid1",
+                name_in_workspace="test",
+                name_in_account="test",
+                temporary_name="db-temp-test",
+                members=None,
+                entitlements=None,
+                external_id="eid1",
+                roles=None,
+            )
+        ]
+    )
+    verify_applied_scope_acls = mocker.patch(
+        "databricks.labs.ucx.workspace_access.verification.VerificationManager.verify_applied_scope_acls"
+    )
+    verify_applied_permissions = mocker.patch(
+        "databricks.labs.ucx.workspace_access.verification.VerificationManager.verify_applied_permissions"
+    )
+    verify_roles_and_entitlements = mocker.patch(
+        "databricks.labs.ucx.workspace_access.verification.VerificationManager.verify_roles_and_entitlements"
+    )
+    manager.verify(state, "backup", [("ot1", "oid1"), ("secrets", "oid2")])
+    verify_applied_scope_acls.assert_called_once()
+    verify_applied_permissions.assert_called_once()
+    verify_roles_and_entitlements.assert_called_once()
+
+
+def test_verify_applied_scope_acls_fails(mocker):
+    ws = create_autospec(WorkspaceClient)
+    secret_scopes = SecretScopesSupport(ws)
+    manager = VerificationManager(ws, secret_scopes)
+
+    state = MigrationState(
+        [
+            MigratedGroup(
+                id_in_workspace="wid1",
+                name_in_workspace="test",
+                name_in_account="test",
+                temporary_name="db-temp-test",
+                members=None,
+                entitlements=None,
+                external_id="eid1",
+                roles=None,
+            )
+        ]
+    )
+
+    def mock_secret_scope_permission(scope_name, group_name):
+        permissions_map = {
+            ("oid1", "db-temp-test"): "WRITE",
+            ("oid1", "test"): "READ",
+        }
+        return permissions_map.get((scope_name, group_name))
+
+    mock = mocker.patch(
+        'databricks.labs.ucx.workspace_access.secrets.SecretScopesSupport.secret_scope_permission',
+        side_effect=mock_secret_scope_permission,
+    )
+
+    with pytest.raises(AssertionError, match="Scope ACLs were not applied correctly"):
+        manager.verify_applied_scope_acls("oid1", state, "backup")
+
+    mock.assert_any_call("oid1", "db-temp-test")
+    mock.assert_any_call("oid1", "test")
+
+
+def test_verify_applied_scope_acls(mocker, caplog):
+    ws = create_autospec(WorkspaceClient)
+    secret_scopes = SecretScopesSupport(ws)
+    manager = VerificationManager(ws, secret_scopes)
+
+    state = MigrationState(
+        [
+            MigratedGroup(
+                id_in_workspace="wid2",
+                name_in_workspace="test2",
+                name_in_account="test2",
+                temporary_name="db-temp-test2",
+                members=None,
+                entitlements=None,
+                external_id="eid2",
+                roles=None,
+            )
+        ]
+    )
+
+    def mock_secret_scope_permission(scope_name, group_name):
+        permissions_map = {
+            ("oid1", "db-temp-test2"): "MANAGE",
+            ("oid1", "test2"): "MANAGE",
+        }
+        return permissions_map.get((scope_name, group_name))
+
+    mock = mocker.patch(
+        'databricks.labs.ucx.workspace_access.secrets.SecretScopesSupport.secret_scope_permission',
+        side_effect=mock_secret_scope_permission,
+    )
+
+    manager.verify_applied_scope_acls("oid1", state, "backup")
+
+    mock.assert_any_call("oid1", "db-temp-test2")
+    mock.assert_any_call("oid1", "test2")
