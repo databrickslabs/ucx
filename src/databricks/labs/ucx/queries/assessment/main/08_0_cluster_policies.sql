7f6d8b501363d28df064c3241fcad974bf2ccfa9
Prajin <prajin.sivaprasad@databricks.com>
2024-03-08 15:58:35 +0530

    Added Unit Test case

--- src/databricks/labs/ucx/queries/assessment/main/08_0_cluster_policies.sql
+++ src/databricks/labs/ucx/queries/assessment/main/08_0_cluster_policies.sql
@@ -1,8 +1,9 @@
--- viz type=table, name=Cluster Policies, columns=policy_name,dbr_version
+-- viz type=table, name=Cluster Policies, columns=policy_name,cluster_dbr_version,policy_spark_version
 -- widget title=Cluster Policies, row=8, col=0, size_x=3, size_y=8
 SELECT
     distinct policy_name,
-    spark_version as dbr_version
+    cluster.spark_version as cluster_dbr_version,
+    policy.spark_version as policy_spark_version
 FROM $inventory.clusters as cluster
 JOIN $inventory.policies as policy
 ON cluster.policy_id=policy.policy_id
