409e18a2d0b3cc928cf13d767180eb80c933bd61
Serge Smertin <serge.smertin@databricks.com>
2024-03-10 15:43:09 +0100

    ..

--- tests/unit/workspace_access/test_listing.py
+++ tests/unit/workspace_access/test_listing.py
@@ -20,6 +20,7 @@ def test_workspace_listing():
     # TODO: there's a huge chance that we'll be rewriting this code to use WSFS FUSE listing, so it'll be irrelevant
     # pylint: disable-next=prohibited-patch
     with patch("databricks.labs.ucx.workspace_access.generic.WorkspaceListing.snapshot", return_value=listing_instance):
+        # pylint: disable-next=obscure-mock
         results = generic.WorkspaceListing(ws=MagicMock(), sql_backend=MagicMock(), inventory_database=MagicMock())
         assert len(list(results)) == 4
         for res in results:
