22575d4e9e32a50f357cb612a14e5116801732de
David Leiva <david.leiva04@gmail.com>
2024-03-11 15:47:45 -0600

    added disable pylint rules for upgrade script

--- src/databricks/labs/ucx/upgrades/v0.16.0_add_is_partitioned_column.py
+++ src/databricks/labs/ucx/upgrades/v0.16.0_add_is_partitioned_column.py
@@ -1,8 +1,14 @@
+# pylint: disable=invalid-name
+
+import logging
+
 from databricks.labs.blueprint.installation import Installation
 from databricks.sdk import WorkspaceClient
 
 from databricks.labs.ucx.config import WorkspaceConfig
 from databricks.labs.ucx.framework.crawlers import StatementExecutionBackend
+
+logger = logging.getLogger(__name__)
 
 
 def upgrade(installation: Installation, ws: WorkspaceClient):
