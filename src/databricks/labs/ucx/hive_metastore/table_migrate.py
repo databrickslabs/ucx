ce0f5a24feecc6e30e2e9632a347327d9c59bbe9
FastLee <liran.bareket@databricks.com>
2024-03-11 18:47:02 -0400

    Fixed code coverage

--- src/databricks/labs/ucx/hive_metastore/table_migrate.py
+++ src/databricks/labs/ucx/hive_metastore/table_migrate.py
@@ -72,10 +72,6 @@ class TablesMigrate:
                 tasks.append(partial(self._migrate_table, table.src, table.rule))
         Threads.strict("migrate tables", tasks)
 
-    def get_seen_tables(self):
-        self._init_seen_tables()
-        return self._seen_tables
-
     def _migrate_table(self, src_table: Table, rule: Rule):
         if self._table_already_upgraded(rule.as_uc_table_key):
             logger.info(f"Table {src_table.key} already upgraded to {rule.as_uc_table_key}")
