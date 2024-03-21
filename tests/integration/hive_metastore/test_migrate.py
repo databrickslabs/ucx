f551685a48349438b59531d288468f176ce0b2f7
Vuong <vuong.nguyen@databricks.com>
2024-03-21 11:56:17 +0000

    add integration test

--- tests/integration/hive_metastore/test_migrate.py
+++ tests/integration/hive_metastore/test_migrate.py
@@ -1,25 +1,35 @@
 import logging
 from datetime import timedelta
+from unittest.mock import create_autospec
 
 import pytest
 from databricks.sdk.errors import NotFound
 from databricks.sdk.retries import retried
+from databricks.sdk.service.catalog import SecurableType, Privilege
 
 from databricks.labs.ucx.hive_metastore import GrantsCrawler
+from databricks.labs.ucx.hive_metastore.grants import Grant
 from databricks.labs.ucx.hive_metastore.mapping import Rule
 from databricks.labs.ucx.hive_metastore.table_migrate import (
     MigrationStatusRefresher,
     TablesMigrate,
 )
-from databricks.labs.ucx.hive_metastore.tables import Table
+from databricks.labs.ucx.hive_metastore.tables import AclMigrationWhat, Table
+from databricks.labs.ucx.workspace_access.groups import GroupManager
 
-from ..conftest import StaticTableMapping, StaticTablesCrawler, StaticUdfsCrawler
+from ..conftest import (
+    StaticGrantsCrawler,
+    StaticTableMapping,
+    StaticTablesCrawler,
+    StaticUdfsCrawler,
+)
 
 logger = logging.getLogger(__name__)
 
 
 @retried(on=[NotFound], timeout=timedelta(minutes=2))
 def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog, make_schema, make_table):
+    # pylint: disable=too-many-locals
     if not ws.config.is_azure:
         pytest.skip("temporary: only works in azure test env")
     src_schema = make_schema(catalog_name="hive_metastore")
@@ -44,9 +54,10 @@ def test_migrate_managed_tables(ws, sql_backend, inventory_schema, make_catalog,
         ),
     ]
     table_mapping = StaticTableMapping(ws, sql_backend, rules=rules)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
     table_migrate = TablesMigrate(
-        table_crawler, grant_crawler, ws, sql_backend, table_mapping, migration_status_refresher
+        table_crawler, grant_crawler, ws, sql_backend, table_mapping, group_manager, migration_status_refresher
     )
 
     table_migrate.migrate_tables()
@@ -104,9 +115,10 @@ def test_migrate_tables_with_cache_should_not_create_table(
         ),
     ]
     table_mapping = StaticTableMapping(ws, sql_backend, rules=rules)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
     table_migrate = TablesMigrate(
-        table_crawler, grant_crawler, ws, sql_backend, table_mapping, migration_status_refresher
+        table_crawler, grant_crawler, ws, sql_backend, table_mapping, group_manager, migration_status_refresher
     )
 
     # FIXME: flaky: databricks.sdk.errors.platform.NotFound: Catalog 'ucx_cjazg' does not exist.
@@ -155,6 +167,7 @@ def test_migrate_external_table(  # pylint: disable=too-many-locals
             src_external_table.name,
         ),
     ]
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
     table_migrate = TablesMigrate(
         table_crawler,
@@ -162,6 +175,7 @@ def test_migrate_external_table(  # pylint: disable=too-many-locals
         ws,
         sql_backend,
         StaticTableMapping(ws, sql_backend, rules=rules),
+        group_manager,
         migration_status_refresher,
     )
 
@@ -199,6 +213,7 @@ def test_migrate_external_table_failed_sync(
     existing_mounted_location = f'dbfs:/mnt/{env_or_skip("TEST_MOUNT_NAME")}/a/b/c'
     src_external_table = make_table(schema_name=src_schema.name, external_csv=existing_mounted_location)
     table_crawler = StaticTablesCrawler(sql_backend, inventory_schema, [src_external_table])
+    grant_crawler = create_autospec(GrantsCrawler)
     # create a mapping that will fail the SYNC because the target catalog and schema does not exist
     rules = [
         Rule(
@@ -210,9 +225,16 @@ def test_migrate_external_table_failed_sync(
             src_external_table.name,
         ),
     ]
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
     table_migrate = TablesMigrate(
-        table_crawler, ws, sql_backend, StaticTableMapping(ws, sql_backend, rules=rules), migration_status_refresher
+        table_crawler,
+        grant_crawler,
+        ws,
+        sql_backend,
+        StaticTableMapping(ws, sql_backend, rules=rules),
+        group_manager,
+        migration_status_refresher,
     )
 
     table_migrate.migrate_tables()
@@ -257,9 +279,10 @@ def test_revert_migrated_table(
         ),
     ]
     table_mapping = StaticTableMapping(ws, sql_backend, rules=rules)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
     table_migrate = TablesMigrate(
-        table_crawler, grant_crawler, ws, sql_backend, table_mapping, migration_status_refresher
+        table_crawler, grant_crawler, ws, sql_backend, table_mapping, group_manager, migration_status_refresher
     )
     table_migrate.migrate_tables()
 
@@ -369,8 +392,9 @@ def test_mapping_reverts_table(
     ]
     table_mapping = StaticTableMapping(ws, sql_backend, rules=rules)
     migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
     table_migrate = TablesMigrate(
-        table_crawler, grant_crawler, ws, sql_backend, table_mapping, migration_status_refresher
+        table_crawler, grant_crawler, ws, sql_backend, table_mapping, group_manager, migration_status_refresher
     )
     table_migrate.migrate_tables()
 
@@ -421,3 +445,55 @@ def test_mapping_reverts_table(
         _["key"]: _["value"] for _ in list(sql_backend.fetch(f"SHOW TBLPROPERTIES {table_to_revert.full_name}"))
     }
     assert "upgraded_to" not in results2
+
+
+@retried(on=[NotFound], timeout=timedelta(minutes=2))
+def test_migrate_managed_tables_with_acl(
+    ws, sql_backend, inventory_schema, make_catalog, make_schema, make_table, make_user
+):  # pylint: disable=too-many-locals
+    if not ws.config.is_azure:
+        pytest.skip("temporary: only works in azure test env")
+    src_schema = make_schema(catalog_name="hive_metastore")
+    src_managed_table = make_table(catalog_name=src_schema.catalog_name, schema_name=src_schema.name)
+    user = make_user()
+    src_grant = [Grant(principal=user.user_name, action_type="SELECT",
+                      table=src_managed_table.name, database = src_schema.name),
+                 Grant(principal=user.user_name, action_type="MODIFY",
+                      table=src_managed_table.name, database = src_schema.name)]
+
+    dst_catalog = make_catalog()
+    dst_schema = make_schema(catalog_name=dst_catalog.name, name=src_schema.name)
+
+    logger.info(f"dst_catalog={dst_catalog.name}, managed_table={src_managed_table.full_name}")
+
+    table_crawler = StaticTablesCrawler(sql_backend, inventory_schema, [src_managed_table])
+    udf_crawler = StaticUdfsCrawler(sql_backend, inventory_schema, [])
+    grant_crawler = StaticGrantsCrawler(table_crawler, udf_crawler, src_grant)
+    rules = [
+        Rule(
+            "workspace",
+            dst_catalog.name,
+            src_schema.name,
+            dst_schema.name,
+            src_managed_table.name,
+            src_managed_table.name,
+        ),
+    ]
+    table_mapping = StaticTableMapping(ws, sql_backend, rules=rules)
+    group_manager = GroupManager(sql_backend, ws, inventory_schema)
+    migration_status_refresher = MigrationStatusRefresher(ws, sql_backend, inventory_schema, table_crawler)
+    table_migrate = TablesMigrate(
+        table_crawler, grant_crawler, ws, sql_backend, table_mapping, group_manager, migration_status_refresher
+    )
+
+    table_migrate.migrate_tables(acl_strategy=AclMigrationWhat.LEGACY_TACL)
+
+    target_tables = list(sql_backend.fetch(f"SHOW TABLES IN {dst_schema.full_name}"))
+    assert len(target_tables) == 1
+
+    target_table_properties = ws.tables.get(f"{dst_schema.full_name}.{src_managed_table.name}").properties
+    target_table_grants = ws.grants.get(SecurableType.TABLE, f"{dst_schema.full_name}.{src_managed_table.name}")
+    assert target_table_properties["upgraded_from"] == src_managed_table.full_name
+    assert target_table_properties[Table.UPGRADED_FROM_WS_PARAM] == str(ws.get_workspace_id())
+    assert target_table_grants.privilege_assignments[0].principal == user.user_name
+    assert target_table_grants.privilege_assignments[0].privileges == [Privilege.SELECT, Privilege.MODIFY]
