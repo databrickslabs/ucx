dd35e038125ed19ef0e2f1ee4477504f769039c1
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-03-26 13:48:11 +0100

    increase test coverage for Files

--- tests/unit/code/test_files.py
+++ tests/unit/code/test_files.py
@@ -14,6 +14,14 @@ def test_files_fix_ignores_unsupported_extensions():
     assert not files.apply(path)
 
 
+def test_files_fix_ignores_unsupported_language():
+    languages = create_autospec(Languages)
+    files = Files(languages)
+    files._extensions[".py"] = None
+    path = Path('unsupported.py')
+    assert not files.apply(path)
+
+
 def test_files_fix_reads_supported_extensions():
     languages = create_autospec(Languages)
     files = Files(languages)
@@ -38,3 +46,14 @@ def test_files_supported_language_no_fixer():
     path = Path(__file__)
     files.apply(path)
     languages.fixer.assert_called_once_with(Language.PYTHON, 'some-code')
+
+
+def test_files_walks_directory():
+    languages = create_autospec(Languages)
+    languages.linter(Language.PYTHON).lint.return_value = [Mock(code='some-code')]
+    languages.fixer.return_value = None
+    files = Files(languages)
+    path = Path(__file__).parent
+    files.apply(path)
+    languages.fixer.assert_called_with(Language.PYTHON, 'some-code')
+    assert languages.fixer.call_count > 1
