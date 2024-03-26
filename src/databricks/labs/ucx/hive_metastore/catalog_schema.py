5eef78e488fb48c511848b993fc007470a3253a0
FastLee <liran.bareket@databricks.com>
2024-03-26 10:37:00 -0400

    Addressed Review Comments

--- src/databricks/labs/ucx/hive_metastore/catalog_schema.py
+++ src/databricks/labs/ucx/hive_metastore/catalog_schema.py
@@ -14,11 +14,40 @@ logger = logging.getLogger(__name__)
 
 
 class CatalogSchema:
-    def __init__(self, ws: WorkspaceClient, table_mapping: TableMapping, prompts: Prompts):
+    def __init__(self, ws: WorkspaceClient, table_mapping: TableMapping):
         self._ws = ws
         self._table_mapping = table_mapping
-        self._prompts = prompts
         self._external_locations = self._ws.external_locations.list()
+
+    @classmethod
+    def for_cli(cls, ws: WorkspaceClient, installation: Installation):
+        config = installation.load(WorkspaceConfig)
+        sql_backend = StatementExecutionBackend(ws, config.warehouse_id)
+        table_mapping = TableMapping(installation, ws, sql_backend)
+        return cls(ws, table_mapping)
+
+    def create_all_catalogs_schemas(self, prompts: Prompts):
+        candidate_catalogs, candidate_schemas = self._get_missing_catalogs_schemas()
+        for candidate_catalog in candidate_catalogs:
+            self._create_catalog_validate(candidate_catalog, prompts)
+        for candidate_catalog, schemas in candidate_schemas.items():
+            for candidate_schema in schemas:
+                self._create_schema(candidate_catalog, candidate_schema)
+
+    def _create_catalog_validate(self, catalog, prompts: Prompts):
+        logger.info(f"Creating UC catalog: {catalog}")
+        # create catalogs
+        attempts = 3
+        while True:
+            catalog_storage = prompts.question(
+                f"Please provide storage location url for catalog:{catalog}.", default="metastore"
+            )
+            if self._validate_location(catalog_storage):
+                break
+            attempts -= 1
+            if attempts == 0:
+                raise NotFound(f"Failed to validate location for {catalog} catalog")
+        self._create_catalog(catalog, catalog_storage)
 
     def _list_existing(self) -> tuple[set[str], dict[str, set[str]]]:
         """generate a list of existing UC catalogs and schema."""
@@ -52,7 +81,7 @@ class CatalogSchema:
             target_schemas[target_catalog].add(target_schema)
         return target_catalogs, target_schemas
 
-    def get_missing_catalogs_schemas(self) -> tuple[set[str], dict[str, set[str]]]:
+    def _get_missing_catalogs_schemas(self) -> tuple[set[str], dict[str, set[str]]]:
         """prepare a list of catalogs and schema to be created"""
         existing_catalogs, existing_schemas = self._list_existing()
         target_catalogs, target_schemas = self._list_target()
@@ -68,7 +97,7 @@ class CatalogSchema:
                 target_schemas[catalog] = target_schemas[catalog] - schemas
         return target_catalogs, target_schemas
 
-    def validate_location(self, location: str):
+    def _validate_location(self, location: str):
         if location == "metastore":
             return True
         try:
@@ -83,50 +112,13 @@ class CatalogSchema:
                 return True
         return False
 
-    def create_catalog(self, catalog, catalog_storage):
+    def _create_catalog(self, catalog, catalog_storage):
         logger.info(f"Creating UC catalog: {catalog}")
         if catalog_storage == "metastore":
             self._ws.catalogs.create(catalog, comment="Created by UCX")
         else:
             self._ws.catalogs.create(catalog, storage_root=catalog_storage, comment="Created by UCX")
 
-    def create_schema(self, catalog, schema):
+    def _create_schema(self, catalog, schema):
         logger.info(f"Creating UC schema: {schema} in catalog: {catalog}")
         self._ws.schemas.create(schema, catalog, comment="Created by UCX")
-
-
-class CatalogSchemaCLI:
-    def __init__(self, cat_schema: CatalogSchema, prompts: Prompts):
-        self._cat_schema = cat_schema
-        self._prompts = prompts
-
-    @classmethod
-    def for_cli(cls, ws: WorkspaceClient, installation: Installation, prompts: Prompts):
-        config = installation.load(WorkspaceConfig)
-        sql_backend = StatementExecutionBackend(ws, config.warehouse_id)
-        table_mapping = TableMapping(installation, ws, sql_backend)
-        cat_schema = CatalogSchema(ws, table_mapping, prompts)
-        return cls(cat_schema, prompts)
-
-    def create_all_catalogs_schemas(self):
-        candidate_catalogs, candidate_schemas = self._cat_schema.get_missing_catalogs_schemas()
-        for candidate_catalog in candidate_catalogs:
-            self._create_catalog(candidate_catalog)
-        for candidate_catalog, schemas in candidate_schemas.items():
-            for candidate_schema in schemas:
-                self._cat_schema.create_schema(candidate_catalog, candidate_schema)
-
-    def _create_catalog(self, catalog):
-        logger.info(f"Creating UC catalog: {catalog}")
-        # create catalogs
-        attempts = 3
-        while True:
-            catalog_storage = self._prompts.question(
-                f"Please provide storage location url for catalog:{catalog}.", default="metastore"
-            )
-            if self._cat_schema.validate_location(catalog_storage):
-                break
-            attempts -= 1
-            if attempts == 0:
-                raise NotFound(f"Failed to validate location for {catalog} catalog")
-        self._cat_schema.create_catalog(catalog, catalog_storage)
