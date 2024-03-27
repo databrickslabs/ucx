b4b3b521f428e708c53047686c330ee734ec2944
Douglas Moore <dmoore247@users.noreply.github.com>
2024-03-27 19:01:20 -0400

    Deploy static views needed by #1123 interactive dashboard (#1139)
    ## Changes
    Deploy views `misc_patterns_vw` and `code_patterns_vw` that should have
    been deployed with #1123 and fixes the `interactive` dashboard queries.
    
    ### Functionality 
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs ucx ...`
    - [ ] added a new workflow
    - [ ] modified existing workflow: `...`
    - [x] added a new table
    - [ ] modified existing table: `...`
    
    ### Tests
    <!-- How is this tested? Please see the checklist below and also
    describe any other relevant tests -->
    
    - [x] manually tested
    - [ ] added unit tests
    - [ ] added integration tests
    - [x] verified on staging environment (screenshot attached)
    - [ ] 
    
    ![image](https://github.com/databrickslabs/ucx/assets/1122251/1e5a64cf-52f9-414b-b213-2a1dc3c58841)

--- src/databricks/labs/ucx/install.py
+++ src/databricks/labs/ucx/install.py
@@ -95,6 +95,8 @@ def deploy_schema(sql_backend: SqlBackend, inventory_schema: str):
     deployer.deploy_view("objects", "queries/views/objects.sql")
     deployer.deploy_view("grant_detail", "queries/views/grant_detail.sql")
     deployer.deploy_view("table_estimates", "queries/views/table_estimates.sql")
+    deployer.deploy_view("misc_patterns", "queries/views/misc_patterns.sql")
+    deployer.deploy_view("code_patterns", "queries/views/code_patterns.sql")
 
 
 def extract_major_minor(version_string):
