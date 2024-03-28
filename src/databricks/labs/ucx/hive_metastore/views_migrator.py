1cbb04fa537be6bea14147c9ce0be27e32bebd11
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-03-28 10:11:29 +0100

    sequences 0 and 1 views

--- src/databricks/labs/ucx/hive_metastore/views_migrator.py
+++ src/databricks/labs/ucx/hive_metastore/views_migrator.py
@@ -1,5 +1,5 @@
 from databricks.labs.ucx.hive_metastore import TablesCrawler
-from databricks.labs.ucx.hive_metastore.table_migrate import Index
+from databricks.labs.ucx.hive_metastore.table_migrate import Index, MigrationStatus
 from databricks.labs.ucx.hive_metastore.tables import Table
 
 
@@ -9,5 +9,17 @@ class ViewsMigrator:
         self.index = index
         self.crawler = crawler
 
-    def sequence(self) -> list[Table]:
-        return []
+    def sequence(self) -> list[MigrationStatus]:
+        views = self._list_views()
+        if len(views) == 0:
+            return []
+        if len(views) == 1:
+            view = views[0]
+            migration = self.index.get(view.database, view.name)
+            return [] if migration is None else [migration]
+        # TODO
+        raise Exception("Not implemented yet!")
+
+    def _list_views(self) -> list[Table]:
+        tables = self.crawler.snapshot()
+        return list(filter(lambda t: t.view_text is not None, tables))
--- tests/unit/hive_metastore/test_views.py
+++ tests/unit/hive_metastore/test_views.py
@@ -9,15 +9,29 @@ from databricks.labs.ucx.hive_metastore import TablesCrawler
 from databricks.labs.ucx.hive_metastore.table_migrate import Index, MigrationStatus
 from databricks.labs.ucx.hive_metastore.views_migrator import ViewsMigrator
 
+SCHEMA_NAME = "schema"
+
 
 def test_migrates_no_view_returns_empty_sequence():
     samples = Samples.load("db1_t1", "db2_t1")
     index = create_index(samples)
     sql_backend = mock_backend(samples, "db1", "db2")
-    crawler = TablesCrawler(sql_backend, "hive_metastore", ["db1", "db2"])
+    crawler = TablesCrawler(sql_backend, SCHEMA_NAME, ["db1", "db2"])
     migrator = ViewsMigrator(index, crawler)
     sequence = migrator.sequence()
     assert len(sequence) == 0
+
+
+def test_migrates_direct_view_returns_singleton_sequence() -> None:
+    samples = Samples.load("db1_t1", "db1_v1")
+    index = create_index(samples)
+    sql_backend = mock_backend(samples, "db1")
+    crawler = TablesCrawler(sql_backend, SCHEMA_NAME, ["db1", "db2"])
+    migrator = ViewsMigrator(index, crawler)
+    sequence = migrator.sequence()
+    assert len(sequence) == 1
+    migration: MigrationStatus = sequence[0]
+    assert migration.src_schema == "db1" and migration.src_table == "v1"
 
 
 def create_index(samples: list[dict]) -> Index:
@@ -33,13 +47,28 @@ def create_index(samples: list[dict]) -> Index:
 
 
 def mock_backend(samples: list[dict], *dbnames: str) -> SqlBackend:
-    db_rows = {}
-    template = Template('SHOW TABLES FROM hive_metastore.$db')
+    db_rows: dict[str, list[tuple]] = {}
+    show_template = Template('SHOW TABLES FROM hive_metastore.$db')
+    select_query = 'SELECT \\* FROM hive_metastore.schema.tables'
     for dbname in dbnames:
-        # pylint warning W0640 here is a pylint bug (verified manually), see https://github.com/pylint-dev/pylint/issues/5263
+        # pylint warning W0640 is a pylint bug (verified manually), see https://github.com/pylint-dev/pylint/issues/5263
         legacy_samples = [s["legacy"] for s in filter(lambda s: s["legacy"]["database"] == dbname, samples)]
-        tuples = [("hive_metastore", s["database"], s["table"], s.get("view_text", None)) for s in legacy_samples]
-        db_rows[template.substitute({'db': dbname})] = tuples
+        show_tuples = [(s["database"], s["table"], "true") for s in legacy_samples]
+        db_rows[show_template.substitute({'db': dbname})] = show_tuples
+        # catalog, database, table, object_type, table_format, location, view_text
+        select_tuples = [
+            (
+                "hive_metastore",
+                s["database"],
+                s["table"],
+                "type",
+                "DELTA" if s.get("view_text", None) is None else "VIEW",
+                None,
+                s.get("view_text", None),
+            )
+            for s in legacy_samples
+        ]
+        db_rows[select_query] = select_tuples
     return MockBackend(rows=db_rows)
 
 
