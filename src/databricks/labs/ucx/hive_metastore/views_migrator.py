be076a432c8e917e36159aaba2106717f7384385
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-03-29 09:00:57 +0100

    rename fields

--- src/databricks/labs/ucx/hive_metastore/views_migrator.py
+++ src/databricks/labs/ucx/hive_metastore/views_migrator.py
@@ -9,45 +9,46 @@ from databricks.labs.ucx.hive_metastore.tables import Table
 
 class ViewToMigrate:
 
-    table: Table
-    table_dependencies: set[Table]
-    view_dependencies: set[Table]
+    _view: Table
+    _table_dependencies: set[Table]
+    _view_dependencies: set[Table]
 
     def __init__(self, table: Table):
         if table.view_text is None:
             raise RuntimeError("Should never get there! A view must have 'view_text'!")
-        self.table = table
-        self.table_dependencies = set()
-        self.view_dependencies = set()
+        self._view = table
+        self._table_dependencies = set()
+        self._view_dependencies = set()
 
     def compute_dependencies(self, all_tables: dict[str, Table]):
-        if len(self.table_dependencies) + len(self.view_dependencies) == 0:
-            statement = self._parse_view_text()
-            for sql_table in statement.find_all(SqlTable):
-                catalog = self._catalog(sql_table)
-                if catalog != 'hive_metastore':
-                    continue
-                table_with_key = Table(catalog, sql_table.db, sql_table.name, "type", "")
-                table = all_tables.get(table_with_key.key)
-                if table is None:
-                    raise ValueError(
-                        f"Unknown schema object: {table_with_key.key} in view SQL: {self.table.view_text} of table {self.table.key}"
-                    )
-                if table.view_text is None:
-                    self.table_dependencies.add(table)
-                else:
-                    self.view_dependencies.add(table)
+        if len(self._table_dependencies) + len(self._view_dependencies) > 0:
+            return
+        statement = self._parse_view_text()
+        for sql_table in statement.find_all(SqlTable):
+            catalog = self._catalog(sql_table)
+            if catalog != 'hive_metastore':
+                continue
+            table_with_key = Table(catalog, sql_table.db, sql_table.name, "type", "")
+            table = all_tables.get(table_with_key.key)
+            if table is None:
+                raise ValueError(
+                    f"Unknown schema object: {table_with_key.key} in view SQL: {self._view.view_text} of table {self._view.key}"
+                )
+            if table.view_text is None:
+                self._table_dependencies.add(table)
+            else:
+                self._view_dependencies.add(table)
 
     def _parse_view_text(self) -> SqlExpression:
         try:
             # below can never happen but avoids a pylint error
-            assert self.table.view_text is not None
-            statements = sqlglot.parse(self.table.view_text)
+            assert self._view.view_text is not None
+            statements = sqlglot.parse(self._view.view_text)
             if len(statements) != 1 or statements[0] is None:
-                raise ValueError(f"Could not analyze view SQL: {self.table.view_text} of table {self.table.key}")
+                raise ValueError(f"Could not analyze view SQL: {self._view.view_text} of table {self._view.key}")
             return statements[0]
         except ParseError as e:
-            raise ValueError(f"Could not analyze view SQL: {self.table.view_text} of table {self.table.key}") from e
+            raise ValueError(f"Could not analyze view SQL: {self._view.view_text} of table {self._view.key}") from e
 
     # duplicated from FromTable._catalog, not sure if it's worth factorizing
     @staticmethod
@@ -57,15 +58,15 @@ class ViewToMigrate:
         return 'hive_metastore'
 
     def __hash__(self):
-        return hash(self.table)
+        return hash(self._view)
 
 
 class ViewsMigrator:
 
     def __init__(self, crawler: TablesCrawler):
-        self.crawler = crawler
-        self.result_view_list: list[ViewToMigrate] = []
-        self.result_tables_set: set[Table] = set()
+        self._crawler = crawler
+        self._result_view_list: list[ViewToMigrate] = []
+        self._result_tables_set: set[Table] = set()
 
     def sequence(self) -> list[Table]:
         # sequencing is achieved using a very simple algorithm:
@@ -76,19 +77,19 @@ class ViewsMigrator:
         # if none, then it's safe to add that view to the next batch of views
         # the complexity for a given set of views v and a dependency depth d looks like Ov^d
         # this seems enormous but in practice d remains small and v decreases rapidly
-        table_values = self.crawler.snapshot()
-        raw_tables = set(filter(lambda t: t.view_text is None, table_values))
-        raw_views = set(table_values)
+        table_list = self._crawler.snapshot()
+        raw_tables = set(filter(lambda t: t.view_text is None, table_list))
+        raw_views = set(table_list)
         raw_views.difference_update(raw_tables)
-        table_keys = [table.key for table in table_values]
-        all_tables = dict(zip(table_keys, table_values))
+        table_keys = [table.key for table in table_list]
+        all_tables = dict(zip(table_keys, table_list))
         views = {ViewToMigrate(view) for view in raw_views}
         while len(views) > 0:
             next_batch = self._next_batch(views, all_tables)
-            self.result_view_list.extend(next_batch)
-            self.result_tables_set.update([v.table for v in next_batch])
+            self._result_view_list.extend(next_batch)
+            self._result_tables_set.update([v._view for v in next_batch])
             views.difference_update(next_batch)
-        return [v.table for v in self.result_view_list]
+        return [v._view for v in self._result_view_list]
 
     def _next_batch(self, views: set[ViewToMigrate], all_tables: dict[str, Table]) -> set[ViewToMigrate]:
         # we can't (slightly) optimize by checking len(views) == 0 or 1,
@@ -96,7 +97,7 @@ class ViewsMigrator:
         result: set[ViewToMigrate] = set()
         for view in views:
             view.compute_dependencies(all_tables)
-            not_batched_yet = list(filter(lambda v: v not in self.result_tables_set, view.view_dependencies))
+            not_batched_yet = list(filter(lambda v: v not in self._result_tables_set, view._view_dependencies))
             if len(not_batched_yet) == 0:
                 result.add(view)
         # prevent infinite loop
