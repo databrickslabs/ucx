de05703bd80cac35aa73b0bc2119a430341487d2
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-02 13:59:29 +0200

    Support table migration to Unity Catalog in Python code (#1210)
    ## Changes
    Enhance SparkSql linter/fixer to support migration of spark.sql function
    calls that receive a table name as parameter
    
    ### Linked issues
    Resolves # 1082
    
    ### Functionality 
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [x] modified existing command: `databricks labs ucx
    migrate_local_code`
    - [ ] added a new workflow
    - [ ] modified existing workflow: `...`
    - [ ] added a new table
    - [ ] modified existing table: `...`
    
    ### Tests
    - [ ] manually tested
    - [x] added unit tests
    - [ ] added integration tests
    - [ ] verified on staging environment (screenshot attached)
    
    missing integration tests
    
    ---------
    
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- tests/unit/source_code/test_pyspark.py
+++ tests/unit/source_code/test_pyspark.py
@@ -1,18 +1,20 @@
-from databricks.labs.ucx.source_code.base import Deprecation
-from databricks.labs.ucx.source_code.pyspark import SparkSql
+import pytest
+
+from databricks.labs.ucx.source_code.base import Advisory, Deprecation
+from databricks.labs.ucx.source_code.pyspark import SparkMatchers, SparkSql
 from databricks.labs.ucx.source_code.queries import FromTable
 
 
-def test_spark_not_sql(empty_index):
+def test_spark_no_sql(empty_index):
     ftf = FromTable(empty_index)
-    sqf = SparkSql(ftf)
+    sqf = SparkSql(ftf, empty_index)
 
     assert not list(sqf.lint("print(1)"))
 
 
 def test_spark_sql_no_match(empty_index):
     ftf = FromTable(empty_index)
-    sqf = SparkSql(ftf)
+    sqf = SparkSql(ftf, empty_index)
 
     old_code = """
 spark.read.csv("s3://bucket/path")
@@ -26,7 +28,7 @@ for i in range(10):
 
 def test_spark_sql_match(migration_index):
     ftf = FromTable(migration_index)
-    sqf = SparkSql(ftf)
+    sqf = SparkSql(ftf, migration_index)
 
     old_code = """
 spark.read.csv("s3://bucket/path")
@@ -46,9 +48,197 @@ for i in range(10):
     ] == list(sqf.lint(old_code))
 
 
+def test_spark_sql_match_named(migration_index):
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+
+    old_code = """
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    result = spark.sql(args=[1], sqlQuery = "SELECT * FROM old.things").collect()
+    print(len(result))
+"""
+    assert [
+        Deprecation(
+            code='table-migrate',
+            message='Table old.things is migrated to brand.new.stuff in Unity Catalog',
+            start_line=4,
+            start_col=13,
+            end_line=4,
+            end_col=71,
+        )
+    ] == list(sqf.lint(old_code))
+
+
+METHOD_NAMES = [
+    "cacheTable",
+    "createTable",
+    "createExternalTable",
+    "getTable",
+    "isCached",
+    "listColumns",
+    "tableExists",
+    "recoverPartitions",
+    "refreshTable",
+    "uncacheTable",
+    "table",
+    "insertInto",
+    "saveAsTable",
+    "register",
+]
+
+
+@pytest.mark.parametrize("method_name", METHOD_NAMES)
+def test_spark_table_match(migration_index, method_name):
+    spark_matchers = SparkMatchers()
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    matcher = spark_matchers.matchers[method_name]
+    args_list = ["a"] * min(5, matcher.max_args)
+    args_list[matcher.table_arg_index] = '"old.things"'
+    args = ",".join(args_list)
+    old_code = f"""
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.{method_name}({args})
+    do_stuff_with_df(df)
+"""
+    assert [
+        Deprecation(
+            code='table-migrate',
+            message='Table old.things is migrated to brand.new.stuff in Unity Catalog',
+            start_line=4,
+            start_col=9,
+            end_line=4,
+            end_col=17 + len(method_name) + len(args),
+        )
+    ] == list(sqf.lint(old_code))
+
+
+@pytest.mark.parametrize("method_name", METHOD_NAMES)
+def test_spark_table_no_match(migration_index, method_name):
+    spark_matchers = SparkMatchers()
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    matcher = spark_matchers.matchers[method_name]
+    args_list = ["a"] * min(5, matcher.max_args)
+    args_list[matcher.table_arg_index] = '"table.we.know.nothing.about"'
+    args = ",".join(args_list)
+    old_code = f"""
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.{method_name}({args})
+    do_stuff_with_df(df)
+"""
+    assert not list(sqf.lint(old_code))
+
+
+@pytest.mark.parametrize("method_name", METHOD_NAMES)
+def test_spark_table_too_many_args(migration_index, method_name):
+    spark_matchers = SparkMatchers()
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    matcher = spark_matchers.matchers[method_name]
+    if matcher.max_args > 100:
+        return
+    args_list = ["a"] * (matcher.max_args + 1)
+    args_list[matcher.table_arg_index] = '"table.we.know.nothing.about"'
+    args = ",".join(args_list)
+    old_code = f"""
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.{method_name}({args})
+    do_stuff_with_df(df)
+"""
+    assert not list(sqf.lint(old_code))
+
+
+def test_spark_table_named_args(migration_index):
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    old_code = """
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.saveAsTable(format="xyz", name="old.things")
+    do_stuff_with_df(df)
+"""
+    assert [
+        Deprecation(
+            code='table-migrate',
+            message='Table old.things is migrated to brand.new.stuff in Unity Catalog',
+            start_line=4,
+            start_col=9,
+            end_line=4,
+            end_col=59,
+        )
+    ] == list(sqf.lint(old_code))
+
+
+def test_spark_table_variable_arg(migration_index):
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    old_code = """
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.saveAsTable(name)
+    do_stuff_with_df(df)
+"""
+    assert [
+        Advisory(
+            code='table-migrate',
+            message="Can't migrate 'saveAsTable' because its table name argument is not a constant",
+            start_line=4,
+            start_col=9,
+            end_line=4,
+            end_col=32,
+        )
+    ] == list(sqf.lint(old_code))
+
+
+def test_spark_table_fstring_arg(migration_index):
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    old_code = """
+spark.read.csv("s3://bucket/path")
+for i in range(10):
+    df = spark.saveAsTable(f"boop{stuff}")
+    do_stuff_with_df(df)
+"""
+    assert [
+        Advisory(
+            code='table-migrate',
+            message="Can't migrate 'saveAsTable' because its table name argument is not a constant",
+            start_line=4,
+            start_col=9,
+            end_line=4,
+            end_col=42,
+        )
+    ] == list(sqf.lint(old_code))
+
+
+def test_spark_table_return_value(migration_index):
+    ftf = FromTable(migration_index)
+    sqf = SparkSql(ftf, migration_index)
+    old_code = """
+spark.read.csv("s3://bucket/path")
+for table in spark.listTables():
+    do_stuff_with_table(table)
+"""
+    assert [
+        Advisory(
+            code='table-migrate',
+            message="Call to 'listTables' will return a list of <catalog>.<database>.<table> instead of <database>.<table>.",
+            start_line=3,
+            start_col=13,
+            end_line=3,
+            end_col=31,
+        )
+    ] == list(sqf.lint(old_code))
+
+
 def test_spark_sql_fix(migration_index):
     ftf = FromTable(migration_index)
-    sqf = SparkSql(ftf)
+    sqf = SparkSql(ftf, migration_index)
 
     old_code = """spark.read.csv("s3://bucket/path")
 for i in range(10):
