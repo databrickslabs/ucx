a854ffee0c84ac9a1e2242a78fe60b6a3401de8f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-04 10:24:54 +0200

    Test parse logs

--- tests/unit/installer/test_workflows.py
+++ tests/unit/installer/test_workflows.py
@@ -1,35 +1,38 @@
 import re
+from pathlib import Path
 
 import pytest
 
 from databricks.labs.ucx.installer import workflows
 from databricks.labs.ucx.installer.workflows import LogRecord
 
-
-@pytest.mark.parametrize(
-    "line,expected_log_record",
-    [
-        (
-            "07:09 ERROR [module] Message.",
-            LogRecord(40, "Message."),
-        ),
-        (
-            "07:09 INFO [module] Other message.",
-            LogRecord(20, "Other message."),
-        ),
-        (
-            "07:09 WARNING [module] Warning message.",
-            LogRecord(30, "Warning message."),
-        ),
-        (
-            "07:09 CRITICAL [module] Watch out!",
-            LogRecord(50, "Watch out!"),
-        ),
-    ],
+LOGS = (
+    "07:09 ERROR [module] Message.",
+    "07:09 INFO [module] Other message.",
+    "07:09 WARNING [module] Warning message.",
+    "07:09 CRITICAL [module] Watch out!",
 )
+LOG_RECORDS = (
+    LogRecord(40, "Message."),
+    LogRecord(20, "Other message."),
+    LogRecord(30, "Warning message."),
+    LogRecord(50, "Watch out!"),
+)
+
+
+@pytest.mark.parametrize("line,expected_log_record", list(zip(LOGS, LOG_RECORDS)))
 def test_parse_log_record_examples(line: str, expected_log_record: LogRecord) -> None:
     log_format = r"\d+:\d+\s(\w+)\s\[.+\]\s(.+)"
     pattern = re.compile(log_format)
 
     log_record = workflows.parse_log_record(line, pattern)
     assert log_record == expected_log_record
+
+
+def test_parse_logs(tmp_path: Path) -> None:
+    log_path = tmp_path / "test.log"
+    with log_path.open("w") as f:
+        f.writelines(LOGS)
+
+    log_records = list(workflows.parse_logs(log_path))
+    assert log_records == LOG_RECORDS
