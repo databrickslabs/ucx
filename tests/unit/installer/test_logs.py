fba9d609b52c52f7bcc8ed61799701608e887c88
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-04 15:53:14 +0200

    Test multi line log record

--- tests/unit/installer/test_logs.py
+++ tests/unit/installer/test_logs.py
@@ -7,17 +7,32 @@ from databricks.labs.lsql.backends import MockBackend
 from databricks.labs.ucx.installer import logs
 from databricks.labs.ucx.installer.logs import LogRecord, LogsCrawler
 
+MULTILINE_LOG_MESSAGE = (
+    "{MainThread} GET /api/2.0/preview/scim/v2/Groups?attributes=id,displayName,meta,roles,entitlements&startIndex=1&count=100\n"
+    "< 200 OK\n"
+    "< {\n"
+    '<   "Resources": [\n'
+    "<     {\n"
+    '<       "displayName": "sdk-5cGx,"\n'
+    '<       "id": "7325451755795",\n'
+    '<       "meta": {\n'
+    '<         "resourceType": "WorkspaceGroup"\n'
+    "<       }\n"
+    "<     }"
+)
 LOGS = [
     "07:09 ERROR [module] Message.\n",
     "07:09 INFO [module] Other message.\n",
     "07:09 WARNING [module] Warning message.\n",
     "07:09 CRITICAL [module] Watch out!\n",
+    f"04:36 DEBUG [databricks.sdk] {MULTILINE_LOG_MESSAGE}"
 ]
 LOG_RECORDS = [
     LogRecord(40, "Message."),
     LogRecord(20, "Other message."),
     LogRecord(30, "Warning message."),
     LogRecord(50, "Watch out!"),
+    LogRecord(10, MULTILINE_LOG_MESSAGE),
 ]
 
 
