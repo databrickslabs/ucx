c080e90dea22a450158905a7900f616af180e318
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-04 10:12:41 +0200

    Add log record parsing tests

--- tests/unit/installer/test_workflows.py
+++ tests/unit/installer/test_workflows.py
@@ -12,7 +12,19 @@ from databricks.labs.ucx.installer.workflows import LogRecord
         (
             "07:09 ERROR [module] Message.",
             LogRecord(40, "Message."),
-        )
+        ),
+        (
+            "07:09 INFO [module] Other message.",
+            LogRecord(20, "Other message."),
+        ),
+        (
+            "07:09 WARNING [module] Warning message.",
+            LogRecord(30, "Warning message."),
+        ),
+        (
+            "07:09 CRITICAL [module] Watch out!",
+            LogRecord(50, "Watch out!"),
+        ),
     ],
 )
 def test_parse_log_record_examples(line: str, expected_log_record: LogRecord) -> None:
