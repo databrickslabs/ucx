d902e5c6d67100e983bc83049cc84446ea34c9ed
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-09 13:49:55 +0200

    Fix parse logs integration test

--- tests/integration/installer/test_logs.py
+++ tests/integration/installer/test_logs.py
@@ -1,5 +1,3 @@
-import datetime as dt
-import logging
 import pytest
 from pathlib import Path
 
@@ -8,43 +6,43 @@ from databricks.labs.lsql.backends import StatementExecutionBackend
 from databricks.labs.ucx.installer import logs
 
 
+WORKFLOW = "assessment"
+JOB_ID = 123
+JOB_RUN_ID = 456
 LOG = """
 12:33:01 INFO [databricks.labs.ucx] {MainThread} UCX v0.21.1 After job finishes, see debug logs at /Workspace/Users/user.name@databricks.com/.installation/logs/assessment/run-766/crawl_grants.log
 12:33:01 DEBUG [databricks.labs.ucx.framework.crawlers] {MainThread} [hive_metastore.ucx_si10.grants] fetching grants inventory
 12:33:01 DEBUG [databricks.labs.lsql.backends] {MainThread} [spark][fetch] SELECT * FROM hive_metastore.ucx_si10.grants
 12:33:02 DEBUG [databricks.labs.ucx.framework.crawlers] {MainThread} [hive_metastore.ucx_si10t.grants] crawling new batch for grants
+12:33:02 WARNING [databricks.labs.ucx.framework.crawlers] {MainThread} Missing permissions for grants
 """.lstrip()
 
 
 @pytest.fixture
 def log_file(tmp_path: Path) -> Path:
-    log_file_path = tmp_path / "crawl_grants.log"
+    log_file_path = tmp_path / "logs" / WORKFLOW / f"run-{JOB_RUN_ID}" / "crawl_grants.log"
+    log_file_path.parent.mkdir(parents=True, exist_ok=True)
     with log_file_path.open("w") as f:
         f.write(LOG)
     return log_file_path
 
 
 def test_log_recorder_record_number_of_records(
+    tmp_path: Path,
     log_file: Path,
     sql_backend: StatementExecutionBackend,
     inventory_schema: str,
 ) -> None:
+    _ = log_file
     log_recorder = logs.TaskRunWarningRecorder(
-        log_file.parent,
-        workflow="assessment",
-        job_id=123,
-        job_run_id=456,
+        tmp_path,
+        workflow=WORKFLOW,
+        job_id=JOB_ID,
+        job_run_id=JOB_RUN_ID,
         sql_backend=sql_backend,
         schema=inventory_schema,
-        minimum_log_level=logging.DEBUG,
     )
-
-    with log_file.open("r") as f:
-        log_recorder.record(
-            log_file.stem,
-            f,
-            dt.datetime.now(),
-        )
+    log_recorder.snapshot()
 
     row = next(sql_backend.fetch(f"SELECT COUNT(*) AS value FROM {log_recorder.full_name}"))
-    assert row.value == 4
+    assert row.value == 1
