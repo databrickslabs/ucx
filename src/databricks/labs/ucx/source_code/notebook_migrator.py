f704ac206c13487463a26ae0949c89023217faaa
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-10 12:20:12 +0200

    Integrate detection of notebook dependencies (#1338)
    ## Changes
     - integrates dependency graph to NotebookMigrator
    
    ### Linked issues
    #1204 
    #1286 
    #1326
    
    ---------
    
    Co-authored-by: Cor <jczuurmond@protonmail.com>

--- src/databricks/labs/ucx/source_code/notebook_migrator.py
+++ src/databricks/labs/ucx/source_code/notebook_migrator.py
@@ -1,7 +1,8 @@
 from databricks.sdk import WorkspaceClient
-from databricks.sdk.service.workspace import ExportFormat, ObjectInfo
+from databricks.sdk.service.workspace import ExportFormat, ObjectInfo, ObjectType
 
 from databricks.labs.ucx.source_code.languages import Languages
+from databricks.labs.ucx.source_code.notebook import DependencyGraph, Notebook, RunCell
 
 
 class NotebookMigrator:
@@ -14,19 +15,64 @@ class NotebookMigrator:
             return False
         with self._ws.workspace.download(object_info.path + ".bak", format=ExportFormat.SOURCE) as f:
             code = f.read().decode("utf-8")
-        self._ws.workspace.upload(object_info.path, code.encode("utf-8"))
+            self._ws.workspace.upload(object_info.path, code.encode("utf-8"))
         return True
 
     def apply(self, object_info: ObjectInfo) -> bool:
+        if not object_info.path or not object_info.language or object_info.object_type is not ObjectType.NOTEBOOK:
+            return False
+        notebook = self._load_notebook(object_info)
+        return self._apply(notebook)
+
+    def build_dependency_graph(self, object_info: ObjectInfo) -> DependencyGraph:
+        if not object_info.path or not object_info.language or object_info.object_type is not ObjectType.NOTEBOOK:
+            raise ValueError("Not a valid Notebook")
+        notebook = self._load_notebook(object_info)
+        dependencies = DependencyGraph(object_info.path, None, self._load_notebook_from_path)
+        notebook.build_dependency_graph(dependencies)
+        return dependencies
+
+    def _apply(self, notebook: Notebook) -> bool:
+        changed = False
+        for cell in notebook.cells:
+            # %run is not a supported language, so this needs to come first
+            if isinstance(cell, RunCell):
+                # TODO data on what to change to ?
+                if cell.migrate_notebook_path():
+                    changed = True
+                continue
+            if not self._languages.is_supported(cell.language.language):
+                continue
+            migrated_code = self._languages.apply_fixes(cell.language.language, cell.original_code)
+            if migrated_code != cell.original_code:
+                cell.migrated_code = migrated_code
+                changed = True
+        if changed:
+            self._ws.workspace.upload(notebook.path + ".bak", notebook.original_code.encode("utf-8"))
+            self._ws.workspace.upload(notebook.path, notebook.to_migrated_code().encode("utf-8"))
+            # TODO https://github.com/databrickslabs/ucx/issues/1327 store 'migrated' status
+        return changed
+
+    def _load_notebook_from_path(self, path: str) -> Notebook:
+        object_info = self._load_object(path)
+        if object_info.object_type is not ObjectType.NOTEBOOK:
+            raise ValueError(f"Not a Notebook: {path}")
+        return self._load_notebook(object_info)
+
+    def _load_object(self, path: str) -> ObjectInfo:
+        result = self._ws.workspace.list(path)
+        object_info = next((oi for oi in result), None)
+        if object_info is None:
+            raise ValueError(f"Could not locate object at '{path}'")
+        return object_info
+
+    def _load_notebook(self, object_info: ObjectInfo) -> Notebook:
+        assert object_info is not None and object_info.path is not None and object_info.language is not None
+        source = self._load_source(object_info)
+        return Notebook.parse(object_info.path, source, object_info.language)
+
+    def _load_source(self, object_info: ObjectInfo) -> str:
         if not object_info.language or not object_info.path:
-            return False
-        if not self._languages.is_supported(object_info.language):
-            return False
+            raise ValueError(f"Invalid ObjectInfo: {object_info}")
         with self._ws.workspace.download(object_info.path, format=ExportFormat.SOURCE) as f:
-            original_code = f.read().decode("utf-8")
-            new_code = self._languages.apply_fixes(object_info.language, original_code)
-            if new_code == original_code:
-                return False
-            self._ws.workspace.upload(object_info.path + ".bak", original_code.encode("utf-8"))
-            self._ws.workspace.upload(object_info.path, new_code.encode("utf-8"))
-            return True
+            return f.read().decode("utf-8")
