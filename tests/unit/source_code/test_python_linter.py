3c32132f7be97afdc96ecc1b101385257d82334e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-10 17:21:05 +0200

    list imports

---
+++ tests/unit/source_code/test_python_linter.py
@@ -0,0 +1,41 @@
+from databricks.labs.ucx.source_code.python_linter import ASTLinter, PythonLinter
+
+
+def test_linter_returns_empty_list_of_dbutils_notebook_run_calls():
+    linter = ASTLinter.parse('')
+    assert [] == PythonLinter.list_dbutils_notebook_run_calls(linter)
+
+
+def test_linter_returns_list_of_dbutils_notebook_run_calls():
+    code = """
+dbutils.notebook.run("stuff")
+for i in z:
+    ww =   dbutils.notebook.run("toto")  
+"""
+    linter = ASTLinter.parse('')
+    assert [] == PythonLinter.list_dbutils_notebook_run_calls(linter)
+
+
+def test_linter_returns_empty_list_of_imports():
+    linter = ASTLinter.parse('')
+    assert [] == PythonLinter.list_import_sources(linter)
+
+
+def test_linter_returns_import():
+    linter = ASTLinter.parse('import x')
+    assert ["x"] == PythonLinter.list_import_sources(linter)
+
+
+def test_linter_returns_import_from():
+    linter = ASTLinter.parse('from x import z')
+    assert ["x"] == PythonLinter.list_import_sources(linter)
+
+
+def test_linter_returns_import_module():
+    linter = ASTLinter.parse('importlib.import_module("x")')
+    assert ["x"] == PythonLinter.list_import_sources(linter)
+
+
+def test_linter_returns__import__():
+    linter = ASTLinter.parse('importlib.__import__("x")')
+    assert ["x"] == PythonLinter.list_import_sources(linter)
