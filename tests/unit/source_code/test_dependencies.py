70b96d2f934f6e60c651d9173f085232639794bd
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-10 18:57:39 +0200

    more tests

--- tests/unit/source_code/test_dependencies.py
+++ tests/unit/source_code/test_dependencies.py
@@ -13,7 +13,7 @@ from databricks.labs.ucx.source_code.notebook_migrator import NotebookMigrator
 from tests.unit import _load_sources
 
 
-def test_build_dependency_graph_visits_dependencies():
+def test_build_dependency_graph_visits_notebook_dependencies():
     paths = ["root3.run.py.txt", "root1.run.py.txt", "leaf1.py.txt", "leaf2.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(Notebook, *paths)))
     visited: dict[str, bool] = {}
@@ -63,3 +63,32 @@ def test_build_dependency_graph_fails_with_unfound_dependency():
     object_info = ObjectInfo(path="root1.run.py.txt", language=Language.PYTHON, object_type=ObjectType.NOTEBOOK)
     with pytest.raises(ValueError):
         migrator.build_dependency_graph(object_info)
+
+def test_build_dependency_graph_visits_local_dependencies():
+    paths = ["root5.py.txt", "leaf4.py.txt"]
+    sources: dict[str, str] = dict(zip(paths, _load_sources(Notebook, *paths)))
+    visited: dict[str, bool] = {}
+
+    # can't remove **kwargs because it receives format=xxx
+    # pylint: disable=unused-argument
+    def download_side_effect(*args, **kwargs):
+        filename = args[0]
+        if filename.startswith('./'):
+            filename = filename[2:]
+        visited[filename] = True
+        result = create_autospec(BinaryIO)
+        result.__enter__.return_value.read.return_value = sources[filename].encode("utf-8")
+        return result
+
+    def list_side_effect(*args):
+        path = args[0]
+        return [ObjectInfo(path=path, language=Language.PYTHON, object_type=ObjectType.FILE)]
+
+    ws = create_autospec(WorkspaceClient)
+    ws.workspace.download.side_effect = download_side_effect
+    ws.workspace.list.side_effect = list_side_effect
+    migrator = NotebookMigrator(ws, Languages(create_autospec(MigrationIndex)), DependencyLoader(ws))
+    object_info = ObjectInfo(path="root5.py.txt", language=Language.PYTHON, object_type=ObjectType.FILE)
+    migrator.build_dependency_graph(object_info)
+    assert len(visited) == len(paths)
+
