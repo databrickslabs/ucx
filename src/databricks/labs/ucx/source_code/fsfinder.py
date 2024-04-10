efb36c31e2522243fcbce1c6f3e1a2f7160176fe
Jim.Idle <jimi@idle.ws>
2024-04-10 09:10:47 -0600

    feat: 1104  -  New linter
    Signed-off-by: Jim.Idle <jimi@idle.ws>

--- src/databricks/labs/ucx/source_code/fsfinder.py
+++ src/databricks/labs/ucx/source_code/fsfinder.py
@@ -2,15 +2,55 @@ from abc import ABC, abstractmethod
 from collections.abc import Iterable
 from databricks.labs.ucx.source_code.base import Advice, Linter
 
+import ast
 
-class FSFinderLinter(Linter, ABC):
+class DetectFSVisitor(ast.NodeVisitor):
+    """
+    Visitor that detects file system paths in Python code and checks them
+    against a list of known deprecated paths.
+    """
+
+    def __init__(self):
+        self.advices: List[Advice] = []
+        self.fs_prefixes = ["/dbfs/mnt", "dbfs:/", "/mnt/"]
+
+    def visit_Call(self, node):
+        print(f"Function call: {ast.unparse(node)}")
+        self.generic_visit(node)
+
+    def visit_Str(self, node):
+        print(f"String literal: {ast.unparse(node)}")
+        if any(node.s.startswith(prefix) for prefix in self.fs_prefixes):
+            self.advices.append(Advice(
+                code='fs-finder',
+                message=f"Deprecated file system path: {node.s}",
+                start_line=node.lineno,
+                start_col=node.col_offset,
+                end_line=node.lineno,
+                end_col=node.col_offset + len(node.s)
+            ))
+        self.generic_visit(node)
+
+    def get_advices(self) -> Iterable[Advice]:
+        for advice in self.advices:
+            yield advice
+
+class FSFinderLinter(Linter):
     def __init__(self):
         pass
 
     def name(self) -> str:
+        """
+        Returns the name of the linter, for reporting etc
+        """
         return 'fs-finder-linter'
 
-    @abstractmethod
     def lint(self, code: str) -> Iterable[Advice]:
-        # TODO: Implement the linting logic here
-        pass
+        """
+        Lints the code looking for file system paths that are deprecated
+        """
+        tree = ast.parse(code)
+        visitor = DetectFSVisitor()
+        visitor.visit(tree)
+        return visitor.get_advices()
+
