4b28c508712ed3256719580cf433cc613f69807a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-11 19:29:46 +0200

    Detect imported files and packages (#1362)
    ## Changes
    parse python code for `import` and `import from` processing instruction
    
    ### Linked issues
    #1202 
    Resolves #1346
    
    ### Functionality 
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs ucx ...`
    - [ ] added a new workflow
    - [ ] modified existing workflow: `...`
    - [ ] added a new table
    - [ ] modified existing table: `...`
    
    ### Tests
    <!-- How is this tested? Please see the checklist below and also
    describe any other relevant tests -->
    
    - [ ] manually tested
    - [x] added unit tests
    - [ ] added integration tests
    - [ ] verified on staging environment (screenshot attached)
    
    ---------
    
    Co-authored-by: Cor <jczuurmond@protonmail.com>

--- src/databricks/labs/ucx/source_code/dependencies.py
+++ src/databricks/labs/ucx/source_code/dependencies.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import abc
 from collections.abc import Callable
 
-from databricks.sdk.service.workspace import ObjectType, ObjectInfo, ExportFormat
+from databricks.sdk.service.workspace import ObjectType, ObjectInfo, ExportFormat, Language
 from databricks.sdk import WorkspaceClient
 
 
@@ -15,7 +15,7 @@ class Dependency:
         assert object_info.path is not None
         return Dependency(object_info.object_type, object_info.path)
 
-    def __init__(self, object_type: ObjectType, path: str):
+    def __init__(self, object_type: ObjectType | None, path: str):
         self._type = object_type
         self._path = path
 
@@ -55,12 +55,16 @@ class DependencyLoader:
         object_info = self._load_object(dependency)
         if object_info.object_type is ObjectType.NOTEBOOK:
             return self._load_notebook(object_info)
+        if object_info.object_type is ObjectType.FILE:
+            return self._load_file(object_info)
+        if object_info.object_type in [ObjectType.LIBRARY, ObjectType.DIRECTORY, ObjectType.DASHBOARD, ObjectType.REPO]:
+            return None
         raise NotImplementedError(str(object_info.object_type))
 
     def _load_object(self, dependency: Dependency) -> ObjectInfo:
-        result = self._ws.workspace.list(dependency.path)
-        object_info = next((oi for oi in result), None)
-        if object_info is None:
+        object_info = self._ws.workspace.get_status(dependency.path)
+        # TODO check error conditions, see https://github.com/databrickslabs/ucx/issues/1361
+        if object_info is None or object_info.object_type is None:
             raise ValueError(f"Could not locate object at '{dependency.path}'")
         if dependency.type is not None and object_info.object_type is not dependency.type:
             raise ValueError(
@@ -69,7 +73,7 @@ class DependencyLoader:
         return object_info
 
     def _load_notebook(self, object_info: ObjectInfo) -> SourceContainer:
-        # local import to avoid circular dependency
+        # local import to avoid cyclic dependency
         # pylint: disable=import-outside-toplevel, cyclic-import
         from databricks.labs.ucx.source_code.notebook import Notebook
 
@@ -78,8 +82,18 @@ class DependencyLoader:
         source = self._load_source(object_info)
         return Notebook.parse(object_info.path, source, object_info.language)
 
+    def _load_file(self, object_info: ObjectInfo) -> SourceContainer:
+        # local import to avoid cyclic dependency
+        # pylint: disable=import-outside-toplevel, cyclic-import
+        from databricks.labs.ucx.source_code.files import WorkspaceFile
+
+        assert object_info.path is not None
+        language = Language.PYTHON if object_info.language is None else object_info.language
+        source = self._load_source(object_info)
+        return WorkspaceFile(object_info.path, source, language)
+
     def _load_source(self, object_info: ObjectInfo) -> str:
-        if not object_info.language or not object_info.path:
+        if not object_info.path:
             raise ValueError(f"Invalid ObjectInfo: {object_info}")
         with self._ws.workspace.download(object_info.path, format=ExportFormat.SOURCE) as f:
             return f.read().decode("utf-8")
