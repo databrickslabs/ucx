0ef552678f0f1222ecb7b0e77d64f3a5ffd59214
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-12 14:22:47 +0200

    Add integration test

--- tests/integration/azure/test_resources.py
+++ tests/integration/azure/test_resources.py
@@ -9,28 +9,24 @@ from databricks.labs.ucx.azure.resources import AccessConnector, AccessConnector
 from databricks.labs.ucx.contexts.cli_command import WorkspaceContext
 
 
-def test_access_connector_client_create_delete(az_cli_ctx, env_or_skip):
-    # add TEST_SUBSCRIPTION_ID and TEST_RESOURCE_GROUP env variables to run this test
-    logging.getLogger('databricks').setLevel(logging.DEBUG)
-    all_connectors = list(az_cli_ctx.azure_resources.access_connectors.list(az_cli_ctx.azure_subscription_id))
-    assert isinstance(all_connectors, list)
-
+def test_access_connector_client_create_delete(az_cli_ctx, env_or_skip, make_random):
+    subscription_id = az_cli_ctx.azure_subscription_id
     resource_group = env_or_skip("TEST_RESOURCE_GROUP")
-    # access_connector_id = (
-    #     f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers"
-    #     "/Microsoft.Databricks/accessConnectors/test"
-    # )
-    #
-    # tomorrow = dt.datetime.now() + dt.timedelta(days=1)
-    # access_connector = AccessConnector(
-    #     id=access_connector_id,
-    #     location="westeurope",
-    #     tags={"RemoveAfter": str(tomorrow.date())},
-    # )
-    #
-    # assert access_connector not in access_connector_client.list(subscription_id)
-    # access_connector_client.create(access_connector)
-    # access_connector_client.create_or_update(access_connector)
-    # assert access_connector in access_connector_client.list(subscription_id)
-    # access_connector_client.delete(access_connector)
-    # assert access_connector not in access_connector_client.list(subscription_id)
+    access_connector_name = f"test-{make_random()}"
+    access_connector_id = (
+        f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers"
+        f"/Microsoft.Databricks/accessConnectors/{access_connector_name}"
+    )
+
+    tomorrow = dt.datetime.now() + dt.timedelta(days=1)
+    access_connector = AccessConnector(
+        id=access_connector_id,
+        name=access_connector_name,
+        location="westeurope",
+        tags={"RemoveAfter": str(tomorrow.date())},
+    )
+    assert access_connector not in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
+    az_cli_ctx.azure_resources.access_connectors.create_or_update(access_connector)
+    assert access_connector in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
+    az_cli_ctx.azure_resources.access_connectors.delete(access_connector)
+    assert access_connector not in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
