bbc3d84cf8def511213f7ec9ffe33985cd00f9e8
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-12 15:29:18 +0200

    Fix access connector integration test

--- tests/integration/azure/test_resources.py
+++ tests/integration/azure/test_resources.py
@@ -16,18 +16,18 @@ def test_access_connector_client_create_get_list_delete(az_cli_ctx, env_or_skip,
         f"/Microsoft.Databricks/accessConnectors/{access_connector_name}"
     )
 
-    access_connectors = az_cli_ctx.azure_resources.access_connectors.list(subscription_id)
+    access_connectors = az_cli_ctx.azure_resources.list_access_connectors(subscription_id)
     access_connector_ids = [access_connector.id for access_connector in access_connectors]
     assert access_connector_id not in access_connector_ids
 
-    az_cli_ctx.azure_resources.access_connectors.create_or_update(
+    az_cli_ctx.azure_resources.create_or_update_access_connector(
         subscription_id,
         resource_group_name,
         access_connector_name,
         location,
         tags,
     )
-    new_access_connector = az_cli_ctx.azure_resources.access_connectors.get(
+    new_access_connector = az_cli_ctx.azure_resources.get_access_connector(
         subscription_id,
         resource_group_name,
         access_connector_name,
@@ -36,13 +36,13 @@ def test_access_connector_client_create_get_list_delete(az_cli_ctx, env_or_skip,
     assert new_access_connector.location == location
     assert new_access_connector.tags == tags
 
-    az_cli_ctx.azure_resources.access_connectors.delete(
+    az_cli_ctx.azure_resources.delete_access_connector(
         subscription_id,
         resource_group_name,
         access_connector_name,
     )
 
-    access_connectors = az_cli_ctx.azure_resources.access_connectors.list(subscription_id)
+    access_connectors = az_cli_ctx.azure_resources.list_access_connectors(subscription_id)
     access_connector_ids = [access_connector.id for access_connector in access_connectors]
     assert access_connector_id not in access_connector_ids
 
