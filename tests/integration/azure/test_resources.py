c7f6390cc966c1bca9003b8fa59ae5a168bf672a
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-12 14:58:24 +0200

    Rename resource_group to resource_group_name

--- tests/integration/azure/test_resources.py
+++ tests/integration/azure/test_resources.py
@@ -6,10 +6,10 @@ from databricks.labs.ucx.azure.resources import AccessConnector
 
 def test_access_connector_client_create_delete(az_cli_ctx, env_or_skip, make_random):
     subscription_id = az_cli_ctx.azure_subscription_id
-    resource_group = env_or_skip("TEST_RESOURCE_GROUP")
+    resource_group_name = env_or_skip("TEST_RESOURCE_GROUP")
     access_connector_name = f"test-{make_random()}"
     access_connector_id = (
-        f"/subscriptions/{subscription_id}/resourceGroups/{resource_group}/providers"
+        f"/subscriptions/{subscription_id}/resourceGroups/{resource_group_name}/providers"
         f"/Microsoft.Databricks/accessConnectors/{access_connector_name}"
     )
 
@@ -23,7 +23,7 @@ def test_access_connector_client_create_delete(az_cli_ctx, env_or_skip, make_ran
     assert access_connector not in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
     az_cli_ctx.azure_resources.access_connectors.create_or_update(
         subscription_id,
-        resource_group,
+        resource_group_name,
         access_connector_name,
         "westeurope",
         {"RemoveAfter": str(tomorrow.date())}
@@ -31,7 +31,7 @@ def test_access_connector_client_create_delete(az_cli_ctx, env_or_skip, make_ran
     assert access_connector in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
     az_cli_ctx.azure_resources.access_connectors.delete(
         subscription_id,
-        resource_group,
+        resource_group_name,
         access_connector_name,
     )
     assert access_connector not in list(az_cli_ctx.azure_resources.access_connectors.list(subscription_id))
