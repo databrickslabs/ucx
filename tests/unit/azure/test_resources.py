04ae106956a8b1ec05e75376314f6b719c276cb3
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-12 09:44:14 +0200

    Merge access connector with existing test setup

--- tests/unit/azure/test_resources.py
+++ tests/unit/azure/test_resources.py
@@ -1,7 +1,6 @@
 from unittest.mock import create_autospec
 
 import pytest
-from databricks.sdk.azure import ENVIRONMENTS as AZURE_ENVIRONMENTS
 from databricks.sdk.errors import NotFound, PermissionDenied, ResourceConflict
 
 from databricks.labs.ucx.azure.resources import (
@@ -14,40 +13,6 @@ from databricks.labs.ucx.azure.resources import (
 )
 
 from . import azure_api_client, get_az_api_mapping
-
-
-SUBSCRIPTION_ID = "test"
-RESOURCE_GROUP = "rg-test"
-ACCESS_CONNECTOR_NAME = "test-access-connector"
-ACCESS_CONNECTOR_ID = (
-    f"/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP}"
-    f"/providers/Microsoft.Databricks/accessConnectors/{ACCESS_CONNECTOR_NAME}"
-)
-LOCATION = "westeurope"
-TEST_ACCESS_CONNECTOR_RESPONSE = {"value": [{
-    "id": ACCESS_CONNECTOR_ID,
-    "name": ACCESS_CONNECTOR_NAME,
-    "type": "Microsoft.Databricks/accessConnectors",
-    "location": LOCATION,
-    "identity": {
-        "principalId": "test",
-        "tenantId": "test",
-        "type": "SystemAssigned"
-    },
-    "tags": {
-        "application": "databricks",
-        "Owner": "cor.zuurmond@databricks.com",
-        "RemoveAfter": "2030-01-01",
-        "NoAutoRemove": "True"
-    },
-    "properties": {
-        "provisioningState": "Succeeded"
-    },
-    "systemData": {
-        "createdAt": "0001-01-01T00:00:00+00:00",
-        "lastModifiedAt": "0001-01-01T00:00:00+00:00"
-    }
-}]}
 
 
 def test_subscriptions_no_subscription():
@@ -302,38 +267,30 @@ def test_managed_identity_not_found():
 
 
 @pytest.fixture
-def azure_management_api_client() -> AzureAPIClient:
-    azure_mgmt_client = AzureAPIClient(
-        AZURE_ENVIRONMENTS["PUBLIC"].resource_manager_endpoint,
-        AZURE_ENVIRONMENTS["PUBLIC"].service_management_endpoint,
-    )
-    azure_mgmt_client = create_autospec(azure_mgmt_client)
-    return azure_mgmt_client
-
-
-@pytest.fixture
 def access_connector() -> AccessConnector:
-    access_connector_client = AccessConnector(id=ACCESS_CONNECTOR_ID, location=LOCATION)
-    return access_connector_client
-
-
-@pytest.fixture
-def access_connector_client(azure_management_api_client: AzureAPIClient) -> AccessConnectorClient:
-    access_connector_client = AccessConnectorClient(azure_management_api_client)
+    access_connector_id = (
+        "/subscriptions/test/resourceGroups/rg-test/providers"
+        "/Microsoft.Databricks/accessConnectors/test-access-connector"
+    )
+    access_connector_client = AccessConnector(id=access_connector_id, location="westeurope")
     return access_connector_client
 
 
 def test_access_connector_parse_subscription_id(access_connector: AccessConnector) -> None:
-    assert access_connector.subscription_id == SUBSCRIPTION_ID
+    assert access_connector.subscription_id == "test"
 
 
 def test_access_connector_parse_resource_group(access_connector: AccessConnector) -> None:
-    assert access_connector.resource_group == RESOURCE_GROUP
+    assert access_connector.resource_group == "rg-test"
+
+
+@pytest.fixture
+def access_connector_client() -> AccessConnectorClient:
+    access_connector_client = AccessConnectorClient(azure_api_client())
+    return access_connector_client
 
 
 def test_access_connector_handler_list_access_connectors(
-    azure_management_api_client: AzureAPIClient,
     access_connector_client: AccessConnectorClient,
 ) -> None:
-    azure_management_api_client.get.return_value = TEST_ACCESS_CONNECTOR_RESPONSE
     assert len(access_connector_client.list("test")) > 0
