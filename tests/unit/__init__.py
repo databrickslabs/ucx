56048163d468de12f8e4e8401687c1106ab69b8c
Jim.Idle <jimi@idle.ws>
2024-04-15 14:14:32 -0600

    feat: Add test for checking for s3fs in nested dependencies
      - Uses file base tests to ensure Deprecated is raised against imports in dependencies
      - Removes repeated use of the same code in many tests, replaces with lambda
    
    Signed-off-by: Jim.Idle <jimi@idle.ws>

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -3,6 +3,7 @@ import json
 import logging
 import pathlib
 from unittest.mock import create_autospec
+from typing import BinaryIO
 
 from databricks.labs.blueprint.installation import MockInstallation
 from databricks.labs.lsql.backends import MockBackend
@@ -129,6 +130,23 @@ def _pipeline(pipeline_id: str):
 def _secret_not_found(secret_scope, _):
     msg = f"Secret Scope {secret_scope} does not exist!"
     raise NotFound(msg)
+
+    # can't remove **kwargs because it receives format=xxx
+    # pylint: disable=unused-argument
+
+
+def _download_side_effect(sources: dict[str, str], visited: dict[str, bool], *args, **kwargs):
+    filename = args[0]
+    if filename.startswith('./'):
+        filename = filename[2:]
+    visited[filename] = True
+    if filename.find(".py") < 0:
+        filename = filename + ".py"
+    if filename.find(".txt") < 0:
+        filename = filename + ".txt"
+    result = create_autospec(BinaryIO)
+    result.__enter__.return_value.read.return_value = sources[filename].encode("utf-8")
+    return result
 
 
 def workspace_client_mock(
--- tests/unit/source_code/test_s3fs.py
+++ tests/unit/source_code/test_s3fs.py
@@ -9,7 +9,7 @@ from databricks.sdk.service.workspace import ObjectInfo, Language, ObjectType
 from databricks.labs.ucx.source_code.dependencies import DependencyLoader, SourceContainer, DependencyResolver
 from databricks.labs.ucx.source_code.notebook_migrator import NotebookMigrator
 from databricks.labs.ucx.source_code.whitelist import Whitelist
-from tests.unit import _load_sources
+from tests.unit import _load_sources, _download_side_effect
 
 S3FS_DEPRECATION_MESSAGE = "Use of dependency s3fs is deprecated"
 
@@ -110,6 +110,43 @@ def test_detect_s3fs_import(empty_index, source: str, expected: list[Advice]):
     ws.workspace.get_status.return_value = ObjectInfo(path="path", object_type=ObjectType.FILE)
     migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws), resolver)
     object_info = ObjectInfo(path="path", language=Language.PYTHON, object_type=ObjectType.FILE)
+    migrator.build_dependency_graph(object_info)
+    advices = list(resolver.get_advices())
+    assert advices == expected
+
+
+@pytest.mark.parametrize(
+    " expected",
+    (
+        [
+            Deprecation(
+                code='dependency-check',
+                message='Use of dependency s3fs is deprecated',
+                start_line=0,
+                start_col=0,
+                end_line=0,
+                end_col=0,
+            ),
+        ],
+    ),
+)
+def test_detect_s3fs_import_in_dependencies(empty_index, expected: list[Advice]):
+    paths = ["root9.py.txt", "leaf9.py.txt"]
+    sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
+    visited: dict[str, bool] = {}
+
+    def get_status_side_effect(*args):
+        path = args[0]
+        return ObjectInfo(path=path, object_type=ObjectType.FILE)
+
+    datas = _load_sources(SourceContainer, "s3fs-python-compatibility-catalog.yml")
+    whitelist = Whitelist.parse(datas[0])
+    resolver = DependencyResolver(whitelist)
+    ws = create_autospec(WorkspaceClient)
+    ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
+    ws.workspace.get_status.side_effect = get_status_side_effect
+    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws), resolver)
+    object_info = ObjectInfo(path="root9.py.txt", object_type=ObjectType.FILE)
     migrator.build_dependency_graph(object_info)
     advices = list(resolver.get_advices())
     assert advices == expected
