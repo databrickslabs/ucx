40869ee6504516a3a9c70e0efc0ea8e222b439fa
Jim.Idle <jimi@idle.ws>
2024-04-16 16:29:19 -0600

    fix: Fixes noitebook tests which relied on an incorrect behavior
       - The dependency builder was building leaf dependencies which included
         itself. The tests assumed that was correct.
    
    Signed-off-by: Jim.Idle <jimi@idle.ws>

--- tests/unit/source_code/test_notebook.py
+++ tests/unit/source_code/test_notebook.py
@@ -142,7 +142,9 @@ def test_notebook_builds_leaf_dependency_graph():
     graph = DependencyGraph(dependency, None, loader)
     container = loader.load_dependency(dependency)
     container.build_dependency_graph(graph)
-    assert graph.paths == {"leaf1.py.txt"}
+    # Note that dependencies should not include itself as it leads to
+    # infinite walks.
+    assert graph.paths == set()
 
 
 def test_notebook_builds_depth1_dependency_graph():
@@ -155,7 +157,7 @@ def test_notebook_builds_depth1_dependency_graph():
     container = loader.load_dependency(dependency)
     container.build_dependency_graph(graph)
     actual = {path[2:] if path.startswith('./') else path for path in graph.paths}
-    assert actual == set(paths)
+    assert actual == set(paths[1:])
 
 
 def test_notebook_builds_depth2_dependency_graph():
@@ -168,7 +170,9 @@ def test_notebook_builds_depth2_dependency_graph():
     container = loader.load_dependency(dependency)
     container.build_dependency_graph(graph)
     actual = {path[2:] if path.startswith('./') else path for path in graph.paths}
-    assert actual == set(paths)
+    # Note that dependencies should not include itself as it leads to
+    # infinite walks.
+    assert actual == set(paths[1:])
 
 
 def test_notebook_builds_dependency_graph_avoiding_duplicates():
@@ -216,7 +220,9 @@ def test_notebook_builds_python_dependency_graph():
     container = loader.load_dependency(dependency)
     container.build_dependency_graph(graph)
     actual = {path[2:] if path.startswith('./') else path for path in graph.paths}
-    assert actual == set(paths)
+    # Note that dependencies should not include itself as it leads to
+    # infinite walks.
+    assert actual == set(paths[1:])
 
 
 def test_detects_manual_migration_in_dbutils_notebook_run_in_python_code_():
