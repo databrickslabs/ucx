ecc28806ea11f4e7b2a52c709ec39fd9147654a8
Vuong <vuong.nguyen@databricks.com>
2024-04-18 20:29:05 +0100

    more error

--- src/databricks/labs/ucx/installer/logs.py
+++ src/databricks/labs/ucx/installer/logs.py
@@ -192,7 +192,8 @@ class TaskRunWarningRecorder:
                 continue
             # This error comes up during testing, when ucx crawls tables & schemas created by other tests,
             # but couldn't fetch the grants later as they have already been dropped by those tests. Ignore them
-            if "Couldn't fetch grants" in message and self._is_testing():
+            errors_in_test = ["Couldn't fetch grants", "Couldn't find permission"]
+            if any(error in message for error in errors_in_test) and self._is_testing():
                 continue
             error_messages.append(message)
         if len(error_messages) > 0:
