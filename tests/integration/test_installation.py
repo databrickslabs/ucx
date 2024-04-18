19f142bf05e91bc367d318a66d30292ba2a75933
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-18 15:49:50 +0200

    Wait for the group to be deleted

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os.path
 import sys
+import time
 from dataclasses import replace
 from datetime import timedelta
 
@@ -155,6 +156,8 @@ def test_job_cluster_policy(ws, installation_ctx):
 
 @retried(on=[NotFound, InvalidParameterValue], timeout=timedelta(minutes=5))
 def test_running_real_remove_backup_groups_job(ws, installation_ctx):
+    timeout_in_seconds, seconds_inbetween_tries = 60, 5
+
     ws_group_a, _ = installation_ctx.make_ucx_group()
 
     installation_ctx.__dict__['include_group_names'] = [ws_group_a.display_name]
@@ -166,8 +169,18 @@ def test_running_real_remove_backup_groups_job(ws, installation_ctx):
 
     installation_ctx.deployed_workflows.run_workflow("remove-workspace-local-backup-groups")
 
-    with pytest.raises(NotFound):
-        ws.groups.get(ws_group_a.id)
+    # It takes a moment to delete the group through the API
+    start_time = time.time()
+    while time.time() - start_time < timeout_in_seconds:
+        try:
+            ws.groups.get(ws_group_a.id)
+        except NotFound:
+            assert True, "Group removed"
+            break
+        else:
+            time.sleep(seconds_inbetween_tries)
+    else:
+        assert False, "Group not removed"
 
 
 @retried(on=[NotFound, InvalidParameterValue], timeout=timedelta(minutes=3))
