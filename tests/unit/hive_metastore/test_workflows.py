1327b4286c647b07672491fefd489eb160ec0619
qziyuan <ziyuan.qin@databricks.com>
2024-04-23 09:43:43 -0700

    add ctas external table migration workflow; add unit tests to the workflow run

--- tests/unit/hive_metastore/test_workflows.py
+++ tests/unit/hive_metastore/test_workflows.py
@@ -1,4 +1,5 @@
-from databricks.labs.ucx.hive_metastore.workflows import TableMigration
+from databricks.labs.ucx.hive_metastore.workflows import TableMigration, MigrateHiveSerdeTablesInPlace, \
+    MigrateExternalTablesCTAS
 
 
 def test_migrate_external_tables_sync(run_workflow):
@@ -8,4 +9,19 @@ def test_migrate_external_tables_sync(run_workflow):
 
 def test_migrate_dbfs_root_delta_tables(run_workflow):
     ctx = run_workflow(TableMigration.migrate_dbfs_root_delta_tables)
+    ctx.workspace_client.catalogs.list.assert_called_once()
+
+
+def test_migrate_hive_serde_in_place(run_workflow):
+    ctx = run_workflow(MigrateHiveSerdeTablesInPlace.migrate_hive_serde_in_place)
+    ctx.workspace_client.catalogs.list.assert_called_once()
+
+
+def test_migrate_other_external_ctas(run_workflow):
+    ctx = run_workflow(MigrateExternalTablesCTAS.migrate_other_external_ctas)
+    ctx.workspace_client.catalogs.list.assert_called_once()
+    
+
+def test_migrate_hive_serde_ctas(run_workflow):
+    ctx = run_workflow(MigrateExternalTablesCTAS.migrate_hive_serde_ctas)
     ctx.workspace_client.catalogs.list.assert_called_once()
