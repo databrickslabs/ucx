ec1801cbbe445a323b23c4564de6efc2cc158ff6
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2024-04-23 11:26:25 +0200

    Build dependency graph for local files (#1462)
    ## Changes
    Refactored dependency classes to clarify resolution vs loading
    Implemented LocalFileMigrator.build_dependency_graph (same pattern as
    NotebookMigrator)
    
    ### Linked issues
    https://github.com/databrickslabs/ucx/issues/1202
    Resolves https://github.com/databrickslabs/ucx/issues/1360
    
    ### Functionality 
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs ucx ...`
    - [ ] added a new workflow
    - [ ] modified existing workflow: `...`
    - [ ] added a new table
    - [ ] modified existing table: `...`
    
    ### Tests
    - [ ] manually tested
    - [x] added unit tests
    - [ ] added integration tests
    - [ ] verified on staging environment (screenshot attached)

--- tests/unit/source_code/test_dependencies.py
+++ tests/unit/source_code/test_dependencies.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import BinaryIO
 from unittest.mock import create_autospec
 
@@ -5,13 +6,19 @@ import pytest
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.workspace import ObjectInfo, Language, ObjectType
 
-from databricks.labs.ucx.source_code.dependencies import DependencyLoader, SourceContainer, DependencyResolver
-from databricks.labs.ucx.source_code.notebook_migrator import NotebookMigrator
+from databricks.labs.ucx.source_code.dependencies import (
+    SourceContainer,
+    DependencyResolver,
+    LocalFileLoader,
+    DependencyGraphBuilder,
+    WorkspaceNotebookLoader,
+    LocalNotebookLoader,
+)
 from databricks.labs.ucx.source_code.whitelist import Whitelist
-from tests.unit import _load_sources, _download_side_effect
+from tests.unit import _load_sources, _download_side_effect, whitelist_mock, _load_dependency_side_effect
 
 
-def test_build_dependency_graph_visits_notebook_notebook_dependencies(empty_index):
+def test_dependency_graph_builder_visits_workspace_notebook_dependencies():
     paths = ["root3.run.py.txt", "root1.run.py.txt", "leaf1.py.txt", "leaf2.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     visited: dict[str, bool] = {}
@@ -22,15 +29,38 @@ def test_build_dependency_graph_visits_notebook_notebook_dependencies(empty_inde
 
     ws = create_autospec(WorkspaceClient)
     ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
-
     ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root3.run.py.txt", language=Language.PYTHON, object_type=ObjectType.NOTEBOOK)
-    migrator.build_dependency_graph(object_info)
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.is_notebook.return_value = False
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, WorkspaceNotebookLoader(ws)))
+    builder.build_notebook_dependency_graph(Path("root3.run.py.txt"))
     assert len(visited) == len(paths)
 
 
-def test_build_dependency_graph_visits_notebook_file_dependencies(empty_index):
+def test_dependency_graph_builder_visits_local_notebook_dependencies():
+    paths = ["root4.py.txt", "leaf3.py.txt"]
+    sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
+    visited: dict[str, bool] = {}
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    file_loader.is_notebook.return_value = True
+    file_loader.is_file.return_value = True
+    notebook_loader = create_autospec(LocalNotebookLoader)
+    notebook_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    notebook_loader.is_notebook.return_value = True
+    notebook_loader.is_file.return_value = True
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, notebook_loader))
+    builder.build_notebook_dependency_graph(Path("root4.py.txt"))
+    assert len(visited) == len(paths)
+
+
+def test_dependency_graph_builder_visits_workspace_file_dependencies():
     paths = ["root8.py.txt", "leaf1.py.txt", "leaf2.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     visited: dict[str, bool] = {}
@@ -46,13 +76,18 @@ def test_build_dependency_graph_visits_notebook_file_dependencies(empty_index):
     ws = create_autospec(WorkspaceClient)
     ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
     ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root8.py.txt", language=Language.PYTHON, object_type=ObjectType.NOTEBOOK)
-    migrator.build_dependency_graph(object_info)
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.is_notebook.return_value = False
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, WorkspaceNotebookLoader(ws)))
+    builder.build_local_file_dependency_graph(Path("root8.py.txt"))
     assert len(visited) == len(paths)
 
 
-def test_build_dependency_graph_fails_with_unfound_dependency(empty_index):
+def test_dependency_graph_builder_fails_with_unfound_dependency():
     paths = ["root1.run.py.txt", "leaf1.py.txt", "leaf2.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
 
@@ -66,115 +101,110 @@ def test_build_dependency_graph_fails_with_unfound_dependency(empty_index):
         result.__enter__.return_value.read.return_value = sources[filename].encode("utf-8")
         return result
 
+    def get_status_side_effect(*args):
+        path = args[0]
+        if "leaf2" in path:
+            return None
+        return ObjectInfo(object_type=ObjectType.NOTEBOOK, language=Language.PYTHON, path=path)
+
     ws = create_autospec(WorkspaceClient)
+    ws.workspace.is_notebook.return_value = True
     ws.workspace.download.side_effect = download_side_effect
-    ws.workspace.list.return_value = []
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root1.run.py.txt", language=Language.PYTHON, object_type=ObjectType.NOTEBOOK)
+    ws.workspace.get_status.side_effect = get_status_side_effect
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.is_notebook.return_value = False
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, WorkspaceNotebookLoader(ws)))
     with pytest.raises(ValueError):
-        migrator.build_dependency_graph(object_info)
+        builder.build_notebook_dependency_graph(Path("root1.run.py.txt"))
 
 
-def test_build_dependency_graph_visits_file_dependencies(empty_index):
+def test_dependency_graph_builder_visits_file_dependencies():
     paths = ["root5.py.txt", "leaf4.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     visited: dict[str, bool] = {}
-
-    def get_status_side_effect(*args):
-        path = args[0]
-        return ObjectInfo(path=path, object_type=ObjectType.FILE)
-
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
-    ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root5.py.txt", object_type=ObjectType.FILE)
-    migrator.build_dependency_graph(object_info)
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    file_loader.is_file.return_value = True
+    file_loader.is_notebook.return_value = False
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, LocalNotebookLoader()))
+    builder.build_local_file_dependency_graph(Path("root5.py.txt"))
     assert len(visited) == len(paths)
 
 
-def test_build_dependency_graph_visits_recursive_file_dependencies(empty_index):
+def test_dependency_graph_builder_visits_recursive_file_dependencies():
     paths = ["root6.py.txt", "root5.py.txt", "leaf4.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     visited: dict[str, bool] = {}
-
-    def get_status_side_effect(*args):
-        path = args[0]
-        return ObjectInfo(path=path, object_type=ObjectType.FILE)
-
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
-    ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root6.py.txt", object_type=ObjectType.FILE)
-    migrator.build_dependency_graph(object_info)
+    whi = whitelist_mock()
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    file_loader.is_file.return_value = True
+    file_loader.is_notebook.return_value = False
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, LocalNotebookLoader()))
+    builder.build_local_file_dependency_graph(Path("root6.py.txt"))
     assert len(visited) == len(paths)
 
 
-def test_build_dependency_graph_safely_visits_non_file_dependencies(empty_index):
+def test_dependency_graph_builder_throws_with_invalid_dependencies():
     paths = ["root7.py.txt"]
     sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     visited: dict[str, bool] = {}
+    whi = whitelist_mock()
 
-    def get_status_side_effect(*args):
+    def is_file_side_effect(*args):
         path = args[0]
-        return (
-            ObjectInfo(path=path, object_type=ObjectType.LIBRARY)
-            if path == "some_library"
-            else ObjectInfo(path=path, object_type=ObjectType.FILE)
-        )
+        return str(path) in paths
 
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
-    ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root7.py.txt", object_type=ObjectType.FILE)
-    migrator.build_dependency_graph(object_info)
-    assert len(visited) == len(paths)
-
-
-def test_build_dependency_graph_throws_with_invalid_dependencies(empty_index):
-    paths = ["root7.py.txt"]
-    sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
-    visited: dict[str, bool] = {}
-
-    def get_status_side_effect(*args):
-        path = args[0]
-        return ObjectInfo(path=path) if path == "some_library" else ObjectInfo(path=path, object_type=ObjectType.FILE)
-
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.side_effect = lambda *args, **kwargs: _download_side_effect(sources, visited, *args, **kwargs)
-    ws.workspace.get_status.side_effect = get_status_side_effect
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="root7.py.txt", language=Language.PYTHON, object_type=ObjectType.FILE)
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(
+        sources, visited, *args
+    )
+    file_loader.is_file.side_effect = is_file_side_effect
+    file_loader.is_notebook.return_value = False
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, LocalNotebookLoader()))
     with pytest.raises(ValueError):
-        migrator.build_dependency_graph(object_info)
+        builder.build_local_file_dependency_graph(Path("root7.py.txt"))
 
 
-def test_build_dependency_graph_ignores_builtin_dependencies(empty_index):
-    source = """
-import os
-from path import Path    
-"""
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.return_value.__enter__.return_value.read.return_value = source.encode("utf-8")
-    ws.workspace.get_status.return_value = ObjectInfo(path="path", object_type=ObjectType.FILE)
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws))
-    object_info = ObjectInfo(path="path", language=Language.PYTHON, object_type=ObjectType.FILE)
-    migrator.build_dependency_graph(object_info)
+def test_dependency_graph_builder_ignores_builtin_dependencies():
+    paths = ["python_builtins.py.txt"]
+    sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
+    whi = Whitelist()
+
+    def is_file_side_effect(*args):
+        path = args[0]
+        return str(path) in paths
+
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.is_file.side_effect = is_file_side_effect
+    file_loader.is_notebook.return_value = False
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(sources, {}, *args)
+    builder = DependencyGraphBuilder(DependencyResolver(whi, file_loader, LocalNotebookLoader()))
+    graph = builder.build_local_file_dependency_graph(Path("python_builtins.py.txt"))
+    assert not graph.locate_dependency("os")
+    assert not graph.locate_dependency("path")
 
 
-def test_build_dependency_graph_ignores_known_dependencies(empty_index):
-    source = """
-import databricks
-from databricks.sdk.service.compute import Thing    
-"""
+def test_dependency_graph_builder_ignores_known_dependencies():
+    paths = ["python_builtins.py.txt"]
+    sources: dict[str, str] = dict(zip(paths, _load_sources(SourceContainer, *paths)))
     datas = _load_sources(SourceContainer, "sample-python-compatibility-catalog.yml")
     whitelist = Whitelist.parse(datas[0])
-    resolver = DependencyResolver(whitelist)
-    ws = create_autospec(WorkspaceClient)
-    ws.workspace.download.return_value.__enter__.return_value.read.return_value = source.encode("utf-8")
-    ws.workspace.get_status.return_value = ObjectInfo(path="path", object_type=ObjectType.FILE)
-    migrator = NotebookMigrator(ws, empty_index, DependencyLoader(ws), resolver)
-    object_info = ObjectInfo(path="path", language=Language.PYTHON, object_type=ObjectType.FILE)
-    migrator.build_dependency_graph(object_info)
+
+    def is_file_side_effect(*args):
+        path = args[0]
+        return str(path) in paths
+
+    file_loader = create_autospec(LocalFileLoader)
+    file_loader.is_file.side_effect = is_file_side_effect
+    file_loader.is_notebook.return_value = False
+    file_loader.load_dependency.side_effect = lambda *args, **kwargs: _load_dependency_side_effect(sources, {}, *args)
+    builder = DependencyGraphBuilder(DependencyResolver(whitelist, file_loader, LocalNotebookLoader()))
+    graph = builder.build_local_file_dependency_graph(Path("python_builtins.py.txt"))
+    assert not graph.locate_dependency("databricks")
