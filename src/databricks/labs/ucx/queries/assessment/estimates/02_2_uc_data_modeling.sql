015c2e813d01c7f9b92b9a93dffef91f7ffffb87
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-23 17:25:59 +0200

    Update tables to migrate query

--- src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
@@ -1,3 +1,22 @@
--- viz type=table, name=Tables to migrate, columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
+-- viz type=table, name=Tables to migrate, search_by=catalog,database,name,upgraded_status columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
 -- widget title=Tables to migrate, row=2, col=2, size_x=3, size_y=8
-select * from $inventory.tables;
+SELECT
+  tables.`catalog`,
+  tables.`database`,
+  tables.name,
+  tables.object_type,
+  tables.table_format,
+  tables.location,
+  tables.view_text,
+  tables.upgraded_to,
+  CASE
+    WHEN migration_status.src_table IS NOT NULL THEN 'Migrated'
+    ELSE 'Not migrated'
+  END AS upgraded_status,
+  tables.storage_properties,
+  tables.is_partitioned
+FROM
+    $inventory.tables AS tables
+  LEFT JOIN
+    $inventory.migration_status AS migration_status
+  ON tables.`database` = migration_status.src_schema AND tables.name = migration_status.src_table
