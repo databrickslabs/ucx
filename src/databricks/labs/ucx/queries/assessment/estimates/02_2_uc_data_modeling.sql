23b281c9c31ae1afa407fd9c95b8aca61c36d142
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-25 11:46:08 +0200

    Test with one search by

--- src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
@@ -1,4 +1,4 @@
--- viz type=table, name=Tables to migrate, search_by=catalog,database,name,upgraded_status, columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
+-- viz type=table, name=Tables to migrate, search_by=catalog columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
 -- widget title=Tables to migrate, row=2, col=2, size_x=3, size_y=8
 SELECT
   tables.`catalog`,
