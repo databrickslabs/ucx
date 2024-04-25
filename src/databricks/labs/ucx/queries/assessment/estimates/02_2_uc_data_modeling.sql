af6bdf38b7d8643ec7adac27ac0369164fa538bc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-25 12:05:42 +0200

    Concat catalog, schema/database and table

--- src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
@@ -1,21 +1,17 @@
 -- viz type=table, name=Tables to migrate, search_by=catalog,database,name,upgraded_status, columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
 -- widget title=Tables to migrate, row=2, col=2, size_x=3, size_y=8
 SELECT
-  tables.`catalog`,
-  tables.`database`,
-  tables.name,
+  concat_ws('.', tables.`catalog`, tables.`database`, tables.name) AS table_name,
   tables.object_type,
   tables.table_format,
   tables.location,
-  tables.view_text,
   CASE
       WHEN migration_status.dst_table IS NOT NULL THEN 'Migrated'
       ELSE 'Not migrated'
   END AS upgraded_status,
-  migration_status.dst_catalog AS upgraded_to_catalog,
-  migration_status.dst_schema AS upgraded_to_schema,
-  migration_status.dst_table AS upgraded_to_table,
+  concat_ws('.', migration_status.`dst_catalog`, migration_status.`dst_schema`, migration_status.dst_table) AS upgraded_to_table_name,
   migration_status.update_ts AS upgraded_timestamp,
+  tables.view_text,
   tables.storage_properties,
   tables.is_partitioned
 FROM
