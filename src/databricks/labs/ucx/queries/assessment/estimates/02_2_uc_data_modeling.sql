3c4327053e5f03b631b84dd7344c16ba8f78337b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-29 11:03:59 +0200

    Update not migrated status
    Useful for filtering

--- src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
@@ -7,7 +7,7 @@ SELECT
   tables.location,
   CASE
       WHEN migration_status.dst_table IS NOT NULL THEN 'Migrated'
-      ELSE 'Not migrated'
+      ELSE 'Stayed in place'
   END AS upgraded_status,
   concat_ws('.', migration_status.dst_catalog, migration_status.dst_schema, migration_status.dst_table) AS upgraded_to_table_name,
   tables.view_text,
