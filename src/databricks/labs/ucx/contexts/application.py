89c6af442b16a965c66fc4fdfa8eea0cf011376a
Vuong <vuong.nguyen@databricks.com>
2024-04-30 21:35:54 +0100

    secrets

--- src/databricks/labs/ucx/contexts/application.py
+++ src/databricks/labs/ucx/contexts/application.py
@@ -170,7 +170,7 @@ class GlobalContext(abc.ABC):
     @cached_property
     def secret_scope_acl_support(self):
         # Secret ACLs are not used much in tests, so skipping include_object_permissions
-        return SecretScopesSupport(self.workspace_client)
+        return SecretScopesSupport(self.workspace_client, include_group_names=self.config.include_group_names)
 
     @cached_property
     def legacy_table_acl_support(self):
