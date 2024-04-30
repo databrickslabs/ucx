b2e3c73015c2c963a6ec5da4506e3fdfb00bf44b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-04-30 16:48:48 +0200

    Set back UC data modeling

--- src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/02_2_uc_data_modeling.sql
@@ -1,20 +1,3 @@
--- viz type=table, name=Tables to migrate, search_by=table_name,upgraded_status, columns=table_name,object_type,table_format,location,upgraded_status,upgraded_to_table_name,view_text,storage_properties,is_partitioned
+-- viz type=table, name=Tables to migrate, columns=catalog,database,name,object_type,table_format,location,view_text,upgraded_to,storage_properties
 -- widget title=Tables to migrate, row=2, col=2, size_x=3, size_y=8
-SELECT
-  concat_ws('.', tables.`catalog`, tables.`database`, tables.name) AS table_name,
-  tables.object_type,
-  tables.table_format,
-  tables.location,
-  CASE
-      WHEN migration_status.dst_table IS NOT NULL THEN 'Migrated'
-      ELSE 'Stayed in place'
-  END AS upgraded_status,
-  concat_ws('.', migration_status.dst_catalog, migration_status.dst_schema, migration_status.dst_table) AS upgraded_to_table_name,
-  tables.view_text,
-  tables.storage_properties,
-  tables.is_partitioned
-FROM
-    $inventory.tables AS tables
-  LEFT JOIN
-    $inventory.migration_status AS migration_status
-  ON tables.`database` = migration_status.src_schema AND tables.name = migration_status.src_table
+select * from $inventory.tables;
