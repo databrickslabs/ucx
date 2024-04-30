3f2c56bbe2c2e1115b8981d89171d9ea40de626e
Eric Vergnaud <eic.vergnaud@databricks.com>
2024-04-30 18:29:34 +0200

    more tests, factorize them

--- tests/unit/source_code/test_syspath_simulator.py
+++ tests/unit/source_code/test_syspath_simulator.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from databricks.labs.ucx.source_code.dependency_containers import SourceContainer
 from databricks.labs.ucx.source_code.dependency_graph import DependencyGraphBuilder
 from databricks.labs.ucx.source_code.dependency_resolvers import DependencyResolver
@@ -8,9 +10,20 @@ from databricks.labs.ucx.source_code.syspath_provider import SysPathProvider
 from tests.unit import _samples_path, whitelist_mock, VisitingFileLoader, VisitingNotebookLoader, locate_site_packages
 
 
-def test_locates_sibling_notebook():
+@pytest.mark.parametrize(
+    "source, expected",
+    [ (["walk-sys-path", "siblings", "sibling1_notebook"], 2),
+      (["walk-sys-path", "parent-child", "in_parent_folder_notebook"], 3),
+      (["walk-sys-path", "child-parent", "child-folder", "in_child_folder_notebook"], 3),
+      (["walk-sys-path", "parent-grand-child", "in_parent_folder_notebook"], 3),
+      (["walk-sys-path", "child-grand-parent", "child-folder", "child-folder", "in_grand_child_folder_notebook"], 3)
+
+      ])
+def test_locates_notebooks(source: list[str], expected: int):
     visited: dict[str, bool] = {}
-    notebook_path = Path(_samples_path(SourceContainer), "walk-sys-path", "siblings", "sibling1_notebook")
+    elems = [_samples_path(SourceContainer)]
+    elems.extend(source)
+    notebook_path = Path(*elems)
     whi = whitelist_mock()
     provider = SysPathProvider.from_sys_path()
     file_loader = VisitingFileLoader(provider, visited)
@@ -20,34 +33,5 @@ def test_locates_sibling_notebook():
         DependencyResolver.initialize(whi, site_packages, file_loader, notebook_loader, provider), provider
     )
     builder.build_notebook_dependency_graph(notebook_path)
-    assert len(visited) == 2
+    assert len(visited) == expected
 
-
-def test_locates_notebook_in_child_folder():
-    visited: dict[str, bool] = {}
-    notebook_path = Path(_samples_path(SourceContainer), "walk-sys-path", "parent-child", "in_parent_folder_notebook")
-    whi = whitelist_mock()
-    provider = SysPathProvider.from_sys_path()
-    file_loader = VisitingFileLoader(provider, visited)
-    notebook_loader = VisitingNotebookLoader(provider, visited)
-    site_packages = SitePackages.parse(locate_site_packages())
-    builder = DependencyGraphBuilder(
-        DependencyResolver.initialize(whi, site_packages, file_loader, notebook_loader, provider), provider
-    )
-    builder.build_notebook_dependency_graph(notebook_path)
-    assert len(visited) == 2
-
-
-def test_locates_notebook_in_parent_folder():
-    visited: dict[str, bool] = {}
-    notebook_path = Path(_samples_path(SourceContainer), "walk-sys-path", "child-parent", "child-folder", "in_child_folder_notebook")
-    whi = whitelist_mock()
-    provider = SysPathProvider.from_sys_path()
-    file_loader = VisitingFileLoader(provider, visited)
-    notebook_loader = VisitingNotebookLoader(provider, visited)
-    site_packages = SitePackages.parse(locate_site_packages())
-    builder = DependencyGraphBuilder(
-        DependencyResolver.initialize(whi, site_packages, file_loader, notebook_loader, provider), provider
-    )
-    builder.build_notebook_dependency_graph(notebook_path)
-    assert len(visited) == 2
