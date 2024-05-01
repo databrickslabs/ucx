63ac9114bf77f42944873dbcad80c675456f92c9
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-01 16:01:58 +0200

    Extend integration test for other workflows

--- tests/integration/hive_metastore/test_workflows.py
+++ tests/integration/hive_metastore/test_workflows.py
@@ -10,8 +10,16 @@ from databricks.sdk.retries import retried
 
 
 @retried(on=[NotFound], timeout=timedelta(minutes=5))
-@pytest.mark.parametrize('prepare_tables_for_migration', [('regular')], indirect=True)
-def test_table_migration_job_refreshes_migration_status(ws, installation_ctx, prepare_tables_for_migration):
+@pytest.mark.parametrize(
+    "prepare_tables_for_migration,workflow",
+    [
+        ("regular", "migrate-tables"),
+        ("hiveserde", "migrate-external-hiveserde-tables-in-place-experimental"),
+        ("hiveserde", "migrate-external-tables-ctas"),
+    ],
+    indirect=("prepare_tables_for_migration",),
+)
+def test_table_migration_job_refreshes_migration_status(ws, installation_ctx, prepare_tables_for_migration, workflow):
     """The migration status should be refreshed after the migration job."""
     tables, dst_schema = prepare_tables_for_migration
     ctx = installation_ctx.replace(
@@ -21,7 +29,7 @@ def test_table_migration_job_refreshes_migration_status(ws, installation_ctx, pr
     )
 
     ctx.workspace_installation.run()
-    ctx.deployed_workflows.run_workflow("migrate-tables")
+    ctx.deployed_workflows.run_workflow(workflow)
 
     for table in tables.values():
         # Avoiding MigrationStatusRefresh as it will refresh the status before fetching
