79e0fa0ca287e1669d1115dbe9f3ea2b6e0db197
pritishpai <pritish.pai@databricks.com>
2024-05-01 14:20:14 -0400

    Add basic unit test for readiness_report

---
+++ tests/unit/account/test_aggregate.py
@@ -0,0 +1,16 @@
+import logging
+from unittest.mock import create_autospec
+from databricks.sdk.service.provisioning import Workspace
+from databricks.sdk import WorkspaceClient
+from databricks.labs.ucx.account.aggregate import AccountAggregate
+from databricks.labs.ucx.account.workspaces import AccountWorkspaces
+
+
+def test_basic_readiness_report(acc_client, caplog):
+    account_ws = AccountWorkspaces(acc_client)
+    account_aggregate_obj = AccountAggregate(account_ws)
+
+    with caplog.at_level(logging.INFO):
+        account_aggregate_obj.readiness_report()
+
+    assert 'UC compatibility' in caplog.text
