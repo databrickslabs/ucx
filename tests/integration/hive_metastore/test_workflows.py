f2afb7faf0df36def07403b06ef5383665aa2fed
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-02 09:00:47 +0200

    Fix typo

--- tests/integration/hive_metastore/test_workflows.py
+++ tests/integration/hive_metastore/test_workflows.py
@@ -31,7 +31,7 @@ def test_table_migration_job_refreshes_migration_status(ws, installation_ctx, pr
         # Avoiding MigrationStatusRefresh as it will refresh the status before fetching
         query_migration_status = (
             f"SELECT * FROM {ctx.config.inventory_database}.migration_status "
-            f"WHERE src_schema = '{tables.schema_name}' AND src_table = '{table.name}'"
+            f"WHERE src_schema = '{table.schema_name}' AND src_table = '{table.name}'"
         )
         migration_status = list(ctx.sql_backend.fetch(query_migration_status))
         assert len(migration_status) == 1
