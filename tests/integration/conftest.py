4106777c86d4f8491c8c3f30fe97c91f92c6e13b
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-05-03 00:23:39 +0200

    Avoid cyclic import issues, when we need to initialize `WorkspaceContext` from `AccountContext` (#1611)
    This PR splits `cli_command.py` into `workspace_cli.py` and
    `account_cli.py`

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -27,7 +27,7 @@ from databricks.labs.ucx.assessment.azure import (
 from databricks.labs.ucx.aws.access import AWSResourcePermissions
 from databricks.labs.ucx.azure.access import AzureResourcePermissions, StoragePermissionMapping
 from databricks.labs.ucx.config import WorkspaceConfig
-from databricks.labs.ucx.contexts.cli_command import WorkspaceContext
+from databricks.labs.ucx.contexts.workspace_cli import WorkspaceContext
 from databricks.labs.ucx.contexts.workflow_task import RuntimeContext
 from databricks.labs.ucx.hive_metastore import TablesCrawler
 from databricks.labs.ucx.hive_metastore.grants import Grant
