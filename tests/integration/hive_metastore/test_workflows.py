78ed404463686af6b2b507599db4762f245e5f6b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-02 08:37:58 +0200

    Extend test with dst checks

--- tests/integration/hive_metastore/test_workflows.py
+++ tests/integration/hive_metastore/test_workflows.py
@@ -31,7 +31,9 @@ def test_table_migration_job_refreshes_migration_status(ws, installation_ctx, pr
         # Avoiding MigrationStatusRefresh as it will refresh the status before fetching
         query_migration_status = (
             f"SELECT * FROM {ctx.config.inventory_database}.migration_status "
-            f"WHERE src_schema = '{dst_schema.name}' AND src_table = '{table.name}'"
+            f"WHERE src_schema = '{tables.schema_name}' AND src_table = '{table.name}'"
         )
         migration_status = list(ctx.sql_backend.fetch(query_migration_status))
         assert len(migration_status) == 1
+        assert migration_status[0].dst_schema is not None
+        assert migration_status[0].dst_table is not None
