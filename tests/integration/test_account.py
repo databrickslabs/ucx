582372f84627902321c289dc495a4e071682a7ed
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-07 12:33:14 +0200

    Use fixture to clean account level groups

--- tests/integration/test_account.py
+++ tests/integration/test_account.py
@@ -2,18 +2,34 @@ from datetime import timedelta
 
 from databricks.labs.blueprint.tui import MockPrompts
 from databricks.sdk import AccountClient
-from databricks.sdk.errors import NotFound
 from databricks.sdk.retries import retried
 from databricks.sdk.service.iam import Group
+import pytest
 
 from databricks.labs.ucx.account.workspaces import AccountWorkspaces
 
 
-def test_create_account_level_groups(make_ucx_group, make_group, make_user, acc, ws, make_random):
+@pytest.fixture
+def clean_account_level_groups(acc: AccountClient):
+    """Clean test generated account level groups."""
+
+    def delete_ucx_created_groups():
+        for group in acc.groups.list():
+            if group.display_name.startswith("created_by_ucx"):
+                acc.groups.delete(group.id)
+
+    delete_ucx_created_groups()
+    yield
+    delete_ucx_created_groups()
+
+
+def test_create_account_level_groups(
+    make_ucx_group, make_group, make_user, acc, ws, make_random, clean_account_level_groups
+):
     suffix = make_random()
     make_ucx_group(f"test_ucx_migrate_invalid_{suffix}", f"test_ucx_migrate_invalid_{suffix}")
 
-    group_display_name = f"regular_group_{suffix}"
+    group_display_name = f"created_by_ucx_regular_group_{suffix}"
     make_group(display_name=group_display_name, members=[make_user().id])
     AccountWorkspaces(acc).create_account_level_groups(MockPrompts({}), [ws.get_workspace_id()])
 
@@ -25,12 +41,4 @@ def test_create_account_level_groups(make_ucx_group, make_group, make_user, acc,
         raise KeyError(f"Group not found {display_name}")
 
     group = get_group(group_display_name)
-    try_delete_group(acc, group.id)  # Avoids flakiness for future runs
     assert group
-
-
-def try_delete_group(acc: AccountClient, grp_id: str):
-    try:
-        acc.groups.delete(grp_id)
-    except NotFound:
-        pass
