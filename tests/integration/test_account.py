457634406b8b43223dc777774a4ffcd6fdb28e16
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-07 12:12:11 +0200

    Use retry mechanism in test create account level groups

--- tests/integration/test_account.py
+++ tests/integration/test_account.py
@@ -1,6 +1,10 @@
+from datetime import timedelta
+
 from databricks.labs.blueprint.tui import MockPrompts
 from databricks.sdk import AccountClient
 from databricks.sdk.errors import NotFound
+from databricks.sdk.retries import retried
+from databricks.sdk.service.iam import Group
 
 from databricks.labs.ucx.account.workspaces import AccountWorkspaces
 
@@ -9,16 +13,20 @@ def test_create_account_level_groups(make_ucx_group, make_group, make_user, acc,
     suffix = make_random()
     make_ucx_group(f"test_ucx_migrate_invalid_{suffix}", f"test_ucx_migrate_invalid_{suffix}")
 
-    make_group(display_name=f"regular_group_{suffix}", members=[make_user().id])
+    group_display_name = f"regular_group_{suffix}"
+    make_group(display_name=group_display_name, members=[make_user().id])
     AccountWorkspaces(acc).create_account_level_groups(MockPrompts({}), [ws.get_workspace_id()])
 
-    results = []
-    for grp in acc.groups.list():
-        if grp.display_name in {f"regular_group_{suffix}"}:
-            results.append(grp)
-            try_delete_group(acc, grp.id)  # Avoids flakiness for future runs
+    @retried(on=[KeyError], timeout=timedelta(minutes=2))
+    def get_group(display_name: str) -> Group:
+        for grp in acc.groups.list():
+            if grp.display_name == display_name:
+                return grp
+        raise KeyError(f"Group not found {display_name}")
 
-    assert len(results) == 1
+    group = get_group(group_display_name)
+    try_delete_group(acc, group.id)  # Avoids flakiness for future runs
+    assert group
 
 
 def try_delete_group(acc: AccountClient, grp_id: str):
