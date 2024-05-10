1d67dd77e2a27529008706b772b6ec6e9d5a07a0
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-10 11:17:58 +0200

    Assert PipCell calls register library

---
+++ tests/unit/source_code/notebooks/test_cells.py
@@ -0,0 +1,16 @@
+from unittest.mock import create_autospec
+
+from databricks.labs.ucx.source_code.graph import DependencyGraph
+from databricks.labs.ucx.source_code.notebooks.cells import PipCell
+
+
+def test_pip_cell_build_dependency_graph_register_pip_install_library():
+    """A pip install library should be registered"""
+    graph = create_autospec(DependencyGraph)
+
+    code = "%pip install databricks"
+    cell = PipCell(code)
+
+    cell.build_dependency_graph(graph)
+
+    graph.register_library.assert_called_once_with("databricks")
