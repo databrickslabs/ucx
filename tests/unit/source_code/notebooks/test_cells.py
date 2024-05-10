fde98716e228af597c2c76a586b3fd48f7df54ba
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-10 11:19:13 +0200

    Test pip cell langauge

--- tests/unit/source_code/notebooks/test_cells.py
+++ tests/unit/source_code/notebooks/test_cells.py
@@ -1,7 +1,12 @@
 from unittest.mock import create_autospec
 
 from databricks.labs.ucx.source_code.graph import DependencyGraph
-from databricks.labs.ucx.source_code.notebooks.cells import PipCell
+from databricks.labs.ucx.source_code.notebooks.cells import CellLanguage, PipCell
+
+
+def test_pip_cell_langauge():
+    """Pip cell langauge should be CellLanguage.PIP"""
+    assert PipCell("code").language == CellLanguage.PIP
 
 
 def test_pip_cell_build_dependency_graph_register_pip_install_library():
