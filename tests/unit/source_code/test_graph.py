7a0248daf3092135c58673b52b95de8fc194d048
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-10 11:54:54 +0200

    Create a fixture for the file dependency

--- tests/unit/source_code/test_graph.py
+++ tests/unit/source_code/test_graph.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-from unittest.mock import create_autospec
-
+import pytest
 from databricks.labs.ucx.source_code.graph import DependencyGraph, Dependency, SourceContainer, WrappingLoader
 from databricks.labs.ucx.source_code.files import FileLoader
 
@@ -15,19 +14,22 @@ def test_dependency_path():
     assert dependency.path == path
 
 
-def test_wrapping_loader_load_dependency(mock_path_lookup):
+@pytest.fixture()
+def file_dependency() -> Dependency:
+    yield Dependency(FileLoader(), Path("test"))
+
+
+def test_wrapping_loader_load_dependency(mock_path_lookup, file_dependency):
     """Should return the source container"""
     sources = _load_sources(SourceContainer, "root4.py.txt")
     wrapping_loader = WrappingLoader(sources[0])
-    dependency = create_autospec(Dependency)  # TODO: Replace autospec with object
-    assert wrapping_loader.load_dependency(mock_path_lookup, dependency) == sources[0]
+    assert wrapping_loader.load_dependency(mock_path_lookup, file_dependency) == sources[0]
 
 
-def test_dependency_graph_without_parent_root_is_self(mock_path_lookup):
+def test_dependency_graph_without_parent_root_is_self(mock_path_lookup, file_dependency):
     """The dependency graph root should be itself when there is no parent."""
-    dependency = create_autospec(Dependency)
     graph = DependencyGraph(
-        dependency=dependency,  # TODO: Replace autospec with object
+        dependency=file_dependency,
         parent=None,
         resolver=None,  # TODO: Replace None with object
         path_lookup=mock_path_lookup
