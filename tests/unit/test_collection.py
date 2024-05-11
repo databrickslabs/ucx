4cb71095d8ba6bf8153714bd0e529616489a5f57
hari-selvarajan_data <hari.selvarajan@databricks.com>
2024-05-11 08:03:31 +0100

    increasing coverage

--- tests/unit/test_collection.py
+++ tests/unit/test_collection.py
@@ -1,4 +1,8 @@
+import io
 from unittest.mock import create_autospec
+
+import yaml
+
 from databricks.sdk.service.provisioning import Workspace
 
 from databricks.labs.blueprint.tui import MockPrompts
@@ -48,6 +52,44 @@ def test_join_collection_no_sync_called():
     account_installer.join_collection(123)
     account_client.workspaces.list.assert_called()
     account_client.get_workspace_client.assert_not_called()
+
+
+def test_join_collection_join_collection_no_installation_id():
+    ws = workspace_client_mock()
+    download_yaml = yaml.dump(
+        {
+            'version': 1,
+            'inventory_database': 'ucx_exists',
+            'connect': {
+                'host': '...',
+                'token': '...',
+            },
+        }
+    )
+    ws.workspace.download.return_value = io.StringIO(download_yaml)
+    account_client = create_autospec(AccountClient)
+    account_client.workspaces.list.return_value = [
+        Workspace(workspace_id=123, deployment_name="test"),
+        Workspace(workspace_id=456, deployment_name="test2"),
+    ]
+    account_client.get_workspace_client.return_value = ws
+    account_installer = AccountInstaller(account_client)
+    prompts = MockPrompts(
+        {
+            r".*PRO or SERVERLESS SQL warehouse.*": "1",
+            r"Open job overview.*": "no",
+            r"Do you want to join the current.*": "yes",
+            r"Select the workspace to join current installation as a collection group": 1,
+            r".*": "",
+        }
+    )
+    account_installer.replace(
+        prompts=prompts,
+        product_info=ProductInfo.for_testing(WorkspaceConfig),
+    )
+
+    account_installer.join_collection(789)
+    ws.workspace.upload.assert_not_called()
 
 
 def test_join_collection_join_collection():
