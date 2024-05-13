abf77da23677fb3c0ac3aa64ab021c3c479c673e
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-13 12:34:14 +0200

    Test installing failed library

--- tests/unit/source_code/test_site_packages.py
+++ tests/unit/source_code/test_site_packages.py
@@ -1,4 +1,5 @@
 from databricks.labs.ucx.source_code.files import FileLoader
+from databricks.labs.ucx.source_code.graph import DependencyProblem
 from databricks.labs.ucx.source_code.site_packages import PipResolver, SitePackages
 from tests.unit import locate_site_packages
 
@@ -10,6 +11,15 @@ def test_pip_resolver_resolve_library(mock_path_lookup):
 
     assert len(maybe.problems) == 0
     assert maybe.dependency.path.as_posix() == "pytest"
+
+
+def test_pip_resolver_resolve_library_unknown_library(mock_path_lookup):
+    """Verify installing unknown library."""
+    pip_resolver = PipResolver(FileLoader())
+    maybe = pip_resolver.resolve_library(mock_path_lookup, "unknown-library-name")
+
+    assert len(maybe.problems) == 1
+    assert maybe.problems[0] == DependencyProblem("library-install-failed", "Failed to install unknown-library-name")
 
 
 def test_reads_site_packages():
