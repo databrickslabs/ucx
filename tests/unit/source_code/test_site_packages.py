7dad20befeee17bf5d32fa3241b3269ad0bbc7cc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-13 13:52:22 +0200

    Add test for parsing site package

--- tests/unit/source_code/test_site_packages.py
+++ tests/unit/source_code/test_site_packages.py
@@ -1,6 +1,10 @@
+import pytest
+from pathlib import Path
+
 from databricks.labs.ucx.source_code.files import FileLoader
 from databricks.labs.ucx.source_code.graph import DependencyProblem
-from databricks.labs.ucx.source_code.site_packages import PipResolver, SitePackages
+from databricks.labs.ucx.source_code.site_packages import PipResolver, SitePackages, SitePackage
+from databricks.labs.ucx.mixins.fixtures import make_random
 from tests.unit import locate_site_packages
 
 
@@ -26,3 +30,36 @@ def test_reads_site_packages():
     site_packages_path = locate_site_packages()
     site_packages = SitePackages.parse(str(site_packages_path))
     assert site_packages["astroid"] is not None
+
+
+@pytest.fixture
+def site_package_path_without_top_level(tmp_path, make_random) -> Path:
+    """Mock a site package"""
+    package_path = tmp_path / f"ucx-{make_random()}.dist-info"
+    package_path.mkdir(parents=True, exist_ok=True)
+    record_file = package_path / "RECORD"
+    records = (
+        "ucx-0.13.13.dist-info/AUTHORS,sha256=6Xr,236",
+        "ucx-0.13.13.dist-info/COPYING.txt,sha256=Tutb,1492",
+        "ucx-0.13.13.dist-info/INSTALLER,sha256=zuuu,4",
+        "ucx-0.13.13.dist-info/METADATA,sha256=s5kX,14421",
+        "ucx-0.13.13.dist-info/RECORD,,",
+        "ucx-0.13.13.dist-info/REQUESTED,sha256=47DE,0",
+        "ucx-0.13.13.dist-info/WHEEL,sha256=2wepM,92",
+        "ucx-0.13.13.dist-info/entry_points.txt,sha256=6Xet,45",
+        "ucx-0.13.13.dist-info/top_level.txt,sha256=Oywf,5",
+        "ucx-0.13.13.dist-info/zip-safe,sha256=AbpH,1",
+        "ucx/__init__.py,sha256=u616,507",
+        "ucx/__main__.py,sha256=HIRU,10773",
+        "ucx/__pycache__/__init__.cpython-310.pyc,,",
+        "ucx/__pycache__/__main__.cpython-310.pyc,,",
+    )
+    record_file.write_text("\n".join(records))
+    return package_path
+
+
+def test_site_package_parse(site_package_path_without_top_level):
+    """Parse a mock site package"""
+    site_package = SitePackage.parse(site_package_path_without_top_level)
+    assert site_package.top_levels == ["ucx"]
+    assert site_package._dist_info_path == site_package_path_without_top_level
