20317b49f2b01b6ef98a1f0db075285fce9f267d
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-13 12:26:54 +0200

    Test pip resolve pytest library

--- tests/unit/source_code/test_site_packages.py
+++ tests/unit/source_code/test_site_packages.py
@@ -1,5 +1,15 @@
-from databricks.labs.ucx.source_code.site_packages import SitePackages
+from databricks.labs.ucx.source_code.files import FileLoader
+from databricks.labs.ucx.source_code.site_packages import PipResolver, SitePackages
 from tests.unit import locate_site_packages
+
+
+def test_pip_resolver_resolve_library(mock_path_lookup):
+    """Verify pytest can be resolved"""
+    pip_resolver = PipResolver(FileLoader())
+    maybe = pip_resolver.resolve_library(mock_path_lookup, "pytest")
+
+    assert len(maybe.problems) == 0
+    assert maybe.dependency.path.as_posix() == "pytest"
 
 
 def test_reads_site_packages():
