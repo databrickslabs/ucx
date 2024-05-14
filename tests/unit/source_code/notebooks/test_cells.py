d3b4b4454e46a965ffae915a44a47bb87269b4e1
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-14 13:14:19 +0200

    Add unit tests
    Copy from [PR](https://github.com/databrickslabs/ucx/pull/1662)

---
+++ tests/unit/source_code/notebooks/test_cells.py
@@ -0,0 +1,21 @@
+from unittest.mock import create_autospec
+
+from databricks.labs.ucx.source_code.graph import DependencyGraph
+from databricks.labs.ucx.source_code.notebooks.cells import CellLanguage, PipCell
+
+
+def test_pip_cell_langauge():
+    """Pip cell langauge should be CellLanguage.PIP"""
+    assert PipCell("code").language == CellLanguage.PIP
+
+
+def test_pip_cell_build_dependency_graph_register_pip_install_library():
+    """A pip install library should be registered"""
+    graph = create_autospec(DependencyGraph)
+
+    code = "%pip install databricks"
+    cell = PipCell(code)
+
+    cell.build_dependency_graph(graph)
+
+    graph.register_import.assert_called_once_with("databricks")
