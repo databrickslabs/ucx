60091faba6db2b4ba0dc17815446d50ae4aa02fe
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-14 13:44:15 +0200

    Refactor resolve library to resolve import

--- tests/unit/source_code/test_site_packages.py
+++ tests/unit/source_code/test_site_packages.py
@@ -12,7 +12,7 @@ from tests.unit import locate_site_packages
 def test_pip_resolver_resolve_library(mock_path_lookup):
     """Verify pytest can be resolved"""
     pip_resolver = PipResolver(FileLoader())
-    maybe = pip_resolver.resolve_library(mock_path_lookup, "pytest")
+    maybe = pip_resolver.resolve_import(mock_path_lookup, "pytest")
 
     assert len(maybe.problems) == 0
     assert maybe.dependency.path.as_posix() == "pytest"
@@ -21,7 +21,7 @@ def test_pip_resolver_resolve_library(mock_path_lookup):
 def test_pip_resolver_resolve_library_unknown_library(mock_path_lookup):
     """Verify installing unknown library."""
     pip_resolver = PipResolver(FileLoader())
-    maybe = pip_resolver.resolve_library(mock_path_lookup, "unknown-library-name")
+    maybe = pip_resolver.resolve_import(mock_path_lookup, "unknown-library-name")
 
     assert len(maybe.problems) == 1
     assert maybe.problems[0] == DependencyProblem("library-install-failed", "Failed to install unknown-library-name")
