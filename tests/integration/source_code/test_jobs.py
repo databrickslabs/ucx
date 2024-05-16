17c0ab5d67cf04295b252e73d7ed9bec0d04bad5
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-16 15:57:37 +0200

    Shorten variable names

--- tests/integration/source_code/test_jobs.py
+++ tests/integration/source_code/test_jobs.py
@@ -95,14 +95,14 @@ def test_workflow_linter_lints_job_with_import_pypi_library(
     notebook = entrypoint / "notebook.ipynb"
     make_notebook(path=notebook, content=b"import pytest")
 
-    job_with_import_pytest_problem = make_job(notebook_path=notebook)
-    problems = simple_ctx.workflow_linter.lint_job(job_with_import_pytest_problem.job_id)
+    job_without_pytest_library = make_job(notebook_path=notebook)
+    problems = simple_ctx.workflow_linter.lint_job(job_without_pytest_library.job_id)
 
     assert len([problem for problem in problems if problem.message == "Could not locate import: pytest"]) > 0
 
     library = compute.Library(pypi=compute.PythonPyPiLibrary(package="pytest"))
-    job_without_import_pytest_problem = make_job(notebook_path=notebook, libraries=[library])
+    job_with_pytest_library = make_job(notebook_path=notebook, libraries=[library])
 
-    problems = simple_ctx.workflow_linter.lint_job(job_without_import_pytest_problem.job_id)
+    problems = simple_ctx.workflow_linter.lint_job(job_with_pytest_library.job_id)
 
     assert len([problem for problem in problems if problem.message == "Could not locate import: pytest"]) == 0
