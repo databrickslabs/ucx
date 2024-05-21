a5686bb1ccd9ef8f5e788f271009331e5505dddc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-21 13:56:24 +0200

    Add unit test for emitting lint problem

--- tests/unit/source_code/test_jobs.py
+++ tests/unit/source_code/test_jobs.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from unittest.mock import create_autospec
 
@@ -9,16 +10,21 @@ from databricks.sdk.service import compute, jobs
 
 from databricks.labs.ucx.source_code.files import FileLoader
 from databricks.labs.ucx.source_code.graph import Dependency, DependencyGraph, DependencyResolver
-from databricks.labs.ucx.source_code.jobs import WorkflowTaskContainer
+from databricks.labs.ucx.source_code.jobs import WorkflowLinter, WorkflowTaskContainer
 from databricks.labs.ucx.source_code.notebooks.loaders import NotebookResolver, NotebookLoader
 
 
 @pytest.fixture
-def graph(mock_path_lookup) -> DependencyGraph:
-    dependency = Dependency(FileLoader(), Path("test"))
-    dependency_resolver = DependencyResolver(
+def dependency_resolver(mock_path_lookup) -> DependencyResolver:
+    resolver = DependencyResolver(
         [PipResolver(FileLoader(), Whitelist())], NotebookResolver(NotebookLoader()), [], mock_path_lookup
     )
+    return resolver
+
+
+@pytest.fixture
+def graph(mock_path_lookup, dependency_resolver) -> DependencyGraph:
+    dependency = Dependency(FileLoader(), Path("test"))
     dependency_graph = DependencyGraph(dependency, None, dependency_resolver, mock_path_lookup)
     return dependency_graph
 
@@ -74,3 +80,22 @@ def test_workflow_task_container_builds_dependency_graph_unknown_pypi_library(mo
     assert problems[0].message.startswith("Failed to install unknown-library-name")
     assert mock_path_lookup.resolve(Path("unknown-library-name")) is None
     ws.assert_not_called()
+
+
+def test_workflow_linter_lint_job_logs_problems(dependency_resolver, mock_path_lookup, empty_index, caplog):
+    expected_message = "Found job problems:\nUNKNOWN:-1 [library-install-failed] Failed to install unknown-library"
+
+    ws = create_autospec(WorkspaceClient)
+    linter = WorkflowLinter(ws, dependency_resolver, mock_path_lookup, empty_index)
+
+    libraries = [compute.Library(pypi=compute.PythonPyPiLibrary(package="unknown-library-name"))]
+    task = jobs.Task(task_key="test-task", libraries=libraries)
+    settings = jobs.JobSettings(name="test-job", tasks=[task])
+    job = jobs.Job(job_id=1234, settings=settings)
+
+    ws.jobs.get.return_value = job
+
+    with caplog.at_level(logging.WARNING, logger="databricks.labs.ucx.source_code.jobs"):
+        linter.lint_job(1234)
+
+    assert any(message.startswith(expected_message) for message in caplog.messages)
