00bc0359059b64702a880626e0f49819a95edc06
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-22 13:45:02 +0200

    Fix query columns

--- src/databricks/labs/ucx/queries/migration/main/02_1_workflow_migration_problems.sql
+++ src/databricks/labs/ucx/queries/migration/main/02_1_workflow_migration_problems.sql
@@ -1,4 +1,4 @@
--- viz type=table, name=Workflow migration problems, search_by=path,code,job_name, columns=job_id,job_name,task_key,code,message,start_line,start_col,end_line,end_cole
+-- viz type=table, name=Workflow migration problems, search_by=path,code,job_name, columns=path,code,message,workflow_id,workflow_name,task_key,start_line,start_col,end_line,end_col
 -- widget title=Workflow migration problems, row=2, col=2, size_x=4, size_y=8
 SELECT
     path,
