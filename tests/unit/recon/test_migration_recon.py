de83f1ad1790fcee0e9870f24a41dfed76b1028a
Vuong <vuong.nguyen@databricks.com>
2024-05-23 12:50:58 +0100

    cov

--- tests/unit/recon/test_migration_recon.py
+++ tests/unit/recon/test_migration_recon.py
@@ -39,60 +39,52 @@ def test_migrate_recon_should_produce_proper_queries(
     row_count_row_factory,
     data_comp_row_factory,
 ):
-    source = TableIdentifier("hive_metastore", "db1", "table1")
-    source_2 = TableIdentifier("hive_metastore", "db2", "table2")
-    target = TableIdentifier("catalog1", "schema1", "table1")
-    target_2 = TableIdentifier("catalog2", "schema2", "table2")
+    src = TableIdentifier("hive_metastore", "db1", "table1")
+    src_2 = TableIdentifier("hive_metastore", "db2", "table2")
+    src_3 = TableIdentifier("hive_metastore", "db3", "table3")
+    tgt = TableIdentifier("catalog1", "schema1", "table1")
+    tgt_2 = TableIdentifier("catalog2", "schema2", "table2")
+    tgt_3 = TableIdentifier("catalog3", "schema3", "table3")
     errors = {}
     rows = {
         'SELECT \\* FROM inventory_database.migration_status': MIGRATION_STATUS[
-            (source.schema, source.table, target.catalog, target.schema, target.table, "2021-01-01T00:00:00Z"),
-            (
-                source_2.schema,
-                source_2.table,
-                target_2.catalog,
-                target_2.schema,
-                target_2.table,
-                "2021-01-01T00:00:00Z",
-            ),
+            (src.schema, src.table, tgt.catalog, tgt.schema, tgt.table, "2021-01-01T00:00:00Z"),
+            (src_2.schema, src_2.table, tgt_2.catalog, tgt_2.schema, tgt_2.table, "2021-01-01T00:00:00Z"),
+            (src_3.schema, src_3.table, tgt_3.catalog, tgt_3.schema, tgt_3.table, "2021-01-01T00:00:00Z"),
             ("schema_none", "table_none", None, "schema_a", "table_a", "2021-01-01T00:00:00Z"),
         ],
-        f"SHOW TBLPROPERTIES {source.schema}.{source.table} \\('upgraded_to'\\)": UPGRADED_TO[("value", "fake_dest"),],
-        f"SHOW TBLPROPERTIES {source_2.schema}.{source_2.table} \\('upgraded_to'\\)": UPGRADED_TO[
-            ("value", "fake_dest"),
-        ],
+        f"SHOW TBLPROPERTIES {src.schema}.{src.table} \\('upgraded_to'\\)": UPGRADED_TO[("value", "fake_dest"),],
+        f"SHOW TBLPROPERTIES {src_2.schema}.{src_2.table} \\('upgraded_to'\\)": UPGRADED_TO[("value", "fake_dest"),],
         "DESCRIBE TABLE": metadata_row_factory[
             ("col1", "int"),
             ("col2", "string"),
         ],
-        f"{target.catalog_escaped}\\.information_schema\\.columns": metadata_row_factory[
+        f"{tgt.catalog_escaped}\\.information_schema\\.columns": metadata_row_factory[
             ("col1", "int"),
             ("col2", "string"),
         ],
-        f"{target_2.catalog_escaped}\\.information_schema\\.columns": metadata_row_factory[
+        f"{tgt_2.catalog_escaped}\\.information_schema\\.columns": metadata_row_factory[
             ("col1", "int"),
             ("col2", "string"),
         ],
-        f"SELECT COUNT\\(\\*\\) as row_count FROM {source.fqn_escaped}": row_count_row_factory[100,],
-        f"SELECT COUNT\\(\\*\\) as row_count FROM {target.fqn_escaped}": row_count_row_factory[2,],
-        f"SELECT COUNT\\(\\*\\) as row_count FROM {source_2.fqn_escaped}": row_count_row_factory[0,],
-        f"SELECT COUNT\\(\\*\\) as row_count FROM {target_2.fqn_escaped}": row_count_row_factory[0,],
+        f"SELECT COUNT\\(\\*\\) as row_count FROM {src.fqn_escaped}": row_count_row_factory[100,],
+        f"SELECT COUNT\\(\\*\\) as row_count FROM {tgt.fqn_escaped}": row_count_row_factory[2,],
+        f"SELECT COUNT\\(\\*\\) as row_count FROM {src_2.fqn_escaped}": row_count_row_factory[0,],
+        f"SELECT COUNT\\(\\*\\) as row_count FROM {tgt_2.fqn_escaped}": row_count_row_factory[0,],
         "WITH compare_results": data_comp_row_factory[(102, 100, 2),],
     }
     backend = MockBackend(fails_on_first=errors, rows=rows)
     table_crawler = TablesCrawler(backend, "inventory_database")
     migration_status_refresher = MigrationStatusRefresher(ws, backend, "inventory_database", table_crawler)
     metadata_retriever = DatabricksTableMetadataRetriever(backend)
-    schema_comparator = StandardSchemaComparator(metadata_retriever)
     data_profiler = StandardDataProfiler(backend, metadata_retriever)
-    data_comparator = StandardDataComparator(backend, data_profiler)
     migration_recon = MigrationRecon(
         backend,
         "inventory_database",
         migration_status_refresher,
         mock_table_mapping(["managed_dbfs", "managed_mnt", "managed_other"]),
-        schema_comparator,
-        data_comparator,
+        StandardSchemaComparator(metadata_retriever),
+        StandardDataComparator(backend, data_profiler),
         0,
     )
     results = list(migration_recon.snapshot())
