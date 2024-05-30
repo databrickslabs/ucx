3db04a62e9722bdf75aa47986389f9280789db8c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-05-30 09:03:01 +0200

    Fix rebase

--- src/databricks/labs/ucx/source_code/python_libraries.py
+++ src/databricks/labs/ucx/source_code/python_libraries.py
@@ -1,26 +1,20 @@
 from __future__ import annotations
 
 import logging
-import os
-import subprocess
 import tempfile
+from collections.abc import Callable
+from functools import cached_property
 from pathlib import Path
-from subprocess import CalledProcessError
-
-# mypy can not analyze setuptools
-from setuptools import setup  # type: ignore
+from setuptools import setup
 
 from databricks.labs.blueprint.entrypoint import is_in_debug
-from databricks.labs.ucx.source_code.files import FileLoader
+from databricks.labs.ucx.framework.utils import run_command
 from databricks.labs.ucx.source_code.graph import (
     LibraryResolver,
     DependencyProblem,
 )
 from databricks.labs.ucx.source_code.path_lookup import PathLookup
 from databricks.labs.ucx.source_code.known import Whitelist
-
-logger = logging.getLogger(__name__)
-
 
 logger = logging.getLogger(__name__)
 
@@ -33,90 +27,17 @@ class PipResolver(LibraryResolver):
         self._whitelist = whitelist
         self._runner = runner
 
-    def resolve_library(self, path_lookup: PathLookup, library: Path) -> MaybeDependency:
-        library_path = self._locate_library(path_lookup, library)
-        if library_path is None or library_path.suffix == ".egg":  # not installed yet
-            return self._install_library(path_lookup, library)
-        dist_info_path = self._locate_dist_info(library_path, library)
-        if dist_info_path is None:  # old package style
-            problem = DependencyProblem('no-dist-info', f"No dist-info found for {library.name}")
-            return MaybeDependency(None, [problem])
-        return self._create_dependency(library, dist_info_path)
+    def register_library(self, path_lookup: PathLookup, library: Path) -> list[DependencyProblem]:
+        """We delegate to pip to install the library and augment the path look-up to resolve the library at import.
+        This gives us the flexibility to install any library that is not in the whitelist, and we don't have to
+        bother about parsing cross-version dependencies in our code."""
+        compatibility = self._whitelist.distribution_compatibility(library.name)
+        if compatibility.known:
+            return compatibility.problems
+        return self._install_library(path_lookup, library)
 
-    def _locate_library(self, path_lookup: PathLookup, library: Path) -> Path | None:
-        # start the quick way
-        full_path = path_lookup.resolve(library)
-        if full_path is not None:
-            return full_path
-        # maybe the name needs tweaking
-        if "-" in library.name:
-            name = library.name.replace("-", "_")
-            return self._locate_library(path_lookup, Path(name))
-        return None
-
-    def _locate_dist_info(self, library_path: Path, library: Path) -> Path | None:
-        if not library_path.parent.exists():
-            return None
-        dist_info_dir = None
-        for package in library_path.parent.iterdir():
-            if package.name.startswith(library.name) and package.name.endswith(".dist-info"):
-                dist_info_dir = package
-                break  # TODO: Matching multiple .dist-info's, https://github.com/databrickslabs/ucx/issues/1751
-        if dist_info_dir is not None:
-            return dist_info_dir
-        # maybe the name needs tweaking
-        if "-" in library.name:
-            name = library.name.replace("-", "_")
-            return self._locate_dist_info(library_path, Path(name))
-        return None
-
-    def _install_library(self, path_lookup: PathLookup, library: Path) -> MaybeDependency:
-        """Pip install library and augment path look-up to resolve the library at import"""
-        # invoke pip install via subprocess to install this library into self._lib_install_folder
-        venv = self._temporary_virtual_environment(path_lookup).as_posix()
-        existing_packages = os.listdir(venv)
-        if library.suffix == ".egg":
-            verbosity = "--verbose" if is_in_debug() else "--quiet"
-            easy_install_arguments = [
-                "easy_install",
-                verbosity,
-                "--always-unzip",
-                "--install-dir",
-                venv,
-                library.as_posix(),
-            ]
-            try:
-                setup(script_args=easy_install_arguments)
-            except SystemExit as e:
-                problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {e}")
-                return MaybeDependency(None, [problem])
-        else:
-            try:
-                subprocess.run(["pip", "install", library.name, "-t", venv], check=True)
-            except CalledProcessError as e:
-                problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {e}")
-                return MaybeDependency(None, [problem])
-        added_packages = set(os.listdir(venv)).difference(existing_packages)
-        dist_info_dirs = list(filter(lambda package: package.endswith(".dist-info"), added_packages))
-        dist_info_dir = next((dir for dir in dist_info_dirs if dir.startswith(library.name)), None)
-        if dist_info_dir is None and "-" in library.name:
-            name = library.name.replace("-", "_")
-            dist_info_dir = next((dir for dir in dist_info_dirs if dir.startswith(name)), None)
-        if dist_info_dir is None:
-            # TODO handle other distribution types
-            problem = DependencyProblem('no-dist-info', f"No dist-info found for {library.name}")
-            return MaybeDependency(None, [problem])
-        dist_info_path = path_lookup.resolve(Path(dist_info_dir))
-        assert dist_info_path is not None
-        return self._create_dependency(library, dist_info_path)
-
-    def _create_dependency(self, library: Path, dist_info_path: Path):
-        package = DistInfo(dist_info_path)
-        container = DistInfoContainer(self._file_loader, self._white_list, package)
-        dependency = Dependency(WrappingLoader(container), library)
-        return MaybeDependency(dependency, [])
-
-    def _temporary_virtual_environment(self, path_lookup: PathLookup) -> Path:
+    @cached_property
+    def _temporary_virtual_environment(self):
         # TODO: for `databricks labs ucx lint-local-code`, detect if we already have a virtual environment
         # and use that one. See Databricks CLI code for the labs command to see how to detect the virtual
         # environment. If we don't have a virtual environment, create a temporary one.
@@ -132,11 +53,28 @@ class PipResolver(LibraryResolver):
         maybe_library = path_lookup.resolve(library)
         if maybe_library is not None:
             library = maybe_library
-        return_code, stdout, stderr = self._runner(f"pip install {library} -t {venv}")
-        logger.debug(f"pip output:\n{stdout}\n{stderr}")
-        if return_code != 0:
-            problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {stderr}")
-            return [problem]
+
+        if library.suffix == ".egg":
+            verbosity = "--verbose" if is_in_debug() else "--quiet"
+            easy_install_arguments = [
+                "easy_install",
+                verbosity,
+                "--always-unzip",
+                "--install-dir",
+                venv.as_posix(),
+                library.as_posix(),
+            ]
+            try:
+                setup(script_args=easy_install_arguments)
+            except SystemExit as e:
+                problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {e}")
+                return [problem]
+        else:
+            return_code, stdout, stderr = self._runner(f"pip install {library} -t {venv}")
+            logger.debug(f"pip output:\n{stdout}\n{stderr}")
+            if return_code != 0:
+                problem = DependencyProblem("library-install-failed", f"Failed to install {library}: {stderr}")
+                return [problem]
         return []
 
     def __str__(self) -> str:
