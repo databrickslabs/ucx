612d3e2d372a0304f5b6881af7dd283f7ddd4b8a
Vsevolod Stepanov <vsevolod.stepanov@databricks.com>
2024-05-30 19:41:35 +0200

    Add linters to check for spark logging and configuration access (#1808)
    ## Changes
    Add new linters to check for uses of spark logging, `sc.conf` access and
    `rdd.mapPartitions`
    
    ### Linked issues
    Resolves [#1805](https://github.com/databrickslabs/ucx/issues/1805),
    enhances [#1603](https://github.com/databrickslabs/ucx/issues/1603),
    [#1604](https://github.com/databrickslabs/ucx/issues/1604)
    
    ### Functionality 
    
    - [ ] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs ucx ...`
    - [ ] added a new workflow
    - [ ] modified existing workflow: `...`
    - [ ] added a new table
    - [ ] modified existing table: `...`
    
    ### Tests
    <!-- How is this tested? Please see the checklist below and also
    describe any other relevant tests -->
    
    - [x] manually tested
    - [x] added unit tests
    - [ ] added integration tests
    - [ ] verified on staging environment (screenshot attached)

--- src/databricks/labs/ucx/source_code/spark_connect.py
+++ src/databricks/labs/ucx/source_code/spark_connect.py
@@ -68,15 +68,37 @@ class RDDApiMatcher(SharedClusterMatcher):
     ]
 
     def lint(self, node: ast.AST) -> Iterator[Advice]:
-        if not isinstance(node, ast.Call):
+        yield from self._lint_sc(node)
+        yield from self._lint_rdd_use(node)
+
+    def _lint_rdd_use(self, node: ast.AST) -> Iterator[Advice]:
+        if isinstance(node, ast.Attribute):
+            if node.attr == 'rdd':
+                yield self._rdd_failure(node)
+                return
+        if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute) and node.func.attr == 'mapPartitions':
+            yield Failure(
+                code='rdd-in-shared-clusters',
+                message=f'RDD APIs are not supported on {self._cluster_type_str()}. '
+                f'Use mapInArrow() or Pandas UDFs instead',
+                start_line=node.lineno,
+                start_col=node.col_offset,
+                end_line=node.end_lineno or 0,
+                end_col=node.end_col_offset or 0,
+            )
+
+    def _lint_sc(self, node: ast.AST) -> Iterator[Advice]:
+        if not isinstance(node, ast.Call) or not isinstance(node.func, ast.Attribute):
             return
-        assert isinstance(node.func, ast.Attribute)  # Avoid linter warning
         if node.func.attr not in self._SC_METHODS:
             return
         function_name = AstHelper.get_full_function_name(node)
         if not function_name or not function_name.endswith(f"sc.{node.func.attr}"):
             return
-        yield Failure(
+        yield self._rdd_failure(node)
+
+    def _rdd_failure(self, node: ast.AST) -> Advice:
+        return Failure(
             code='rdd-in-shared-clusters',
             message=f'RDD APIs are not supported on {self._cluster_type_str()}. Rewrite it using DataFrame API',
             start_line=node.lineno,
@@ -88,20 +110,78 @@ class RDDApiMatcher(SharedClusterMatcher):
 
 class SparkSqlContextMatcher(SharedClusterMatcher):
     _ATTRIBUTES = ["sc", "sqlContext", "sparkContext"]
+    _KNOWN_REPLACEMENTS = {"getConf": "conf", "_conf": "conf"}
 
     def lint(self, node: ast.AST) -> Iterator[Advice]:
         if not isinstance(node, ast.Attribute):
             return
-        if not isinstance(node.value, ast.Name) or node.value.id not in SparkSqlContextMatcher._ATTRIBUTES:
-            return
-        yield Failure(
+
+        if isinstance(node.value, ast.Name) and node.value.id in SparkSqlContextMatcher._ATTRIBUTES:
+            yield self._get_advice(node, node.value.id)
+        # sparkContext can be an attribute as in df.sparkContext.getConf()
+        if isinstance(node.value, ast.Attribute) and node.value.attr == 'sparkContext':
+            yield self._get_advice(node, node.value.attr)
+
+    def _get_advice(self, node: ast.Attribute, name: str) -> Advice:
+        if node.attr in SparkSqlContextMatcher._KNOWN_REPLACEMENTS:
+            replacement = SparkSqlContextMatcher._KNOWN_REPLACEMENTS[node.attr]
+            return Failure(
+                code='legacy-context-in-shared-clusters',
+                message=f'{name} and {node.attr} are not supported on {self._cluster_type_str()}. '
+                f'Rewrite it using spark.{replacement}',
+                start_line=node.lineno,
+                start_col=node.col_offset,
+                end_line=node.end_lineno or 0,
+                end_col=node.end_col_offset or 0,
+            )
+        return Failure(
             code='legacy-context-in-shared-clusters',
-            message=f'{node.value.id} is not supported on {self._cluster_type_str()}. Rewrite it using spark',
+            message=f'{name} is not supported on {self._cluster_type_str()}. Rewrite it using spark',
             start_line=node.lineno,
             start_col=node.col_offset,
             end_line=node.end_lineno or 0,
             end_col=node.end_col_offset or 0,
         )
+
+
+class LoggingMatcher(SharedClusterMatcher):
+    def lint(self, node: ast.AST) -> Iterator[Advice]:
+        yield from self._match_sc_set_log_level(node)
+        yield from self._match_jvm_log(node)
+
+    def _match_sc_set_log_level(self, node: ast.AST) -> Iterator[Advice]:
+        if not isinstance(node, ast.Call) or not isinstance(node.func, ast.Attribute):
+            return
+        if node.func.attr != 'setLogLevel':
+            return
+        function_name = AstHelper.get_full_function_name(node)
+        if not function_name or not function_name.endswith('sc.setLogLevel'):
+            return
+
+        yield Failure(
+            code='spark-logging-in-shared-clusters',
+            message=f'Cannot set Spark log level directly from code on {self._cluster_type_str()}. '
+            f'Remove the call and set the cluster spark conf \'spark.log.level\' instead',
+            start_line=node.lineno,
+            start_col=node.col_offset,
+            end_line=node.end_lineno or 0,
+            end_col=node.end_col_offset or 0,
+        )
+
+    def _match_jvm_log(self, node: ast.AST) -> Iterator[Advice]:
+        if not isinstance(node, ast.Attribute):
+            return
+        attribute_name = AstHelper.get_full_attribute_name(node)
+        if attribute_name and attribute_name.endswith('org.apache.log4j'):
+            yield Failure(
+                code='spark-logging-in-shared-clusters',
+                message=f'Cannot access Spark Driver JVM logger on {self._cluster_type_str()}. '
+                f'Use logging.getLogger() instead',
+                start_line=node.lineno,
+                start_col=node.col_offset,
+                end_line=node.end_lineno or 0,
+                end_col=node.end_col_offset or 0,
+            )
 
 
 class SparkConnectLinter(Linter):
@@ -110,6 +190,7 @@ class SparkConnectLinter(Linter):
             JvmAccessMatcher(is_serverless=is_serverless),
             RDDApiMatcher(is_serverless=is_serverless),
             SparkSqlContextMatcher(is_serverless=is_serverless),
+            LoggingMatcher(is_serverless=is_serverless),
         ]
 
     def lint(self, code: str) -> Iterator[Advice]:
