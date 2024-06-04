4cb155e8ba7b3a323137a3ad198bb64c98f697fa
Andrew Snare <andrew.snare@xebia.com>
2024-06-04 12:39:41 +0200

    Handle some false-positives differently.

--- tests/unit/source_code/samples/functional/pyspark/catalog/spark-catalog-create-table.py
+++ tests/unit/source_code/samples/functional/pyspark/catalog/spark-catalog-create-table.py
@@ -12,6 +12,7 @@ for i in range(10):
     do_stuff_with(df)
 
     ## Check that a call with too many positional arguments is ignored as (presumably) something else; we expect no warning.
-    ## DISABLED: failing test due to error in matching specification: only 5 positional args are allowed with unlimited keyword args.
-    #df = spark.catalog.createTable("old.things", None, None, None, None, "extra-argument")
+    # FIXME: This is a false positive due to an error in the matching specification; only 5 positional args are allowed.
+    # ucx[table-migrate:+1:0:+1:0] Table old.things is migrated to brand.new.stuff in Unity Catalog
+    df = spark.catalog.createTable("old.things", None, None, None, None, "extra-argument")
     do_stuff_with(df)
