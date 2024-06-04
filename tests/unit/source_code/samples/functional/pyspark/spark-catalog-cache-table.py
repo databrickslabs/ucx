040ebb220e90620cf067e84e8ca60241e96199eb
Andrew Snare <andrew.snare@xebia.com>
2024-06-04 09:58:34 +0200

    Remove some unrelated code from the sample.

--- tests/unit/source_code/samples/functional/pyspark/spark-catalog-cache-table.py
+++ tests/unit/source_code/samples/functional/pyspark/spark-catalog-cache-table.py
@@ -1,13 +1,9 @@
-# ucx[direct-filesystem-access:+1:0:+1:0] The use of direct filesystem references is deprecated: s3://bucket/path
-spark.read.csv("s3://bucket/path")
-for i in range(10):
+## Check a literal reference to a known table that is migrated.
+# ucx[table-migrate:+1:0:+1:0] Table old.things is migrated to brand.new.stuff in Unity Catalog
+spark.catalog.cacheTable("old.things")
 
-    ## Check a literal reference to a known table that is migrated.
-    # ucx[table-migrate:+1:0:+1:0] Table old.things is migrated to brand.new.stuff in Unity Catalog
-    spark.catalog.cacheTable("old.things")
+## Check a literal reference to an unknown table (that is not migrated); we expect no warning.
+spark.catalog.cacheTable("table.we.know.nothing.about")
 
-    ## Check a literal reference to an unknown table (that is not migrated); we expect no warning.
-    spark.catalog.cacheTable("table.we.know.nothing.about")
-
-    ## Check that a call with too many positional arguments is ignored as (presumably) something else; we expect no warning.
-    spark.catalog.cacheTable("old.things", None, "extra-argument")
+## Check that a call with too many positional arguments is ignored as (presumably) something else; we expect no warning.
+spark.catalog.cacheTable("old.things", None, "extra-argument")
