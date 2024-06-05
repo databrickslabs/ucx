7cf27403713993543d9a78819e32b97578312f47
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-06-05 18:52:54 +0200

    Fix crasher

--- src/databricks/labs/ucx/source_code/linters/ast_helpers.py
+++ src/databricks/labs/ucx/source_code/linters/ast_helpers.py
@@ -1,10 +1,14 @@
+import logging
+
 from astroid import Attribute, Call, Name  # type: ignore
+
+logger = logging.getLogger(__file__)
 
 
 class AstHelper:
     @classmethod
     def get_full_attribute_name(cls, node: Attribute) -> str:
-        return AstHelper._get_attribute_value(node)
+        return cls._get_attribute_value(node)
 
     @classmethod
     def get_full_function_name(cls, node: Call) -> str | None:
@@ -21,8 +25,10 @@ class AstHelper:
         if isinstance(node.expr, Name):
             return node.expr.name + '.' + node.attrname
         if isinstance(node.expr, Attribute):
-            return cls._get_attribute_value(node.expr) + '.' + node.attrname
+            parent = cls._get_attribute_value(node.expr)
+            return node.attrname if parent is None else parent + '.' + node.attrname
         if isinstance(node.expr, Call):
             name = cls.get_full_function_name(node.expr)
             return node.attrname if name is None else name + '.' + node.attrname
+        # logger.error(f"Missing handler for {type(node.expr).__name__}")
         return None
