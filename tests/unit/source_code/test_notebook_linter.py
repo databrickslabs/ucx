c5c8a59fdd2929e5d39dceba18bc9ce683b67837
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-06-05 14:32:24 +0200

    remove whitespace

--- tests/unit/source_code/test_notebook_linter.py
+++ tests/unit/source_code/test_notebook_linter.py
@@ -476,7 +476,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
             [
                 Deprecation(
                     code='table-migrate',
-                    message='Table people is migrated to cata4.nondefault.newpeople ' 'in Unity Catalog',
+                    message='Table people is migrated to cata4.nondefault.newpeople in Unity Catalog',
                     start_line=6,
                     start_col=8,
                     end_line=6,
@@ -484,7 +484,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Advice(
                     code='table-migrate',
-                    message='The default format changed in Databricks Runtime 8.0, from ' 'Parquet to Delta',
+                    message='The default format changed in Databricks Runtime 8.0, from Parquet to Delta',
                     start_line=6,
                     start_col=8,
                     end_line=6,
@@ -492,7 +492,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Deprecation(
                     code='table-migrate',
-                    message='Table persons is migrated to cata4.newsomething.persons ' 'in Unity Catalog',
+                    message='Table persons is migrated to cata4.newsomething.persons in Unity Catalog',
                     start_line=14,
                     start_col=8,
                     end_line=14,
@@ -500,7 +500,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Advice(
                     code='table-migrate',
-                    message='The default format changed in Databricks Runtime 8.0, from ' 'Parquet to Delta',
+                    message='The default format changed in Databricks Runtime 8.0, from Parquet to Delta',
                     start_line=14,
                     start_col=8,
                     end_line=14,
@@ -508,7 +508,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Deprecation(
                     code='table-migrate',
-                    message='Table kittens is migrated to cata4.felines.toms in Unity ' 'Catalog',
+                    message='Table kittens is migrated to cata4.felines.toms in Unity Catalog',
                     start_line=22,
                     start_col=8,
                     end_line=22,
@@ -516,7 +516,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Advice(
                     code='table-migrate',
-                    message='The default format changed in Databricks Runtime 8.0, from ' 'Parquet to Delta',
+                    message='The default format changed in Databricks Runtime 8.0, from Parquet to Delta',
                     start_line=22,
                     start_col=8,
                     end_line=22,
@@ -524,7 +524,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Deprecation(
                     code='table-migrate',
-                    message='Table numbers is migrated to cata4.counting.numbers in ' 'Unity Catalog',
+                    message='Table numbers is migrated to cata4.counting.numbers in Unity Catalog',
                     start_line=26,
                     start_col=0,
                     end_line=26,
@@ -532,7 +532,7 @@ spark.range(10).saveAsTable('numbers') # we are saving to whatever.numbers table
                 ),
                 Advice(
                     code='table-migrate',
-                    message='The default format changed in Databricks Runtime 8.0, from ' 'Parquet to Delta',
+                    message='The default format changed in Databricks Runtime 8.0, from Parquet to Delta',
                     start_line=26,
                     start_col=0,
                     end_line=26,
