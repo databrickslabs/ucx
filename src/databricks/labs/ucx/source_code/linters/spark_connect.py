f988c6dda54f57157f3c89f51e87e7b055cd25a9
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-06-05 14:36:37 +0200

    fix regression

--- src/databricks/labs/ucx/source_code/linters/spark_connect.py
+++ src/databricks/labs/ucx/source_code/linters/spark_connect.py
@@ -23,6 +23,15 @@ class SharedClusterMatcher:
     def lint(self, node: NodeNG) -> Iterator[Advice]:
         pass
 
+    def lint_tree(self, tree: NodeNG) -> Iterator[Advice]:
+        reported_locations = set()
+        for node in TreeWalker.walk(tree):
+            for advice in self.lint(node):
+                loc = (advice.start_line, advice.start_col)
+                if loc not in reported_locations:
+                    reported_locations.add(loc)
+                    yield advice
+
 
 class JvmAccessMatcher(SharedClusterMatcher):
     _FIELDS = [
