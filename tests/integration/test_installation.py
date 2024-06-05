f2661793195a0d96f7190923667cba0aa9adcf17
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-06-05 19:36:04 +0200

    Add retry annotations

--- tests/integration/test_installation.py
+++ tests/integration/test_installation.py
@@ -20,6 +20,7 @@ from databricks.sdk.errors import (
     AlreadyExists,
     InvalidParameterValue,
     NotFound,
+    ResourceConflict,
 )
 
 from databricks.sdk.retries import retried
@@ -90,6 +91,7 @@ def new_installation(ws, env_or_skip, make_random):
         pending.remove()
 
 
+@retried(on=[NotFound, ResourceConflict], timeout=timedelta(minutes=2))
 def test_experimental_permissions_migration_for_group_with_same_name(
     installation_ctx,
     make_cluster_policy,
