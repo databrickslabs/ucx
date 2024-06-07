5211d3276a52c54e6162d92272157ef629c1a969
Andrew Snare <asnare@users.noreply.github.com>
2024-06-07 17:52:58 +0200

    Check offsets of expected warnings in functional linter tests (#1843)
    ## Changes
    
    This PR is stacked on #1855 and includes the following changes:
    
    - Updates the fixtures for the functional linter tests so that the
    expected offsets are correct.
    - Updates the functional tests to also validate the region offsets are
    as expected.
    - The test itself has also been updated so that test failures are
    reported as lists of missing/unexpected comments in the form used by the
    functional tests.
    
    ### Tests
    
    - [ ] manually tested
    - [X] added unit tests
    - [ ] added integration tests
    - [ ] verified on staging environment (screenshot attached)

--- tests/unit/source_code/samples/functional/pyspark/catalog/spark-catalog-get-table.py
+++ tests/unit/source_code/samples/functional/pyspark/catalog/spark-catalog-get-table.py
@@ -1,9 +1,9 @@
-# ucx[direct-filesystem-access:+1:0:+1:0] The use of direct filesystem references is deprecated: s3://bucket/path
+# ucx[direct-filesystem-access:+1:0:+1:34] The use of direct filesystem references is deprecated: s3://bucket/path
 spark.read.csv("s3://bucket/path")
 for i in range(10):
 
     ## Check a literal reference to a known table that is migrated.
-    # ucx[table-migrate:+1:0:+1:0] Table old.things is migrated to brand.new.stuff in Unity Catalog
+    # ucx[table-migrate:+1:12:+1:48] Table old.things is migrated to brand.new.stuff in Unity Catalog
     table = spark.catalog.getTable("old.things")
     do_stuff_with(table)
 
@@ -16,15 +16,15 @@ for i in range(10):
     do_stuff_with(table)
 
     ## Some calls that use a variable whose value is unknown: they could potentially reference a migrated table.
-    # ucx[table-migrate:+1:0:+1:0] Can't migrate 'getTable' because its table name argument is not a constant
+    # ucx[table-migrate:+1:12:+1:40] Can't migrate 'getTable' because its table name argument is not a constant
     table = spark.catalog.getTable(name)
     do_stuff_with(table)
-    # ucx[table-migrate:+1:0:+1:0] Can't migrate 'getTable' because its table name argument is not a constant
+    # ucx[table-migrate:+1:12:+1:50] Can't migrate 'getTable' because its table name argument is not a constant
     table = spark.catalog.getTable(f"boop{stuff}")
     do_stuff_with(table)
 
     ## Some trivial references to the method or table in unrelated contexts that should not trigger warnigns.
     # FIXME: This is a false positive; any method named 'getTable' is triggering the warning.
-    # ucx[table-migrate:+1:0:+1:0] Table old.things is migrated to brand.new.stuff in Unity Catalog
+    # ucx[table-migrate:+1:4:+1:41] Table old.things is migrated to brand.new.stuff in Unity Catalog
     something_else.getTable("old.things")
     a_function("old.things")
