1c8144d2f10c8dbfe732ac84e5d37b7f6b691af9
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-07 13:42:05 +0200

    Refactor pip_resolver to python_library_resolver

--- tests/unit/source_code/test_python_libraries.py
+++ tests/unit/source_code/test_python_libraries.py
@@ -5,23 +5,23 @@ from databricks.labs.ucx.source_code.python_libraries import PythonLibraryResolv
 from databricks.labs.ucx.source_code.known import Whitelist
 
 
-def test_pip_resolver_resolves_library(mock_path_lookup):
+def test_python_library_resolver_resolves_library(mock_path_lookup):
     def mock_pip_install(command):
         assert command.startswith("pip install anything -t")
         return 0, "", ""
 
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
-    problems = pip_resolver.register_library(mock_path_lookup, "anything")
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    problems = python_library_resolver.register_library(mock_path_lookup, "anything")
 
     assert len(problems) == 0
 
 
-def test_pip_resolver_failing(mock_path_lookup):
+def test_python_library_resolver_failing(mock_path_lookup):
     def mock_pip_install(_):
         return 1, "", "nope"
 
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
-    problems = pip_resolver.register_library(mock_path_lookup, "anything")
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    problems = python_library_resolver.register_library(mock_path_lookup, "anything")
 
     assert len(problems) == 1
     assert problems[0].code == "library-install-failed"
@@ -29,52 +29,52 @@ def test_pip_resolver_failing(mock_path_lookup):
     assert problems[0].message.endswith("nope'")
 
 
-def test_pip_resolver_adds_to_path_lookup_only_once():
+def test_python_library_resolver_adds_to_path_lookup_only_once():
     def mock_pip_install(_):
         return 0, "", ""
 
     path_lookup = create_autospec(PathLookup)
     path_lookup.resolve.return_value = None
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
 
-    problems = pip_resolver.register_library(path_lookup, "library")
+    problems = python_library_resolver.register_library(path_lookup, "library")
     assert len(problems) == 0
-    problems = pip_resolver.register_library(path_lookup, "library2")
+    problems = python_library_resolver.register_library(path_lookup, "library2")
     assert len(problems) == 0
 
-    venv = pip_resolver._temporary_virtual_environment  # pylint: disable=protected-access
+    venv = python_library_resolver._temporary_virtual_environment  # pylint: disable=protected-access
     path_lookup.append_path.assert_has_calls([call(venv), call(venv)])
 
 
-def test_pip_resolver_resolves_library_with_known_problems(mock_path_lookup):
+def test_python_library_resolver_resolves_library_with_known_problems(mock_path_lookup):
     def mock_pip_install(_):
         return 0, "", ""
 
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
-    problems = pip_resolver.register_library(mock_path_lookup, "boto3==1.17.0")
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    problems = python_library_resolver.register_library(mock_path_lookup, "boto3==1.17.0")
 
     assert len(problems) == 1
     assert problems[0].code == "direct-filesystem-access"
 
 
-def test_pip_resolver_installs_with_command(mock_path_lookup):
+def test_python_library_resolver_installs_with_command(mock_path_lookup):
     def mock_pip_install(_):
         return 0, "", ""
 
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
-    problems = pip_resolver.register_library(
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    problems = python_library_resolver.register_library(
         mock_path_lookup, "library.whl", installation_arguments=["library.whl", "--verbose"]
     )
 
     assert len(problems) == 0
 
 
-def test_pip_resolver_warns_when_install_command_misses_library(mock_path_lookup):
+def test_python_library_resolver_warns_when_install_command_misses_library(mock_path_lookup):
     def mock_pip_install(_):
         return 0, "", ""
 
-    pip_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
-    problems = pip_resolver.register_library(
+    python_library_resolver = PythonLibraryResolver(Whitelist(), mock_pip_install)
+    problems = python_library_resolver.register_library(
         mock_path_lookup, "library.whl", installation_arguments=["other_library.whl", "--verbose"]
     )
 
