b5bfb80db53628982e99200e7fec73f84dd7816c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-07 10:53:12 +0200

    Format

--- tests/integration/source_code/test_libraries.py
+++ tests/integration/source_code/test_libraries.py
@@ -17,7 +17,7 @@ from tests.unit.conftest import MockPathLookup
         "pip_install_demo_wheel_and_pytest",
         "pip_install_demo_wheel_many_flags",
         "pip_install_demo_wheel_with_target_directory",
-    )
+    ),
 )
 def test_build_notebook_dependency_graphs_installs_wheel_with_pip_cell_in_notebook(simple_ctx, notebook):
     ctx = simple_ctx.replace(path_lookup=MockPathLookup())
@@ -29,7 +29,7 @@ def test_build_notebook_dependency_graphs_installs_wheel_with_pip_cell_in_notebo
 
 
 @pytest.mark.xfail(reason="Spaces in path are not handled by subprocess when quoted")
-@pytest.mark.parametrize("notebook", ("pip_install_demo_wheel_with_spaces_in_target_directory", ))
+@pytest.mark.parametrize("notebook", ("pip_install_demo_wheel_with_spaces_in_target_directory",))
 def test_build_notebook_dependency_graphs_fails_installing_when_spaces(simple_ctx, notebook):
     ctx = simple_ctx.replace(path_lookup=MockPathLookup())
 
