9065467ab40f6296a2f477832206dcd8423dd658
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-06-10 16:39:27 +0200

    fix failing acceptance test

--- tests/integration/source_code/test_jobs.py
+++ tests/integration/source_code/test_jobs.py
@@ -114,8 +114,8 @@ def test_job_linter_some_notebook_graph_with_problems(simple_ctx, ws, make_job, 
     expected_messages = {
         "second_notebook:3 [direct-filesystem-access] The use of default dbfs: references is deprecated: /mnt/something",
         "some_file.py:0 [direct-filesystem-access] The use of default dbfs: references is deprecated: /mnt/foo/bar",
-        "some_file.py:0 [dbfs-usage] Deprecated file system path in call to: /mnt/foo/bar",
-        "second_notebook:3 [dbfs-usage] Deprecated file system path in call to: /mnt/something",
+        "some_file.py:0 [dbfs-usage] Deprecated file system path: /mnt/foo/bar",
+        "second_notebook:3 [dbfs-usage] Deprecated file system path: /mnt/something",
     }
 
     entrypoint = WorkspacePath(ws, f"~/linter-{make_random(4)}").expanduser()
