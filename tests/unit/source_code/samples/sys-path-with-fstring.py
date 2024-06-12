1a0cdf7cc9d1739326385f491170b663b32a8584
Serge Smertin <serge.smertin@databricks.com>
2024-06-11 20:07:50 -0700

    ..

--- tests/unit/source_code/samples/sys-path-with-fstring.py
+++ tests/unit/source_code/samples/sys-path-with-fstring.py
@@ -3,6 +3,6 @@ import sys
 name_1 = "whatever"
 name_2 = f"{name_1}"
 sys.path.append(f"{name_2}")
-names = [ f"{name_2}", name_2 ]
+names = [f"{name_2}", name_2]
 for name in names:
     sys.path.append(name)
