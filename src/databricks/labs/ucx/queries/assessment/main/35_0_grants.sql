8375de43848b7cdd16ab6394dfa3d79fe7684176
Andrew Snare <andrew.snare@databricks.com>
2024-06-13 17:34:14 +0200

    New assessment pane for showing problems with permissions that cannot be migrated.

---
+++ src/databricks/labs/ucx/queries/assessment/main/35_0_grants.sql
@@ -0,0 +1,15 @@
+-- viz type=table, name=Grants, columns=finding, principal,action_type,catalog,database,object_type,object_id,principal
+-- widget title=Incompatible Object Privileges, row=41, col=0, size_x=6, size_y=8
+SELECT
+    EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
+    action_type,
+    catalog,
+    database,
+    object_type,
+    object_id,
+    principal,
+    principal_type
+FROM $inventory.grant_detail
+WHERE startswith(action_type, 'DENIED_')
+ORDER BY
+    catalog, database, object_id, object_type, action_type, principal, principal_type
--- src/databricks/labs/ucx/queries/assessment/main/40___last_summary.md
+++ src/databricks/labs/ucx/queries/assessment/main/40___last_summary.md
@@ -1,6 +1,7 @@
 -- widget title=Assessment overview, row=40, col=2, size_x=8, size_y=3
 
 ## Other
+- Object permissions
 - Delta Live Tables
 - Global Init Scripts
 - Warning messages
