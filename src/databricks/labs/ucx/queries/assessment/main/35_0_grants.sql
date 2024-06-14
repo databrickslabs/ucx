425c49fb48ce4cfd949188c08e2905689ece686b
Andrew Snare <asnare@users.noreply.github.com>
2024-06-14 22:21:32 +0200

    Include findings on `DENY` grants during assessment (#1903)
    ## Changes
    
    This PR adds support for flagging DENY permissions on objects, which
    cannot be migrated to UC.
    
    It also adds some new integration tests for existing grant-scanning.
    
    ### Linked issues
    
    Resolves #1869, supersedes #1890.
    
    ### Functionality 
    
    - [x] added relevant user documentation
    - [ ] added new CLI command
    - [ ] modified existing command: `databricks labs ucx ...`
    - [ ] added a new workflow
    - [X] modified existing workflow: `assessment`
    - [ ] added a new table
    - [ ] modified existing table: `...`
    - [x] modified existing view: `grant_detail`
    
    ### Tests
    
    - [x] manually tested
    - [ ] added unit tests
    - [X] added integration tests
    - [x] verified on staging environment (screenshot attached)

---
+++ src/databricks/labs/ucx/queries/assessment/main/35_0_grants.sql
@@ -0,0 +1,13 @@
+-- viz type=table, name=Grants, columns=finding,action_type,object_type,object_id,principal,principal_type
+-- widget title=Incompatible Object Privileges, row=41, col=0, size_x=6, size_y=8
+SELECT
+    EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
+    action_type,
+    object_type,
+    object_id,
+    principal,
+    principal_type
+FROM $inventory.grant_detail
+WHERE startswith(action_type, 'DENIED_')
+ORDER BY
+    object_id, object_type, action_type, principal, principal_type
