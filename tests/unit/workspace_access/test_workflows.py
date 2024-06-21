6710a13632f3428ce6eab203767084afbe541255
Andrew Snare <andrew.snare@databricks.com>
2024-06-21 13:14:29 +0200

    Test handling of permission-duplication problems during group migration.

--- tests/unit/workspace_access/test_workflows.py
+++ tests/unit/workspace_access/test_workflows.py
@@ -1,8 +1,11 @@
+import logging
 from unittest.mock import create_autospec, call
 
 import pytest
+from databricks.labs.blueprint.parallel import ManyError
 from databricks.labs.lsql.backends import MockBackend
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.errors import DatabricksError
 from databricks.sdk.service.iam import PermissionMigrationResponse
 
 from databricks.labs.ucx.workspace_access.workflows import (
@@ -97,3 +100,39 @@ def test_migrate_permissions_experimental_error(run_workflow):
     ws.permission_migration.migrate_permissions.side_effect = NotImplementedError("api not enabled")
     with pytest.raises(NotImplementedError):
         run_workflow(PermissionsMigrationAPI.apply_permissions, sql_backend=sql_backend, workspace_client=ws)
+
+
+def test_migrate_permissions_continue_on_error(run_workflow, caplog) -> None:
+    """Check that permission migration continues for other groups even if it fails for a single group."""
+    rows = {
+        'SELECT \\* FROM hive_metastore.ucx.groups': GROUPS[
+            ("", "workspace_group_1", "account_group_1", "temp_1", "", "", "", ""),  # Will fail immediately.
+            ("", "workspace_group_2", "account_group_2", "temp_2", "", "", "", ""),  # Will fail midway.
+            ("", "workspace_group_3", "account_group_3", "temp_3", "", "", "", ""),  # Will succeed.
+        ],
+    }
+    sql_backend = MockBackend(rows=rows)
+    ws = create_autospec(WorkspaceClient)
+    ws.get_workspace_id.return_value = "12345678"
+    ws.permission_migration.migrate_permissions.side_effect = [
+        # First group: fails immediately.
+        DatabricksError("simulate group failure: immediately"),
+        # Second group; fails mid-migration.
+        PermissionMigrationResponse(permissions_migrated=10),
+        DatabricksError("simulate group failure: midway"),
+        # Third group.
+        PermissionMigrationResponse(permissions_migrated=50),
+        PermissionMigrationResponse(permissions_migrated=0),
+    ]
+
+    with pytest.raises(ManyError) as exc_info, caplog.at_level(logging.INFO):
+        run_workflow(PermissionsMigrationAPI.apply_permissions, sql_backend=sql_backend, workspace_client=ws)
+
+    raised_exception = exc_info.value
+    assert len(raised_exception.errs) == 2
+    expected_exceptions = {"simulate group failure: immediately", "simulate group failure: midway"}
+    assert {str(e) for e in raised_exception.errs} == expected_exceptions
+    assert "Migration of group permissions failed: temp_1" in caplog.text
+    assert "Migration of group permissions failed: temp_2" in caplog.text
+    assert "Migrated 50 permissions for 1/3 groups successfully." in caplog.messages
+    assert "Migrating permissions failed for 2/3 groups." in caplog.messages
