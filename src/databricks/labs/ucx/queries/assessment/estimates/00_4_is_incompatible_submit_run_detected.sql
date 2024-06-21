da0b1520212a7ded629bc0079c1da7126054c907
Cor Zuurmond <jczuurmond@protonmail.com>
2024-06-21 16:23:06 +0200

    Use explicit columns in dashboard

--- src/databricks/labs/ucx/queries/assessment/estimates/00_4_is_incompatible_submit_run_detected.sql
+++ src/databricks/labs/ucx/queries/assessment/estimates/00_4_is_incompatible_submit_run_detected.sql
@@ -1,7 +1,13 @@
 -- --title 'Incompatible submit runs detected'
-SELECT * FROM
-(SELECT object_type, object_id, EXPLODE(from_json(failures, 'array<string>')) AS finding
-FROM inventory.objects)
-WHERE finding = "no data security mode specified"
-  AND object_type = "submit_runs"
+SELECT
+    object_type,
+    object_id,
+    finding,
+FROM
+    (
+        SELECT object_type, object_id, EXPLODE(from_json(failures, 'array<string>')) AS finding
+        FROM inventory.objects
+    )
+    WHERE finding = "no data security mode specified"
+      AND object_type = "submit_runs"
 
