64acd34e50782aac5668bcbe949f5534dc4e5489
Vsevolod Stepanov <vsevolod.stepanov@databricks.com>
2024-06-24 14:04:13 +0200

    address review feedback

--- src/databricks/labs/ucx/source_code/lsp_plugin.py
+++ src/databricks/labs/ucx/source_code/lsp_plugin.py
@@ -5,12 +5,11 @@ from databricks.sdk.service.workspace import Language
 from databricks.labs.ucx.source_code.linters.context import LinterContext
 from databricks.labs.ucx.source_code.lsp import Diagnostic
 
-# TODO: initialize migration index and session state from config / env variables
-languages = LinterContext(index=None, session_state=None)
-
 
 @hookimpl
 def pylsp_lint(workspace: Workspace, document: Document) -> list[dict]:
+    # TODO: initialize migration index and session state from config / env variables
+    languages = LinterContext(index=None, session_state=None)
     analyser = languages.linter(Language.PYTHON)
     code = document.source
     diagnostics = [Diagnostic.from_advice(_) for _ in analyser.lint(code)]
