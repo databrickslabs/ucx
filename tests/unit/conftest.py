e3671cc6f5995f9de1239b16d765863e46d64482
Cor Zuurmond <jczuurmond@protonmail.com>
2024-07-16 11:29:49 +0200

    Format

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -43,13 +43,7 @@ def mock_installation() -> MockInstallation:
                     'workspace_name': 'workspace',
                 },
             ],
-            'state.json': {
-                'resources': {
-                    'jobs': {
-                        'test': '123'
-                    }
-                }
-            }
+            'state.json': {'resources': {'jobs': {'test': '123'}}},
         }
     )
 
