d7d2eecc10441a81ce7e4f8888536d6ac5d321c2
Serge Smertin <259697+nfx@users.noreply.github.com>
2024-07-22 14:12:20 +0200

    also create dashboards in parallel

--- src/databricks/labs/ucx/install.py
+++ src/databricks/labs/ucx/install.py
@@ -485,7 +485,7 @@ class WorkspaceInstallation(InstallationMixin):
         """
         logger.info(f"Installing UCX v{self._product_info.version()}")
         install_tasks = [self._create_database]  # Need the database before creating the dashboards
-        install_tasks.extend(self._create_dashboards())
+        install_tasks.extend(self._create_dashboards)
         Threads.strict("installing components", install_tasks)
         readme_url = self._workflows_installer.create_jobs()
         if not self._is_account_install and self._prompts.confirm(f"Open job overview in your browser? {readme_url}"):
