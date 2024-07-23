30be26a939afc4d3ae33b0967113e75624f1fcc6
pritishpai <pritish.pai@databricks.com>
2024-07-23 09:42:44 -0400

    Extend overrides json

--- src/databricks/labs/ucx/queries/assessment/main/00_7_count_storage_credentials.sql
+++ src/databricks/labs/ucx/queries/assessment/main/00_7_count_storage_credentials.sql
@@ -1,5 +1,5 @@
 /* --title 'Total Storage Credentials'
---overrides '{"spec":{"encodings":{"value":{"style":{"rules":[{"condition":{"operator":">","operand":{"type":"data-value","value":"200"}},"color":"#E92828"}]}}}}}' */
+--overrides '{"spec":{"encodings":{"value":{"fieldName":"count_total_storage_credentials","rowNumber":2,"style":{"rules":[{"condition":{"operator":">","operand":{"type":"data-value","value":"200"}},"color":"#E92828"}]},"displayName":"count_total_storage_credentials"}}}}' */
 SELECT
   COUNT(*) AS count_total_storage_credentials
 FROM inventory.external_locations
