8bf143ffa61e5d4eb5dedfca8e4d772074141f54
Serge Smertin <serge.smertin@databricks.com>
2024-07-25 12:32:56 +0200

    fmt

--- src/databricks/labs/ucx/queries/assessment/main/01_0_compatibility.sql
+++ src/databricks/labs/ucx/queries/assessment/main/01_0_compatibility.sql
@@ -1,10 +1,10 @@
 /* --title 'UC readiness' --height 3 */
 SELECT
   COALESCE(CONCAT(ROUND(SUM(ready) / COUNT(*) * 100, 1), '%'), 'N/A') AS readiness
-FROM
-(SELECT
-         object_type,
-         object_id,
-         IF(failures = '[]', 1, 0) AS ready
-       FROM inventory.objects
-     )
+FROM (
+  SELECT
+    object_type,
+    object_id,
+    IF(failures = '[]', 1, 0) AS ready
+  FROM inventory.objects
+)
