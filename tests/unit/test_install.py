b3a67026293008f0c84f8d6443112e7a8422ec49
Cor Zuurmond <jczuurmond@protonmail.com>
2024-07-29 13:15:40 +0200

    Test recreating trashed dashboard

--- tests/unit/test_install.py
+++ tests/unit/test_install.py
@@ -713,6 +713,20 @@ def test_installation_recreates_lakeview_dashboard_without_lifecycle_state(caplo
     ws.lakeview.update.assert_not_called()
 
 
+@pytest.mark.parametrize(
+    "new_workspace_installation",
+    [MockInstallation({'state.json': {'resources': {'dashboards': {"assessment_main": "lakeview_id"}}}})],
+    indirect=True,
+)
+def test_installation_recreates_trashed_lakeview_dashboard(caplog, ws, new_workspace_installation):
+    ws.lakeview.get.return_value = LakeviewDashboard(lifecycle_state=LifecycleState.TRASHED)
+    with caplog.at_level(logging.INFO, logger="databricks.labs.ucx.install"):
+        new_workspace_installation.run()
+    assert "Recreating trashed dashboard: " in caplog.text
+    ws.lakeview.create.assert_called()
+    ws.lakeview.update.assert_not_called()
+
+
 def test_validate_dashboards(ws):
     queries_path = find_project_root(__file__) / "src/databricks/labs/ucx/queries"
     for step_folder in queries_path.iterdir():
