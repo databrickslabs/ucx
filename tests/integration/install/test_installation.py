databrickslabs/ucx

commit ef61b256b34c22d8287fbe2db6486e6f82c5ffb2

Update installation parallelization (#2229)
Stacked on top of #2224

## Changes
Parallelize the install deployment as follows:


![image](https://github.com/user-attachments/assets/1d83aba1-28e9-4a56-b959-99f769063e19)

### Functionality

- [x] modified existing command: `databricks labs install ucx`

### Tests

- [x] manually tested

src/databricks/labs/ucx/install.py
@@ -498,10 +498,9 @@ class WorkspaceInstallation(InstallationMixin):
                 True, installation finished. False installation did not finish.
         """
         logger.info(f"Installing UCX v{self._product_info.version()}")
-        install_tasks = [self._create_database]  # Need the database before creating the dashboards
-        install_tasks.extend(self._get_create_dashboard_tasks())
+        install_tasks = [self._create_database_and_dashboards, self._workflows_installer.create_jobs]
         Threads.strict("installing components", install_tasks)
-        readme_url = self._workflows_installer.create_jobs()
+        readme_url = self._installation.workspace_link("README")
         if not self._is_account_install and self._prompts.confirm(f"Open job overview in your browser? {readme_url}"):
             webbrowser.open(readme_url)
         logger.info(f"Installation completed successfully! Please refer to the {readme_url} for the next steps.")
@@ -509,6 +508,10 @@ class WorkspaceInstallation(InstallationMixin):
             logger.info("Triggering the assessment workflow")
             self._trigger_workflow("assessment")
         return True
+
+    def _create_database_and_dashboards(self) -> None:
+        self._create_database()  # Need the database before creating the dashboards
+        Threads.strict("installing dashboards", list(self._get_create_dashboard_tasks()))
 
     def _create_database(self):
         try:

src/databricks/labs/ucx/installer/workflows.py
@@ -417,11 +417,11 @@ class WorkflowsDeployment(InstallationMixin):
         self._this_file = Path(__file__)
         super().__init__(config, installation, ws)
 
-    def create_jobs(self):
+    def create_jobs(self) -> None:
         remote_wheels = self._upload_wheel()
         desired_workflows = {t.workflow for t in self._tasks if t.cloud_compatible(self._ws.config)}
-        wheel_runner = None
 
+        wheel_runner = ""
         if self._config.override_clusters:
             wheel_runner = self._upload_wheel_runner(remote_wheels)
         for workflow_name in desired_workflows:
@@ -446,7 +446,7 @@ class WorkflowsDeployment(InstallationMixin):
 
         self._install_state.save()
         self._create_debug(remote_wheels)
-        return self._create_readme()
+        self._create_readme()
 
     @property
     def _config_file(self):
@@ -464,7 +464,7 @@ class WorkflowsDeployment(InstallationMixin):
         timeout = TEST_NIGHTLY_CI_JOBS_PURGE_TIMEOUT if self._is_nightly() else TEST_JOBS_PURGE_TIMEOUT
         return (datetime.utcnow() + timeout).strftime("%Y%m%d%H")
 
-    def _create_readme(self) -> str:
+    def _create_readme(self) -> None:
         debug_notebook_link = self._installation.workspace_markdown_link('debug notebook', 'DEBUG.py')
         markdown = [
             "# UCX - The Unity Catalog Migration Assistant",
@@ -496,8 +496,6 @@ class WorkflowsDeployment(InstallationMixin):
         preamble = ["# Databricks notebook source", "# MAGIC %md"]
         intro = "\n".join(preamble + [f"# MAGIC {line}" for line in markdown])
         self._installation.upload('README.py', intro.encode('utf8'))
-        readme_url = self._installation.workspace_link('README')
-        return readme_url
 
     def _create_dashboard_links(self, step_name, dashboard_link):
         dashboards_per_step = [d for d in self._install_state.dashboards.keys() if d.startswith(step_name)]
@@ -570,7 +568,7 @@ class WorkflowsDeployment(InstallationMixin):
             wheel_paths = [f"/Workspace{wheel}" for wheel in wheel_paths]
             return wheel_paths
 
-    def _upload_wheel_runner(self, remote_wheels: list[str]):
+    def _upload_wheel_runner(self, remote_wheels: list[str]) -> str:
         # TODO: we have to be doing this workaround until ES-897453 is solved in the platform
         remote_wheels_str = " ".join(remote_wheels)
         code = TEST_RUNNER_NOTEBOOK.format(remote_wheel=remote_wheels_str, config_file=self._config_file).encode("utf8")

tests/unit/test_install.py
@@ -228,7 +228,7 @@ def test_create_database(ws, caplog, mock_installation, any_prompt):
             raise e.errs[0]
 
     assert "Kindly uninstall and reinstall UCX" in str(failure.value)
-    wheels.upload_to_wsfs.assert_not_called()
+    wheels.upload_to_wsfs.assert_called_once()
 
 
 def test_install_cluster_override_jobs(ws, mock_installation):

commit 966fc759f6ef8e4862fcbf6ba3f00dd4a2e4d7a4

Install state misses `dashboards` fields (#2275)
## Changes
Save installation at the end of the `WorkspaceInstallation.run`.

This bug was silently introduced in #2229, where the installation is
parallelized. The installation state was saved [in the
`WorkflowsDeployment`](https://github.com/databrickslabs/ucx/blob/58607092f49b470b7f34577afda9b641dddb97cc/src/databricks/labs/ucx/installer/workflows.py#L449),
which happened after dashboard deployment before parralelization.

### Linked issues
Resolves #2273

### Functionality

- [x] modified existing command: `databricks labs install ucx`

### Tests

- [x] added integration tests

src/databricks/labs/ucx/install.py
@@ -508,6 +508,7 @@ class WorkspaceInstallation(InstallationMixin):
         if self.config.trigger_job:
             logger.info("Triggering the assessment workflow")
             self._trigger_workflow("assessment")
+        self._install_state.save()
         return True
 
     def _create_database_and_dashboards(self) -> None:

tests/integration/install/test_installation.py
@@ -8,6 +8,7 @@ import pytest
 
 import databricks
 from databricks.labs.blueprint.installation import Installation
+from databricks.labs.blueprint.installer import InstallState
 from databricks.labs.blueprint.parallel import ManyError
 from databricks.labs.blueprint.tui import MockPrompts
 from databricks.labs.blueprint.wheels import ProductInfo
@@ -246,6 +247,17 @@ def test_installation_when_dashboard_id_is_invalid(ws, installation_ctx, dashboa
     installation_ctx.workspace_installation.run()
     new_dashboard_id = installation_ctx.install_state.dashboards[dashboard_key]
     assert dashboard_id != new_dashboard_id, "Dashboard id is not updated"
+
+
+def test_installation_stores_install_state_keys(ws, installation_ctx):
+    """The installation should store the keys in the installation state."""
+    expected_keys = "jobs", "dashboards"
+    installation_ctx.workspace_installation.run()
+    # Refresh the installation state, the installation context uses `@cached_property`
+    install_state = InstallState.from_installation(installation_ctx.installation)
+    for key in expected_keys:
+        assert hasattr(install_state, key), f"Missing key in install state: {key}"
+        assert getattr(install_state, key), f"Installation state is empty: {key}"
 
 
 @retried(on=[NotFound], timeout=timedelta(minutes=2))