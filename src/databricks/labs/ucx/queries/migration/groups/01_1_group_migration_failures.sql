b54b9e305d76f55c6e302e47fff0cea38a428eac
Andrew Snare <andrew.snare@databricks.com>
2024-07-31 18:10:10 +0200

    Query formatting.

--- src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
+++ src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
@@ -1,15 +1,15 @@
-/* --title 'Group Migration Failures' --height 4 --width 4 */
-/*
+/* --title 'Group Migration Failures' --height 4 --width 4
+ *
  * Messages that we're looking for are of the form:
  *   failed-group-migration: {name_in_workspace} -> {name_in_account}: {reason}
  */
 SELECT
-    cast(`timestamp` as timestamp) as `timestamp`,
-    job_run_id,
-    level,
-    message
+  CAST(`timestamp` AS TIMESTAMP) as `timestamp`,
+  job_run_id,
+  level,
+  message
 FROM inventory.logs
 WHERE
-  workflow_name in ('migrate-groups', 'migrate-groups-experimental')
-  and startswith(message, 'failed-group-migration:')
-ORDER BY 1
+  workflow_name IN ('migrate-groups', 'migrate-groups-experimental') AND STARTSWITH(message, 'failed-group-migration:')
+ORDER BY
+  1
