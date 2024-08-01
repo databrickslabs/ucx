7d0c65eb58b3d1d1eb26c81ddf945f5f1b277456
Andrew Snare <andrew.snare@databricks.com>
2024-08-01 18:08:25 +0200

    Adjust widget formatting.
    This is still buggy; the invisible columns are not being loaded properly, meaning template references are not rendered.

--- src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
+++ src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
@@ -2,19 +2,21 @@
 --title 'Group Migration Failures'
 --height 4
 --width 5
---overrides '{"spec":
-    {"encodings":{"columns": [
+--overrides '{"spec":{
+    "encodings":{
+      "columns": [
         {"fieldName": "timestamp", "title": "Time of Failure", "booleanValues": ["false", "true"], "type": "datetime", "displayAs": "datetime", "dateTimeFormat": "ll LTS (z)"},
-        {"fieldName": "job_run_id", "title": "Workflow/Task", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}/runs/{{ job_run_id }}", "linkTextTemplate": "{{ workflow_name }}/{{ task_name }}", "linkTitleTemplate": "Job Run: {{ job_run_id }}", "useMonospaceFont": true},
+        {"fieldName": "task_name", "title": "Workflow/Task", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}/{{ @ }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "linkOpenInNewTab": true, "useMonospaceFont": true},
+        {"fieldName": "job_run_id", "title": "Run ID", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}/runs/{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "Job Run: {{ @ }}", "linkOpenInNewTab": true},
         {"fieldName": "workspace_group", "title": "Workspace Group", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"},
         {"fieldName": "account_group", "title": "Account Group", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"},
         {"fieldName": "error_message", "title": "Error Message", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"}
-    ]}},
+      ]},
     "invisibleColumns": [
         {"fieldName": "job_id", "title": "Workflow", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true},
-        {"fieldName": "workflow_name", "title": "Workflow", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true},
-        {"fieldName": "task_name", "title": "Task", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}/{{ task_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true}
-    ]}'
+        {"fieldName": "workflow_name", "title": "Workflow", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true}
+      ]
+   }}'
 */
 WITH latest_job_runs AS (
   SELECT
