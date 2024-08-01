d636d19f3a544be87d56662a0892cecdc780931a
Cor Zuurmond <jczuurmond@protonmail.com>
2024-08-01 16:37:54 +0200

    Refactor tests to avoid duplication

--- tests/unit/account/test_aggregate.py
+++ tests/unit/account/test_aggregate.py
@@ -1,7 +1,9 @@
 import logging
 from unittest.mock import create_autospec
 
-from databricks.sdk import Workspace, WorkspaceClient
+import pytest
+
+from databricks.sdk import AccountClient, Workspace, WorkspaceClient
 from databricks.sdk.service import iam, sql
 from databricks.labs.lsql.backends import MockBackend
 
@@ -11,88 +13,57 @@ from databricks.labs.ucx.config import WorkspaceConfig
 from databricks.labs.ucx.contexts.workspace_cli import WorkspaceContext
 
 
+UCX_OBJECTS = MockBackend.rows("object_type", "object_id", "failures")
 UCX_TABLES = MockBackend.rows("catalog", "database", "table", "object_type", "table_format", "location", "view_text")
 
 
-def test_basic_readiness_report_no_workspaces(acc_client, caplog):
-    account_ws = AccountWorkspaces(acc_client)
-    account_aggregate_obj = AccountAggregate(account_ws)
-
-    with caplog.at_level(logging.INFO):
-        account_aggregate_obj.readiness_report()
-
-    assert 'UC compatibility' in caplog.text
-
-
-def test_readiness_report_ucx_installed(acc_client, caplog):
-    account_ws = AccountWorkspaces(acc_client)
-    acc_client.workspaces.list.return_value = [
-        Workspace(workspace_name="foo", workspace_id=123, workspace_status_message="Running", deployment_name="abc")
-    ]
-
+@pytest.fixture
+def ws() -> WorkspaceClient:
     ws = create_autospec(WorkspaceClient)
-    acc_client.get_workspace_client.return_value = ws
-    ws.statement_execution.execute_statement.return_value = sql.ExecuteStatementResponse(
-        status=sql.StatementStatus(state=sql.StatementState.SUCCEEDED),
-        result=sql.ResultData(
-            data_array=[
-                [
-                    "jobs",
-                    "32432123",
-                    """["cluster type not supported : LEGACY_TABLE_ACL",
-                     "cluster type not supported : LEGACY_SINGLE_USER"]""",
-                ],
-                [
-                    "jobs",
-                    "234234234",
-                    """["cluster type not supported : LEGACY_SINGLE_USER"]""",
-                ],
-                [
-                    "clusters",
-                    "21312312",
-                    """[]""",
-                ],
-                [
-                    "tables",
-                    "34234324",
-                    """["listTables returned null"]""",
-                ],
-            ],
-            row_count=2,
-        ),
-        manifest=sql.ResultManifest(
-            schema=sql.ResultSchema(
-                columns=[
-                    sql.ColumnInfo(name="object_type", type_name=sql.ColumnInfoTypeName.STRING),
-                    sql.ColumnInfo(name="object_id", type_name=sql.ColumnInfoTypeName.STRING),
-                    sql.ColumnInfo(name="failures", type_name=sql.ColumnInfoTypeName.STRING),
-                ],
-                column_count=3,
-            )
-        ),
-        statement_id='123',
-    )
-    ws.current_user.me.return_value = iam.User(user_name="me@example.com", groups=[iam.ComplexValue(display="admins")])
-
-    ctx = WorkspaceContext(ws).replace(config=WorkspaceConfig(inventory_database="something", warehouse_id="1234"))
-    account_aggregate_obj = AccountAggregate(account_ws, workspace_context_factory=lambda _: ctx)
-
-    with caplog.at_level(logging.INFO):
-        account_aggregate_obj.readiness_report()
-
-    assert 'UC compatibility: 25.0% (3/4)' in caplog.text
-    assert 'cluster type not supported : LEGACY_TABLE_ACL: 1 objects' in caplog.text
-    assert 'cluster type not supported : LEGACY_SINGLE_USER: 2 objects' in caplog.text
-
-
-def test_account_aggregate_logs_overlapping_tables(caplog, acc_client):
-    ws = create_autospec(WorkspaceClient)
-    acc_client.workspaces.list.return_value = [
-        Workspace(workspace_name="foo", workspace_id=123, workspace_status_message="Running", deployment_name="abc")
-    ]
-    acc_client.get_workspace_client.return_value = ws
     ws.current_user.me.return_value = iam.User(user_name="user", groups=[iam.ComplexValue(display="admins")])
+    return ws
 
+
+@pytest.fixture
+def account_client(ws, acc_client) -> AccountClient:
+    workspace = Workspace(workspace_name="test", workspace_id=123)
+    acc_client.workspaces.list.return_value = [workspace]
+    acc_client.get_workspace_client.return_value = ws
+    return acc_client
+
+
+def test_basic_readiness_report_no_workspaces(caplog, ws, account_client):
+    rows = UCX_OBJECTS[("tables", "34234324", '["listTables returned null"]'),]
+    mock_backend = MockBackend(rows={"SELECT \\* FROM hive_metastore.ucx.objects": rows})
+    ctx = WorkspaceContext(ws).replace(config=WorkspaceConfig(inventory_database="ucx"), sql_backend=mock_backend)
+
+    account_workspaces = AccountWorkspaces(account_client)
+    account_aggregate = AccountAggregate(account_workspaces, workspace_context_factory=lambda _: ctx)
+    with caplog.at_level(logging.INFO, logger="databricks.labs.ucx.account.aggregate"):
+        account_aggregate.readiness_report()
+    assert "UC compatibility" in caplog.text
+
+
+def test_readiness_report_ucx_installed(caplog, ws, account_client):
+    rows = UCX_OBJECTS[
+        ("jobs", "32432123", '["cluster type not supported : LEGACY_TABLE_ACL", "cluster type not supported : LEGACY_SINGLE_USER"]'),
+        ("jobs", "234234234", '["cluster type not supported : LEGACY_SINGLE_USER"]'),
+        ("clusters", "21312312", '[]'),
+        ("tables", "34234324", '["listTables returned null"]'),
+    ]
+    mock_backend = MockBackend(rows={"SELECT \\* FROM hive_metastore.ucx.objects": rows})
+    ctx = WorkspaceContext(ws).replace(config=WorkspaceConfig(inventory_database="ucx"), sql_backend=mock_backend)
+
+    account_workspaces = AccountWorkspaces(account_client)
+    account_aggregate = AccountAggregate(account_workspaces, workspace_context_factory=lambda _: ctx)
+    with caplog.at_level(logging.INFO, logger="databricks.labs.ucx.account.aggregate"):
+        account_aggregate.readiness_report()
+    assert "UC compatibility: 25.0% (3/4)" in caplog.text
+    assert "cluster type not supported : LEGACY_TABLE_ACL: 1 objects" in caplog.text
+    assert "cluster type not supported : LEGACY_SINGLE_USER: 2 objects" in caplog.text
+
+
+def test_account_aggregate_logs_overlapping_tables(caplog, ws, account_client):
     rows = UCX_TABLES[
         ("catalog", "database", "t1", "TABLE", "DELTA", "/foo/bar/test", None),
         ("catalog", "database", "t2", "TABLE", "DELTA", "/foo/bar/", None),
@@ -100,7 +71,7 @@ def test_account_aggregate_logs_overlapping_tables(caplog, acc_client):
     mock_backend = MockBackend(rows={"SELECT \\* FROM hive_metastore.ucx.tables": rows})
     ctx = WorkspaceContext(ws).replace(config=WorkspaceConfig(inventory_database="ucx"), sql_backend=mock_backend)
 
-    account_ws = AccountWorkspaces(acc_client)
+    account_ws = AccountWorkspaces(account_client)
     account_aggregate = AccountAggregate(account_ws, workspace_context_factory=lambda _: ctx)
     with caplog.at_level(logging.WARNING, logger="databricks.labs.ucx.account.aggregate"):
         account_aggregate.validate()
