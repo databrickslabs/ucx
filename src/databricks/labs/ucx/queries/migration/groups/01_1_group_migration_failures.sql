f2d992abb28c13510e092a4dfa60289edc377fe9
Andrew Snare <andrew.snare@databricks.com>
2024-08-01 14:13:22 +0200

    Fix templates.

--- src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
+++ src/databricks/labs/ucx/queries/migration/groups/01_1_group_migration_failures.sql
@@ -5,15 +5,15 @@
 --overrides '{"spec":
     {"encodings":{"columns": [
         {"fieldName": "timestamp", "title": "Time of Failure", "booleanValues": ["false", "true"], "type": "datetime", "displayAs": "datetime", "dateTimeFormat": "ll LTS (z)"},
-        {"fieldName": "job_run_id", "title": "Workflow/Task", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}/runs/{{ job_run_id }}", "linkTextTemplate": "{{workflow_name}}/{{task_name}}", "linkTitleTemplate": "Job Run: {{ job_run_id }}", "useMonospaceFont": true},
+        {"fieldName": "job_run_id", "title": "Workflow/Task", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}/runs/{{ job_run_id }}", "linkTextTemplate": "{{ workflow_name }}/{{ task_name }}", "linkTitleTemplate": "Job Run: {{ job_run_id }}", "useMonospaceFont": true},
         {"fieldName": "workspace_group", "title": "Workspace Group", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"},
         {"fieldName": "account_group", "title": "Account Group", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"},
         {"fieldName": "error_message", "title": "Error Message", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string"}
     ]}},
     "invisibleColumns": [
-        {"fieldName": "job_id", "title": "Workflow", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{workflow_name}}", "linkTitleTemplate": "{{workflow_name}} (ID: {{job_id}})", "useMonospaceFont": true},
-        {"fieldName": "workflow_name", "title": "Workflow", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{workflow_name}}", "linkTitleTemplate": "{{workflow_name}} (ID: {{job_id}})", "useMonospaceFont": true},
-        {"fieldName": "task_name", "title": "Task", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{workflow_name}}/{{task_name}}", "linkTitleTemplate": "{{workflow_name}} (ID: {{job_id}})", "useMonospaceFont": true}
+        {"fieldName": "job_id", "title": "Workflow", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true},
+        {"fieldName": "workflow_name", "title": "Workflow", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true},
+        {"fieldName": "task_name", "title": "Task", "booleanValues": ["false", "true"], "type": "string", "displayAs": "link", "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ workflow_name }}/{{ task_name }}", "linkTitleTemplate": "{{ workflow_name }} (ID: {{ job_id }})", "useMonospaceFont": true}
     ]}'
 */ /*
  * Messages that we're looking for are of the form:
