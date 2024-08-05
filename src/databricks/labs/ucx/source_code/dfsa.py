4ea5a22dc70981117c28a61b651cb9ebc480fc96
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-08-05 10:51:51 +0200

    progress

--- src/databricks/labs/ucx/source_code/dfsa.py
+++ src/databricks/labs/ucx/source_code/dfsa.py
@@ -1,7 +1,8 @@
 from collections.abc import Iterable
 from dataclasses import dataclass
+from pathlib import Path
 
-from databricks.labs.ucx.source_code.graph import DependencyGraph
+from databricks.labs.ucx.source_code.graph import DependencyGraph, Dependency
 
 
 @dataclass
@@ -14,4 +15,14 @@ class DfsaCollector:
     """DfsaCollector is responsible for collecting and storing DFSAs i.e. Direct File System Access records"""
 
     def collect(self, graph: DependencyGraph) -> Iterable[DFSA]:
+        collected_paths: set[Path] = set()
+        for dependency in graph.root_dependencies:
+            root = dependency.path  # since it's a root
+            yield from self._collect_one(dependency, graph, root, collected_paths)
+
+    def _collect_one(self, dependency: Dependency, graph: DependencyGraph, root_path: Path, collected_paths: set[Path]) -> Iterable[DFSA]:
+        if dependency.path in collected_paths:
+            return
+        collected_paths.add(dependency.path)
+
         yield from []
