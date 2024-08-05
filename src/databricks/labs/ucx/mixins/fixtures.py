f37d34b1522717ebec80ee430304db70e8700de4
FastLee <liran.bareket@databricks.com>
2024-08-05 12:16:37 -0400

    Addressed Name Duplication on Dashboard Fixtures

--- src/databricks/labs/ucx/mixins/fixtures.py
+++ src/databricks/labs/ucx/mixins/fixtures.py
@@ -1409,7 +1409,7 @@ def make_dashboard(ws: WorkspaceClient, make_random: Callable[[int], str], make_
 
 @pytest.fixture
 def make_lakeview_dashboard(ws, make_random, env_or_skip):
-    """Create a lakeview dashboard. """
+    """Create a lakeview dashboard."""
     warehouse_id = env_or_skip("TEST_DEFAULT_WAREHOUSE_ID")
     serialized_dashboard = {
         "datasets": [{"name": "count", "displayName": "count", "query": "SELECT 42 AS count"}],
