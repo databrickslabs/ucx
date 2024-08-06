cf9fc3b64d10ac19909621e817f7b81f53ac13ba
Cor Zuurmond <jczuurmond@protonmail.com>
2024-08-06 14:21:31 +0200

    Add missing default for wheelhouse

--- src/databricks/labs/ucx/config.py
+++ src/databricks/labs/ucx/config.py
@@ -67,7 +67,7 @@ class WorkspaceConfig:  # pylint: disable=too-many-instance-attributes
 
     # Whether to upload dependent libraries to the workspace
     upload_dependencies: bool = False
-    wheelhouse: str | None
+    wheelhouse: str | None = None
 
     # [INTERNAL ONLY] Whether the assessment should capture only specific object permissions.
     include_object_permissions: list[str] | None = None
