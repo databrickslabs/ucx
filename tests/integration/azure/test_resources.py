ecb62febaabaddf185dc8e9a803f92fdb7e00d36
Cor Zuurmond <jczuurmond@protonmail.com>
2024-08-08 09:43:24 +0200

    Add integration test for applying storage permissions

--- tests/integration/azure/test_resources.py
+++ tests/integration/azure/test_resources.py
@@ -1,4 +1,7 @@
 import datetime as dt
+import uuid
+
+import pytest
 
 from databricks.labs.ucx.azure.resources import AzureResource
 
@@ -7,6 +10,36 @@ def test_azure_resource_storage_accounts_list_non_zero(az_cli_ctx):
     """Expect at least one storage account."""
     storage_accounts = az_cli_ctx.azure_resources.storage_accounts()
     assert len(list(storage_accounts)) > 0
+
+
+def test_azure_resource_applies_storage_permissions(az_cli_ctx, env_or_skip, make_random):
+    storage_account_name = env_or_skip("TEST_STORAGE_ACCOUNT_NAME")
+    storage_accounts = []
+    for storage_account in az_cli_ctx.azure_resources.storage_accounts():
+        if storage_account.name == storage_account_name:
+            storage_accounts.append(storage_account)
+    if len(storage_accounts) == 0:
+        pytest.skip("No storage account found in the test resource group.")
+
+    storage_account = storage_accounts[0]
+    access_connector_name = f"test-{make_random()}"
+    tomorrow = dt.datetime.now() + dt.timedelta(days=1)
+    tags = {"RemoveAfter": str(tomorrow), "NoAutoRemove": "False"}
+    access_connector = az_cli_ctx.azure_resources.create_or_update_access_connector(
+        storage_account.id.subscription_id,
+        storage_account.id.resource_group,
+        access_connector_name,
+        storage_account.location,
+        tags,
+    )
+
+    role_guid = str(uuid.uuid4())
+    az_cli_ctx.azure_resources.apply_storage_permission(
+        access_connector.principal_id,
+        storage_account,
+        "STORAGE_BLOB_DATA_READER",
+        role_guid,
+    )
 
 
 def test_azure_resource_access_connector_list_create_get_delete(az_cli_ctx, env_or_skip, make_random):
