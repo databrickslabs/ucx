622b66684da3ed31027052185f9a9b49c34e52af
Cor Zuurmond <jczuurmond@protonmail.com>
2024-08-14 10:32:29 +0200

    Set back security policy from main

--- src/databricks/labs/ucx/azure/access.py
+++ src/databricks/labs/ucx/azure/access.py
@@ -353,10 +353,25 @@ class AzureResourcePermissions:
                 if configuration_pair in sql_dac:
                     sql_dac.remove(configuration_pair)
 
-        self._ws.warehouses.set_workspace_warehouse_config(
-            data_access_config=sql_dac,
-            sql_configuration_parameters=warehouse_config.sql_configuration_parameters,
+        security_policy = (
+            SetWorkspaceWarehouseConfigRequestSecurityPolicy(warehouse_config.security_policy.value)
+            if warehouse_config.security_policy
+            else SetWorkspaceWarehouseConfigRequestSecurityPolicy.NONE
         )
+        try:
+            self._ws.warehouses.set_workspace_warehouse_config(
+                data_access_config=sql_dac,
+                sql_configuration_parameters=warehouse_config.sql_configuration_parameters,
+                security_policy=security_policy,
+            )
+        # TODO: Remove following try except once https://github.com/databricks/databricks-sdk-py/issues/305 is fixed
+        except InvalidParameterValue as error:
+            sql_dac_log_msg = "\n".join(f"{config_pair.key} {config_pair.value}" for config_pair in sql_dac)
+            logger.error(
+                f'Adding uber principal to SQL warehouse Data Access Properties is failed using Python SDK with error "{error}". '
+                f'Please try applying the following configs manually in the worksapce admin UI:\n{sql_dac_log_msg}'
+            )
+            raise error
 
     def create_uber_principal(self, prompts: Prompts):
         config = self._installation.load(WorkspaceConfig)
