ad9958b7e534368dcca6e931cf80273450a72b27
pritishpai <pritish.pai@databricks.com>
2024-08-22 15:29:46 -0400

    metastore id

--- src/databricks/labs/ucx/aws/credentials.py
+++ src/databricks/labs/ucx/aws/credentials.py
@@ -233,5 +233,5 @@ class IamRoleCreation:
         self._resource_permissions.save_uc_compatible_roles()
 
     def _generate_role_name(self, role_name: str) -> str:
-        metastore_id = self._ws.metastores.current().metastore_id
-        return f"{role_name}_{self._ws.metastores.get(metastore_id).name}"
+        metastore_id = self._ws.metastores.current().as_dict()["metastore_id"]
+        return f"{role_name}_{metastore_id}"
