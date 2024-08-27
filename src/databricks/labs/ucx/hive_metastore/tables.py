c468b660879b93b8cd974d29967f5f2b9d0ae917
pritishpai <pritish.pai@databricks.com>
2024-08-27 12:02:58 -0400

    Use Py4j for Scala functions

--- src/databricks/labs/ucx/hive_metastore/tables.py
+++ src/databricks/labs/ucx/hive_metastore/tables.py
@@ -386,13 +386,19 @@ class TablesCrawler(CrawlerBase):
         for row in self._fetch(f"SELECT * FROM {escape_sql_identifier(self.full_name)}"):
             yield Table(*row)
 
+    def _iterator(self, result: any):
+        it = result.iterator()
+        while it.hasNext():
+            yield it.next()
+
     def _show_tables_in_database(self, catalog: str, database: str) -> list[Table]:
 
         table_rows: list[Table] = []
         try:
             logger.debug(f"[{catalog}.{database}] listing tables and views")
-            external_catalog = spark.catalog
-            for row in external_catalog.listTables(catalog, database):
+            external_catalog = self._spark._jsparkSession.sharedState().externalCatalog()
+            table_list = list(self._iterator(external_catalog.listTables(database)))
+            for row in table_list:
                 table_rows.append(
                     Table(
                         catalog=catalog,
@@ -402,18 +408,6 @@ class TablesCrawler(CrawlerBase):
                         table_format='UNKNOWN',
                     )
                 )
-            # for row in self._fetch(
-            #     f"SHOW TABLES FROM {escape_sql_identifier(catalog)}.{escape_sql_identifier(database)}"
-            # ):
-            #     table_rows.append(
-            #         Table(
-            #             catalog=catalog,
-            #             database=database,
-            #             name=row[1],
-            #             object_type='UNKNOWN',
-            #             table_format='UNKNOWN',
-            #         )
-            #     )
         except NotFound:
             # This make the integration test more robust as many test schemas are being created and deleted quickly.
             # In case a schema is deleted, StatementExecutionBackend returns empty result but RuntimeBackend
