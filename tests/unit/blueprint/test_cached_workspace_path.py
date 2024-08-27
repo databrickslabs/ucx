2ada277bc9628681331dc90f3739df1db30063aa
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-08-27 23:44:33 +0200

    formatting

--- tests/unit/blueprint/test_cached_workspace_path.py
+++ tests/unit/blueprint/test_cached_workspace_path.py
@@ -1,11 +1,11 @@
 import io
 from unittest.mock import create_autospec
 
-from databricks.labs.ucx.blueprint.CachedWorkspacePath import CachedWorkspacePath
 from tests.unit import mock_workspace_client
 
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.workspace import ObjectInfo, ObjectType
+from databricks.labs.ucx.blueprint.CachedWorkspacePath import CachedWorkspacePath
 
 
 def test_path_like_returns_cached_instance():
@@ -17,7 +17,7 @@ def test_path_like_returns_cached_instance():
 def test_iterdir_returns_cached_instances():
     ws = create_autospec(WorkspaceClient)
     ws.workspace.get_status.return_value = ObjectInfo(object_type=ObjectType.DIRECTORY)
-    ws.workspace.list.return_value = list([ObjectInfo(object_type=ObjectType.FILE, path=s) for s in ["a", "b", "c"]])
+    ws.workspace.list.return_value = list(ObjectInfo(object_type=ObjectType.FILE, path=s) for s in ("a", "b", "c"))
     parent = CachedWorkspacePath(ws, "dir")
     assert parent.is_dir()
     for child in parent.iterdir():
@@ -28,7 +28,7 @@ def test_download_is_only_called_once_per_instance():
     ws = mock_workspace_client()
     ws.workspace.download.side_effect = lambda _, *, format: io.BytesIO("abc".encode())
     path = CachedWorkspacePath(ws, "path")
-    for i in range(0, 4):
+    for _ in range(0, 4):
         _ = path.read_text()
     assert ws.workspace.download.call_count == 1
 
@@ -36,7 +36,7 @@ def test_download_is_only_called_once_per_instance():
 def test_download_is_only_called_once_across_instances():
     ws = mock_workspace_client()
     ws.workspace.download.side_effect = lambda _, *, format: io.BytesIO("abc".encode())
-    for i in range(0, 4):
+    for _ in range(0, 4):
         path = CachedWorkspacePath(ws, "path")
         _ = path.read_text()
     assert ws.workspace.download.call_count == 1
