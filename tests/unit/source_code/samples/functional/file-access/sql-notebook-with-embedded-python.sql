b63cb19178b5142bdb6e3626029cba3a9a49c2e0
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-08-30 12:16:55 +0200

    more functional tests

---
+++ tests/unit/source_code/samples/functional/file-access/sql-notebook-with-embedded-python.sql
@@ -0,0 +1,18 @@
+-- Databricks notebook source
+-- MAGIC %md # This is a SQL notebook, that has Python cell embedded
+
+-- COMMAND ----------
+
+-- ucx[dbfs-read-from-sql-query:+0:0:+0:1024] The use of DBFS is deprecated: dbfs:/mnt/whatever
+SELECT * FROM csv.`dbfs:/mnt/whatever`
+
+
+
+
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- ucx[implicit-dbfs-usage:+2:8:+2:43] The use of default dbfs: references is deprecated: /mnt/things/e/f/g
+-- ucx[dbfs-usage:+1:23:+1:42] Deprecated file system path: /mnt/things/e/f/g
+-- MAGIC display(spark.read.csv('/mnt/things/e/f/g'))
---
+++ tests/unit/source_code/samples/functional/table-migration/table-migration-notebook.sql
@@ -0,0 +1,67 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC #Test notebook for Use tracking in Notebooks
+
+-- COMMAND ----------
+-- DBTITLE 1,A SQL cell that changes the DB
+
+USE different_db
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table different_db.testtable is migrated to cata2.newspace.table in Unity Catalog
+-- DBTITLE 1,A SQL cell that references tables
+
+SELECT * FROM  testtable LIMIT 10
+
+-- COMMAND ----------
+-- DBTITLE 1,A SQL cell that changes the DB to one we migrate from
+
+USE old
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table old.testtable is migrated to cata3.newspace.table in Unity Catalog
+-- DBTITLE 1,A SQL cell that references tables
+
+SELECT * FROM  testtable LIMIT 10
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table old.stuff is migrated to brand.new.things in Unity Catalog
+-- DBTITLE 1,A SQL cell that references tables
+
+SELECT * FROM  stuff LIMIT 10
+
+-- COMMAND ----------
+-- DBTITLE 1,A Python cell that uses calls to change the USE
+-- MAGIC %python
+-- MAGIC # This is a Python cell that uses calls to change the USE...
+
+spark.sql("use different_db")
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table different_db.testtable is migrated to cata2.newspace.table in Unity Catalog
+-- DBTITLE 1,A SQL cell that references DBFS
+
+SELECT * FROM testtable LIMIT 10
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table old.testtable is migrated to cata3.newspace.table in Unity Catalog
+-- DBTITLE 1,A SQL cell that references DBFS
+
+SELECT * FROM old.testtable LIMIT 10
+
+-- COMMAND ----------
+-- DBTITLE 1,A SQL cell that changes the DB to the default
+
+USE default
+
+-- COMMAND ----------
+-- ucx[table-migrated-to-uc:+0:0:+0:1024] Table default.testtable is migrated to cata.nondefault.table in Unity Catalog
+-- DBTITLE 1,A SQL cell that references DBFS
+
+SELECT * FROM testtable LIMIT 10
+
+-- COMMAND ----------
+-- DBTITLE 1,A SQL cell that references tables
+
+MERGE INTO catalog.schema.testtable t USING source ON t.key = source.key WHEN MATCHED THEN DELETE
+
