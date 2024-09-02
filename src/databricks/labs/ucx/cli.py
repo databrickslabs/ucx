689826060761b53b497a7bb299c96acd668c914d
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-02 10:53:25 +0200

    Convert file to path

--- src/databricks/labs/ucx/cli.py
+++ src/databricks/labs/ucx/cli.py
@@ -107,6 +107,7 @@ def sync_workspace_info(a: AccountClient):
 @ucx.command
 def upload(file: Path, w: WorkspaceClient, run_as_collection: bool = False, a: AccountClient | None = None):
     """Upload a file to the (collection of) workspace(s)"""
+    file = Path(file)
     logger.warning("The schema of CSV files is NOT validated, ensure it is correct")
     for ctx in _get_workspace_contexts(w, a, run_as_collection):
         ctx.installation.upload(file.name, file.read_bytes())
