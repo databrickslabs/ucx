cb6e45d409a29efaf249caedf2638a59991b1bf5
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-09-06 18:14:38 +0200

    hdfs is irrelevant, replace with dbfs

--- tests/unit/source_code/samples/functional/file-access/select_format.sql
+++ tests/unit/source_code/samples/functional/file-access/select_format.sql
@@ -1,3 +1,3 @@
 -- Databricks notebook source
--- ucx[direct-filesystem-access-in-sql-query:+0:0:+0:1024] The use of direct filesystem references is deprecated: hdfs://examples/src/main/resources/users.parquet
-SELECT * FROM parquet.`hdfs://examples/src/main/resources/users.parquet`
+-- ucx[direct-filesystem-access-in-sql-query:+0:0:+0:1024] The use of direct filesystem references is deprecated: dbfs://examples/src/main/resources/users.parquet
+SELECT * FROM parquet.`dbfs://examples/src/main/resources/users.parquet`
