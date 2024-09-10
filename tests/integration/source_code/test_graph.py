79c6510ad72bf1953ed23160de7f7370eacba6a3
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-09-10 15:41:21 +0200

    fix failing test

--- tests/integration/source_code/test_graph.py
+++ tests/integration/source_code/test_graph.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
+from databricks.sdk.service.workspace import Language
+
 from databricks.labs.ucx.source_code.base import CurrentSessionState
-from databricks.labs.ucx.source_code.graph import DependencyResolver, DependencyGraph
+from databricks.labs.ucx.source_code.graph import DependencyResolver, DependencyGraph, WrappingLoader, Dependency
 from databricks.labs.ucx.source_code.known import KnownList, Compatibility, UNKNOWN
-from databricks.labs.ucx.source_code.linters.files import FileLoader, ImportFileResolver
+from databricks.labs.ucx.source_code.linters.files import FileLoader, ImportFileResolver, LocalFile
 from databricks.labs.ucx.source_code.notebooks.loaders import NotebookLoader, NotebookResolver
 from databricks.labs.ucx.source_code.path_lookup import PathLookup
 from databricks.labs.ucx.source_code.python_libraries import PythonLibraryResolver
@@ -18,19 +20,20 @@ def test_graph_visits_package_with_recursive_imports():
                 return UNKNOWN
             return super().module_compatibility(name)
 
-    root_path = Path("../../unit/source_code/samples/import-sqlglot.py").resolve()
     allow_list = TestKnownList()
     library_resolver = PythonLibraryResolver(allow_list)
     notebook_resolver = NotebookResolver(NotebookLoader())
     import_resolver = ImportFileResolver(FileLoader(), allow_list)
-    path_lookup = PathLookup.from_sys_path(root_path.parent)
+    path_lookup = PathLookup.from_sys_path(Path(__file__).parent)
     dependency_resolver = DependencyResolver(
         library_resolver, notebook_resolver, import_resolver, import_resolver, path_lookup
     )
-    maybe = dependency_resolver.resolve_file(path_lookup, root_path)
-    assert maybe.dependency
-    graph = DependencyGraph(maybe.dependency, None, dependency_resolver, path_lookup, CurrentSessionState())
-    container = maybe.dependency.load(path_lookup)
+    root_path = Path(__file__).parent.parent.parent / "unit" / "source_code" / "samples" / "import-sqlglot.py"
+    assert root_path.is_file()
+    container = LocalFile(root_path, "from sqlglot import Expression\n", Language.PYTHON)
+    loader = WrappingLoader(container)
+    dependency = Dependency(loader, root_path, False)
+    graph = DependencyGraph(dependency, None, dependency_resolver, path_lookup, CurrentSessionState())
     container.build_dependency_graph(graph)
     assert len(graph.all_dependencies) > 10
     # visit the graph without a 'visited' set
