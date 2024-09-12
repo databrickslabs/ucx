746eb6df03d77be6373bda7a856c833d2403fcc0
Andrew Snare <andrew.snare@databricks.com>
2024-09-12 11:12:34 +0200

    Import formatting.

--- src/databricks/labs/ucx/source_code/lsp.py
+++ src/databricks/labs/ucx/source_code/lsp.py
@@ -12,10 +12,7 @@ from urllib.parse import parse_qsl
 from databricks.labs.blueprint.logger import install_logger
 from databricks.sdk.service.workspace import Language
 
-from databricks.labs.ucx.hive_metastore.table_migration_status import (
-    TableMigrationStatus,
-)
-from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationIndex
+from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationIndex, TableMigrationStatus
 from databricks.labs.ucx.source_code.base import (
     Advice,
     Advisory,
