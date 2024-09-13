73d34a75a437d2f89d1b35556c86215fd2f29a45
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-09-13 16:16:45 +0200

    fix view DDL

--- src/databricks/labs/ucx/queries/views/direct_fs_access.sql
+++ src/databricks/labs/ucx/queries/views/direct_fs_access.sql
@@ -1,7 +1,7 @@
 SELECT
     *
 FROM $inventory.direct_file_system_access_in_paths
-UNION
+UNION ALL
 SELECT
-    *
+    *, NULL as job_id, NULL as job_name, null as task_key
 FROM $inventory.direct_file_system_access_in_queries
