1ab776b588eef9dfabfcc9404e64e2d66c987ea8
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-09-13 16:12:14 +0200

    add inventory prefix

--- src/databricks/labs/ucx/queries/views/direct_fs_access.sql
+++ src/databricks/labs/ucx/queries/views/direct_fs_access.sql
@@ -1,7 +1,7 @@
 SELECT
     *
-FROM direct_file_system_access_in_paths
+FROM $inventory.direct_file_system_access_in_paths
 UNION
 SELECT
     *
-FROM direct_file_system_access_in_queries
+FROM $inventory.direct_file_system_access_in_queries
