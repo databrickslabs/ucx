39df9f75aa1499214ba6d016dd04b541460a34c3
pritishpai <pritish.pai@databricks.com>
2024-09-13 14:09:59 -0400

    extend original assessment test and use tables_crawler

--- tests/integration/assessment/test_workflows.py
+++ tests/integration/assessment/test_workflows.py
@@ -1,17 +1,17 @@
 from datetime import timedelta
 
+from databricks.labs.pytester.fixtures.catalog import make_schema
 from databricks.sdk.errors import NotFound, InvalidParameterValue
 from databricks.sdk.retries import retried
 from databricks.sdk.service.iam import PermissionLevel
 
+from databricks.labs.ucx.hive_metastore import TablesCrawler
+
 
 @retried(on=[NotFound, InvalidParameterValue])
 def test_running_real_assessment_job(
-    ws,
-    installation_ctx,
-    make_cluster_policy,
-    make_cluster_policy_permissions,
-    populate_for_linting,
+    ws, installation_ctx, make_cluster_policy, make_cluster_policy_permissions, make_job, make_notebook, make_dashboard,
+    make_schema, make_table, sql_backend, inventory_schema, populate_for_linting,
 ):
     ws_group, _ = installation_ctx.make_ucx_group()
     cluster_policy = make_cluster_policy()
@@ -20,6 +20,13 @@ def test_running_real_assessment_job(
         permission_level=PermissionLevel.CAN_USE,
         group_name=ws_group.display_name,
     )
+    source_schema = make_schema(catalog_name="hive_metastore")
+    managed_table = make_table(schema_name=source_schema.name)
+    external_table = make_table(schema_name=source_schema.name, external=True)
+    tmp_table = make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four")
+    view = make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four", view=True)
+    non_delta = make_table(schema_name=source_schema.name, non_delta=True)
+
     installation_ctx.__dict__['include_object_permissions'] = [f"cluster-policies:{cluster_policy.policy_id}"]
     installation_ctx.workspace_installation.run()
 
@@ -33,42 +40,17 @@ def test_running_real_assessment_job(
     assert after[ws_group.display_name] == PermissionLevel.CAN_USE
 
 
-@retried(on=[NotFound, InvalidParameterValue], timeout=timedelta(minutes=8))
-def test_running_assessment_job_crawl_tables(
-    ws, installation_ctx, make_cluster_policy, make_cluster_policy_permissions, make_schema, make_table
-):
-    ws_group_a, _ = installation_ctx.make_ucx_group()
-    cluster_policy = make_cluster_policy()
-    make_cluster_policy_permissions(
-        object_id=cluster_policy.policy_id,
-        permission_level=PermissionLevel.CAN_USE,
-        group_name=ws_group_a.display_name,
-    )
-    source_schema = make_schema(catalog_name="hive_metastore")
-    managed_table = make_table(schema_name=source_schema.name)
-    external_table = make_table(schema_name=source_schema.name, external=True)
-    tmp_table = make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four")
-    view = make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four", view=True)
-    non_delta = make_table(schema_name=source_schema.name, non_delta=True)
+    tables_crawler = TablesCrawler(sql_backend, inventory_schema, [source_schema.name])
+    tables_crawled = tables_crawler.snapshot()
 
-    installation_ctx.__dict__['include_object_permissions'] = [f"cluster-policies:{cluster_policy.policy_id}"]
-    installation_ctx.workspace_installation.run()
+    tables = []
+    for _ in tables_crawled:
+        tables.append(_.name)
 
-    installation_ctx.deployed_workflows.run_workflow("assessment")
+    assert len(tables) == 5
 
-    tables_crawled = []
-    table_list = list(
-        installation_ctx.sql_backend.fetch(
-            f"SELECT * FROM {installation_ctx.inventory_database}.tables WHERE database='{source_schema.name}'"
-        )
-    )
-    for table in table_list:
-        tables_crawled.append(table.name)
-
-    assert len(tables_crawled) == 5
-
-    assert managed_table.name in tables_crawled
-    assert external_table.name in tables_crawled
-    assert tmp_table.name in tables_crawled
-    assert view.name in tables_crawled
-    assert non_delta.name in tables_crawled
+    assert managed_table.name in tables
+    assert external_table.name in tables
+    assert tmp_table.name in tables
+    assert view.name in tables
+    assert non_delta.name in tables
