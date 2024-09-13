0b01e3df7a82d28bc3772b69dcbeb691565e8b51
pritishpai <pritish.pai@databricks.com>
2024-09-13 16:47:19 -0400

    ask for custom catalog name during installation

--- src/databricks/labs/ucx/install.py
+++ src/databricks/labs/ucx/install.py
@@ -227,12 +227,14 @@ class WorkspaceInstaller(WorkspaceContext):
     def _prompt_for_new_installation(self) -> WorkspaceConfig:
         logger.info("Please answer a couple of questions to configure Unity Catalog migration")
         default_database = "ucx"
+        default_catalog = "hive_metastore"
         # if a workspace is configured to use external hive metastore, the majority of the time that metastore will be
         # shared with other workspaces. we need to add the suffix to ensure uniqueness of the inventory database
         if self.policy_installer.has_ext_hms():
             default_database = f"ucx_{self.workspace_client.get_workspace_id()}"
+        inventory_catalog = self.prompts.question("Inventory Catalog", default=default_catalog, valid_regex=r"^\w+$")
         inventory_database = self.prompts.question(
-            "Inventory Database stored in hive_metastore", default=default_database, valid_regex=r"^\w+$"
+            f"Inventory Database stored in {inventory_catalog}", default=default_database, valid_regex=r"^\w+$"
         )
         log_level = self.prompts.question("Log level", default="INFO").upper()
         num_threads = int(self.prompts.question("Number of threads", default="8", valid_number=True))
@@ -247,6 +249,7 @@ class WorkspaceInstaller(WorkspaceContext):
             self.prompts.question("Reconciliation threshold, in percentage", default="5", valid_number=True)
         )
         return WorkspaceConfig(
+            inventory_catalog=inventory_catalog,
             inventory_database=inventory_database,
             workspace_group_regex=configure_groups.workspace_group_regex,
             workspace_group_replace=configure_groups.workspace_group_replace,
