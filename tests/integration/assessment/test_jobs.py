8fe3b0133f544bfa0d6a30505cc56ba4e018325d
pritishpai <pritish.pai@databricks.com>
2024-09-13 16:46:34 -0400

    modify jobs crawler integration tests

--- tests/integration/assessment/test_jobs.py
+++ tests/integration/assessment/test_jobs.py
@@ -13,9 +13,9 @@ from .test_assessment import _SPARK_CONF
 
 
 @retried(on=[NotFound], timeout=timedelta(minutes=5))
-def test_job_crawler(ws, make_job, inventory_schema, sql_backend):
+def test_job_crawler(ws, make_job, inventory_catalog, inventory_schema, sql_backend):
     new_job = make_job(spark_conf=_SPARK_CONF)
-    job_crawler = JobsCrawler(ws=ws, sbe=sql_backend, schema=inventory_schema)
+    job_crawler = JobsCrawler(ws=ws, sbe=sql_backend, catalog=inventory_catalog, schema=inventory_schema)
     jobs = job_crawler.snapshot()
     results = []
     for job in jobs:
