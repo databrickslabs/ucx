61877cbc3217c16d99369dc8a36d63b535e69a53
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-13 10:02:08 +0200

    Extend MockBackend to check if empty rows are written

--- tests/unit/hive_metastore/test_workflows.py
+++ tests/unit/hive_metastore/test_workflows.py
@@ -1,4 +1,6 @@
 import pytest
+from databricks.labs.lsql.backends import DataclassInstance, MockBackend
+
 from databricks.labs.ucx.hive_metastore.workflows import (
     TableMigration,
     MigrateExternalTablesCTAS,
@@ -53,6 +55,26 @@ def test_migrate_ctas_views(run_workflow):
     ctx.workspace_client.catalogs.list.assert_called()
 
 
+class MockBackendFriend(MockBackend):
+    """A wrapper class to change the return type on :meth:`rows_written_for`."""
+
+    def rows_written_for(self, full_name: str, mode: str) -> list[DataclassInstance] | None:
+        """Retrieve the rows written for a table name given a mode.
+
+        Additionally to the logic of the parent class, differentiate between no rows (empty list) and no match (None).
+        """
+        rows: list[DataclassInstance] = []
+        found_write_match = False
+        for stub_full_name, stub_rows, stub_mode in self._save_table:
+            if not (stub_full_name == full_name and stub_mode == mode):
+                continue
+            found_write_match = True
+            rows += stub_rows
+        if len(rows) == 0 and not found_write_match:
+            return None
+        return rows
+
+
 @pytest.mark.parametrize(
     "workflow",
     [
@@ -65,6 +87,6 @@ def test_migrate_ctas_views(run_workflow):
 )
 def test_update_migration_status(run_workflow, workflow):
     """Migration status is refreshed by deleting and showing new tables"""
-    ctx = run_workflow(getattr(workflow, "update_migration_status"))
-    assert "TRUNCATE TABLE `hive_metastore`.`ucx`.`migration_status`" in ctx.sql_backend.queries
+    ctx = run_workflow(getattr(workflow, "update_migration_status"), replace={"sql_backend": MockBackendFriend()})
+    assert ctx.sql_backend.rows_written_for("hive_metastore.ucx.migration_status", "overwrite") is not None
     assert "SHOW DATABASES" in ctx.sql_backend.queries
