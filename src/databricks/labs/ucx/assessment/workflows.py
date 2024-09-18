042c160fadbd527fa00861bec9aa1618463c68c0
Andrew Snare <andrew.snare@databricks.com>
2024-09-18 14:46:23 +0200

    Type hints.

--- src/databricks/labs/ucx/assessment/workflows.py
+++ src/databricks/labs/ucx/assessment/workflows.py
@@ -194,7 +194,7 @@ class MigrationProgress(Workflow):
         super().__init__('migration-progress-experimental')
 
     @job_task
-    def crawl_tables(self, ctx: RuntimeContext):
+    def crawl_tables(self, ctx: RuntimeContext) -> None:
         """Iterates over all tables in the Hive Metastore of the current workspace and persists their metadata, such
         as _database name_, _table name_, _table type_, _table location_, etc., in the table named
         `$inventory_database.tables`. The metadata stored is then used in the subsequent tasks and workflows to, for
@@ -202,18 +202,18 @@ class MigrationProgress(Workflow):
         ctx.tables_crawler.snapshot(force_refresh=True)
 
     @job_task
-    def crawl_udfs(self, ctx: RuntimeContext):
+    def crawl_udfs(self, ctx: RuntimeContext) -> None:
         """Iterates over all UDFs in the Hive Metastore of the current workspace and persists their metadata in the
         table named `$inventory_database.udfs`. This inventory is currently used when scanning securable objects for
         issues with grants that cannot be migrated to Unit Catalog."""
         ctx.udfs_crawler.snapshot(force_refresh=True)
 
     @job_task(job_cluster="tacl")
-    def setup_tacl(self, ctx: RuntimeContext):
+    def setup_tacl(self, ctx: RuntimeContext) -> None:
         """(Optimization) Starts `tacl` job cluster in parallel to crawling tables."""
 
     @job_task(depends_on=[crawl_tables, crawl_udfs, setup_tacl], job_cluster="tacl")
-    def crawl_grants(self, ctx: RuntimeContext):
+    def crawl_grants(self, ctx: RuntimeContext) -> None:
         """Scans all securable objects for permissions that have been assigned: this include database-level permissions,
         as well permissions directly configured on objects in the (already gathered) table and UDF inventories. The
         captured information is stored in the `$inventory_database.grants` inventory table for further use during the
@@ -224,7 +224,7 @@ class MigrationProgress(Workflow):
         ctx.grants_crawler.snapshot(force_refresh=True)
 
     @job_task
-    def assess_jobs(self, ctx: RuntimeContext):
+    def assess_jobs(self, ctx: RuntimeContext) -> None:
         """Scans through all the jobs and identifies those that are not compatible with UC. The list of all the jobs is
         stored in the `$inventory.jobs` table.
 
@@ -237,7 +237,7 @@ class MigrationProgress(Workflow):
         ctx.jobs_crawler.snapshot(force_refresh=True)
 
     @job_task
-    def assess_clusters(self, ctx: RuntimeContext):
+    def assess_clusters(self, ctx: RuntimeContext) -> None:
         """Scan through all the clusters and identifies those that are not compatible with UC. The list of all the clusters
         is stored in the`$inventory.clusters` table.
 
@@ -250,7 +250,7 @@ class MigrationProgress(Workflow):
         ctx.clusters_crawler.snapshot(force_refresh=True)
 
     @job_task
-    def assess_pipelines(self, ctx: RuntimeContext):
+    def assess_pipelines(self, ctx: RuntimeContext) -> None:
         """This module scans through all the Pipelines and identifies those pipelines which has Azure Service Principals
         embedded (who has been given access to the Azure storage accounts via spark configurations) in the pipeline
         configurations.
@@ -263,7 +263,7 @@ class MigrationProgress(Workflow):
         ctx.pipelines_crawler.snapshot(force_refresh=True)
 
     @job_task
-    def crawl_cluster_policies(self, ctx: RuntimeContext):
+    def crawl_cluster_policies(self, ctx: RuntimeContext) -> None:
         """This module scans through all the Cluster Policies and get the necessary information
 
         It looks for:
@@ -274,11 +274,11 @@ class MigrationProgress(Workflow):
         ctx.policies_crawler.snapshot(force_refresh=True)
 
     @job_task(job_cluster="table_migration")
-    def setup_table_migration(self, ctx: RuntimeContext):
+    def setup_table_migration(self, ctx: RuntimeContext) -> None:
         """(Optimization) Starts `table_migration` job cluster in parallel to crawling tables."""
 
     @job_task(depends_on=[crawl_tables], job_cluster="table_migration")
-    def refresh_table_migration_status(self, ctx: RuntimeContext):
+    def refresh_table_migration_status(self, ctx: RuntimeContext) -> None:
         """Scan the tables (and views) in the inventory and record whether each has been migrated or not.
 
         The results of the scan are stored in the `$inventory.migration_status` inventory table.
