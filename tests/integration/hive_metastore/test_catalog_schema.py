2d0aa5ace9dc2629761a7da5a64640cd46a84573
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-20 15:41:10 +0200

    Fix lint

--- tests/integration/hive_metastore/test_catalog_schema.py
+++ tests/integration/hive_metastore/test_catalog_schema.py
@@ -41,8 +41,8 @@ def test_create_ucx_catalog_creates_catalog(
     runtime_ctx.catalog_schema.create_ucx_catalog(prompts, properties={"RemoveAfter": watchdog_remove_after})
 
     @retried(on=[KeyError], timeout=timedelta(seconds=20))
-    def get_catalog(catalog_name: str) -> CatalogInfo:
-        return ws.catalogs.get(catalog_name)
+    def get_catalog(name: str) -> CatalogInfo:
+        return ws.catalogs.get(name)
 
     assert get_catalog(catalog_name)
 
@@ -57,7 +57,7 @@ def test_create_catalog_schema_with_principal_acl_azure(
 ):
     if not ws.config.is_azure:
         pytest.skip("only works in azure test env")
-    ctx, _, schema_name, catalog_name = prepared_principal_acl
+    ctx, _, schema_name, catalog_name_ = prepared_principal_acl
 
     cluster = make_cluster(single_node=True, spark_conf=_SPARK_CONF, data_security_mode=DataSecurityMode.NONE)
     user = make_user()
@@ -71,7 +71,7 @@ def test_create_catalog_schema_with_principal_acl_azure(
     catalog_schema.create_all_catalogs_schemas(mock_prompts)
 
     schema_grants = ws.grants.get(SecurableType.SCHEMA, schema_name)
-    catalog_grants = ws.grants.get(SecurableType.CATALOG, catalog_name)
+    catalog_grants = ws.grants.get(SecurableType.CATALOG, catalog_name_)
     schema_grant = PrivilegeAssignment(user.user_name, [Privilege.USE_SCHEMA])
     catalog_grant = PrivilegeAssignment(user.user_name, [Privilege.USE_CATALOG])
     assert schema_grant in schema_grants.privilege_assignments
@@ -82,7 +82,7 @@ def test_create_catalog_schema_with_principal_acl_azure(
 def test_create_catalog_schema_with_principal_acl_aws(
     ws, make_user, prepared_principal_acl, make_cluster_permissions, make_cluster, env_or_skip
 ):
-    ctx, _, schema_name, catalog_name = prepared_principal_acl
+    ctx, _, schema_name, catalog_name_ = prepared_principal_acl
 
     cluster = make_cluster(
         single_node=True,
@@ -100,7 +100,7 @@ def test_create_catalog_schema_with_principal_acl_aws(
     catalog_schema.create_all_catalogs_schemas(mock_prompts)
 
     schema_grants = ws.grants.get(SecurableType.SCHEMA, schema_name)
-    catalog_grants = ws.grants.get(SecurableType.CATALOG, catalog_name)
+    catalog_grants = ws.grants.get(SecurableType.CATALOG, catalog_name_)
     schema_grant = PrivilegeAssignment(user.user_name, [Privilege.USE_SCHEMA])
     catalog_grant = PrivilegeAssignment(user.user_name, [Privilege.USE_CATALOG])
     assert schema_grant in schema_grants.privilege_assignments
