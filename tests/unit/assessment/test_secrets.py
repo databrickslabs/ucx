8c3f0b698f84f70632bffae0e950754c237413fd
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-23 16:10:19 +0200

    Test with non existing key

--- tests/unit/assessment/test_secrets.py
+++ tests/unit/assessment/test_secrets.py
@@ -7,6 +7,7 @@ from databricks.labs.ucx.assessment.secrets import SecretsMixin
     "key,expected",
     [
         ("spark_conf.spark.hadoop.javax.jdo.option.ConnectionURL", "url"),
+        ("NonExistentKey", ""),
     ]
 )
 def test_secrets_mixin_gets_value_from_config_key(key, expected) -> None:
