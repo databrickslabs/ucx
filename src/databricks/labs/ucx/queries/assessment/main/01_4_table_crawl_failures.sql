77c39315006349a3a1562b7c374332ee739b31e0
pritishpai <pritish.pai@databricks.com>
2024-09-24 15:58:45 -0400

    add table failures widget

---
+++ src/databricks/labs/ucx/queries/assessment/main/01_4_table_crawl_failures.sql
@@ -0,0 +1,21 @@
+--title 'Table Crawl Failures'
+--height 4
+--width 5
+WITH table_crawl_failures AS (
+SELECT
+    timestamp,
+    REGEXP_EXTRACT(message, '^failed-table-crawl: (.+?) -> (.+?): (.+)$', 1) AS error_reason,
+    REGEXP_EXTRACT(message, '^failed-table-crawl: (.+?) -> (.+?): (.+)$', 2) AS error_entity,
+    REGEXP_EXTRACT(message, '^failed-table-crawl: (.+?) -> (.+?): (.+)$', 3) AS error_message,
+FROM inventory.logs
+  WHERE
+    STARTSWITH(message, 'failed-table-crawl: ')
+)
+SELECT
+  timestamp,
+  error_entity,
+  error_reason,
+  error_message
+FROM table_crawl_failures
+ORDER BY
+  1
