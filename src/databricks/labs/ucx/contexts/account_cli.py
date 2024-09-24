b90440b384e8d61509525935ccd8006a8edad387
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-24 15:19:21 +0200

    Initialise `CatalogSchema` on `WorkspaceContext` and `AccountContext` separately
    TODO: Split `CatalogSchema` into `Catalogs` and `Schemas` https://github.com/databrickslabs/ucx/issues/2735

--- src/databricks/labs/ucx/contexts/account_cli.py
+++ src/databricks/labs/ucx/contexts/account_cli.py
@@ -1,12 +1,13 @@
 from functools import cached_property
 from os import environ
 
-from databricks.sdk import AccountClient, Workspace, WorkspaceClient
+from databricks.sdk import AccountClient, WorkspaceClient
 
 from databricks.labs.ucx.account.aggregate import AccountAggregate
 from databricks.labs.ucx.account.metastores import AccountMetastores
 from databricks.labs.ucx.account.workspaces import AccountWorkspaces
 from databricks.labs.ucx.contexts.application import CliContext
+from databricks.labs.ucx.hive_metastore.catalog_schema import CatalogSchema
 
 
 class AccountContext(CliContext):
@@ -47,3 +48,8 @@ class AccountContext(CliContext):
     @cached_property
     def account_metastores(self):
         return AccountMetastores(self.account_client)
+
+    @cached_property
+    def catalog_schema(self):
+        # TODO: Split `CatalogSchema` into `Catalogs` and `Schemas` https://github.com/databrickslabs/ucx/issues/2735
+        return CatalogSchema(self.workspace_client, self.config.ucx_catalog)
