1e7cdac089c92ebc7a337982b232af0f48e311ea
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-25 12:07:07 +0200

    Test history installation to create history schema

---
+++ tests/unit/progress/test_install.py
@@ -0,0 +1,7 @@
+from databricks.labs.ucx.progress.install import HistoryInstallation
+
+
+def test_history_installation_run_creates_history_schema(mock_backend) -> None:
+    installation = HistoryInstallation(mock_backend, "ucx")
+    installation.run()
+    assert "CREATE SCHEMA IF NOT EXISTS `ucx`.`history`" == mock_backend.queries[0]
