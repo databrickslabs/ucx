01b438f17895e6716f89f1d14fe714702bdb6c28
Cor Zuurmond <jczuurmond@protonmail.com>
2024-09-26 17:06:10 +0200

    Add job name and start time to task parameters

--- src/databricks/labs/ucx/installer/workflows.py
+++ src/databricks/labs/ucx/installer/workflows.py
@@ -55,9 +55,12 @@ logger = logging.getLogger(__name__)
 
 TEST_RESOURCE_PURGE_TIMEOUT = timedelta(hours=1)
 TEST_NIGHTLY_CI_RESOURCES_PURGE_TIMEOUT = timedelta(hours=3)  # Buffer for debugging nightly integration test runs
+# See https://docs.databricks.com/en/jobs/parameter-value-references.html#supported-value-references
 EXTRA_TASK_PARAMS = {
     "job_id": "{{job_id}}",
+    "job_name": "{{job.name}}",
     "run_id": "{{run_id}}",
+    "start_time": "{{job.start_time.iso_datetime}}",
     "attempt": "{{job.repair_count}}",
     "parent_run_id": "{{parent_run_id}}",
 }
