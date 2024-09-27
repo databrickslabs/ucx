d5951601ba03336995e56648d4c1210cb043dbd3
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-09-27 11:36:28 +0200

    prevent linting twice

--- src/databricks/labs/ucx/source_code/linters/files.py
+++ src/databricks/labs/ucx/source_code/linters/files.py
@@ -167,6 +167,8 @@ class LocalCodeLinter:
             def _process_dependency(
                 self, dependency: Dependency, path_lookup: PathLookup, inherited_tree: Tree | None
             ) -> Iterable[LocatedAdvice]:
+                if dependency.path in linted_paths:
+                    return
                 ctx = context_factory()
                 # FileLinter will determine which file/notebook linter to use
                 linter = FileLinter(ctx, path_lookup, session_state, dependency.path, inherited_tree)
