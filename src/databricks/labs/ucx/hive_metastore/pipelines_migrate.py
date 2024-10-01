dfe9b45a70fd03342ef25587f388cf60fed1e6a7
pritishpai <pritish.pai@databricks.com>
2024-10-01 17:03:54 -0400

    WIP rule class for source to destination pipeline mapping

--- src/databricks/labs/ucx/hive_metastore/pipelines_migrate.py
+++ src/databricks/labs/ucx/hive_metastore/pipelines_migrate.py
@@ -1,11 +1,21 @@
 import dataclasses
 import logging
+from dataclasses import dataclass
 
 from databricks.sdk import WorkspaceClient
 
 from databricks.labs.ucx.assessment.pipelines import PipelinesCrawler
 
 logger = logging.getLogger(__name__)
+
+@dataclass
+class Rule:
+    workspace_name: str
+    source_pipeline_id: str
+    source_pipeline_name: str
+    target_schema_name: str
+    target_pipeline_name: str
+
 
 class PipelinesMigrator:
     def __init__(self,
