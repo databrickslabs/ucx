9337be2baefce8729e9ce33cd131501d5375c90a
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-02 16:46:53 +0200

    Set max wait on run assessment workflow to 25 minutes

--- tests/integration/progress/test_workflows.py
+++ tests/integration/progress/test_workflows.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+import datetime as dt
 
 from databricks.sdk.errors import NotFound, InvalidParameterValue
 from databricks.sdk.retries import retried
@@ -6,7 +6,7 @@ from databricks.sdk.retries import retried
 from ..conftest import MockInstallationContext
 
 
-@retried(on=[NotFound, InvalidParameterValue], timeout=timedelta(minutes=12))
+@retried(on=[NotFound, InvalidParameterValue], timeout=dt.timedelta(minutes=12))
 def test_running_real_migration_progress_job(installation_ctx: MockInstallationContext) -> None:
     """Ensure that the migration-progress workflow can complete successfully."""
     installation_ctx.workspace_installation.run()
@@ -14,7 +14,7 @@ def test_running_real_migration_progress_job(installation_ctx: MockInstallationC
     # The assessment workflow is a prerequisite for migration-progress: it needs to successfully complete before we can
     # test the migration-progress workflow.
     workflow = "assessment"
-    installation_ctx.deployed_workflows.run_workflow(workflow)
+    installation_ctx.deployed_workflows.run_workflow(workflow, max_wait=dt.timedelta(minutes=25))
     assert installation_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     # After the assessment, a user (maybe) installs the progress tracking
