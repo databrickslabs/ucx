b03e2696ee0cdd722a63c41850b7974daec8bb4c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-02 15:50:06 +0200

    Add assert message for failing workflow

--- tests/integration/progress/test_workflows.py
+++ tests/integration/progress/test_workflows.py
@@ -13,15 +13,17 @@ def test_running_real_migration_progress_job(installation_ctx: MockInstallationC
 
     # The assessment workflow is a prerequisite for migration-progress: it needs to successfully complete before we can
     # test the migration-progress workflow.
-    installation_ctx.deployed_workflows.run_workflow("assessment")
-    assert installation_ctx.deployed_workflows.validate_step("assessment")
+    workflow = "assessment"
+    installation_ctx.deployed_workflows.run_workflow(workflow)
+    assert installation_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     # After the assessment, a user (maybe) installs the progress tracking
     installation_ctx.progress_tracking_installation.run()
 
     # Run the migration-progress workflow until completion.
-    installation_ctx.deployed_workflows.run_workflow("migration-progress-experimental")
-    assert installation_ctx.deployed_workflows.validate_step("migration-progress-experimental")
+    workflow = "migration-progress-experimental"
+    installation_ctx.deployed_workflows.run_workflow(workflow)
+    assert installation_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     # Ensure that the migration-progress workflow populated the `workflow_runs` table.
     query = f"SELECT 1 FROM {installation_ctx.ucx_catalog}.multiworkspace.workflow_runs"
