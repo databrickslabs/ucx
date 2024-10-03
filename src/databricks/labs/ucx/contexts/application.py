4fe46ff6b57957906f6ba221f4227eb16af4a8e6
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-03 11:53:33 +0200

    Fix import Iterable

--- src/databricks/labs/ucx/contexts/application.py
+++ src/databricks/labs/ucx/contexts/application.py
@@ -1,10 +1,9 @@
 import abc
 import logging
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from datetime import timedelta
 from functools import cached_property
 from pathlib import Path
-from typing import Iterable
 
 from databricks.labs.blueprint.installation import Installation
 from databricks.labs.blueprint.installer import InstallState
