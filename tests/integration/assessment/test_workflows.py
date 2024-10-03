61783cc046578526c1b6481cc5226c49fe0e3e4b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-03 13:22:32 +0200

    Include database for linting

--- tests/integration/assessment/test_workflows.py
+++ tests/integration/assessment/test_workflows.py
@@ -36,6 +36,7 @@ def test_running_real_assessment_job(
         config_transform = lambda wc: dataclasses.replace(
             wc,
             include_object_permissions=[f"cluster-policies:{cluster_policy.policy_id}"],
+            include_databases=[source_schema.name]
         ),
     )
     installation_ctx.workspace_installation.run()
