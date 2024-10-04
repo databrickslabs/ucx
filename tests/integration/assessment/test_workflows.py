f418d468358ebda8326c840a58cbea7708512239
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-04 18:03:36 +0200

    Reuse populate for linting in integration tests that trigger assessment job

--- tests/integration/assessment/test_workflows.py
+++ tests/integration/assessment/test_workflows.py
@@ -11,14 +11,20 @@ def test_running_real_assessment_job(
     make_cluster_policy,
     make_cluster_policy_permissions,
     make_dashboard,
-    populate_for_linting,
 ) -> None:
     ws_group, _ = installation_ctx.make_ucx_group()
+    # TODO: Move `make_cluster_policy` and `make_cluster_policy_permissions` to context like other `make_` methods
     cluster_policy = make_cluster_policy()
     make_cluster_policy_permissions(
         object_id=cluster_policy.policy_id,
         permission_level=PermissionLevel.CAN_USE,
         group_name=ws_group.display_name,
+    )
+    installation_ctx = installation_ctx.replace(
+        config_transform=lambda wc: dataclasses.replace(
+            wc,
+            include_object_permissions=[f"cluster-policies:{cluster_policy.policy_id}"],
+        ),
     )
 
     source_schema = installation_ctx.make_schema(catalog_name="hive_metastore")
@@ -27,28 +33,17 @@ def test_running_real_assessment_job(
     tmp_table = installation_ctx.make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four")
     view = installation_ctx.make_table(schema_name=source_schema.name, ctas="SELECT 2+2 AS four", view=True)
     non_delta = installation_ctx.make_table(schema_name=source_schema.name, non_delta=True)
-    installation_ctx = installation_ctx.replace(
-        config_transform=lambda wc: dataclasses.replace(
-            wc,
-            include_object_permissions=[f"cluster-policies:{cluster_policy.policy_id}"],
-            include_databases=[source_schema.name],
-        ),
-    )
+    installation_ctx.make_linting_resources()
     installation_ctx.workspace_installation.run()
-    populate_for_linting(installation_ctx.installation)
 
     workflow = "assessment"
     installation_ctx.deployed_workflows.run_workflow(workflow)
     assert installation_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     after = installation_ctx.generic_permissions_support.load_as_dict("cluster-policies", cluster_policy.policy_id)
-
     assert after[ws_group.display_name] == PermissionLevel.CAN_USE
 
-    tables = set[str]()
-    for table in installation_ctx.tables_crawler.snapshot():
-        tables.add(table.name)
-
+    tables = set(table.name for table in installation_ctx.tables_crawler.snapshot())
     expected_tables = {managed_table.name, external_table.name, tmp_table.name, view.name, non_delta.name}
     assert len(tables) == len(expected_tables)
     assert set(tables) == expected_tables
