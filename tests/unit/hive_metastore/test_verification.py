e36db5ad55a9c129a2949b66800aa662ca1ea3b2
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-07 10:32:36 +0200

    Add return types to tests

--- tests/unit/hive_metastore/test_verification.py
+++ tests/unit/hive_metastore/test_verification.py
@@ -7,13 +7,13 @@ from databricks.labs.ucx.hive_metastore.verification import (
 from tests.unit import mock_workspace_client
 
 
-def test_verify_has_metastore():
+def test_verify_has_metastore() -> None:
     ws = mock_workspace_client()
     verify = VerifyHasMetastore(ws)
     assert verify.verify_metastore()
 
 
-def test_verify_missing_metastore():
+def test_verify_missing_metastore() -> None:
     ws = mock_workspace_client()
     ws.metastores.current.return_value = None
     verify = VerifyHasMetastore(ws)
