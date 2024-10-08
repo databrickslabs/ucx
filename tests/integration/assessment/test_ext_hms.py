ca42c02480dd27a33e601ab78c6b8f6b4b12f6d4
Cor <jczuurmond@protonmail.com>
2024-10-08 13:14:56 +0200

    Limit resources to assess in integration tests that run the `assessment` workflow (#2815)
    ## Changes
    Scope resources to run the assessment workflow for in integration tests
    to shorten the time it takes for the assessment to run:
    
    <img width="1582" alt="Screenshot 2024-10-04 at 17 45 52"
    src="https://github.com/user-attachments/assets/8ee12e89-1068-45f4-a5de-c2da121a6101">
    
    - Move the populate for linting logic to the context
    - Set the `include_job_ids` to the job ids created by the populate for
    linting logic
    - Set the `include_dashboard_ids` to the dashboard ids created by the
    populate for linting logic
    - Move the create job based on Python file fixture to
    [pytester](https://github.com/databrickslabs/pytester/pull/60)
    
    ### Linked issues
    
    Resolves #2637
    Resolves #2849
    
    ### Tests
    
    - [x] modified integration tests: 
           - `test_running_real_assessment_job`
           - `test_running_real_assessment_job_ext_hms`
           - `test_running_real_migration_progress_job`

--- tests/integration/assessment/test_ext_hms.py
+++ tests/integration/assessment/test_ext_hms.py
@@ -1,5 +1,4 @@
 import dataclasses
-import datetime as dt
 
 from databricks.labs.lsql.backends import CommandExecutionBackend
 from databricks.sdk.service.iam import PermissionLevel
@@ -8,18 +7,25 @@ from databricks.sdk.service.iam import PermissionLevel
 def test_running_real_assessment_job_ext_hms(
     ws,
     installation_ctx,
-    product_info,
     env_or_skip,
     make_cluster_policy,
     make_cluster_policy_permissions,
-    populate_for_linting,
-):
+) -> None:
     cluster_id = env_or_skip('TEST_EXT_HMS_CLUSTER_ID')
+    ws_group, _ = installation_ctx.make_ucx_group(wait_for_provisioning=True)
+    # TODO: Move `make_cluster_policy` and `make_cluster_policy_permissions` to context like other `make_` methods
+    cluster_policy = make_cluster_policy()
+    make_cluster_policy_permissions(
+        object_id=cluster_policy.policy_id,
+        permission_level=PermissionLevel.CAN_USE,
+        group_name=ws_group.display_name,
+    )
     ext_hms_ctx = installation_ctx.replace(
         sql_backend=CommandExecutionBackend(ws, cluster_id),
         config_transform=lambda wc: dataclasses.replace(
             wc,
             override_clusters=None,
+            include_object_permissions=[f"cluster-policies:{cluster_policy.policy_id}"],
         ),
         extend_prompts={
             r"Instance pool id to be set.*": env_or_skip("TEST_INSTANCE_POOL_ID"),
@@ -28,26 +34,15 @@ def test_running_real_assessment_job_ext_hms(
             r"Choose a cluster policy": "0",
         },
     )
-    ws_group_a, _ = ext_hms_ctx.make_ucx_group(wait_for_provisioning=True)
-
-    cluster_policy = make_cluster_policy()
-    make_cluster_policy_permissions(
-        object_id=cluster_policy.policy_id,
-        permission_level=PermissionLevel.CAN_USE,
-        group_name=ws_group_a.display_name,
-    )
-    ext_hms_ctx.__dict__['include_object_permissions'] = [f"cluster-policies:{cluster_policy.policy_id}"]
+    ext_hms_ctx.make_linting_resources()
+    source_schema = ext_hms_ctx.make_schema(catalog_name="hive_metastore")
+    ext_hms_ctx.make_table(schema_name=source_schema.name)
     ext_hms_ctx.workspace_installation.run()
 
-    populate_for_linting(installation_ctx.installation)
-
-    # Under ideal circumstances this can take 10-16 minutes (depending on whether there are compute instances available
-    # via the integration pool). Allow some margin to reduce spurious failures.
-    ext_hms_ctx.deployed_workflows.run_workflow("assessment", max_wait=dt.timedelta(minutes=25))
-
-    # assert the workflow is successful. the tasks on sql warehouse will fail so skip checking them
-    assert ext_hms_ctx.deployed_workflows.validate_step("assessment")
+    workflow = "assessment"
+    ext_hms_ctx.deployed_workflows.run_workflow(workflow)
+    assert ext_hms_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     after = ext_hms_ctx.generic_permissions_support.load_as_dict("cluster-policies", cluster_policy.policy_id)
-    assert ws_group_a.display_name in after, f"Group {ws_group_a.display_name} not found in cluster policy"
-    assert after[ws_group_a.display_name] == PermissionLevel.CAN_USE
+    assert ws_group.display_name in after, f"Group {ws_group.display_name} not found in cluster policy"
+    assert after[ws_group.display_name] == PermissionLevel.CAN_USE
--- tests/integration/progress/test_workflows.py
+++ tests/integration/progress/test_workflows.py
@@ -9,12 +9,18 @@ from ..conftest import MockInstallationContext
 @retried(on=[NotFound, InvalidParameterValue], timeout=dt.timedelta(minutes=12))
 def test_running_real_migration_progress_job(installation_ctx: MockInstallationContext) -> None:
     """Ensure that the migration-progress workflow can complete successfully."""
+    # Limit the resources crawled by the assessment
+    source_schema = installation_ctx.make_schema()
+    installation_ctx.make_table(schema_name=source_schema.name)
+    installation_ctx.make_linting_resources()
+    installation_ctx.make_group()
+
     installation_ctx.workspace_installation.run()
 
     # The assessment workflow is a prerequisite for migration-progress: it needs to successfully complete before we can
     # test the migration-progress workflow.
     workflow = "assessment"
-    installation_ctx.deployed_workflows.run_workflow(workflow, max_wait=dt.timedelta(minutes=25))
+    installation_ctx.deployed_workflows.run_workflow(workflow)
     assert installation_ctx.deployed_workflows.validate_step(workflow), f"Workflow failed: {workflow}"
 
     # After the assessment, a user (maybe) installs the progress tracking
