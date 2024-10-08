e15795c58e26f254ce7b44c5cc37fb663b8c140f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-08 10:10:36 +0200

    Find exact matches and parent module matches

--- src/databricks/labs/ucx/source_code/known.py
+++ src/databricks/labs/ucx/source_code/known.py
@@ -91,8 +91,14 @@ class KnownList:
     def module_compatibility(self, name: str) -> Compatibility:
         if not name:
             return UNKNOWN
+        # Find exact matches.
         for module, problems in self._module_problems.items():
-            if not name.startswith(module):
+            if name != module:
+                continue
+            return Compatibility(True, problems)
+        # Find parent module matches.
+        for module, problems in self._module_problems.items():
+            if not name.startswith(module + "."):
                 continue
             return Compatibility(True, problems)
         return UNKNOWN
