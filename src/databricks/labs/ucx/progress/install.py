62e8da3df7722000af3e3611c2a1b93983e969e6
Cor <jczuurmond@protonmail.com>
2024-10-09 20:00:50 +0200

    Add UCX history schema and table for storing UCX's artifact (#2744)
    <!-- REMOVE IRRELEVANT COMMENTS BEFORE CREATING A PULL REQUEST -->
    ## Changes
    Add UCX history schema and table for storing UCX's artifact. The
    artifacts are, amongst other things, going to be used for migration
    progress tracking.
    
    ### Linked issues
    
    Resolves #2572 
    
    ### Functionality
    
    - [x] modified existing command: `databricks labs ucx
    create-ucx-catalog`
    
    ### Tests
    
    - [ ] manually tested
    - [x] added unit tests
    - [x] added integration tests

--- src/databricks/labs/ucx/progress/install.py
+++ src/databricks/labs/ucx/progress/install.py
@@ -1,10 +1,41 @@
 import logging
+from dataclasses import dataclass
 
 from databricks.labs.lsql.backends import SqlBackend
 from databricks.labs.lsql.deployment import SchemaDeployer
+
+from databricks.labs.ucx.__about__ import __version__
 from databricks.labs.ucx.progress.workflow_runs import WorkflowRun
 
+
 logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Historical:
+    workspace_id: int
+    """The identifier of the workspace where this historical record was generated."""
+
+    job_run_id: int
+    """The identifier of the job run that generated this historical record."""
+
+    object_type: str
+    """The inventory table for which this historical record was generated."""
+
+    object_id: list[str]
+    """The type-specific identifier for the corresponding inventory record."""
+
+    data: dict[str, str]
+    """Type-specific JSON-encoded data of the inventory record."""
+
+    failures: list[str]
+    """The list of problems associated with the object that this inventory record covers."""
+
+    owner: str
+    """The identity that has ownership of the object."""
+
+    ucx_version: str = __version__
+    """The UCX semantic version."""
 
 
 class ProgressTrackingInstallation:
@@ -19,4 +50,5 @@ class ProgressTrackingInstallation:
     def run(self) -> None:
         self._schema_deployer.deploy_schema()
         self._schema_deployer.deploy_table("workflow_runs", WorkflowRun)
+        self._schema_deployer.deploy_table("historical", Historical)
         logger.info("Installation completed successfully!")
--- tests/unit/progress/test_install.py
+++ tests/unit/progress/test_install.py
@@ -7,8 +7,8 @@ def test_progress_tracking_installation_run_creates_progress_tracking_schema(moc
     assert "CREATE SCHEMA IF NOT EXISTS ucx.multiworkspace" in mock_backend.queries[0]
 
 
-def test_progress_tracking_installation_run_creates_workflow_runs_table(mock_backend) -> None:
+def test_progress_tracking_installation_run_creates_tables(mock_backend) -> None:
     installation = ProgressTrackingInstallation(mock_backend, "ucx")
     installation.run()
     # Dataclass to schema conversion is tested within the lsql package
-    assert any("CREATE TABLE IF NOT EXISTS" in query for query in mock_backend.queries)
+    assert sum("CREATE TABLE IF NOT EXISTS" in query for query in mock_backend.queries) == 2
