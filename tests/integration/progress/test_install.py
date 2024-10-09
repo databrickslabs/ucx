d2556a55e7cd2a8a6994216e30f6f0d5670a59ec
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-09 14:25:24 +0200

    Test for historical records table to be created

--- tests/integration/progress/test_install.py
+++ tests/integration/progress/test_install.py
@@ -1,7 +1,11 @@
-def test_progress_tracking_installer_creates_workflow_runs_table(az_cli_ctx) -> None:
+import pytest
+
+
+@pytest.mark.parametrize("table_name", ["workflow_runs", "historical_records"])
+def test_progress_tracking_installer_creates_table(az_cli_ctx, table_name) -> None:
     az_cli_ctx.progress_tracking_installation.run()
     query = (
         f"SELECT 1 FROM tables WHERE table_catalog = '{az_cli_ctx.config.ucx_catalog}' "
-        "AND table_schema = 'multiworkspace' AND table_name = 'workflow_runs'"
+        f"AND table_schema = 'multiworkspace' AND table_name = '{table_name}'"
     )
     assert any(az_cli_ctx.sql_backend.fetch(query, catalog="system", schema="information_schema"))
