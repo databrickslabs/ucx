4774759eebe9af0ddef6513a1714e35c1ed98fea
Eric Vergnaud <eric.vergnaud@databricks.com>
2024-10-10 14:00:27 +0200

    reproduce error

--- tests/integration/source_code/test_cells.py
+++ tests/integration/source_code/test_cells.py
@@ -5,6 +5,7 @@ from databricks.sdk.service.workspace import Language
 from databricks.labs.ucx.source_code.base import CurrentSessionState
 from databricks.labs.ucx.source_code.graph import Dependency, DependencyGraph
 from databricks.labs.ucx.source_code.linters.files import FileLoader
+from databricks.labs.ucx.source_code.notebooks.loaders import NotebookLoader
 from databricks.labs.ucx.source_code.notebooks.sources import Notebook
 
 
@@ -24,4 +25,21 @@ def test_malformed_pip_cell_is_supported(simple_ctx):
         dependency, None, simple_ctx.dependency_resolver, simple_ctx.path_lookup, CurrentSessionState()
     )
     problems = notebook.build_dependency_graph(parent)
+    assert not problems
+
+
+def test_relative_grand_parent_path_is_supported(simple_ctx, make_notebook, make_directory, make_random, watchdog_purge_suffix):
+    grand_parent = make_notebook()
+    top_dir = make_directory()
+    child_dir = make_directory(path = f"~/{top_dir.name}/dummy-{make_random(4)}-{watchdog_purge_suffix}")
+    source=f"""
+%run ../../{grand_parent.name}
+"""
+    notebook_path = make_notebook(path=f"{child_dir.as_posix()}/dummy-{make_random(4)}-{watchdog_purge_suffix}", content=source.encode("utf-8"))
+    dependency = Dependency(NotebookLoader(), notebook_path)
+    root = DependencyGraph(
+        dependency, None, simple_ctx.dependency_resolver, simple_ctx.path_lookup, CurrentSessionState()
+    )
+    container = dependency.load(simple_ctx.path_lookup)
+    problems = container.build_dependency_graph(root)
     assert not problems
