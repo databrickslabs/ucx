0977e56b2e6f3a3bb87ba6d105db2e787d6abbc5
pritishpai <pritish.pai@databricks.com>
2024-10-10 17:49:13 -0400

    Add PipelineMapping

--- src/databricks/labs/ucx/contexts/application.py
+++ src/databricks/labs/ucx/contexts/application.py
@@ -12,6 +12,8 @@ from databricks.labs.blueprint.tui import Prompts
 from databricks.labs.blueprint.wheels import ProductInfo, WheelsV2
 from databricks.labs.lsql.backends import SqlBackend
 
+from databricks.labs.ucx.assessment.pipelines import PipelinesCrawler
+from databricks.labs.ucx.hive_metastore.pipelines_migrate import PipelinesMigrator
 from databricks.labs.ucx.recon.data_comparator import StandardDataComparator
 from databricks.labs.ucx.recon.data_profiler import StandardDataProfiler
 from databricks.labs.ucx.recon.metadata_retriever import DatabricksTableMetadataRetriever
@@ -338,6 +340,13 @@ class GlobalContext(abc.ABC):
         return TableOwnershipGrantLoader(self.tables_crawler, self.default_securable_ownership)
 
     @cached_property
+    def pipeline_migrator(self) -> PipelinesMigrator:
+        return PipelinesMigrator(
+            self.workspace_client,
+            self.pipelines_crawler
+        )
+
+    @cached_property
     def migrate_grants(self) -> MigrateGrants:
         # owner grants have to come first
         grant_loaders: list[Callable[[], Iterable[Grant]]] = [
@@ -364,6 +373,10 @@ class GlobalContext(abc.ABC):
             self.inventory_database,
             self.config.enable_hms_federation,
         )
+
+    @cached_property
+    def pipelines_crawler(self) -> PipelinesCrawler:
+        return PipelinesCrawler(self.workspace_client, self.sql_backend, self.inventory_database)
 
     @cached_property
     def azure_service_principal_crawler(self) -> AzureServicePrincipalCrawler:
--- src/databricks/labs/ucx/hive_metastore/pipelines_migrate.py
+++ src/databricks/labs/ucx/hive_metastore/pipelines_migrate.py
@@ -2,7 +2,9 @@ import dataclasses
 import logging
 from dataclasses import dataclass
 
+from databricks.labs.lsql.backends import SqlBackend
 from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.marketplace import Installation
 
 from databricks.labs.ucx.assessment.pipelines import PipelinesCrawler, PipelineInfo
 
@@ -27,17 +29,56 @@ class PipelineToMigrate:
     def __eq__(self, other):
         return isinstance(other, PipelineToMigrate) and self.src == other.src
 
+class PipelineMapping:
+    FILENAME = "pipeline_mapping.csv"
+
+    def __init__(self,
+                 installation: Installation,
+                 ws: WorkspaceClient,
+                 sql_backend: SqlBackend,
+                 ):
+        self._installation = installation
+        self._ws = ws
+        self._sql_backend = sql_backend
+
+    def current_pipelines(self,
+                          pipelines: PipelinesCrawler,
+                          workspace_name: str,
+                          catalog_name: str):
+        pipeline_snapshot = list(pipelines.snapshot())
+        if not pipeline_snapshot:
+            msg = "No pipelines found."
+            raise ValueError(msg)
+        for pipelines in pipeline_snapshot:
+            yield Rule.initial()
+
+        return self._pc.snapshot()
+
 
 class PipelinesMigrator:
     def __init__(self,
-                 pipeline_crawler: PipelinesCrawler,
-                 ws: WorkspaceClient):
-        self._pc = pipeline_crawler
+                 ws: WorkspaceClient,
+                 pipeline_crawler: PipelinesCrawler):
         self._ws = ws
+        self._pc = pipeline_crawler
 
 
     def migrate_pipelines(self):
+        self._migrate_pipelines()
+
+    def _migrate_pipelines(self):
         pipelines = self._pc.snapshot()
         logger.info(f"Found {len(pipelines)} pipelines to migrate")
 
+        # get pipelines to migrate
+
+        # get rules
+
+        # call api to migrate
+
+        # return or add to
+
+    def _get_pipelines_to_migrate(self):
+
+        pass
 
