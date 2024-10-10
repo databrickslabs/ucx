a0a1d33b69e7492f3a6ac41b94fc78677d15eb7b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-10 15:24:05 +0200

    Add missing type hints

--- src/databricks/labs/ucx/source_code/python/python_infer.py
+++ src/databricks/labs/ucx/source_code/python/python_infer.py
@@ -66,7 +66,7 @@ class InferredValue:
             yield from cls._safe_infer_internal(node)
 
     @classmethod
-    def _safe_infer_internal(cls, node: NodeNG):
+    def _safe_infer_internal(cls, node: NodeNG) -> Iterator[Iterable[NodeNG]]:
         try:
             yield from cls._unsafe_infer_internal(node)
         except InferenceError as e:
@@ -74,7 +74,7 @@ class InferredValue:
             yield [Uninferable]
 
     @classmethod
-    def _unsafe_infer_internal(cls, node: NodeNG):
+    def _unsafe_infer_internal(cls, node: NodeNG) -> Iterator[Iterable[NodeNG]]:
         all_inferred = node.inferred()
         if len(all_inferred) == 0 and isinstance(node, Instance):
             yield [node]
@@ -148,7 +148,7 @@ class _DbUtilsWidgetsGetCall(NodeNG):
 class _LocalInferredValue(InferredValue):
 
     @classmethod
-    def do_infer_values(cls, node: NodeNG):
+    def do_infer_values(cls, node: NodeNG) -> Iterator[Iterable[NodeNG]]:
         yield from cls._infer_values(node)
 
 
