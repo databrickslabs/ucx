2480d4c4c1fe92d82e48f9bdd627ef87e15c7c76
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-11 16:28:41 +0200

    Use literal to limit strings

--- src/databricks/labs/ucx/azure/resources.py
+++ src/databricks/labs/ucx/azure/resources.py
@@ -3,7 +3,7 @@ import time
 from collections.abc import Iterable
 from dataclasses import dataclass, field
 from datetime import timedelta
-from typing import Any
+from typing import Any, Literal
 
 from databricks.sdk.core import (
     ApiClient,
@@ -114,7 +114,7 @@ class StorageAccount:
     id: AzureResource
     name: str
     location: str
-    default_network_action: str  # "Unknown", "Deny" or "Allow"
+    default_network_action: Literal["Unknown", "Deny", "Allow"]
 
     @classmethod
     def from_raw_resource(cls, raw: RawResource) -> "StorageAccount":
