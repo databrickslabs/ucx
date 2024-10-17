6607b9827f6797912491a52e7bbd828b392a7568
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-17 10:30:36 +0200

    Skip ACL for system groups

--- src/databricks/labs/ucx/hive_metastore/grants.py
+++ src/databricks/labs/ucx/hive_metastore/grants.py
@@ -592,6 +592,8 @@ class AzureACL:
 
 
 class PrincipalACL:
+    _system_groups = {"admins", "users"}
+
     def __init__(
         self,
         ws: WorkspaceClient,
@@ -692,7 +694,7 @@ class PrincipalACL:
             if acl.user_name is not None:
                 principal_list.append(acl.user_name)
             if acl.group_name is not None:
-                if acl.group_name == "admins":
+                if acl.group_name in self._system_groups:
                     continue
                 principal_list.append(acl.group_name)
             if acl.service_principal_name is not None:
