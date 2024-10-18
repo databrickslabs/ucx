1b94caf0adb4264a0d3ebe36da2978868ac4a2a9
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-18 13:25:22 +0200

    Fix typo

--- src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
@@ -1,4 +1,4 @@
-/* --title 'Readiness (%)' --description 'Ready to-be migrated' */
+/* --title 'Readiness (%)' --description 'Ready to be migrated' */
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM inventory.historical  -- TODO: Rename inventory
