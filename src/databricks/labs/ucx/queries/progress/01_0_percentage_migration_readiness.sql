d8f64ff362d1fdd535a709a15ca420e4850424f6
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-18 11:14:41 +0200

    Add tables to populated schema

--- src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
@@ -1,4 +1,4 @@
-/* --title 'Successfully migrated resources (%)' */
+/* --title 'Readiness (%)' --description 'Ready to-be migrated' */
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM inventory.historical
