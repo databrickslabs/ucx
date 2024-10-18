a41607ebbda043c56f7dbcce348c2897d309cb31
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-18 11:14:41 +0200

    Add tables to populated schema

--- tests/integration/queries/test_migration_progress.py
+++ tests/integration/queries/test_migration_progress.py
@@ -1,3 +1,4 @@
+import dataclasses
 import webbrowser
 
 import pytest
@@ -7,19 +8,45 @@ from databricks.labs.blueprint.wheels import find_project_root
 from databricks.labs.lsql.dashboards import DashboardMetadata, Dashboards
 
 from databricks.labs.ucx.progress.install import Historical
+from databricks.labs.ucx.hive_metastore.tables import Table
 
 
 @pytest.fixture
-def schema_populated(ws: WorkspaceClient, sql_backend, make_catalog, make_schema) -> SchemaInfo:
+def tables() -> list[Table]:
+    return [
+        Table("hive_metastore", "foo", "bar", "MANAGED", "delta"),
+        Table("hive_metastore", "foo", "bar", "MANAGED", "delta", location="s3://test_location/test1/table1"),
+        Table("hive_metastore", "foo", "bar", "EXTERNAL", "delta", location="s3://test_location/test2/table2"),
+        Table("hive_metastore", "foo", "bar", "EXTERNAL", "delta", location="dbfs:/mnt/foo/test3/table3"),
+    ]
+
+
+@pytest.fixture
+def schema_populated(ws: WorkspaceClient, sql_backend, make_catalog, make_schema, tables) -> SchemaInfo:
     # Different to the other dashboards, the migration process dashboard uses data from a UC catalog,
     # not from the Hive metastore
     catalog = make_catalog()
     schema = make_schema(catalog_name=catalog.name)
     workspace_id = ws.get_workspace_id()
-    historicals = [
-        Historical(workspace_id, 1, "test", ["test"], {"test": "test"}, [], "Cor"),
-        Historical(workspace_id, 1, "test", ["test"], {"test": "test"}, ["failure"], "Cor"),
-    ]
+    historicals = []
+    for table in tables:
+        # TODO: Use historical encoder from https://github.com/databrickslabs/ucx/pull/2743/
+        data = {
+            field.name: str(getattr(table, field.name))
+            for table in tables
+            for field in dataclasses.fields(table)
+            if getattr(table, field.name) is not None
+        }
+        historical = Historical(
+            workspace_id,
+            1,
+            "tables",
+            [table.catalog, table.database, table.name],
+            data,
+            [],
+            "Cor",
+        )
+        historicals.append(historical)
     sql_backend.save_table(f"{schema.full_name}.historical", historicals, Historical, mode="overwrite")
     return schema
 
