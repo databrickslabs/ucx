3d6c58a5c68759d35639b00556ca4b5f5493b5c1
hari-selvarajan_data <hari.selvarajan@databricks.com>
2024-10-20 13:18:01 +0100

    int test fix

--- tests/integration/hive_metastore/test_workflows.py
+++ tests/integration/hive_metastore/test_workflows.py
@@ -1,5 +1,7 @@
 import pytest
 from databricks.sdk.errors import NotFound
+
+from databricks.labs.ucx.framework.utils import escape_sql_identifier
 from databricks.labs.ucx.hive_metastore.tables import Table
 
 
@@ -69,7 +71,7 @@ def test_table_migration_job_refreshes_migration_status(
 )
 def test_table_migration_for_managed_tabl(ws, installation_ctx, prepare_tables_for_migration, workflow, sql_backend):
     """The migration status should be refreshed after the migration job."""
-    tables, _ = prepare_tables_for_migration
+    tables, dst_schema = prepare_tables_for_migration
     ctx = installation_ctx.replace(
         extend_prompts={
             r"If hive_metastore contains managed table with external.*": "0",
@@ -80,35 +82,18 @@ def test_table_migration_for_managed_tabl(ws, installation_ctx, prepare_tables_f
     ctx.workspace_installation.run()
     ctx.deployed_workflows.run_workflow(workflow)
 
-    # Avoiding MigrationStatusRefresh as it will refresh the status before fetching
-    migration_status_query = f"SELECT * FROM {ctx.config.inventory_database}.migration_status"
-    migration_statuses = list(ctx.sql_backend.fetch(migration_status_query))
-
-    if len(migration_statuses) == 0:
-        ctx.deployed_workflows.relay_logs(workflow)
-        assert False, "No migration statuses found"
-
-    asserts = []
     for table in tables.values():
-        migration_status = []
-        for status in migration_statuses:
-            if status.src_schema == table.schema_name and status.src_table == table.name:
-                migration_status.append(status)
+        try:
+            assert ws.tables.get(f"{dst_schema.catalog_name}.{dst_schema.name}.{table.name}").name
+        except NotFound:
+            assert False, f"{table.name} not found in {dst_schema.catalog_name}.{dst_schema.name}"
+    managed_table = tables["src_managed_table"]
 
-        assert_message_postfix = f" found for {table.table_type} {table.full_name}"
-        if len(migration_status) == 0:
-            asserts.append("No migration status" + assert_message_postfix)
-        elif len(migration_status) > 1:
-            asserts.append("Multiple migration statuses" + assert_message_postfix)
-        elif migration_status[0].dst_schema is None:
-            asserts.append("No destination schema" + assert_message_postfix)
-        elif migration_status[0].dst_table is None:
-            asserts.append("No destination table" + assert_message_postfix)
-
-    assert_message = (
-        "\n".join(asserts) + " given migration statuses " + "\n".join([str(status) for status in migration_statuses])
-    )
-    assert len(asserts) == 0, assert_message
+    for key, value, _ in installation_ctx.sql_backend.fetch(
+        f"DESCRIBE TABLE EXTENDED {escape_sql_identifier(managed_table.full_name)}"
+    ):
+        if key == "Type":
+            assert value == "EXTERNAL"
 
 
 @pytest.mark.parametrize('prepare_tables_for_migration', [('hiveserde')], indirect=True)
