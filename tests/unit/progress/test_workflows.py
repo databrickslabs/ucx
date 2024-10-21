fed9a15433ce2d92ab5ea357ce0734becc4599e3
Andrew Snare <andrew.snare@databricks.com>
2024-10-21 11:18:12 +0200

    Update unit test to work with one less parameter.

--- tests/unit/progress/test_workflows.py
+++ tests/unit/progress/test_workflows.py
@@ -1,3 +1,4 @@
+from typing import get_type_hints
 from unittest.mock import create_autospec
 
 import pytest
@@ -5,35 +6,25 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.catalog import CatalogInfo, MetastoreAssignment
 from databricks.sdk.service.jobs import BaseRun, RunResultState, RunState
 
-from databricks.labs.ucx.assessment.clusters import ClustersCrawler, PoliciesCrawler
-from databricks.labs.ucx.assessment.jobs import JobsCrawler
-from databricks.labs.ucx.assessment.pipelines import PipelinesCrawler
 from databricks.labs.ucx.progress.workflows import MigrationProgress
 from databricks.labs.ucx.contexts.workflow_task import RuntimeContext
-from databricks.labs.ucx.hive_metastore import TablesCrawler
-from databricks.labs.ucx.hive_metastore.grants import GrantsCrawler
-from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationStatusRefresher
-from databricks.labs.ucx.hive_metastore.udfs import UdfsCrawler
 
 
 @pytest.mark.parametrize(
-    "task, crawler, crawler_class",
-    [
-        (MigrationProgress.crawl_tables, RuntimeContext.tables_crawler, TablesCrawler),
-        (MigrationProgress.crawl_udfs, RuntimeContext.udfs_crawler, UdfsCrawler),
-        (MigrationProgress.crawl_grants, RuntimeContext.grants_crawler, GrantsCrawler),
-        (MigrationProgress.assess_jobs, RuntimeContext.jobs_crawler, JobsCrawler),
-        (MigrationProgress.assess_clusters, RuntimeContext.clusters_crawler, ClustersCrawler),
-        (MigrationProgress.assess_pipelines, RuntimeContext.pipelines_crawler, PipelinesCrawler),
-        (MigrationProgress.crawl_cluster_policies, RuntimeContext.policies_crawler, PoliciesCrawler),
-        (
-            MigrationProgress.refresh_table_migration_status,
-            RuntimeContext.migration_status_refresher,
-            TableMigrationStatusRefresher,
-        ),
-    ],
+    "task, crawler",
+    (
+        (MigrationProgress.crawl_tables, RuntimeContext.tables_crawler),
+        (MigrationProgress.crawl_udfs, RuntimeContext.udfs_crawler),
+        (MigrationProgress.crawl_grants, RuntimeContext.grants_crawler),
+        (MigrationProgress.assess_jobs, RuntimeContext.jobs_crawler),
+        (MigrationProgress.assess_clusters, RuntimeContext.clusters_crawler),
+        (MigrationProgress.assess_pipelines, RuntimeContext.pipelines_crawler),
+        (MigrationProgress.crawl_cluster_policies, RuntimeContext.policies_crawler),
+        (MigrationProgress.refresh_table_migration_status, RuntimeContext.migration_status_refresher),
+    ),
 )
-def test_migration_progress_runtime_refresh(run_workflow, task, crawler, crawler_class) -> None:
+def test_migration_progress_runtime_refresh(run_workflow, task, crawler) -> None:
+    crawler_class = get_type_hints(crawler.func)["return"]
     mock_crawler = create_autospec(crawler_class)
     crawler_name = crawler.attrname
     run_workflow(task, **{crawler_name: mock_crawler})
