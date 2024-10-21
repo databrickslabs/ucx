3aad8bf2949f1a5d482a7cb6280bd8a2f4d4ed7b
Andrew Snare <andrew.snare@databricks.com>
2024-10-21 16:25:26 +0200

    Mark linters as still needing to be done.

--- src/databricks/labs/ucx/progress/workflows.py
+++ src/databricks/labs/ucx/progress/workflows.py
@@ -150,12 +150,14 @@ class MigrationProgress(Workflow):
     def assess_dashboards(self, ctx: RuntimeContext):
         """Scans all dashboards for migration issues in SQL code of embedded widgets.
         Also stores direct filesystem accesses for display in the migration dashboard."""
+        # TODO: Ensure these are captured in the history log.
         ctx.query_linter.refresh_report(ctx.sql_backend, ctx.inventory_database)
 
     @job_task
     def assess_workflows(self, ctx: RuntimeContext):
         """Scans all jobs for migration issues in notebooks.
         Also stores direct filesystem accesses for display in the migration dashboard."""
+        # TODO: Ensure these are captured in the history log.
         ctx.workflow_linter.refresh_report(ctx.sql_backend, ctx.inventory_database)
 
     @job_task(
