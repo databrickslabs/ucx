42ea7a2ea257077dce5392d012ec49b10651d449
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-21 16:05:17 +0200

    Add counter for pipelines

---
+++ src/databricks/labs/ucx/queries/progress/01_6_percentage_pipeline_migration_readiness.sql
@@ -0,0 +1,5 @@
+/* --title 'Pipeline migration readiness (%)' */
+SELECT
+    100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
+FROM multiworkspace.historical
+WHERE object_type = "pipelines"
--- tests/integration/queries/test_migration_progress.py
+++ tests/integration/queries/test_migration_progress.py
@@ -238,6 +238,7 @@ def test_migration_progress_dashboard(
         ("01_3_percentage_grant_migration_readiness", [Row(percentage=66.66666666666667)]),
         ("01_4_percentage_job_migration_readiness", [Row(percentage=50.0)]),
         ("01_5_percentage_cluster_migration_readiness", [Row(percentage=50.0)]),
+        ("01_6_percentage_pipeline_migration_readiness", [Row(percentage=50.0)]),
         (
             "02_1_migration_status_by_owner_bar_graph",
             [Row(owner="Andrew", count=1), Row(owner="Cor", count=4)],
