b2e1df6d2ee7f1f14b9868b2225f6dde4d8e5227
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-21 13:52:35 +0200

    Remove redundant descriptions from migration readiness counters

--- src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
@@ -1,4 +1,4 @@
-/* --title 'Overall readiness (%)' --description 'Ready to be migrated' */
+/* --title 'Overall readiness (%)' */
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM multiworkspace.historical
