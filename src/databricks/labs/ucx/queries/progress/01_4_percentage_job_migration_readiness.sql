40269ca765fcaada2064298c2cedb06425b38867
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-21 15:26:55 +0200

    Remove percentage override

--- src/databricks/labs/ucx/queries/progress/01_4_percentage_job_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/01_4_percentage_job_migration_readiness.sql
@@ -1,13 +1,4 @@
-/*
---title 'Job migration readiness'
---overrides '{"spec": {
-    "encodings": {
-        "value": {
-            "format": {"type": "number-percent", "decimalPlaces": {"type": "max", "places": 2}}
-        }
-    }
-}}'
-*/
+/* --title 'Job migration readiness' */
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM multiworkspace.historical
