ecf2afc007d0616bc378b377188373db0c3eaaaf
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-21 16:08:05 +0200

    Update overall readiness size

--- src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/01_0_percentage_migration_readiness.sql
@@ -1,4 +1,4 @@
-/* --title 'Overall readiness (%)' */
+/* --title 'Overall readiness (%)' --width 2 --height 6 */
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM multiworkspace.historical
