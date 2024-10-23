04cbdbfd552b84c89a4a9744b7a24a7ea285af61
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-23 09:37:21 +0200

    Make object type filter inclusive instead of exclusive

--- src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_readiness.sql
@@ -2,4 +2,4 @@
 SELECT
     100 * COUNT_IF(size(failures) = 0) / COUNT(*) AS percentage
 FROM ucx_catalog.multiworkspace.historical
-WHERE object_type != 'migration_status'
+WHERE object_type IN ('udfs', 'grants', 'jobs', 'clusters', 'tables', 'pipelines', 'policies')
