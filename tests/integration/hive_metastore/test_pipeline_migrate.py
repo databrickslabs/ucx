def8bbe279057659c3fa7392310d7d50095ee505
pritishpai <pritish.pai@databricks.com>
2024-10-22 23:04:09 -0400

    added more pipelines to cover every PipelineRule possibility

--- tests/integration/hive_metastore/test_pipeline_migrate.py
+++ tests/integration/hive_metastore/test_pipeline_migrate.py
@@ -1,59 +1,96 @@
-from databricks.sdk.service.pipelines import PipelineLibrary, NotebookLibrary, PipelinesAPI
+from databricks.sdk.service.pipelines import PipelineLibrary, NotebookLibrary
 
-from databricks.get_uninstalled_libraries import libraries
 from databricks.labs.ucx.assessment.pipelines import PipelinesCrawler
 from databricks.labs.ucx.hive_metastore.pipelines_migrate import PipelinesMigrator, PipelineRule, PipelineMapping
-from integration.conftest import runtime_ctx
+from ..assessment.test_assessment import _PIPELINE_CONF
 
 _TEST_STORAGE_ACCOUNT = "storage_acct_1"
 _TEST_TENANT_ID = "directory_12345"
 
-_PIPELINE_CONF = {
-    f"spark.hadoop.fs.azure.account.oauth2.client.id.{_TEST_STORAGE_ACCOUNT}.dfs.core.windows.net": ""
-    "pipeline_dummy_application_id",
-    f"spark.hadoop.fs.azure.account.oauth2.client.endpoint.{_TEST_STORAGE_ACCOUNT}.dfs.core.windows.net": ""
-    "https://login"
-    f".microsoftonline.com/{_TEST_TENANT_ID}/oauth2/token",
-}
 
-_PIPELINE_CONF_WITH_SECRET = {
-    "fs.azure.account.oauth2.client.id.abcde.dfs.core.windows.net": "{{secrets/reallyasecret123/sp_app_client_id}}",
-    "fs.azure.account.oauth2.client.endpoint.abcde.dfs.core.windows.net": "https://login.microsoftonline.com"
-    "/dummy_application/token",
-}
-
-
-def test_pipeline_migrate(ws, make_pipeline, make_notebook, make_directory, inventory_schema, sql_backend, runtime_ctx):
-
+# pylint: disable=too-many-locals
+def test_pipeline_migrate(
+    ws,
+    make_pipeline,
+    make_notebook,
+    make_random,
+    watchdog_purge_suffix,
+    make_directory,
+    inventory_schema,
+    sql_backend,
+    runtime_ctx,
+):
     src_schema = runtime_ctx.make_schema(catalog_name="hive_metastore")
-    target_schema = runtime_ctx.make_schema(catalog_name="hive_metastore")
-
+    target_schemas = [
+        runtime_ctx.make_schema(catalog_name="hive_metastore"),
+        runtime_ctx.make_schema(catalog_name="hive_metastore"),
+        runtime_ctx.make_schema(catalog_name="hive_metastore"),
+        runtime_ctx.make_schema(catalog_name="hive_metastore"),
+        runtime_ctx.make_schema(catalog_name="hive_metastore"),
+    ]
 
     dlt_notebook_path = f"{make_directory()}/dlt_notebook.py"
     src_table = runtime_ctx.make_table(catalog_name="hive_metastore", schema_name=src_schema.name, non_delta=True)
-    dlt_notebook_text = f"""create streaming table st1\nas select * from stream(hive_metastore.{src_schema.name}.{src_table.name})"""
-    dlt_notebook_source = dlt_notebook_text.encode("ASCII")
-    make_notebook(content=dlt_notebook_source, path=dlt_notebook_path)
+    dlt_notebook_text = (
+        f"""create streaming table st1\nas select * from stream(hive_metastore.{src_schema.name}.{src_table.name})"""
+    )
+    make_notebook(content=dlt_notebook_text.encode("ASCII"), path=dlt_notebook_path)
 
-    created_pipeline = make_pipeline(configuration=_PIPELINE_CONF, target=target_schema.name, libraries=[
-        PipelineLibrary(notebook=NotebookLibrary(path=dlt_notebook_path))
-    ])
+    pipeline_names = [
+        f"pipeline-{make_random(4).lower()}-{watchdog_purge_suffix}",
+        f"pipeline-{make_random(4).lower()}-{watchdog_purge_suffix}",
+        f"pipeline-{make_random(4).lower()}-{watchdog_purge_suffix}",
+    ]
+
+    created_pipelines = [
+        make_pipeline(
+            configuration=_PIPELINE_CONF,
+            name=pipeline_names[0],
+            target=target_schemas[0].name,
+            libraries=[PipelineLibrary(notebook=NotebookLibrary(path=dlt_notebook_path))],
+        ),
+        make_pipeline(
+            configuration=_PIPELINE_CONF,
+            name=pipeline_names[1],
+            target=target_schemas[1].name,
+            libraries=[PipelineLibrary(notebook=NotebookLibrary(path=dlt_notebook_path))],
+        ),
+        make_pipeline(
+            configuration=_PIPELINE_CONF,
+            name=pipeline_names[2],
+            target=target_schemas[2].name,
+            libraries=[PipelineLibrary(notebook=NotebookLibrary(path=dlt_notebook_path))],
+        ),
+    ]
+
     pipeline_crawler = PipelinesCrawler(ws=ws, sbe=sql_backend, schema=inventory_schema)
     pipelines = pipeline_crawler.snapshot()
     results = []
     for pipeline in pipelines:
         if pipeline.success != 0:
             continue
-        if pipeline.pipeline_id == created_pipeline.pipeline_id:
+        if pipeline.pipeline_id in [
+            created_pipelines[0].pipeline_id,
+            created_pipelines[1].pipeline_id,
+            created_pipelines[2].pipeline_id,
+        ]:
             results.append(pipeline)
-            created_pipeline_name = pipeline.pipeline_name
-
-    assert len(results) >= 1
-    assert results[0].pipeline_id == created_pipeline.pipeline_id
+    assert len(results) == 3
 
     # TODO: Add other rules as well to test the migration
-    pipeline_rules = [PipelineRule.from_src_dst("test_workspace", created_pipeline.pipeline_id, "test_catalog"
-                                                )]
+    pipeline_rules = [
+        PipelineRule.from_src_dst("test_workspace", created_pipelines[0].pipeline_id, "test_catalog"),
+        PipelineRule.from_src_dst(
+            "test_workspace", created_pipelines[1].pipeline_id, "test_catalog", target_schemas[3].name
+        ),
+        PipelineRule.from_src_dst(
+            "test_workspace",
+            created_pipelines[2].pipeline_id,
+            "test_catalog",
+            target_schemas[4].name,
+            f"{pipeline_names[2]}-migrated",
+        ),
+    ]
     runtime_ctx.with_pipeline_mapping_rules(pipeline_rules)
     pipeline_mapping = PipelineMapping(runtime_ctx.installation, ws, sql_backend)
 
@@ -64,11 +101,11 @@ def test_pipeline_migrate(ws, make_pipeline, make_notebook, make_directory, inve
     pipelines = pipeline_crawler.snapshot(force_refresh=True)
     results = []
     for pipeline in pipelines:
-        # TODO: Commented out as the migrated pipeline currently is in failed state after migration
-        # if pipeline.success != 0:
-        #     continue
-        if pipeline.pipeline_name == f"{created_pipeline_name} [UC]":
+        if pipeline.pipeline_name in [
+            f"{pipeline_names[0]} [UC]",
+            f"{pipeline_names[1]} [UC]",
+            f"{pipeline_names[2]}-migrated",
+        ]:
             results.append(pipeline)
-            break
 
-    assert len(results) == 1
+    assert len(results) == 3
