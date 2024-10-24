d529efe4528adbf6c7ddf642f8967f7892f47f05
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-24 10:33:31 +0200

    Test partial destination

--- tests/unit/hive_metastore/test_table_migration_status.py
+++ tests/unit/hive_metastore/test_table_migration_status.py
@@ -1,3 +1,5 @@
+import pytest
+
 from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationStatus
 
 
@@ -9,3 +11,9 @@ def test_table_migration_status_failures_when_pending_migration() -> None:
 def test_table_migration_status_no_failures_when_destination_is_present() -> None:
     migration_status = TableMigrationStatus("schema", "table", "catalog", "schema", "table")
     assert migration_status.failures == []
+
+
+@pytest.mark.parametrize("attribute", ["dst_catalog", "dst_schema", "dst_table"])
+def test_table_migration_status_failures_when_partial_destination(attribute: str) -> None:
+    migration_status = TableMigrationStatus("schema", "table", **{attribute: "test"})
+    assert migration_status.failures == ["[UCX INTERNAL] Object 'schema.table' has partially destination"]
