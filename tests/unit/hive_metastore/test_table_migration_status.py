5ea184fa65b5b299fabaa728f6f13dcf325acb5c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-24 10:27:27 +0200

    Test failures attribute when migrated

--- tests/unit/hive_metastore/test_table_migration_status.py
+++ tests/unit/hive_metastore/test_table_migration_status.py
@@ -4,3 +4,8 @@ from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigra
 def test_table_migration_status_failures_when_pending_migration() -> None:
     migration_status = TableMigrationStatus("schema", "table")
     assert migration_status.failures == ["Object 'schema.table' pending migration"]
+
+
+def test_table_migration_status_no_failures_when_destination_is_present() -> None:
+    migration_status = TableMigrationStatus("schema", "table", "catalog", "schema", "table")
+    assert migration_status.failures == []
