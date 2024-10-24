cde01998f36d5210a4c8e23483b3dfd88f9f9cfe
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-24 09:16:40 +0200

    Use try divide

--- src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_readiness.sql
@@ -1,5 +1,5 @@
 /* --title 'Overall readiness (%)' --width 2 */
 SELECT
-    ROUND(100 * COUNT_IF(size(failures) = 0) / COUNT(*), 2) AS percentage
+    ROUND(100 * try_divide(COUNT_IF(size(failures) = 0), COUNT(*)), 2) AS percentage
 FROM ucx_catalog.multiworkspace.latest_historical_per_workspace
 WHERE object_type IN ('udfs', 'grants', 'jobs', 'clusters', 'tables', 'pipelines', 'policies')
--- src/databricks/labs/ucx/queries/progress/main/01_4_percentage_cluster_migration_readiness.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_4_percentage_cluster_migration_readiness.sql
@@ -1,5 +1,5 @@
 /* --title 'Cluster migration readiness (%)' */
 SELECT
-    ROUND(100 * COUNT_IF(size(failures) = 0) / COUNT(*), 2) AS percentage
+    ROUND(100 * TRY_DIVIDE(COUNT_IF(size(failures) = 0), COUNT(*)), 2) AS percentage
 FROM ucx_catalog.multiworkspace.latest_historical_per_workspace
 WHERE object_type = "clusters"
