babe86bbb80b2d43e61237f6d9af48605dccc88b
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-25 13:15:13 +0200

    Fix typo in docstrings

--- src/databricks/labs/ucx/progress/clusters.py
+++ src/databricks/labs/ucx/progress/clusters.py
@@ -5,7 +5,7 @@ from databricks.labs.ucx.progress.history import ProgressEncoder
 class ClusterProgressEncoder(ProgressEncoder[ClusterInfo]):
     """Encode class:ClusterInfo to class:History.
 
-    The ClusterInfo failures are added during crawling. For legacy reasons, we keep this logic in the crawler`. This
+    The ClusterInfo failures are added during crawling. For legacy reasons, we keep this logic in the crawler. This
     class is a placeholder for documenting this decision and maintaining a parallel structure across progress modules.
 
     The failures associated with a cluster are partially inherited from the cluster policies and init scripts which are
@@ -16,6 +16,6 @@ class ClusterProgressEncoder(ProgressEncoder[ClusterInfo]):
 class ClusterPolicyProgressEncoder(ProgressEncoder[PolicyInfo]):
     """Encode class:PolicyInfo to class:History.
 
-    The PolicyInfo failures are added during crawling. For legacy reasons, we keep this logic in the crawler`. This
+    The PolicyInfo failures are added during crawling. For legacy reasons, we keep this logic in the crawler. This
     class is a placeholder for documenting this decision and maintaining a parallel structure across progress modules.
     """
