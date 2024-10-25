81c86844f878f4a8faf1b5a1136b561259f35512
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-25 10:07:59 +0200

    Add ClusterProgressEncoder

--- src/databricks/labs/ucx/progress/clusters.py
+++ src/databricks/labs/ucx/progress/clusters.py
@@ -1,5 +1,16 @@
-from databricks.labs.ucx.assessment.clusters import PolicyInfo
+from databricks.labs.ucx.assessment.clusters import ClusterInfo, PolicyInfo
 from databricks.labs.ucx.progress.history import ProgressEncoder
+
+
+class ClusterProgressEncoder(ProgressEncoder[ClusterInfo]):
+    """Encode class:ClusterInfo to class:History.
+
+    The ClusterInfo failures are added during crawling. For legacy reasons, we keep this logic in the crawler`. This
+    class is a placeholder for documenting this decision and maintaining a parallel structure across progress modules.
+
+    The failures associated with a cluster are partially inherited from the cluster policies and init scripts which are
+    attached to a cluster.
+    """
 
 
 class ClusterPolicyProgressEncoder(ProgressEncoder[PolicyInfo]):
