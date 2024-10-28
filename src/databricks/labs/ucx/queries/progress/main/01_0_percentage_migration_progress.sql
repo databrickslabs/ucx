d8fb518220e131481185100fdc9d6579d64defef
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-28 13:13:18 +0100

    Capatilize SIZE

--- src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_progress.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_0_percentage_migration_progress.sql
@@ -1,5 +1,5 @@
 /* --title 'Overall progress (%)' --width 2 */
 SELECT
-    ROUND(100 * try_divide(COUNT_IF(size(failures) = 0), COUNT(*)), 2) AS percentage
+    ROUND(100 * try_divide(COUNT_IF(SIZE(failures) = 0), COUNT(*)), 2) AS percentage
 FROM ucx_catalog.multiworkspace.latest_historical_per_workspace
 WHERE object_type IN ('ClusterInfo', 'Grant', 'JobInfo', 'PipelineInfo', 'PolicyInfo', 'Table', 'Udf')
--- src/databricks/labs/ucx/queries/progress/main/01_4_percentage_cluster_migration_progress.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_4_percentage_cluster_migration_progress.sql
@@ -1,5 +1,5 @@
 /* --title 'Cluster migration progress (%)' */
 SELECT
-    ROUND(100 * TRY_DIVIDE(COUNT_IF(size(failures) = 0), COUNT(*)), 2) AS percentage
+    ROUND(100 * TRY_DIVIDE(COUNT_IF(SIZE(failures) = 0), COUNT(*)), 2) AS percentage
 FROM ucx_catalog.multiworkspace.latest_historical_per_workspace
 WHERE object_type = "ClusterInfo"
