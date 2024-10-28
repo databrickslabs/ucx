e8f524a998c9cf77195432bc35dabd0249a9e047
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-28 11:57:41 +0100

    Fix query filter

--- src/databricks/labs/ucx/queries/progress/main/02_1_pending_migration_data_objects.sql
+++ src/databricks/labs/ucx/queries/progress/main/02_1_pending_migration_data_objects.sql
@@ -1,4 +1,4 @@
 /* --title 'Pending migration' --description 'Total number of tables and views' --height 6 */
 SELECT COUNT(*) AS count
 FROM ucx_catalog.multiworkspace.latest_historical_per_workspace
-WHERE object_type = 'Table' AND 'Pending migration' in failures
+WHERE object_type = 'Table' AND array_contains(failures, 'Pending migration')
