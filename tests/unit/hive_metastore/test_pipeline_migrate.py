f7355c1e7e8cb5efcb15577ba5ab7afe01fec9fa
pritishpai <pritish.pai@databricks.com>
2024-10-28 16:50:57 -0400

    finish test_current_pipelines

--- tests/unit/hive_metastore/test_pipeline_migrate.py
+++ tests/unit/hive_metastore/test_pipeline_migrate.py
@@ -39,16 +39,21 @@ def test_pipeline_rule():
     assert rule.target_schema_name is None
     assert rule.target_pipeline_name == "pipe"
 
-def test_current_pipelines(mock_installation):
+def test_current_pipelines(mock_installation, make_random):
     errors = {}
     rows = {
-        "hive_metastore.inventory_database.pipelines": [],
+        "`hive_metastore`.`inventory_database`.`pipelines`": [
+            ("id1", "pipe1", 1, "[]", "creator1"),
+            ("id2", "pipe2", 1, "[]", "creator2"),
+            ("id3", "pipe3", 1, "[]", "creator3"),
+        ],
     }
     sql_backend = MockBackend(fails_on_first=errors, rows=rows)
     workspace_client = create_autospec(WorkspaceClient)
 
     pipeline_mapping = PipelineMapping(mock_installation, workspace_client, sql_backend)
-    pipelines_crawler = PipelinesCrawler(workspace_client, sql_backend, "schema")
-
+    pipelines_crawler = PipelinesCrawler(workspace_client, sql_backend, "inventory_database")
+    something = pipelines_crawler.snapshot()
     pipelines = pipeline_mapping.current_pipelines(pipelines_crawler, "workspace_name", "catalog_name")
     assert isinstance(pipelines, Generator)
+    assert len(list(pipelines)) == 3
