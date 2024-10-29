66e7122ea03f6f21460c84f5018ec6c56b3a8113
Cor Zuurmond <jczuurmond@protonmail.com>
2024-10-29 10:57:40 +0100

    Test all steps in sequence

--- tests/unit/assessment/test_sequencing.py
+++ tests/unit/assessment/test_sequencing.py
@@ -1,6 +1,6 @@
-import dataclasses
 from unittest.mock import create_autospec
 
+from databricks.sdk import WorkspaceClient
 from databricks.sdk.service import iam, jobs
 from databricks.sdk.service.compute import ClusterDetails
 
@@ -11,29 +11,67 @@ from databricks.labs.ucx.source_code.graph import DependencyGraph
 from databricks.labs.ucx.source_code.jobs import WorkflowTask
 
 
-def test_cluster_from_task_has_children(ws, simple_dependency_resolver, mock_path_lookup) -> None:
-    ws.clusters.get.return_value = ClusterDetails(cluster_name="my-cluster", creator_user_name="John Doe")
+def test_sequence_steps_from_job_task_with_cluster(
+    ws: WorkspaceClient, simple_dependency_resolver, mock_path_lookup
+) -> None:
+    """Sequence a job with a task referencing a cluster.
+
+    Sequence:  # TODO: @JCZuurmond: Would expect cluster first.
+    1. Task
+    2. Job
+    3. Cluster
+    """
     task = jobs.Task(task_key="test-task", existing_cluster_id="cluster-123")
     settings = jobs.JobSettings(name="test-job", tasks=[task])
     job = jobs.Job(job_id=1234, settings=settings)
     ws.jobs.get.return_value = job
-    dependency = WorkflowTask(ws, task, job)
-    graph = DependencyGraph(dependency, None, simple_dependency_resolver, mock_path_lookup, CurrentSessionState())
+
+    # Match task cluster above on cluster id
+    def get_cluster(cluster_id: str) -> ClusterDetails:
+        if cluster_id == "cluster-123":
+            return ClusterDetails(cluster_id="cluster-123", cluster_name="my-cluster", creator_user_name="John Doe")
+        raise ValueError(f"Unknown cluster: {cluster_id}")
+
+    ws.clusters.get.side_effect = get_cluster
+
+    # Match cluster creator above on username
     admin_finder = create_autospec(AdministratorFinder)
     admin_user = iam.User(user_name="John Doe", active=True, roles=[iam.ComplexValue(value="account_admin")])
     admin_finder.find_admin_users.return_value = (admin_user,)
+
+    dependency = WorkflowTask(ws, task, job)
+    graph = DependencyGraph(dependency, None, simple_dependency_resolver, mock_path_lookup, CurrentSessionState())
     sequencer = MigrationSequencer(ws, AdministratorLocator(ws, finders=[lambda _ws: admin_finder]))
     sequencer.register_workflow_task(task, job, graph)
+
     steps = list(sequencer.generate_steps())
-    step = steps[-1]
-    # we don't know the ids of the steps, se let's zero them
-    step = dataclasses.replace(step, step_id=0, required_step_ids=[0] * len(step.required_step_ids))
-    assert step == MigrationStep(
-        step_id=0,
-        step_number=3,
-        object_type="CLUSTER",
-        object_id="cluster-123",
-        object_name="my-cluster",
-        object_owner="John Doe",
-        required_step_ids=[0, 0],
-    )
+
+    assert steps == [
+        MigrationStep(
+            step_id=2,
+            step_number=1,
+            object_type="TASK",
+            object_id="1234/test-task",
+            object_name="test-task",
+            object_owner="John Doe",
+            required_step_ids=[],
+        ),
+        MigrationStep(
+            step_id=1,
+            step_number=2,
+            object_type="JOB",
+            object_id="1234",
+            object_name="test-job",
+            object_owner="John Doe",
+            required_step_ids=[2],
+        ),
+        MigrationStep(
+            step_id=3,
+            step_number=3,
+            object_type="CLUSTER",
+            object_id="cluster-123",
+            object_name="my-cluster",
+            object_owner="John Doe",
+            required_step_ids=[1, 2],
+        ),
+    ]
