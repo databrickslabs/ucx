9bef9961491075e201f91aa8b74f3b8d9e934039
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-05 08:48:36 +0100

    Let overview show object type

--- src/databricks/labs/ucx/queries/progress/main/03_04_data_asset_references_pending_migration_overview.sql
+++ src/databricks/labs/ucx/queries/progress/main/03_04_data_asset_references_pending_migration_overview.sql
@@ -1,15 +1,20 @@
 /* --title 'Overview' --description 'Table, view and dfsa migration' --width 5 */
 WITH migration_statuses AS (
-    SELECT owner, failures
+    SELECT owner, object_type, failures
     FROM ucx_catalog.multiworkspace.objects_snapshot
     WHERE object_type IN ('DirectFsAccess', 'UsedTable')
 )
 
 SELECT
     owner,
+    CASE
+        WHEN object_type = 'DirectFsAccess' THEN 'Direct filesystem access'
+        WHEN object_type = 'UsedTable' THEN 'Table or view reference'
+        ELSE object_type
+    END AS object_type,
     DOUBLE(CEIL(100 * COUNT_IF(SIZE(failures) = 0) / SUM(COUNT(*)) OVER (PARTITION BY owner), 2)) AS percentage,
     COUNT(*) AS total,
     COUNT_IF(SIZE(failures) = 0) AS total_migrated,
     COUNT_IF(SIZE(failures) > 0) AS total_not_migrated
 FROM migration_statuses
-GROUP BY owner
+GROUP BY owner, object_type
