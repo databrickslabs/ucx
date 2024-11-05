204e46558b2e314816cb2c0b3b26db69f0ed822d
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-05 08:23:07 +0100

    Add code migration overview

---
+++ src/databricks/labs/ucx/queries/progress/main/03_04_code_pending_migration_per_owner_overview.sql
@@ -0,0 +1,15 @@
+/* --title 'Overview' --description 'Code migration' --width 5 */
+WITH migration_statuses AS (
+    SELECT *
+    FROM ucx_catalog.multiworkspace.objects_snapshot
+    WHERE object_type IN ('DirectFsAccess', 'QueryProblem', 'UsedTable')
+)
+
+SELECT
+    owner,
+    DOUBLE(CEIL(100 * COUNT_IF(SIZE(failures) = 0) / SUM(COUNT(*)) OVER (PARTITION BY owner), 2)) AS percentage,
+    COUNT(*) AS total,
+    COUNT_IF(SIZE(failures) = 0) AS total_migrated,
+    COUNT_IF(SIZE(failures) > 0) AS total_not_migrated
+FROM migration_statuses
+GROUP BY owner
