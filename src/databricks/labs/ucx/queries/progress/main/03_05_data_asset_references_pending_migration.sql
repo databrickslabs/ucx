713995558eedb96b70e7af531ac4d3a30209283f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-05 10:47:59 +0100

    Fix link for different objects

--- src/databricks/labs/ucx/queries/progress/main/03_05_data_asset_references_pending_migration.sql
+++ src/databricks/labs/ucx/queries/progress/main/03_05_data_asset_references_pending_migration.sql
@@ -1,18 +1,18 @@
 /*
---title 'Workflow migration problems'
+--title 'Data asset references'
 --width 6
 --overrides '{"spec":{
     "encodings":{
       "columns": [
         {"fieldName": "workspace_id", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "workspace_id"},
         {"fieldName": "object_type", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "object_type"},
-        {"fieldName": "object_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "/#workspace/{{ source_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "link"},
+        {"fieldName": "object_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "{{ link }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "object_id"},
         {"fieldName": "failure", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "failure"},
         {"fieldName": "is_read", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "is_read"},
         {"fieldName": "is_write", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "is_write"}
       ]},
     "invisibleColumns": [
-      {"name": "source_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "/#workspace/{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "source_id"}
+      {"name": "link", "booleanValues": ["false", "true"], "linkUrlTemplate": "{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "link"}
     ]
   }}'
 */
@@ -33,6 +33,10 @@ SELECT
   data.is_read,
   data.is_write,
   -- Below are invisible column(s) used in links url templates
-  data.source_id
+  CASE
+    WHEN object_type = 'DirectFsAccess' THEN concat('/#workspace/', data.source_id)
+    WHEN object_type = 'UsedTable' THEN concat('/explore/data/', concat_ws('/', object_id))
+    ELSE ''
+  END AS link
 FROM ucx_catalog.multiworkspace.objects_snapshot
 WHERE object_type IN ('DirectFsAccess', 'UsedTable')
