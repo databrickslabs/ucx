905fefca3f688a71b23c15734aa54594f9bbbf8e
Andrew Snare <andrew.snare@databricks.com>
2024-11-06 18:02:41 +0100

    Remove some unnecessary code.

--- tests/unit/progress/test_tables.py
+++ tests/unit/progress/test_tables.py
@@ -6,7 +6,6 @@ from databricks.labs.ucx.framework.owners import Ownership
 from databricks.labs.ucx.framework.utils import escape_sql_identifier
 from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationIndex
 from databricks.labs.ucx.hive_metastore.tables import Table
-from databricks.labs.ucx.progress.grants import GrantProgressEncoder
 from databricks.labs.ucx.progress.tables import TableProgressEncoder
 
 
@@ -21,7 +20,6 @@ def test_table_progress_encoder_no_failures(mock_backend, table: Table) -> None:
     ownership.owner_of.return_value = "user"
     table_migration_index = create_autospec(TableMigrationIndex)
     table_migration_index.is_migrated.return_value = True
-    grant_progress_encoder = create_autospec(GrantProgressEncoder)
     encoder = TableProgressEncoder(
         mock_backend, ownership, table_migration_index, run_id=1, workspace_id=123456789, catalog="test"
     )
@@ -33,7 +31,6 @@ def test_table_progress_encoder_no_failures(mock_backend, table: Table) -> None:
     assert len(rows[0].failures) == 0
     ownership.owner_of.assert_called_once()
     table_migration_index.is_migrated.assert_called_with(table.database, table.name)
-    grant_progress_encoder.assert_not_called()
 
 
 @pytest.mark.parametrize(
@@ -47,7 +44,6 @@ def test_table_progress_encoder_pending_migration_failure(mock_backend, table: T
     ownership.owner_of.return_value = "user"
     table_migration_index = create_autospec(TableMigrationIndex)
     table_migration_index.is_migrated.return_value = False
-    grant_progress_encoder = create_autospec(GrantProgressEncoder)
     encoder = TableProgressEncoder(
         mock_backend, ownership, table_migration_index, run_id=1, workspace_id=123456789, catalog="test"
     )
@@ -59,4 +55,3 @@ def test_table_progress_encoder_pending_migration_failure(mock_backend, table: T
     assert rows[0].failures == ["Pending migration"]
     ownership.owner_of.assert_called_once()
     table_migration_index.is_migrated.assert_called_with(table.database, table.name)
-    grant_progress_encoder.assert_not_called()
