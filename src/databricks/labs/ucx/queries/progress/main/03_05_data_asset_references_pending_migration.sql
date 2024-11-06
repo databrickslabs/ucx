42793d859cfadfe0374c6a2d65ac562472d5fab0
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-06 11:39:40 +0100

    Fix query

--- src/databricks/labs/ucx/queries/progress/main/03_05_data_asset_references_pending_migration.sql
+++ src/databricks/labs/ucx/queries/progress/main/03_05_data_asset_references_pending_migration.sql
@@ -26,17 +26,18 @@ SELECT
   END AS object_type,
   CASE
       WHEN object_type = 'DirectFsAccess' THEN data.path
-      WHEN object_type = 'UsedTable' THEN concat_ws('.', object_id)
-      ELSE concat_ws('.', object_id)
+      WHEN object_type = 'UsedTable' THEN CONCAT_WS('.', object_id)
+      ELSE CONCAT_WS('.', object_id)
   END AS object_id,
-  explode(failures) AS failure,
-  data.is_read,
-  data.is_write,
+  EXPLODE(failures) AS failure,
+  CAST(data.is_read AS BOOLEAN) AS is_read,
+  CAST(data.is_write AS BOOLEAN) AS is_write,
   -- Below are invisible column(s) used in links url templates
   CASE
     -- SQL queries do NOT point to the workspace, i.e. start with '/'
-    WHEN (object_type = 'DirectFsAccess' AND SUBSTRING(data.source_id, 0, 1) != '/') THEN CONCAT('/sql/editor/', data.source_id)
-    ELSE CONCAT('/#workspace/', data.source_id)
+    WHEN object_type = 'DirectFsAccess' AND SUBSTRING(data.source_id, 0, 1) != '/' THEN CONCAT('/sql/editor/', data.source_id)
+    ELSE CONCAT('/#workspace', data.source_id)
   END AS link
 FROM ucx_catalog.multiworkspace.objects_snapshot
+ORDER BY workspace_id, owner, object_type, object_id
 WHERE object_type IN ('DirectFsAccess', 'UsedTable')
