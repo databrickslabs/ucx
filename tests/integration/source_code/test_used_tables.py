0f90dc5f5ff6a579459c3b1af2e27d5ffe809701
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-12 14:28:07 +0100

    Test UsedTableOwnership to fallback on workspace administrator if not write

--- tests/integration/source_code/test_used_tables.py
+++ tests/integration/source_code/test_used_tables.py
@@ -26,3 +26,22 @@ def test_used_table_ownership_is_workspace_admin_if_not_in_used_tables_snapshot(
     owner = ownership.owner_of(UsedTable())
 
     assert owner == "John Doe"
+
+
+def test_used_table_ownership_is_workspace_admin_if_not_write() -> None:
+    administrator_locator = create_autospec(AdministratorLocator)
+    administrator_locator.get_workspace_administrator.return_value = "John Doe"
+    used_tables_crawler = create_autospec(UsedTablesCrawler)
+    legacy_query_ownership = create_autospec(LegacyQueryOwnership)
+    workspace_path_ownership = create_autospec(WorkspacePathOwnership)
+    ownership = UsedTableOwnership(
+        administrator_locator,
+        used_tables_crawler,
+        used_tables_crawler,
+        legacy_query_ownership,
+        workspace_path_ownership,
+    )
+
+    owner = ownership.owner_of(UsedTable(is_write=False))
+
+    assert owner == "John Doe"
