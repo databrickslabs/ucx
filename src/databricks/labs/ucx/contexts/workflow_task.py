2ebe1b43be4e028b8c846bdf3f02ebae24769754
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-13 15:43:10 +0100

    Add query problem progress encoder

--- src/databricks/labs/ucx/contexts/workflow_task.py
+++ src/databricks/labs/ucx/contexts/workflow_task.py
@@ -30,6 +30,7 @@ from databricks.labs.ucx.progress.history import ProgressEncoder
 from databricks.labs.ucx.progress.jobs import JobsProgressEncoder
 from databricks.labs.ucx.progress.tables import TableProgressEncoder
 from databricks.labs.ucx.progress.workflow_runs import WorkflowRunRecorder
+from databricks.labs.ucx.progress.queries import QueryProblemProgressEncoder
 
 # As with GlobalContext, service factories unavoidably have a lot of public methods.
 # pylint: disable=too-many-public-methods
@@ -237,6 +238,16 @@ class RuntimeContext(GlobalContext):
             self.sql_backend,
             self.udf_ownership,
             Udf,
+            self.parent_run_id,
+            self.workspace_id,
+            self.config.ucx_catalog,
+        )
+
+    @cached_property
+    def query_problem_progress(self) -> QueryProblemProgressEncoder:
+        return QueryProblemProgressEncoder(
+            self.sql_backend,
+            self.query_problem_ownership,
             self.parent_run_id,
             self.workspace_id,
             self.config.ucx_catalog,
