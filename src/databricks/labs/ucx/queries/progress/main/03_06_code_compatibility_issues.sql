24284b4f1919f6b4bde491fab45e0013d9915b46
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-15 13:18:44 +0100

    Copy changes from #3112
    https://github.com/databrickslabs/ucx/pull/3112

---
+++ src/databricks/labs/ucx/queries/progress/main/03_06_code_compatibility_issues.sql
@@ -0,0 +1,29 @@
+/*
+--title 'Code compatability issues'
+--width 6
+--overrides '{"spec":{
+    "encodings":{
+      "columns": [
+        {"fieldName": "workspace_id", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "workspace_id"},
+        {"fieldName": "code", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "code"},
+        {"fieldName": "message", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "message"},
+        {"fieldName": "dashboard_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/sql/dashboards/{{ dashboard_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "dashboard"},
+        {"fieldName": "query_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/sql/editor/{{ query_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "query"}
+      ]},
+    "invisibleColumns": [
+      {"name": "dashboard_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "dashboard_id"},
+      {"name": "query_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "query_id"}
+    ]
+  }}'
+*/
+SELECT
+    workspace_id,
+    data.code,
+    data.message,
+    data.dashboard_name,
+    data.query_name,
+    -- Below are invisible columns used in links url templates
+    data.dashboard_id,
+    data.query_id
+FROM ucx_catalog.multiworkspace.objects_snapshot
+WHERE object_type = 'QueryProblem'
