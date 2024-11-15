7180f87d6751d42918fb897b05024bfe6b33c328
pritishpai <pritish.pai@databricks.com>
2024-11-14 22:13:16 -0500

    Fix incompatible jobs widget

--- src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
+++ src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
@@ -1,8 +1,20 @@
-/* --title 'Incompatible Jobs' --width 6 */
+/*
+--title 'Incompatible Jobs'
+--width 6
+--overrides '{"spec":{
+    "encodings":{
+      "columns": [
+        {"fieldName": "job_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "job_name"},
+        {"fieldName": "job_id", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "job_id"},
+        {"fieldName": "finding", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "finding"},
+        {"fieldName": "creator", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "creator"}
+      ]}
+  }}'
+*/
 SELECT
-  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
-  job_id,
   job_name,
+  job_id,
+  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
   creator
 FROM inventory.jobs
 WHERE
