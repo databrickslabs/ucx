6dcbc80aa472db2d67093448d26c15fc44b8114b
pritishpai <pritish.pai@databricks.com>
2024-11-18 13:33:30 -0500

    modify explode logic to avoid losing rows where there are not failures and hide job_id

--- src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
+++ src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
@@ -1,21 +1,25 @@
 /*
---title 'Incompatible Jobs'
+--title 'Jobs Details'
 --width 6
 --overrides '{"spec":{
     "encodings":{
       "columns": [
         {"fieldName": "job_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "job_name"},
-        {"fieldName": "job_id", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "job_id"},
-        {"fieldName": "finding", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "finding"},
-        {"fieldName": "creator", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "creator"}
+        {"fieldName": "creator", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "creator"},
+        {"fieldName": "problem", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "problem"}
       ]}
   }}'
 */
 SELECT
   job_name,
   job_id,
-  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
-  creator
+  creator,
+  EXPLODE(
+    CASE
+        WHEN failures IS NULL OR failures = '[]' THEN ARRAY(NULL)
+        ELSE FROM_JSON(failures, 'array<string>')
+    END
+) AS problem
 FROM inventory.jobs
 WHERE
   NOT job_name LIKE '[UCX]%'
