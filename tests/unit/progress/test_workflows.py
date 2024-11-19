27662b585c845293d1c256cc27ced0e459305970
Andrew Snare <andrew.snare@databricks.com>
2024-11-19 16:08:19 +0100

    Split multi-stage unit test up into individual tests.

--- tests/unit/progress/test_workflows.py
+++ tests/unit/progress/test_workflows.py
@@ -45,14 +45,12 @@ def test_migration_progress_runtime_refresh(run_workflow, task, crawler, history
     mock_history_log.append_inventory_snapshot.assert_called_once()
 
 
-def test_migration_progress_runtime_tables_refresh(run_workflow) -> None:
-    """Ensure that the split crawl and update-history-log tasks perform their part of the refresh process."""
+def test_migration_progress_runtime_tables_refresh_crawl_tables(run_workflow) -> None:
+    """Ensure that step 1 of the split crawl/update-history-log tasks performs its part of the refresh process."""
     mock_tables_crawler = create_autospec(TablesCrawler)
-    mock_migration_status_refresher = create_autospec(TableMigrationStatusRefresher)
     mock_history_log = create_autospec(ProgressEncoder)
     context_replacements = {
         "tables_crawler": mock_tables_crawler,
-        "migration_status_refresher": mock_migration_status_refresher,
         "tables_progress": mock_history_log,
         "named_parameters": {"parent_run_id": 53},
     }
@@ -60,16 +58,36 @@ def test_migration_progress_runtime_tables_refresh(run_workflow) -> None:
     # The first part of a 3-step update: the table crawl without updating the history log.
     run_workflow(MigrationProgress.crawl_tables, **context_replacements)
     mock_tables_crawler.snapshot.assert_called_once_with(force_refresh=True)
-    mock_tables_crawler.snapshot.reset_mock()
     mock_history_log.append_inventory_snapshot.assert_not_called()
+
+
+def test_migration_progress_runtime_tables_refresh_migration_status(run_workflow) -> None:
+    """Ensure that step 2 of the split crawl/update-history-log tasks performs its part of the refresh process."""
+    mock_migration_status_refresher = create_autospec(TableMigrationStatusRefresher)
+    mock_history_log = create_autospec(ProgressEncoder)
+    context_replacements = {
+        "migration_status_refresher": mock_migration_status_refresher,
+        "tables_progress": mock_history_log,
+        "named_parameters": {"parent_run_id": 53},
+    }
 
     # The second part of a 3-step update: updating table migration status without updating the history log.
     task_dependencies = getattr(MigrationProgress.refresh_table_migration_status, "__task__").depends_on
     assert MigrationProgress.crawl_tables.__name__ in task_dependencies
     run_workflow(MigrationProgress.refresh_table_migration_status, **context_replacements)
     mock_migration_status_refresher.snapshot.assert_called_once_with(force_refresh=True)
-    mock_migration_status_refresher.snapshot.reset_mock()
     mock_history_log.append_inventory_snapshot.assert_not_called()
+
+
+def test_migration_progress_runtime_tables_refresh_update_history_log(run_workflow) -> None:
+    """Ensure that the split crawl and update-history-log tasks perform their part of the refresh process."""
+    mock_tables_crawler = create_autospec(TablesCrawler)
+    mock_history_log = create_autospec(ProgressEncoder)
+    context_replacements = {
+        "tables_crawler": mock_tables_crawler,
+        "tables_progress": mock_history_log,
+        "named_parameters": {"parent_run_id": 53},
+    }
 
     # The final part of the 3-step update: updating the history log (without a forced crawl).
     task_dependencies = getattr(MigrationProgress.update_tables_history_log, "__task__").depends_on
@@ -77,7 +95,6 @@ def test_migration_progress_runtime_tables_refresh(run_workflow) -> None:
     assert MigrationProgress.refresh_table_migration_status.__name__ in task_dependencies
     run_workflow(MigrationProgress.update_tables_history_log, **context_replacements)
     mock_tables_crawler.snapshot.assert_called_once_with()
-    # migration_status_refresher is not directly used within step 3, so interactions don't need to be checked.
     mock_history_log.append_inventory_snapshot.assert_called_once()
 
 
