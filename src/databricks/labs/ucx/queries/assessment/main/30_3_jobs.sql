8714da2e36554a2038c008bdf8a29ea7dd7cf175
Pritish Pai <136742693+pritishpai@users.noreply.github.com>
2024-11-20 10:21:27 -0500

    Rearranged job summary dashboard columns and make job_name clickable (#3311)
    ## Changes
    Rearranged job summary widget dashboard columns and make column name
    clickable
    
    ### Linked issues
    Resolves #3261 
    
    ### Functionality
    
    - [x] modified dashboard widget
    
    ### Tests
    - [x] manually tested
    - [x] verified on labs environment (screenshot attached)

--- src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
+++ src/databricks/labs/ucx/queries/assessment/main/30_3_jobs.sql
@@ -1,9 +1,27 @@
-/* --title 'Incompatible Jobs' --width 6 */
+/*
+--title 'Jobs Details'
+--width 6
+--overrides '{"spec":{
+    "encodings":{
+      "columns": [
+        {"fieldName": "job_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/jobs/{{ job_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "job_name"},
+        {"fieldName": "creator", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "creator"},
+        {"fieldName": "problem", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "problem"}
+      ]},
+      "invisibleColumns": [
+        {"name": "job_id", "booleanValues": ["false", "true"], "type": "integer", "displayAs": "number", "title": "job_id"}
+    ]  }}'
+*/
 SELECT
-  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
-  job_id,
   job_name,
-  creator
+  job_id,
+  creator,
+  EXPLODE(
+    CASE
+        WHEN failures IS NULL OR failures = '[]' THEN ARRAY(NULL)
+        ELSE FROM_JSON(failures, 'array<string>')
+    END
+) AS problem
 FROM inventory.jobs
 WHERE
   NOT job_name LIKE '[UCX]%'
