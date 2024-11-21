eb641171e65233b380b370f1bad420423a9dc2d2
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-21 14:22:47 +0100

    Add table reference to integration test

--- tests/integration/progress/test_tables.py
+++ tests/integration/progress/test_tables.py
@@ -1,8 +1,11 @@
-from databricks.labs.ucx.hive_metastore.tables import Table
+import datetime as dt
+
 from databricks.labs.ucx.framework.utils import escape_sql_identifier
+from databricks.labs.ucx.hive_metastore.tables import Table
+from databricks.labs.ucx.source_code.base import LineageAtom, UsedTable
 
 
-def test_table_progress_encoder_table_pending_migration(runtime_ctx, az_cli_ctx) -> None:
+def test_table_progress_encoder_table_failures(runtime_ctx, az_cli_ctx) -> None:
     az_cli_ctx.progress_tracking_installation.run()
     runtime_ctx = runtime_ctx.replace(
         parent_run_id=1,
@@ -10,10 +13,22 @@ def test_table_progress_encoder_table_pending_migration(runtime_ctx, az_cli_ctx)
         ucx_catalog=az_cli_ctx.ucx_catalog,
     )
     table = Table("hive_metastore", "schema", "name", "UNKNOWN", "UNKNOWN")
+    used_table = UsedTable(
+        catalog_name=table.catalog,
+        schema_name=table.database,
+        table_name=table.name,
+        source_id="test/test.py",
+        source_timestamp=dt.datetime.now(tz=dt.timezone.utc),
+        source_lineage=[LineageAtom(object_type="NOTEBOOK", object_id="test/test.py")],
+        assessment_start_timestamp=dt.datetime.now(tz=dt.timezone.utc),
+        assessment_end_timestamp=dt.datetime.now(tz=dt.timezone.utc),
+    )
+    runtime_ctx.used_tables_crawler_for_paths.dump_all([used_table])
+
     runtime_ctx.tables_progress.append_inventory_snapshot([table])
 
     history_table_name = escape_sql_identifier(runtime_ctx.tables_progress.full_name)
     records = list(runtime_ctx.sql_backend.fetch(f"SELECT * FROM {history_table_name}"))
 
     assert len(records) == 1
-    assert records[0].failures == ["Pending migration"]
+    assert records[0].failures == ["Pending migration", "Used by NOTEBOOK: test/test.py"]
