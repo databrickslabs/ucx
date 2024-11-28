395e09903bb4b054fd1929f639bd798e5ef1b66f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-28 11:36:21 +0100

    Format

--- tests/integration/assessment/test_dashboards.py
+++ tests/integration/assessment/test_dashboards.py
@@ -1,7 +1,12 @@
 from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard
 from databricks.sdk.service.dashboards import Dashboard as SdkLakeviewDashboard
 
-from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, LakeviewDashboardCrawler, RedashDashboard, RedashDashBoardCrawler
+from databricks.labs.ucx.assessment.dashboards import (
+    LakeviewDashboard,
+    LakeviewDashboardCrawler,
+    RedashDashboard,
+    RedashDashBoardCrawler,
+)
 
 
 def test_redash_dashboard_crawler_crawls_dashboards(ws, make_dashboard, inventory_schema, sql_backend) -> None:
@@ -25,7 +30,9 @@ def test_redash_dashboard_crawler_crawls_dashboard(ws, make_dashboard, inventory
     assert dashboards[0] == RedashDashboard(id=dashboard.id)
 
 
-def test_lakeview_dashboard_crawler_crawls_dashboards(ws, make_lakeview_dashboard, inventory_schema, sql_backend) -> None:
+def test_lakeview_dashboard_crawler_crawls_dashboards(
+    ws, make_lakeview_dashboard, inventory_schema, sql_backend
+) -> None:
     dashboard: SdkLakeviewDashboard = make_lakeview_dashboard()
     crawler = LakeviewDashboardCrawler(ws, sql_backend, inventory_schema)
 
@@ -35,10 +42,14 @@ def test_lakeview_dashboard_crawler_crawls_dashboards(ws, make_lakeview_dashboar
     assert dashboard.dashboard_id in {d.id for d in dashboards}, f"Missing dashboard: {dashboard.id}"
 
 
-def test_lakeview_dashboard_crawler_crawls_dashboard(ws, make_lakeview_dashboard, inventory_schema, sql_backend) -> None:
+def test_lakeview_dashboard_crawler_crawls_dashboard(
+    ws, make_lakeview_dashboard, inventory_schema, sql_backend
+) -> None:
     dashboard: SdkLakeviewDashboard = make_lakeview_dashboard()
     make_lakeview_dashboard()  # Ignore second dashboard
-    crawler = LakeviewDashboardCrawler(ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.dashboard_id])
+    crawler = LakeviewDashboardCrawler(
+        ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.dashboard_id]
+    )
 
     dashboards = crawler.snapshot()
 
