d4be79b5ba3c2fada20c227a5bd8a749889dde06
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-28 10:39:54 +0100

    Rename crawler variable

--- tests/integration/assessment/test_dashboards.py
+++ tests/integration/assessment/test_dashboards.py
@@ -5,9 +5,9 @@ from databricks.labs.ucx.assessment.dashboards import Dashboard, RedashDashBoard
 
 def test_redash_dashboard_crawler_crawls_dashboards(ws, make_dashboard, inventory_schema, sql_backend) -> None:
     dashboard: SqlDashboard = make_dashboard()
-    job_crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema)
+    crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema)
 
-    dashboards = job_crawler.snapshot()
+    dashboards = crawler.snapshot()
 
     assert len(dashboards) >= 1
     assert dashboard.id in {d.id for d in dashboards}, f"Missing dashboard: {dashboard.id}"
@@ -16,9 +16,9 @@ def test_redash_dashboard_crawler_crawls_dashboards(ws, make_dashboard, inventor
 def test_redash_dashboard_crawler_crawls_dashboard(ws, make_dashboard, inventory_schema, sql_backend) -> None:
     dashboard: SqlDashboard = make_dashboard()
     make_dashboard()  # Ignore second dashboard
-    job_crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.id])
+    crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.id])
 
-    dashboards = job_crawler.snapshot()
+    dashboards = crawler.snapshot()
 
     assert len(dashboards) == 1
     assert dashboards[0] == Dashboard(id=dashboard.id)
