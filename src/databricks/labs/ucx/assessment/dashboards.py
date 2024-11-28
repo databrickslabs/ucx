304f911e3ffc56b5eb0cb9f1d6a29b02d2fc64fc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-28 10:54:53 +0100

    Differentiate between Redash and Lakeview dashboards

--- src/databricks/labs/ucx/assessment/dashboards.py
+++ src/databricks/labs/ucx/assessment/dashboards.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass
 from databricks.labs.lsql.backends import SqlBackend
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.errors import DatabricksError
-from databricks.sdk.service.dashboards import Dashboard as SDKDashboard
-from databricks.sdk.service.sql import Dashboard as SqlDashboard
+from databricks.sdk.service.dashboards import Dashboard as SdkLakeviewDashboard
+from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard
 
 from databricks.labs.ucx.framework.crawlers import CrawlerBase
 from databricks.labs.ucx.framework.utils import escape_sql_identifier
@@ -18,36 +18,34 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class Dashboard:
-    """UCX representation of a dashboard"""
+class RedashDashboard:
+    """UCX representation of a Redash dashboard.
+
+    Note: We prefer to keep this class similar to the :class:LakeviewDashboard.
+    """
 
     id: str
     """The ID for this dashboard."""
 
     @classmethod
-    def from_sql_dashboard(cls, dashboard: SqlDashboard) -> Dashboard:
+    def from_sdk_dashboard(cls, dashboard: SdkRedashDashboard) -> RedashDashboard:
         assert dashboard.id
         return cls(id=dashboard.id)
 
-    @classmethod
-    def from_sdk_dashboard(cls, dashboard: SDKDashboard) -> Dashboard:
-        assert dashboard.dashboard_id
-        return cls(id=dashboard.dashboard_id)
 
-
-class RedashDashBoardCrawler(CrawlerBase[Dashboard]):
+class RedashDashBoardCrawler(CrawlerBase[RedashDashboard]):
     """Crawler for Redash dashboards."""
 
     def __init__(self, ws: WorkspaceClient, sql_backend: SqlBackend, schema: str, include_dashboard_ids: list[str] | None = None):
-        super().__init__(sql_backend, "hive_metastore", schema, "redash_dashboards", Dashboard)
+        super().__init__(sql_backend, "hive_metastore", schema, "redash_dashboards", RedashDashboard)
         self._ws = ws
         self._include_dashboard_ids = include_dashboard_ids or []
 
-    def _crawl(self) -> Iterable[Dashboard]:
-        dashboards = [Dashboard.from_sql_dashboard(dashboard) for dashboard in self._list_dashboards()]
+    def _crawl(self) -> Iterable[RedashDashboard]:
+        dashboards = [RedashDashboard.from_sdk_dashboard(dashboard) for dashboard in self._list_dashboards()]
         return dashboards
 
-    def _list_dashboards(self) -> list[SqlDashboard]:
+    def _list_dashboards(self) -> list[SdkRedashDashboard]:
         if self._include_dashboard_ids:
             return self._get_dashboards(*self._include_dashboard_ids)
         try:
@@ -56,7 +54,7 @@ class RedashDashBoardCrawler(CrawlerBase[Dashboard]):
             logger.warning("Cannot list Redash dashboards", exc_info=e)
             return []
 
-    def _get_dashboards(self, *dashboard_ids: str) -> list[SqlDashboard]:
+    def _get_dashboards(self, *dashboard_ids: str) -> list[SdkRedashDashboard]:
         dashboards = []
         for dashboard_id in dashboard_ids:
             dashboard = self._get_dashboard(dashboard_id)
@@ -64,31 +62,47 @@ class RedashDashBoardCrawler(CrawlerBase[Dashboard]):
                 dashboards.append(dashboard)
         return dashboards
 
-    def _get_dashboard(self, dashboard_id: str) -> Dashboard | None:
+    def _get_dashboard(self, dashboard_id: str) -> SdkRedashDashboard | None:
         try:
             return self._ws.dashboards.get(dashboard_id)
         except DatabricksError as e:
             logger.warning(f"Cannot get Redash dashboard: {dashboard_id}", exc_info=e)
             return None
 
-    def _try_fetch(self) -> Iterable[Dashboard]:
+    def _try_fetch(self) -> Iterable[RedashDashboard]:
         for row in self._fetch(f"SELECT * FROM {escape_sql_identifier(self.full_name)}"):
-            yield Dashboard(*row)
+            yield RedashDashboard(*row)
 
 
-class LakeviewDashboardCrawler(CrawlerBase[Dashboard]):
+@dataclass
+class LakeviewDashboard:
+    """UCX representation of a Lakeview dashboard.
+
+    Note: We prefer to keep this class similar to the :class:RedashDashboard.
+    """
+
+    id: str
+    """The ID for this dashboard."""
+
+    @classmethod
+    def from_sdk_dashboard(cls, dashboard: SdkLakeviewDashboard) -> LakeviewDashboard:
+        assert dashboard.dashboard_id
+        return cls(id=dashboard.dashboard_id)
+
+
+class LakeviewDashboardCrawler(CrawlerBase[LakeviewDashboard]):
     """Crawler for Lakeview dashboards."""
 
     def __init__(self, ws: WorkspaceClient, sql_backend: SqlBackend, schema: str, include_dashboard_ids: list[str] | None = None):
-        super().__init__(sql_backend, "hive_metastore", schema, "lakeview_dashboards", Dashboard)
+        super().__init__(sql_backend, "hive_metastore", schema, "lakeview_dashboards", LakeviewDashboard)
         self._ws = ws
         self._include_dashboard_ids = include_dashboard_ids or []
 
-    def _crawl(self) -> Iterable[Dashboard]:
-        dashboards = [Dashboard.from_sdk_dashboard(dashboard) for dashboard in self._list_dashboards()]
+    def _crawl(self) -> Iterable[LakeviewDashboard]:
+        dashboards = [LakeviewDashboard.from_sdk_dashboard(dashboard) for dashboard in self._list_dashboards()]
         return dashboards
 
-    def _list_dashboards(self) -> list[SDKDashboard]:
+    def _list_dashboards(self) -> list[SdkLakeviewDashboard]:
         if self._include_dashboard_ids:
             return self._get_dashboards(*self._include_dashboard_ids)
         try:
@@ -97,7 +111,7 @@ class LakeviewDashboardCrawler(CrawlerBase[Dashboard]):
             logger.warning("Cannot list Lakeview dashboards", exc_info=e)
             return []
 
-    def _get_dashboards(self, *dashboard_ids: str) -> list[SDKDashboard]:
+    def _get_dashboards(self, *dashboard_ids: str) -> list[SdkLakeviewDashboard]:
         dashboards = []
         for dashboard_id in dashboard_ids:
             dashboard = self._get_dashboard(dashboard_id)
@@ -105,13 +119,13 @@ class LakeviewDashboardCrawler(CrawlerBase[Dashboard]):
                 dashboards.append(dashboard)
         return dashboards
 
-    def _get_dashboard(self, dashboard_id: str) -> SDKDashboard | None:
+    def _get_dashboard(self, dashboard_id: str) -> SdkLakeviewDashboard | None:
         try:
             return self._ws.lakeview.get(dashboard_id)
         except DatabricksError as e:
             logger.warning(f"Cannot get Lakeview dashboard: {dashboard_id}", exc_info=e)
             return None
 
-    def _try_fetch(self) -> Iterable[Dashboard]:
+    def _try_fetch(self) -> Iterable[LakeviewDashboard]:
         for row in self._fetch(f"SELECT * FROM {escape_sql_identifier(self.full_name)}"):
-            yield Dashboard(*row)
+            yield LakeviewDashboard(*row)
