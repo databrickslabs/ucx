77dd9e63b77f3fd0238c876c877cb1a6675203bb
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-28 10:54:53 +0100

    Differentiate between Redash and Lakeview dashboards

--- tests/integration/assessment/test_dashboards.py
+++ tests/integration/assessment/test_dashboards.py
@@ -1,11 +1,11 @@
-from databricks.sdk.service.sql import Dashboard as SqlDashboard
-from databricks.sdk.service.dashboards import Dashboard as SDKDashboard
+from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard
+from databricks.sdk.service.dashboards import Dashboard as SdkLakeviewDashboard
 
-from databricks.labs.ucx.assessment.dashboards import Dashboard, LakeviewDashboardCrawler, RedashDashBoardCrawler
+from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, LakeviewDashboardCrawler, RedashDashboard, RedashDashBoardCrawler
 
 
 def test_redash_dashboard_crawler_crawls_dashboards(ws, make_dashboard, inventory_schema, sql_backend) -> None:
-    dashboard: SqlDashboard = make_dashboard()
+    dashboard: SdkRedashDashboard = make_dashboard()
     crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema)
 
     dashboards = crawler.snapshot()
@@ -15,18 +15,18 @@ def test_redash_dashboard_crawler_crawls_dashboards(ws, make_dashboard, inventor
 
 
 def test_redash_dashboard_crawler_crawls_dashboard(ws, make_dashboard, inventory_schema, sql_backend) -> None:
-    dashboard: SqlDashboard = make_dashboard()
+    dashboard: SdkRedashDashboard = make_dashboard()
     make_dashboard()  # Ignore second dashboard
     crawler = RedashDashBoardCrawler(ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.id])
 
     dashboards = crawler.snapshot()
 
     assert len(dashboards) == 1
-    assert dashboards[0] == Dashboard(id=dashboard.id)
+    assert dashboards[0] == RedashDashboard(id=dashboard.id)
 
 
 def test_lakeview_dashboard_crawler_crawls_dashboards(ws, make_lakeview_dashboard, inventory_schema, sql_backend) -> None:
-    dashboard: SDKDashboard = make_lakeview_dashboard()
+    dashboard: SdkLakeviewDashboard = make_lakeview_dashboard()
     crawler = LakeviewDashboardCrawler(ws, sql_backend, inventory_schema)
 
     dashboards = crawler.snapshot()
@@ -36,11 +36,11 @@ def test_lakeview_dashboard_crawler_crawls_dashboards(ws, make_lakeview_dashboar
 
 
 def test_lakeview_dashboard_crawler_crawls_dashboard(ws, make_lakeview_dashboard, inventory_schema, sql_backend) -> None:
-    dashboard: SDKDashboard = make_lakeview_dashboard()
+    dashboard: SdkLakeviewDashboard = make_lakeview_dashboard()
     make_lakeview_dashboard()  # Ignore second dashboard
     crawler = LakeviewDashboardCrawler(ws, sql_backend, inventory_schema, include_dashboard_ids=[dashboard.dashboard_id])
 
     dashboards = crawler.snapshot()
 
     assert len(dashboards) == 1
-    assert dashboards[0] == Dashboard(id=dashboard.dashboard_id)
+    assert dashboards[0] == LakeviewDashboard(id=dashboard.dashboard_id)
