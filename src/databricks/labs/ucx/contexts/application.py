93916e9adb834d8010b20a5e2ab231b7ec72fecc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-29 12:05:52 +0100

    Pass Redash crawler in global context

--- src/databricks/labs/ucx/contexts/application.py
+++ src/databricks/labs/ucx/contexts/application.py
@@ -552,7 +552,7 @@ class GlobalContext(abc.ABC):
             TableMigrationIndex([]),
             self.directfs_access_crawler_for_queries,
             self.used_tables_crawler_for_queries,
-            self.config.include_dashboard_ids,
+            self.redash_crawler,
             self.config.debug_listing_upper_limit,
         )
 
@@ -578,6 +578,7 @@ class GlobalContext(abc.ABC):
             self.migration_status_refresher.index(),
             self.workspace_client,
             self.installation,
+            self.redash_crawler,
         )
 
     @cached_property
