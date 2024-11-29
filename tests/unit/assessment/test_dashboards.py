58c80920bc224406480d837449441840a2cf6ea8
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-29 12:59:26 +0100

    Extend RedashDashboard from sdk Dashboard

---
+++ tests/unit/assessment/test_dashboards.py
@@ -0,0 +1,42 @@
+import pytest
+from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard, LegacyVisualization, LegacyQuery, Widget
+
+from databricks.labs.ucx.assessment.dashboards import RedashDashboard
+
+
+@pytest.mark.parametrize(
+    "sdk_dashboard, expected",
+    [
+        (SdkRedashDashboard(id="id"), RedashDashboard("id")),
+        (
+            SdkRedashDashboard(
+                id="did",
+                name="name",
+                parent="parent",
+                tags=["tag1", "tag2"],
+                widgets=[
+                    Widget(visualization=LegacyVisualization(query=LegacyQuery(id="qid1"))),
+                    Widget(visualization=LegacyVisualization(query=LegacyQuery(id="qid2"))),
+                ],
+            ),
+            RedashDashboard("did", "name", "parent", ["qid1", "qid2"], ["tag1", "tag2"]),
+        ),
+        (
+            SdkRedashDashboard(
+                id="did",
+                name="name",
+                parent="parent",
+                tags=["tag1", "tag2"],
+                widgets=[
+                    Widget(),
+                    Widget(visualization=LegacyVisualization()),
+                    Widget(visualization=LegacyVisualization(query=LegacyQuery(id="qid1"))),
+                ],
+            ),
+            RedashDashboard("did", "name", "parent", ["qid1"], ["tag1", "tag2"]),
+        ),
+    ],
+)
+def test_redash_dashboard_from_sdk_dashboard(sdk_dashboard: SdkRedashDashboard, expected: RedashDashboard) -> None:
+    dashboard = RedashDashboard.from_sdk_dashboard(sdk_dashboard)
+    assert dashboard == expected
