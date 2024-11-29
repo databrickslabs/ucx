782aecc59da21045c3b7c6e2601fdd3163c8e7cd
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-29 08:36:11 +0100

    Extend safe read text to handle more exceptions

--- tests/integration/source_code/test_base.py
+++ tests/integration/source_code/test_base.py
@@ -1,12 +1,22 @@
 from pathlib import Path
 from unittest.mock import create_autospec
 
+import pytest
+
 from databricks.labs.ucx.source_code.base import safe_read_text
 
 
-def test_safe_read_text_handles_errors() -> None:
+@pytest.mark.parametrize(
+    "exception",
+    [
+        FileNotFoundError(),
+        UnicodeDecodeError("utf-8", b"\x80\x81\x82", 0, 1, "invalid start byte"),
+        PermissionError(),
+    ],
+)
+def test_safe_read_text_handles_errors(exception: Exception) -> None:
     path = create_autospec(Path)
-    path.open.side_effect = FileNotFoundError()
+    path.open.side_effect = exception
 
     text = safe_read_text(path)
 
