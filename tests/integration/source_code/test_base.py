cfde4e5f32813b2b88d922dd31b4f9f2e747d8b6
Cor Zuurmond <jczuurmond@protonmail.com>
2024-11-29 08:50:32 +0100

    Test log on safe read text

--- tests/integration/source_code/test_base.py
+++ tests/integration/source_code/test_base.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from unittest.mock import create_autospec
 
@@ -14,10 +15,12 @@ from databricks.labs.ucx.source_code.base import safe_read_text
         PermissionError(),
     ],
 )
-def test_safe_read_text_handles_errors(exception: Exception) -> None:
+def test_safe_read_text_handles_and_warns_read_errors(caplog, exception: Exception) -> None:
     path = create_autospec(Path)
     path.open.side_effect = exception
 
-    text = safe_read_text(path)
+    with caplog.at_level(logging.WARNING, logger="databricks.labs.ucx.account.aggregate"):
+        text = safe_read_text(path)
 
     assert not text
+    assert f"Could not read file: {path}" in caplog.messages
