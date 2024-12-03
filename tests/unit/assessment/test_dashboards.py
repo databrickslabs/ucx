0c5feb29374789686ee80142fc064ab8516d1baa
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-03 09:34:14 +0100

    Test redash Dashboard crawler to persist dashboards

--- tests/unit/assessment/test_dashboards.py
+++ tests/unit/assessment/test_dashboards.py
@@ -1,11 +1,14 @@
 import json
+from unittest.mock import create_autospec
 
 import pytest
 from databricks.labs.lsql.lakeview import Dashboard as LsqlLakeviewDashboard, Dataset
+from databricks.labs.lsql.backends import Row
+from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.dashboards import Dashboard as SdkLakeviewDashboard
 from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard, LegacyVisualization, LegacyQuery, Widget
 
-from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, RedashDashboard
+from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, RedashDashboard, RedashDashboardCrawler
 
 
 @pytest.mark.parametrize(
@@ -44,6 +47,32 @@ from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, RedashD
 def test_redash_dashboard_from_sdk_dashboard(sdk_dashboard: SdkRedashDashboard, expected: RedashDashboard) -> None:
     dashboard = RedashDashboard.from_sdk_dashboard(sdk_dashboard)
     assert dashboard == expected
+
+
+def test_redash_dashboard_crawler_snapshot_persists_dashboards(mock_backend) -> None:
+    ws = create_autospec(WorkspaceClient)
+    dashboards = [
+        SdkRedashDashboard(
+            id="did",
+            name="name",
+            parent="parent",
+            tags=["tag1", "tag2"],
+            widgets=[
+                Widget(visualization=LegacyVisualization(query=LegacyQuery(id="qid1"))),
+                Widget(visualization=LegacyVisualization(query=LegacyQuery(id="qid2"))),
+            ],
+        ),
+    ]
+    ws.dashboards.list.side_effect = lambda: (dashboard for dashboard in dashboards)  # Expects an iterator
+    crawler = RedashDashboardCrawler(ws, mock_backend, "test")
+
+    crawler.snapshot()
+
+    rows = mock_backend.rows_written_for("hive_metastore.test.redash_dashboards", "overwrite")
+    assert rows == [
+        Row(id="did", name="name", parent="parent", query_ids=["qid1", "qid2"], tags=["tag1", "tag2"])
+    ]
+    ws.dashboards.list.assert_called_once()
 
 
 @pytest.mark.parametrize(
