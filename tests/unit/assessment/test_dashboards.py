2b1f1bafb8698ca23883a55b2fe6a0998453768f
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-03 08:46:53 +0100

    Test attributes on LakeviewDashboard

--- tests/unit/assessment/test_dashboards.py
+++ tests/unit/assessment/test_dashboards.py
@@ -1,7 +1,11 @@
+import json
+
 import pytest
+from databricks.labs.lsql.lakeview import Dashboard as LsqlLakeviewDashboard, Dataset
+from databricks.sdk.service.dashboards import Dashboard as SdkLakeviewDashboard
 from databricks.sdk.service.sql import Dashboard as SdkRedashDashboard, LegacyVisualization, LegacyQuery, Widget
 
-from databricks.labs.ucx.assessment.dashboards import RedashDashboard
+from databricks.labs.ucx.assessment.dashboards import LakeviewDashboard, RedashDashboard
 
 
 @pytest.mark.parametrize(
@@ -39,4 +43,40 @@ from databricks.labs.ucx.assessment.dashboards import RedashDashboard
 )
 def test_redash_dashboard_from_sdk_dashboard(sdk_dashboard: SdkRedashDashboard, expected: RedashDashboard) -> None:
     dashboard = RedashDashboard.from_sdk_dashboard(sdk_dashboard)
+    assert dashboard == expected
+
+
+@pytest.mark.parametrize(
+    "sdk_dashboard, expected",
+    [
+        (SdkLakeviewDashboard(dashboard_id="id"), LakeviewDashboard("id")),
+        (
+            SdkLakeviewDashboard(
+                dashboard_id="did",
+                display_name="name",
+                parent_path="parent",
+                serialized_dashboard=json.dumps(
+                    LsqlLakeviewDashboard(
+                        datasets=[Dataset("qid1", "SELECT 1"), Dataset("qid2", "SELECT 2")],
+                        pages=[],
+                    ).as_dict()
+                ),
+            ),
+            LakeviewDashboard("did", "name", "parent", ["qid1", "qid2"]),
+        ),
+        (
+            SdkLakeviewDashboard(
+                dashboard_id="did",
+                display_name="name",
+                parent_path="parent",
+                serialized_dashboard=json.dumps(LsqlLakeviewDashboard(datasets=[], pages=[]).as_dict()),
+            ),
+            LakeviewDashboard("did", "name", "parent", []),
+        ),
+    ],
+)
+def test_lakeview_dashboard_from_sdk_dashboard(
+    sdk_dashboard: SdkLakeviewDashboard, expected: LakeviewDashboard
+) -> None:
+    dashboard = LakeviewDashboard.from_sdk_dashboard(sdk_dashboard)
     assert dashboard == expected
