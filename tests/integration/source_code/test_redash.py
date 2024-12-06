1640db1cc2692e3b4a187fc0b79dd042e966127c
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-06 15:08:34 +0100

    Clarify migrate dashboard integration test

--- tests/integration/source_code/test_redash.py
+++ tests/integration/source_code/test_redash.py
@@ -1,12 +1,9 @@
 from databricks.labs.ucx.source_code.redash import Redash
-from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.sql import Dashboard
 
-from ..conftest import MockInstallationContext
 
-
-def test_fix_dashboard(ws: WorkspaceClient, installation_ctx: MockInstallationContext, make_dashboard, make_query):
-    query_in_dashboard, query_outside_dashboard = make_query(), make_query()
+def test_migrate_dashboards_sets_migration_tags(installation_ctx) -> None:
+    query_in_dashboard, query_outside_dashboard = installation_ctx.make_query(), installation_ctx.make_query()
     assert query_in_dashboard.id and query_outside_dashboard.id, "Query from fixture misses id"
     dashboard: Dashboard = installation_ctx.make_dashboard(query=query_in_dashboard)
     assert dashboard.id, "Dashboard from fixture misses id"
@@ -17,9 +14,10 @@ def test_fix_dashboard(ws: WorkspaceClient, installation_ctx: MockInstallationCo
     query_migrated = installation_ctx.workspace_client.queries.get(query_in_dashboard.id)
     assert Redash.MIGRATED_TAG in (query_migrated.tags or [])
 
-    query_not_migrated = ws.queries.get(query_outside_dashboard.id)
+    query_not_migrated = installation_ctx.workspace_client.queries.get(query_outside_dashboard.id)
     assert Redash.MIGRATED_TAG not in (query_not_migrated.tags or [])
 
-    installation_ctx.redash.revert_dashboards(dashboard.id)
+    installation_ctx.redash.revert_dashboards(dashboard.id)  # Revert removes migrated tag
+
     query_reverted = installation_ctx.workspace_client.queries.get(query_in_dashboard.id)
-    assert Redash.MIGRATED_TAG in (query_reverted.tags or [])
+    assert Redash.MIGRATED_TAG not in (query_reverted.tags or [])
