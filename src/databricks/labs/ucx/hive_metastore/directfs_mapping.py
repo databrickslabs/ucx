9e9e85ae68bb29bdb68368cd4f79c7983c412a78
pritishpai <pritish.pai@databricks.com>
2024-12-10 17:05:45 -0500

    add init

--- src/databricks/labs/ucx/hive_metastore/directfs_mapping.py
+++ src/databricks/labs/ucx/hive_metastore/directfs_mapping.py
@@ -1,3 +1,19 @@
+from databricks.labs.lsql.backends import SqlBackend
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service.marketplace import Installation
+
+
 class DirectFsMapping:
     FILENAME = 'directfs_mapping.csv'
     UCX_SKIP_PROPERTY = "databricks.labs.ucx.skip"
+
+
+    def __init__(
+        self,
+        installation: Installation,
+        workspace_client: WorkspaceClient,
+        sql_backend: SqlBackend,
+    ) -> None:
+        self.installation = installation
+        self.workspace_client = workspace_client
+        self.sql_backend = sql_backend
