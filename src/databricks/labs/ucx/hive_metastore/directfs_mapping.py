bf804618e38d7a25b82db6920f7c5532cd9ed675
pritishpai <pritish.pai@databricks.com>
2024-12-11 06:22:21 -0500

    add DirectFsRule

--- src/databricks/labs/ucx/hive_metastore/directfs_mapping.py
+++ src/databricks/labs/ucx/hive_metastore/directfs_mapping.py
@@ -1,8 +1,26 @@
+import logging
+from dataclasses import dataclass
+
 from databricks.labs.lsql.backends import SqlBackend
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.marketplace import Installation
 
 from databricks.labs.ucx.source_code.directfs_access import DirectFsAccessCrawler
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class DirectFsRule:
+        """
+        A rule for direct filesystem access to UC table mapping.
+        """
+        workspace_name:str
+        path: str
+        is_read: bool
+        is_write: bool
+        catalog_name: str
+        dst_schema: str
+        dst_table: str
 
 
 class DirectFsMapping:
