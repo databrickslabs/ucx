3099f8172d000400a880f7acf0d2e5d5039d0ee0
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-11 13:20:19 +0100

    Rewrite tests to assert on historical rows

--- tests/unit/progress/test_dashboards.py
+++ tests/unit/progress/test_dashboards.py
@@ -2,6 +2,7 @@ from unittest.mock import create_autospec
 
 from databricks.labs.lsql.backends import MockBackend, Row
 
+from databricks.labs.ucx.__about__ import __version__ as ucx_version
 from databricks.labs.ucx.assessment.dashboards import Dashboard
 from databricks.labs.ucx.framework.owners import Ownership
 from databricks.labs.ucx.framework.utils import escape_sql_identifier
@@ -12,11 +13,24 @@ from databricks.labs.ucx.source_code.used_table import UsedTablesCrawler
 
 
 def test_dashboard_progress_encoder_no_failures() -> None:
+    expected = [
+        Row(
+            workspace_id=123456789,
+            job_run_id=1,
+            object_type="Dashboard",
+            object_id=["did1"],
+            data={"id": "did1"},
+            failures=[],
+            owner="cor",
+            ucx_version=ucx_version,
+        )
+    ]
+
     mock_backend = MockBackend(
         rows={
             "SELECT \\* FROM query_problems": [
                 Row(
-                    dashboard_id="12345",  # Not a match with dashboard below, hence no failure
+                    dashboard_id="did2",  # Not a match with dashboard below, hence no failure
                     dashboard_parent="dashbards/parent",
                     dashboard_name="my_dashboard",
                     query_id="23456",
@@ -29,16 +43,16 @@ def test_dashboard_progress_encoder_no_failures() -> None:
         }
     )
     ownership = create_autospec(Ownership)
-    ownership.owner_of.return_value = "user"
+    ownership.owner_of.return_value = "cor"
     dfsa = DirectFsAccess(
         source_id="/path/to/write_dfsa.py",
         source_lineage=[
             LineageAtom(
                 object_type="DASHBOARD",
-                object_id="12345",  # Not a match with dashboard below, hence no failure
+                object_id="did3",  # Not a match with dashboard below, hence no failure
                 other={"parent": "parent", "name": "dashboard"},
             ),
-            LineageAtom(object_type="QUERY", object_id="did/qid", other={"name": "query"}),
+            LineageAtom(object_type="QUERY", object_id="did3/qid1", other={"name": "query"}),
         ],
         path="dfsa:/path/to/data/",
         is_read=False,
@@ -53,34 +67,44 @@ def test_dashboard_progress_encoder_no_failures() -> None:
         [direct_fs_access_crawler],
         [used_tables_crawler],
         inventory_database="inventory",
-        run_id=1,
+        job_run_id=1,
         workspace_id=123456789,
         catalog="test",
     )
-    dashboard = Dashboard("did")
+    dashboard = Dashboard("did1")
 
     encoder.append_inventory_snapshot([dashboard])
 
     rows = mock_backend.rows_written_for(escape_sql_identifier(encoder.full_name), "append")
-    assert rows, f"No rows written for: {encoder.full_name}"
-    assert len(rows[0].failures) == 0
+    assert rows == expected
     ownership.owner_of.assert_called_once()
     direct_fs_access_crawler.snapshot.assert_called_once()
     used_tables_crawler.snapshot.assert_called_once()
 
 
 def test_dashboard_progress_encoder_query_problem_as_failure() -> None:
-    failures = ["[sql-parse-error] my_query (12345/23456) : Could not parse SQL"]
+    expected = [
+        Row(
+            workspace_id=123456789,
+            job_run_id=1,
+            object_type="Dashboard",
+            object_id=["did1"],
+            data={"id": "did1"},
+            failures=["[sql-parse-error] my_query (did1/qid1) : Could not parse SQL"],
+            owner="cor",
+            ucx_version=ucx_version,
+        )
+    ]
 
     mock_backend = MockBackend(
         rows={
             # The Mockbackend.fetch ignores the catalog and schema keyword arguments
             "SELECT \\* FROM query_problems": [
                 Row(
-                    dashboard_id="12345",
+                    dashboard_id="did1",
                     dashboard_parent="dashbards/parent",
                     dashboard_name="my_dashboard",
-                    query_id="23456",
+                    query_id="qid1",
                     query_parent="queries/parent",
                     query_name="my_query",
                     code="sql-parse-error",
@@ -90,7 +114,7 @@ def test_dashboard_progress_encoder_query_problem_as_failure() -> None:
         }
     )
     ownership = create_autospec(Ownership)
-    ownership.owner_of.return_value = "user"
+    ownership.owner_of.return_value = "cor"
     direct_fs_access_crawler = create_autospec(DirectFsAccessCrawler)
     used_tables_crawler = create_autospec(UsedTablesCrawler)
     encoder = DashboardProgressEncoder(
@@ -99,39 +123,49 @@ def test_dashboard_progress_encoder_query_problem_as_failure() -> None:
         [direct_fs_access_crawler],
         [used_tables_crawler],
         inventory_database="inventory",
-        run_id=1,
+        job_run_id=1,
         workspace_id=123456789,
         catalog="test",
     )
-    dashboard = Dashboard("12345")
+    dashboard = Dashboard("did1")
 
     encoder.append_inventory_snapshot([dashboard])
 
     rows = mock_backend.rows_written_for(escape_sql_identifier(encoder.full_name), "append")
-    assert rows, f"No rows written for: {encoder.full_name}"
-    assert rows[0].failures == failures
+    assert rows == expected
     ownership.owner_of.assert_called_once()
     direct_fs_access_crawler.snapshot.assert_called_once()
     used_tables_crawler.snapshot.assert_called_once()
 
 
-def test_dashboard_progress_encoder_direct_filesystem_access(mock_backend) -> None:
-    failures = [
-        "[direct-filesystem-access] query (did/qid) : "
-        "The use of direct filesystem references is deprecated: dfsa:/path/to/data/",
+def test_dashboard_progress_encoder_direct_filesystem_access_as_failures(mock_backend) -> None:
+    expected = [
+        Row(
+            workspace_id=123456789,
+            job_run_id=1,
+            object_type="Dashboard",
+            object_id=["did1"],
+            data={"id": "did1"},
+            failures=[
+                "[direct-filesystem-access] query (did1/qid1) : "
+                "The use of direct filesystem references is deprecated: dfsa:/path/to/data/",
+            ],
+            owner="cor",
+            ucx_version=ucx_version,
+        )
     ]
 
     ownership = create_autospec(Ownership)
-    ownership.owner_of.return_value = "user"
+    ownership.owner_of.return_value = "cor"
     dfsa = DirectFsAccess(
         source_id="/path/to/write_dfsa.py",
         source_lineage=[
             LineageAtom(
                 object_type="DASHBOARD",
-                object_id="did",
+                object_id="did1",
                 other={"parent": "parent", "name": "dashboard"},
             ),
-            LineageAtom(object_type="QUERY", object_id="did/qid", other={"name": "query"}),
+            LineageAtom(object_type="QUERY", object_id="did1/qid1", other={"name": "query"}),
         ],
         path="dfsa:/path/to/data/",
         is_read=False,
@@ -146,17 +180,16 @@ def test_dashboard_progress_encoder_direct_filesystem_access(mock_backend) -> No
         [direct_fs_access_crawler],
         [used_tables_crawler],
         inventory_database="inventory",
-        run_id=1,
+        job_run_id=1,
         workspace_id=123456789,
         catalog="test",
     )
-    dashboard = Dashboard("did")
+    dashboard = Dashboard("did1")
 
     encoder.append_inventory_snapshot([dashboard])
 
     rows = mock_backend.rows_written_for(escape_sql_identifier(encoder.full_name), "append")
-    assert rows, f"No rows written for: {encoder.full_name}"
-    assert rows[0].failures == failures
+    assert rows == expected
     ownership.owner_of.assert_called_once()
     direct_fs_access_crawler.snapshot.assert_called_once()
     used_tables_crawler.snapshot.assert_called_once()
