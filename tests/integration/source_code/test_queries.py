d7c0d24d8e6d13796e31feac8980c5d1bdf5fcdc
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-12 10:22:41 +0100

    Change asserts

--- tests/integration/source_code/test_queries.py
+++ tests/integration/source_code/test_queries.py
@@ -27,51 +27,53 @@ def test_query_linter_lints_queries_and_stores_dfsas_and_tables(simple_ctx) -> N
 
     dfsas = list(simple_ctx.directfs_access_crawler_for_queries.snapshot())
     # By comparing the element instead of the list the `field(compare=False)` of the dataclass attributes take effect
-    assert len(dfsas) == 1, "Expected one DFSA"
-    assert dfsas[0] == DirectFsAccess(
-        source_id=f"{dashboard_with_dfsa.id}/{query_with_dfsa.id}",
-        source_lineage=[
-            LineageAtom(
-                object_type="DASHBOARD",
-                object_id=dashboard_with_dfsa.id,
-                other={"parent": dashboard_with_dfsa.parent, "name": dashboard_with_dfsa.name},
-            ),
-            LineageAtom(
-                object_type="QUERY",
-                object_id=f"{dashboard_with_dfsa.id}/{query_with_dfsa.id}",
-                other={"name": query_with_dfsa.name},
-            ),
-        ],
-        path="dbfs://some_folder/some_file.csv",
-        is_read=True,
-        is_write=False,
-    )
+    assert dfsas[0] == [
+        DirectFsAccess(
+            source_id=f"{dashboard_with_dfsa.id}/{query_with_dfsa.id}",
+            source_lineage=[
+                LineageAtom(
+                    object_type="DASHBOARD",
+                    object_id=dashboard_with_dfsa.id,
+                    other={"parent": dashboard_with_dfsa.parent, "name": dashboard_with_dfsa.name},
+                ),
+                LineageAtom(
+                    object_type="QUERY",
+                    object_id=f"{dashboard_with_dfsa.id}/{query_with_dfsa.id}",
+                    other={"name": query_with_dfsa.name},
+                ),
+            ],
+            path="dbfs://some_folder/some_file.csv",
+            is_read=True,
+            is_write=False,
+        )
+    ]
 
     used_tables = list(simple_ctx.used_tables_crawler_for_queries.snapshot())
     # By comparing the element instead of the list the `field(compare=False)` of the dataclass attributes take effect
-    assert len(used_tables) == 1, "Expected one used table"
     # The "query" in the source and object id, and "count" in the name are hardcoded in the
     # `make_lakeview_dashboard` fixture
-    assert used_tables[0] == UsedTable(
-        source_id=f"{dashboard_with_used_table.dashboard_id}/query",
-        source_lineage=[
-            LineageAtom(
-                object_type="DASHBOARD",
-                object_id=dashboard_with_used_table.dashboard_id,
-                other={
-                    "parent": dashboard_with_used_table.parent_path,
-                    "name": dashboard_with_used_table.display_name,
-                },
-            ),
-            LineageAtom(
-                object_type="QUERY",
-                object_id=f"{dashboard_with_used_table.dashboard_id}/query",
-                other={"name": "count"},
-            ),
-        ],
-        catalog_name="hive_metastore",
-        schema_name="some_schema",
-        table_name="some_table",
-        is_read=True,
-        is_write=False,
-    )
+    assert used_tables == [
+        UsedTable(
+            source_id=f"{dashboard_with_used_table.dashboard_id}/query",
+            source_lineage=[
+                LineageAtom(
+                    object_type="DASHBOARD",
+                    object_id=dashboard_with_used_table.dashboard_id,
+                    other={
+                        "parent": dashboard_with_used_table.parent_path,
+                        "name": dashboard_with_used_table.display_name,
+                    },
+                ),
+                LineageAtom(
+                    object_type="QUERY",
+                    object_id=f"{dashboard_with_used_table.dashboard_id}/query",
+                    other={"name": "count"},
+                ),
+            ],
+            catalog_name="hive_metastore",
+            schema_name="some_schema",
+            table_name="some_table",
+            is_read=True,
+            is_write=False,
+        )
+    ]
