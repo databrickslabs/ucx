2a27a4b7f675eeb70ae5d82fabd72dfed22868a3
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-16 15:49:34 +0100

    Add exclude dashboard ids to LakeviewDashboard

--- src/databricks/labs/ucx/assessment/dashboards.py
+++ src/databricks/labs/ucx/assessment/dashboards.py
@@ -310,11 +310,13 @@ class LakeviewDashboardCrawler(CrawlerBase[Dashboard]):
         schema: str,
         *,
         include_dashboard_ids: list[str] | None = None,
+        exclude_dashboard_ids: list[str] | None = None,
         include_query_ids: list[str] | None = None,
     ):
         super().__init__(sql_backend, "hive_metastore", schema, "lakeview_dashboards", Dashboard)
         self._ws = ws
         self._include_dashboard_ids = include_dashboard_ids
+        self._exclude_dashboard_ids = exclude_dashboard_ids
         self._include_query_ids = include_query_ids
 
     def _crawl(self) -> Iterable[Dashboard]:
