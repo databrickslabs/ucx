d2cb7890be161956699afeddbcfd2f5010875e15
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-16 10:24:11 +0100

    Update dashboard progress encoder integration test

--- tests/integration/progress/test_dashboards.py
+++ tests/integration/progress/test_dashboards.py
@@ -1,31 +1,22 @@
-from databricks.labs.lsql.backends import Row
-
-from databricks.labs.ucx.__about__ import __version__ as ucx_version
+from databricks.labs.ucx.assessment.dashboards import Dashboard
 from databricks.labs.ucx.framework.utils import escape_sql_identifier
 
 
-def test_dashboard_progress_encoder_table_failures(runtime_ctx, az_cli_ctx) -> None:
-    row = Row(
-        workspace_id=123456789,
-        job_run_id=1,
-        object_type="Dashboard",
-        object_id=["did1"],
-        data={"id": "did1"},
-        failures=[],
-        owner="cor",
-        ucx_version=ucx_version,
-    )
+def test_dashboard_progress_encoder_table_no_failures(runtime_ctx, az_cli_ctx) -> None:
     az_cli_ctx.progress_tracking_installation.run()
     runtime_ctx = runtime_ctx.replace(
         parent_run_id=1,
         sql_backend=az_cli_ctx.sql_backend,
         ucx_catalog=az_cli_ctx.ucx_catalog,
     )
-    dashboard = az_cli_ctx.make_dashboard()
+    sdk_dashboard = runtime_ctx.make_dashboard()
+    dashboard = Dashboard.from_sdk_redash_dashboard(sdk_dashboard)
+    runtime_ctx.query_linter.refresh_report()
 
-    runtime_ctx.tables_progress.append_inventory_snapshot([dashboard])
+    runtime_ctx.dashboards_progress.append_inventory_snapshot([dashboard])
 
     history_table_name = escape_sql_identifier(runtime_ctx.tables_progress.full_name)
     records = list(runtime_ctx.sql_backend.fetch(f"SELECT * FROM {history_table_name}"))
 
-    assert records == [row]
+    assert len(records) == 1, "Expected one historical record"
+    assert records[0].failures == []
