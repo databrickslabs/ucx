4215db630c460ab69a5d18938445f12b2f071b52
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-17 13:52:24 +0100

    Remove DFSA, QueryProblem and UsedTable queries

--- src/databricks/labs/ucx/queries/progress/main/01_07_count_direct_filesystem_access.sql
+++
@@ -1,7 +0,0 @@
-/* --title 'Direct filesystem access progress (#)' --description 'Unsupported in Unity Catalog' */
-SELECT COUNT(*) AS counter
-FROM ucx_catalog.multiworkspace.objects_snapshot
-WHERE object_type = "DirectFsAccess"
-    -- Redundant filter as a direct filesystem access is a failure by definition (see description above),
-    -- however, filter is defined for explicitness and as this knowledge is not "known" to this query.
-    AND SIZE(failures) > 0
