dcc606294ad54bcfe5042c9e6c8d6f51287e33cf
Cor Zuurmond <jczuurmond@protonmail.com>
2024-12-18 15:41:17 +0100

    Add dashboard to overall progress

--- src/databricks/labs/ucx/queries/progress/main/01_00_percentage_migration_progress.sql
+++ src/databricks/labs/ucx/queries/progress/main/01_00_percentage_migration_progress.sql
@@ -2,4 +2,4 @@
 SELECT
     ROUND(100 * try_divide(COUNT_IF(SIZE(failures) = 0), COUNT(*)), 2) AS percentage
 FROM ucx_catalog.multiworkspace.objects_snapshot
-WHERE object_type IN ('ClusterInfo', 'Grant', 'JobInfo', 'PipelineInfo', 'PolicyInfo', 'Table', 'Udf')
+WHERE object_type IN ('ClusterInfo', 'Grant', 'Dashboard', 'JobInfo', 'PipelineInfo', 'PolicyInfo', 'Table', 'Udf')
