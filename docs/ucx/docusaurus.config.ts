726fc39a8220af76d008b234b3b283a9949f5d57
renardeinside <polarpersonal@gmail.com>
2024-12-20 00:53:26 +0100

    implement landing

---
+++ docs/ucx/docusaurus.config.ts
@@ -0,0 +1,100 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+import * as tailwind from 'tailwindcss';
+import * as autoprefixer from 'autoprefixer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'UCX',
+  tagline: 'The Unity Catalog Migration Assistant',
+  favicon: 'img/logo.svg',
+
+  // Set the production url of your site here
+  url: 'https://databrickslabs.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/ucx/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'databrickslabs', // Usually your GitHub org/user name.
+  projectName: 'ucx', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  onDuplicateRoutes: 'warn',
+  onBrokenAnchors: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  plugins: [
+    async (context, options) => {
+      return {
+        name: "docusaurus-plugin-tailwindcss",
+        configurePostCss(postcssOptions) {
+          postcssOptions.plugins.push(require('tailwindcss'));
+          postcssOptions.plugins.push(require('autoprefixer'));
+          return postcssOptions;
+        },
+      }
+    }
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          // routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/databrickslabs/ucx/tree/main/docs/ucx/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // colorMode: {
+    //   defaultMode: 'dark',
+    //   respectPrefersColorScheme: false,
+    // },
+    navbar: {
+      title: 'UCX',
+      logo: {
+        alt: 'UCX Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          href: 'https://github.com/databrickslabs/ucx',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+    },
+    footer: {
+      links: [
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Databricks Labs. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
