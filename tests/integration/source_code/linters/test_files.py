c23202d8ffa960c7124e9379726ca498af9f49e2
Cor Zuurmond <jczuurmond@protonmail.com>
2025-01-13 13:54:55 +0100

    Move ucx source path to fixture

--- tests/integration/source_code/linters/test_files.py
+++ tests/integration/source_code/linters/test_files.py
@@ -1,7 +1,7 @@
-import logging
 from io import StringIO
 from pathlib import Path
 
+import pytest
 from databricks.labs.blueprint.tui import Prompts
 
 from databricks.labs.ucx.source_code.base import CurrentSessionState
@@ -10,11 +10,16 @@ from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigra
 from databricks.labs.ucx.source_code.linters.context import LinterContext
 
 
-def test_local_code_linter_lints_ucx(simple_ctx) -> None:
+@pytest.fixture
+def ucx_source_path() -> Path:
+    ucx_path = Path(__file__).parent.parent.parent.parent.parent
+    ucx_source_path = Path(ucx_path, "src")
+    return ucx_source_path
+
+
+def test_local_code_linter_lints_ucx(simple_ctx, ucx_source_path) -> None:
     session_state = CurrentSessionState()  # No need to connect
     linter_context = LinterContext(TableMigrationIndex([]), session_state)
-    ucx_path = Path(__file__).parent.parent.parent.parent.parent
-    path_to_scan = Path(ucx_path, "src")
     # TODO: LocalCheckoutContext has to move into GlobalContext because of this hack
     linter = LocalCodeLinter(
         simple_ctx.notebook_loader,
@@ -25,17 +30,15 @@ def test_local_code_linter_lints_ucx(simple_ctx) -> None:
         simple_ctx.dependency_resolver,
         lambda: linter_context,
     )
-    problems = linter.lint(Prompts(), path_to_scan, StringIO())
+    problems = linter.lint(Prompts(), ucx_source_path, StringIO())
     assert len(problems) > 0, f"Found problems while linting ucx: {problems}"
 
 
-def test_local_code_migrator_fixes_ucx(simple_ctx) -> None:
+def test_local_code_migrator_fixes_ucx(simple_ctx, ucx_source_path) -> None:
     session_state = CurrentSessionState()  # No need to connect
     linter_context = LinterContext(TableMigrationIndex([]), session_state)
-    ucx_path = Path(__file__).parent.parent.parent.parent.parent
-    path_to_scan = Path(ucx_path, "src")
     migrator = LocalCodeMigrator(lambda: linter_context)
 
-    has_code_changes = migrator.apply(path_to_scan)
+    has_code_changes = migrator.apply(ucx_source_path)
 
     assert not has_code_changes
