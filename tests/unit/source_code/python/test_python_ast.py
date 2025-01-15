b0e39efd6857aa697ef738d99f853a0155b4e8e4
Cor Zuurmond <jczuurmond@protonmail.com>
2025-01-15 11:49:27 +0100

    Test appending nodes adds nodes to end of body

--- tests/unit/source_code/python/test_python_ast.py
+++ tests/unit/source_code/python/test_python_ast.py
@@ -317,6 +317,16 @@ def test_tree_append_nodes_sets_parent() -> None:
     assert node.parent == maybe_tree.tree.node
 
 
+def test_tree_append_nodes_adds_node_to_body() -> None:
+    node = astroid.extract_node("b = a + 2")
+    maybe_tree = Tree.maybe_normalized_parse("a = 1")
+    assert maybe_tree.tree, maybe_tree.failure
+
+    maybe_tree.tree.append_nodes([node])
+
+    assert maybe_tree.tree.node.body[-1] == node
+
+
 def test_tree_extend_globals_adds_assign_name_to_tree() -> None:
     maybe_tree = Tree.maybe_normalized_parse("a = 1")
     assert maybe_tree.tree, maybe_tree.failure
