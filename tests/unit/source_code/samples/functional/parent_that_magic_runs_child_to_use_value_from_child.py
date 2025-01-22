e430bfa7fc0e2faca3030900985a1bbe3272471b
Cor Zuurmond <jczuurmond@protonmail.com>
2025-01-22 10:59:20 +0100

    Add tests for notebooks that use variable from child notebook
    https://github.com/databrickslabs/ucx/issues/3557

---
+++ tests/unit/source_code/samples/functional/parent_that_magic_runs_child_to_use_value_from_child.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+
+some_table_name = "table"
+
+# COMMAND ----------
+
+# MAGIC %run ./_child_that_uses_missing_value.py
+
+# COMMAND ----------
+
+# ucx[default-format-changed-in-dbr8:+1:0:+1:24] The default format changed in Databricks Runtime 8.0, from Parquet to Delta
+spark.table(f"{some_table_name}_{x}")  # x is introduced into scope by child notebook
