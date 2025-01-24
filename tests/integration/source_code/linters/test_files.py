74278278bac50ef836717606df72ef37d61cf5d3
Cor Zuurmond <jczuurmond@protonmail.com>
2025-01-24 09:20:57 +0100

    Use local checkout context in integration tests

--- tests/integration/source_code/linters/test_files.py
+++ tests/integration/source_code/linters/test_files.py
@@ -4,10 +4,15 @@ from pathlib import Path
 import pytest
 from databricks.labs.blueprint.tui import Prompts
 
-from databricks.labs.ucx.source_code.base import CurrentSessionState
-from databricks.labs.ucx.source_code.linters.files import LocalCodeLinter, LocalCodeMigrator
+from databricks.labs.ucx.contexts.workspace_cli import LocalCheckoutContext
 from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationIndex
 from databricks.labs.ucx.source_code.linters.context import LinterContext
+
+
+@pytest.fixture
+def local_checkout_context(ws) -> LocalCheckoutContext:
+    context = LocalCheckoutContext(ws).replace(linter_context_factory=lambda session_state: LinterContext(TableMigrationIndex([]), session_state))
+    return context
 
 
 @pytest.fixture
@@ -17,28 +22,11 @@ def ucx_source_path() -> Path:
     return path
 
 
-def test_local_code_linter_lints_ucx(simple_ctx, ucx_source_path) -> None:
-    session_state = CurrentSessionState()  # No need to connect
-    linter_context = LinterContext(TableMigrationIndex([]), session_state)
-    # TODO: LocalCheckoutContext has to move into GlobalContext because of this hack
-    linter = LocalCodeLinter(
-        simple_ctx.notebook_loader,
-        simple_ctx.file_loader,
-        simple_ctx.folder_loader,
-        simple_ctx.path_lookup,
-        session_state,
-        simple_ctx.dependency_resolver,
-        lambda: linter_context,
-    )
-    problems = linter.lint(Prompts(), ucx_source_path, StringIO())
+def test_local_code_linter_lints_ucx(local_checkout_context, ucx_source_path) -> None:
+    problems = local_checkout_context.local_code_linter.lint(Prompts(), ucx_source_path, StringIO())
     assert len(problems) > 0, "Found no problems while linting ucx"
 
 
-def test_local_code_migrator_fixes_ucx(simple_ctx, ucx_source_path) -> None:
-    session_state = CurrentSessionState()  # No need to connect
-    linter_context = LinterContext(TableMigrationIndex([]), session_state)
-    migrator = LocalCodeMigrator(lambda: linter_context)
-
-    has_code_changes = migrator.apply(ucx_source_path)
-
-    assert not has_code_changes
+def test_local_code_migrator_fixes_ucx(local_checkout_context, ucx_source_path) -> None:
+    problems = local_checkout_context.local_code_migrator.apply(Prompts(), ucx_source_path)
+    assert len(problems) > 0, "Found no problems while linting ucx"
