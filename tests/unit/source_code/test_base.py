5c00790e3c0153dcdcd067712c19488d9158263d
Cor Zuurmond <jczuurmond@protonmail.com>
2025-01-29 11:12:15 +0100

    Add message to located advice

--- tests/unit/source_code/test_base.py
+++ tests/unit/source_code/test_base.py
@@ -1,12 +1,14 @@
 import dataclasses
 
 import pytest
+from pathlib import Path
 
 from databricks.labs.ucx.source_code.base import (
     Advice,
     Advisory,
     Convention,
     Deprecation,
+    LocatedAdvice,
     Failure,
     UsedTable,
 )
@@ -54,3 +56,17 @@ def test_convention_initialization() -> None:
 )
 def test_used_table_full_name(used_table: UsedTable, expected_name: str) -> None:
     assert used_table.full_name == expected_name
+
+
+def test_located_advice_message() -> None:
+    advice = Advice(
+        code="code",
+        message="message",
+        start_line=0,
+        start_col=0,  # Zero based line number is incremented with one to create the message
+        end_line=1,
+        end_col=1,
+    )
+    located_advice = LocatedAdvice(advice, Path("test.py"))
+
+    assert located_advice.message == "test.py:1:0: [code] message"
