795c0204ff392fc68cd4871be240c10586535bb3
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-04 16:11:49 +0100

    Test known loader to load known container with problems

--- tests/unit/source_code/linters/test_files.py
+++ tests/unit/source_code/linters/test_files.py
@@ -24,6 +24,7 @@ from databricks.labs.ucx.source_code.linters.files import (
     ImportFileResolver,
     KnownContainer,
     KnownDependency,
+    KnownLoader,
 )
 from databricks.labs.ucx.source_code.notebooks.loaders import NotebookResolver, NotebookLoader
 from databricks.labs.ucx.source_code.notebooks.migrator import NotebookMigrator
@@ -190,13 +191,28 @@ def test_folder_has_repr() -> None:
 
 
 @pytest.mark.parametrize("problems", [[], [DependencyProblem("test", "test")]])
-def test_known_container_loads_problems_during_dependency_graph_building(simple_dependency_resolver, problems: list[DependencyProblem]) -> None:
+def test_known_container_loads_problems_during_dependency_graph_building(
+    simple_dependency_resolver, problems: list[DependencyProblem]
+) -> None:
     path_lookup = create_autospec(PathLookup)
     dependency = KnownDependency("test", problems)
     graph = DependencyGraph(dependency, None, simple_dependency_resolver, path_lookup, CurrentSessionState())
     container = KnownContainer(Path("test.py"), problems)
     assert container.build_dependency_graph(graph) == problems
     path_lookup.assert_not_called()
+
+
+@pytest.mark.parametrize("problems", [[], [DependencyProblem("test", "test")]])
+def test_known_loader_loads_known_container_with_problems(
+    simple_dependency_resolver, problems: list[DependencyProblem]
+) -> None:
+    path_lookup = create_autospec(PathLookup)
+    loader = KnownLoader()
+    dependency = KnownDependency("test", problems)
+    graph = DependencyGraph(dependency, None, simple_dependency_resolver, path_lookup, CurrentSessionState())
+    container = loader.load_dependency(path_lookup, dependency)
+    assert container.build_dependency_graph(graph) == problems
+    path_lookup.resolve.assert_called_once_with(dependency.path)
 
 
 site_packages = locate_site_packages()
