77ae2c4445abf53a13af629b80fc794bcf3eb849
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-05 10:39:46 +0100

    Use language on local file instead of cell language

--- src/databricks/labs/ucx/source_code/files.py
+++ src/databricks/labs/ucx/source_code/files.py
@@ -32,15 +32,14 @@ class LocalFile(SourceContainer):
     def __init__(self, path: Path, source: str, language: Language):
         self._path = path
         self._original_code = source
-        # using CellLanguage so we can reuse the facilities it provides
-        self._language = CellLanguage.of_language(language)
+        self._language = language
 
     @property
     def path(self) -> Path:
         return self._path
 
     def build_dependency_graph(self, parent: DependencyGraph) -> list[DependencyProblem]:
-        if self._language is CellLanguage.PYTHON:
+        if self._language == Language.PYTHON:
             context = parent.new_dependency_graph_context()
             analyzer = PythonCodeAnalyzer(context, self._original_code)
             problems = analyzer.build_graph()
@@ -49,13 +48,13 @@ class LocalFile(SourceContainer):
                     problems[idx] = dataclasses.replace(problem, source_path=self._path)
             return problems
         # supported language that does not generate dependencies
-        if self._language is CellLanguage.SQL:
+        if self._language == Language.SQL:
             return []
-        logger.warning(f"Unsupported language: {self._language.language}")
+        logger.warning(f"Unsupported language: {self._language}")
         return []
 
     def build_inherited_context(self, graph: DependencyGraph, child_path: Path) -> InheritedContext:
-        if self._language is CellLanguage.PYTHON:
+        if self._language == Language.PYTHON:
             context = graph.new_dependency_graph_context()
             analyzer = PythonCodeAnalyzer(context, self._original_code)
             inherited = analyzer.build_inherited_context(child_path)
