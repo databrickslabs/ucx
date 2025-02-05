7088943e8d9f2fd048d71cecc0a2a268cbf06fe8
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-05 13:58:19 +0100

    Isort

--- src/databricks/labs/ucx/source_code/python_libraries.py
+++ src/databricks/labs/ucx/source_code/python_libraries.py
@@ -11,12 +11,10 @@ from pathlib import Path
 from databricks.labs.blueprint.entrypoint import is_in_debug
 
 from databricks.labs.ucx.framework.utils import run_command
-from databricks.labs.ucx.source_code.graph import (
-    LibraryResolver,
-    DependencyProblem,
-)
-from databricks.labs.ucx.source_code.path_lookup import PathLookup
+from databricks.labs.ucx.source_code.graph import DependencyProblem, LibraryResolver
 from databricks.labs.ucx.source_code.known import KnownList
+from databricks.labs.ucx.source_code.path_lookup import PathLookup
+
 
 logger = logging.getLogger(__name__)
 
