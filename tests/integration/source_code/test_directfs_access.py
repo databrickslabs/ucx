fb360a7cea692dce00db40d6cf5f386e9d5b054d
pritishpai <pritish.pai@databricks.com>
2025-02-05 09:48:14 -0500

    Fix import

--- tests/integration/source_code/test_directfs_access.py
+++ tests/integration/source_code/test_directfs_access.py
@@ -1,14 +1,11 @@
 import pytest
 
 from databricks.labs.ucx.hive_metastore.table_migration_status import TableMigrationIndex
-from databricks.labs.ucx.source_code.base import DirectFsAccess, LineageAtom
-from databricks.labs.ucx.source_code.linters.jobs import WorkflowLinter
 from databricks.labs.ucx.source_code.base import DirectFsAccess, LineageAtom, CurrentSessionState
-from databricks.labs.ucx.source_code.jobs import WorkflowLinter
+from databricks.labs.ucx.source_code.linters.jobs import WorkflowLinter
 from databricks.labs.ucx.source_code.linters.directfs import DirectFsAccessPyFixer
 from databricks.labs.ucx.source_code.python.python_ast import Tree
 from integration.conftest import runtime_ctx
-from unit.source_code.linters.test_spark_connect import session_state
 
 
 def test_legacy_query_dfsa_ownership(runtime_ctx) -> None:
