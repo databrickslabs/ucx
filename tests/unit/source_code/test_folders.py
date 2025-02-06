5cfdecdeabe39d24ea16b7834af47ae9538c5701
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-06 09:25:39 +0100

    Move graph to fixture

--- tests/unit/source_code/test_folders.py
+++ tests/unit/source_code/test_folders.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from databricks.labs.ucx.source_code.base import CurrentSessionState
 from databricks.labs.ucx.source_code.files import FileLoader
 from databricks.labs.ucx.source_code.folders import FolderLoader, Folder
@@ -14,19 +16,29 @@ def test_folder_has_repr() -> None:
     assert len(repr(folder)) > 0
 
 
-def test_folder_loads_content(mock_path_lookup, simple_dependency_resolver) -> None:
-    expected_dependencies = set()
+@pytest.fixture
+def graph_parent_child_context(mock_path_lookup, simple_dependency_resolver) -> DependencyGraph:
+    """Dependency graph for the parent-child-context sample directory.
+
+    The directory contains three files `grand_parent.py`, `parent.py` and `child.py`. That import each other as such:
+    grand parent imports parent import child.
+    """
+    path = mock_path_lookup.resolve(Path("parent-child-context/"))
+    dependency = Dependency(FolderLoader(NotebookLoader(), FileLoader()), path, False)
+    graph = DependencyGraph(dependency, None, simple_dependency_resolver, mock_path_lookup, CurrentSessionState())
+    return graph
+
+
+def test_folder_loads_content(mock_path_lookup, graph_parent_child_context) -> None:
+    """The files in the folder should be added to the dependency graph after building."""
+    expected_dependencies = {graph_parent_child_context.dependency}
     for relative_path in "grand_parent.py", "parent.py", "child.py":
         path = mock_path_lookup.resolve(Path("parent-child-context") / relative_path)
         dependency = Dependency(FileLoader(), path)
         expected_dependencies.add(dependency)
 
-    path = mock_path_lookup.resolve(Path("parent-child-context/"))
-    dependency = Dependency(FolderLoader(NotebookLoader(), FileLoader()), path, False)
-    expected_dependencies.add(dependency)
-    container = dependency.load(mock_path_lookup)
+    container = graph_parent_child_context.dependency.load(mock_path_lookup)
     assert container is not None
-    graph = DependencyGraph(dependency, None, simple_dependency_resolver, mock_path_lookup, CurrentSessionState())
-    container.build_dependency_graph(graph)
+    container.build_dependency_graph(graph_parent_child_context)
 
-    assert graph.all_dependencies == expected_dependencies
+    assert graph_parent_child_context.all_dependencies == expected_dependencies
