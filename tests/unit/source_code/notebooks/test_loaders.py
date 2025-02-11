0a55ac18e2771339fc12468c7a090636a1bc26dd
Cor <jczuurmond@protonmail.com>
2025-02-11 17:29:42 +0100

    Support ignoring files and notebooks (#3653)
    <!-- REMOVE IRRELEVANT COMMENTS BEFORE CREATING A PULL REQUEST -->
    Support ignoring files and notebooks
    
    Progresses #3645
    
    - [x] modified file and notebook loading in the source code related
    modules
    
    - [x] added unit tests

--- tests/unit/source_code/notebooks/test_loaders.py
+++ tests/unit/source_code/notebooks/test_loaders.py
@@ -1,3 +1,4 @@
+import codecs
 import logging
 from pathlib import Path
 from unittest.mock import create_autospec
@@ -5,8 +6,10 @@ from unittest.mock import create_autospec
 import pytest
 from databricks.sdk.service.workspace import Language
 
-from databricks.labs.ucx.source_code.graph import Dependency
+from databricks.labs.ucx.source_code.base import NOTEBOOK_HEADER
+from databricks.labs.ucx.source_code.graph import Dependency, StubContainer
 from databricks.labs.ucx.source_code.notebooks.loaders import NotebookLoader
+from databricks.labs.ucx.source_code.notebooks.sources import Notebook
 from databricks.labs.ucx.source_code.path_lookup import PathLookup
 
 
@@ -26,25 +29,85 @@ def test_detects_language() -> None:
 
 
 @pytest.mark.parametrize(
-    "error, message",
+    "error",
     [
-        (PermissionError("Permission denied"), "Permission error while reading notebook from workspace"),
-        (
-            UnicodeDecodeError("utf-8", b"\x80\x81\x82", 0, 1, "invalid start byte"),
-            "Cannot decode non-UTF-8 encoded notebook from workspace",
-        ),
+        PermissionError("Permission denied"),
+        FileNotFoundError("File not found"),
+        UnicodeDecodeError("utf-8", b"\x80\x81\x82", 0, 1, "invalid start byte"),
     ],
 )
-def test_notebook_loader_loads_dependency_raises_error(caplog, error: Exception, message: str) -> None:
+def test_notebook_loader_loads_dependency_raises_error(caplog, error: Exception) -> None:
     path = create_autospec(Path)
-    path.read_text.side_effect = error
+    path.suffix = ".py"
+    path.open.side_effect = error
     path_lookup = create_autospec(PathLookup)
     path_lookup.resolve.return_value = path
-    dependency = create_autospec(Dependency)
-    dependency.path.return_value = path
+    dependency = Dependency(NotebookLoader(), path)
 
     with caplog.at_level(logging.WARNING, logger="databricks.labs.ucx.source_code.notebooks.loaders"):
-        found = NotebookLoader().load_dependency(path_lookup, dependency)
+        notebook = dependency.load(path_lookup)
 
-    assert f"{message}: {path}" in caplog.text
-    assert found is None
+    assert f"Could not read file: {path}" in caplog.text
+    assert notebook is None
+
+
+def test_notebook_loader_ignores_loading_non_ascii_file(mock_path_lookup) -> None:
+    dependency = Dependency(NotebookLoader(), Path("nonascii.py"))
+
+    notebook = dependency.load(mock_path_lookup)
+
+    # TODO: Test specific error while loading: https://github.com/databrickslabs/ucx/issues/3584
+    assert notebook is None
+    assert Path("nonascii.py") in mock_path_lookup.successfully_resolved_paths
+
+
+@pytest.mark.parametrize(
+    "bom, encoding",
+    [
+        (codecs.BOM_UTF8, "utf-8"),
+        (codecs.BOM_UTF16_LE, "utf-16-le"),
+        (codecs.BOM_UTF16_BE, "utf-16-be"),
+        (codecs.BOM_UTF32_LE, "utf-32-le"),
+        (codecs.BOM_UTF32_BE, "utf-32-be"),
+    ],
+)
+def test_notebook_loader_loads_file_with_bom(tmp_path, bom, encoding) -> None:
+    path = tmp_path / "file.py"
+    path.write_bytes(bom + f"# {NOTEBOOK_HEADER}\na = 12".encode(encoding))
+    dependency = Dependency(NotebookLoader(), path)
+    path_lookup = create_autospec(PathLookup)
+    path_lookup.resolve.return_value = path
+
+    notebook = dependency.load(path_lookup)
+
+    # TODO: Test specific error while loading: https://github.com/databrickslabs/ucx/issues/3584
+    assert isinstance(notebook, Notebook)
+    assert notebook.original_code == f"# {NOTEBOOK_HEADER}\na = 12"
+    path_lookup.resolve.assert_called_once_with(path)
+
+
+def test_notebook_loader_cannot_load_empty_file(tmp_path) -> None:
+    """Empty file fails because it misses the notebook header."""
+    path = tmp_path / "empty.py"
+    path.write_text("")
+    dependency = Dependency(NotebookLoader(), path)
+    path_lookup = create_autospec(PathLookup)
+    path_lookup.resolve.return_value = path
+
+    notebook = dependency.load(path_lookup)
+
+    # TODO: Test specific error while loading: https://github.com/databrickslabs/ucx/issues/3584
+    assert not notebook
+    path_lookup.resolve.assert_called_once_with(path)
+
+
+def test_notebook_loader_ignores_path_by_loading_it_as_stub_container(tmp_path) -> None:
+    path = tmp_path / "path.py"
+    dependency = Dependency(NotebookLoader(exclude_paths={path}), path)
+    path_lookup = create_autospec(PathLookup)
+    path_lookup.resolve.return_value = path
+
+    stub = dependency.load(path_lookup)
+
+    assert isinstance(stub, StubContainer)
+    path_lookup.resolve.assert_called_once_with(path)
