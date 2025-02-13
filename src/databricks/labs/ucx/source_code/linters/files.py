ed7df6758c9998ce0bd3982a1424001de3573b16
Cor <jczuurmond@protonmail.com>
2025-02-13 10:09:39 +0100

    Rename file language helper function (#3661)
    ## Changes
    Rename and document getting supported file language helper function to
    make more clear what it does.
    
    IMO it was not clear that this function contains essential decision
    power as it is the filter for passing files to the linter only if they
    are supported (in contrast with inferring any language - also the
    non-supported ones).
    
    For example, before #3655, the `FileLinter` contains filters for
    extensions, however this redundant as that is already handled by this
    helper function.
    
    The rename makes it more explicit what this function does. The
    documentation makes it more clear how to use the function
    
    ### Functionality
    
    - [x] modified linter related code.

--- src/databricks/labs/ucx/source_code/linters/files.py
+++ src/databricks/labs/ucx/source_code/linters/files.py
@@ -9,9 +9,9 @@ from typing import cast
 from databricks.sdk.service.workspace import Language
 
 from databricks.labs.ucx.source_code.base import (
-    file_language,
     Advice,
     Failure,
+    infer_file_language_if_supported,
     safe_read_text,
 )
 from databricks.labs.ucx.source_code.files import LocalFile
@@ -202,9 +202,9 @@ class NotebookLinter:
         if resolved is None:
             return None  # already reported during dependency building
         # TODO deal with workspace notebooks
-        language = file_language(resolved)
+        language = infer_file_language_if_supported(resolved)
         # we only support Python notebooks for now
-        if language is not Language.PYTHON:
+        if language != Language.PYTHON:
             logger.warning(f"Unsupported notebook language: {language}")
             return None
         source = safe_read_text(resolved)
