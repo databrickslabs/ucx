855ae587d98c1c93d7f1a5b2fc8db8f986229692
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-14 12:18:28 +0100

    Add docstring to notebook source container

--- src/databricks/labs/ucx/source_code/notebooks/sources.py
+++ src/databricks/labs/ucx/source_code/notebooks/sources.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 
 class Notebook(SourceContainer):
-    """
+    """A notebook source code container.
 
     TODO:
         Let `Notebook` inherit from `LocalFile`
