143a6a233b50446c072a36a1da3618f704f3df6a
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-17 09:27:46 +0100

    Move GITHUB_URL to global variable

--- src/databricks/labs/ucx/__init__.py
+++ src/databricks/labs/ucx/__init__.py
@@ -19,3 +19,7 @@ ua.with_extra("ucx", __version__)
 
 # Add ucx/<version> for re-packaging of ucx, where product name is omitted
 ua.with_product("ucx", __version__)
+
+
+DOCS_URL = "https://databrickslabs.github.io/ucx/docs/"
+GITHUB_URL = "https://github.com/databrickslabs/ucx"
