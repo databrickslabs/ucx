0e1936d0b398e00812162f49fa060ba7659bf761
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-19 17:16:24 +0100

    Test labels in url

--- src/databricks/labs/ucx/github.py
+++ src/databricks/labs/ucx/github.py
@@ -18,10 +18,14 @@ def construct_new_issue_url(
     issue_type: IssueType,
     title: str,
     body: str,
+    *,
+    labels: set[str] | None = None,
 ) -> str:
     """Construct a new issue URL.
 
     References:
     - https://docs.github.com/en/issues/tracking-your-work-with-issues/using-issues/creating-an-issue#creating-an-issue-from-a-url-query
     """
-    return f"{GITHUB_URL}/issues/new?type={issue_type.value}&title={title}&body={body}"
+    labels = labels or set()
+    labels.add("needs-triage")
+    return f"{GITHUB_URL}/issues/new?type={issue_type.value}&title={title}&body={body}&labels={','.join(labels)}"
--- tests/unit/test_github.py
+++ tests/unit/test_github.py
@@ -1,6 +1,23 @@
+import pytest
+
 from databricks.labs.ucx.github import GITHUB_URL, IssueType, construct_new_issue_url
 
 
 def test_construct_new_issue_url_starts_with_github_url() -> None:
+    """Test that the URL starts with the GitHub URL."""
     url = construct_new_issue_url(IssueType.FEATURE, "title", "body")
     assert url.startswith(GITHUB_URL)
+
+
+def test_construct_new_issue_url_with_labels() -> None:
+    """Test that the URL contains the labels."""
+    url = construct_new_issue_url(IssueType.FEATURE, "title", "body", labels={"label1", "label2"})
+    assert "label1" in url
+    assert "label2" in url
+
+
+@pytest.mark.parametrize("labels", [None, {}, {"label1", "label2"}])
+def test_construct_new_issue_url_always_has_needs_triage_label(labels: set[str] | None) -> None:
+    """Test that the URL always has the `needs-triage` label."""
+    url = construct_new_issue_url(IssueType.FEATURE, "title", "body", labels=labels)
+    assert "needs-triage" in url
