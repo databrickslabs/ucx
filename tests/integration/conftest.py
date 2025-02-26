d012656fc0c7cae47afa92827b7a8620086de09f
Cor Zuurmond <jczuurmond@protonmail.com>
2025-02-26 14:32:09 +0100

    Use make_dashboard method instead of fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -632,9 +632,9 @@ class MockRuntimeContext(
         self.make_job(content="spark.read.parquet('dbfs://mnt/file/')", task_type=SparkPythonTask)
         self.make_job(content="spark.table('some.table')", task_type=SparkPythonTask)
         query_1 = self.make_query(sql_query='SELECT * from parquet.`dbfs://mnt/foo2/bar2`')
-        self._make_dashboard(query=query_1)
+        self.make_dashboard(query=query_1)
         query_2 = self.make_query(sql_query='SELECT * from my_schema.my_table')
-        self._make_dashboard(query=query_2)
+        self.make_dashboard(query=query_2)
 
     def add_table(self, table: TableInfo):
         self._tables.append(table)
