f98a710d84f7c0747de78cdea8b196c42cf2b244
Cor Zuurmond <jczuurmond@protonmail.com>
2025-03-03 11:02:16 +0100

    Match grants crawler signature with other signatures

--- tests/unit/hive_metastore/test_grants.py
+++ tests/unit/hive_metastore/test_grants.py
@@ -190,7 +190,7 @@ def test_crawler_no_data() -> None:
     sql_backend = MockBackend()
     table = TablesCrawler(sql_backend, "schema")
     udf = UdfsCrawler(sql_backend, "schema")
-    crawler = GrantsCrawler(table, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf)
     grants = list(crawler.snapshot())
     assert len(grants) == 0
 
@@ -252,7 +252,7 @@ def test_crawler_crawl() -> None:
     }
     table = TablesCrawler(sql_backend, "schema")
     udf = UdfsCrawler(sql_backend, "schema")
-    crawler = GrantsCrawler(table, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf)
     grants = list(crawler.snapshot())
     assert len(grants) == len(expected_grants) and set(grants) == expected_grants
 
@@ -301,7 +301,7 @@ def test_crawler_udf_crawl() -> None:
 
     table = TablesCrawler(sql_backend, "schema")
     udf = UdfsCrawler(sql_backend, "schema")
-    crawler = GrantsCrawler(table, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf)
     grants = list(crawler.snapshot())
 
     assert len(grants) == len(expected_grants) and set(grants) == expected_grants
@@ -311,7 +311,7 @@ def test_crawler_snapshot_when_no_data() -> None:
     sql_backend = MockBackend()
     table = TablesCrawler(sql_backend, "schema")
     udf = UdfsCrawler(sql_backend, "schema")
-    crawler = GrantsCrawler(table, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf)
     snapshot = list(crawler.snapshot())
     assert len(snapshot) == 0
 
@@ -320,7 +320,7 @@ def test_crawler_snapshot_with_data() -> None:
     sql_backend = MockBackend(rows=ROWS)
     table = TablesCrawler(sql_backend, "schema")
     udf = UdfsCrawler(sql_backend, "schema")
-    crawler = GrantsCrawler(table, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf)
     snapshot = list(crawler.snapshot())
     assert len(snapshot) == 3
 
@@ -348,7 +348,7 @@ def test_grants_returning_error_when_showing_grants() -> None:
     backend = MockBackend(fails_on_first=errors, rows=rows)
     table_crawler = TablesCrawler(backend, "default")
     udf = UdfsCrawler(backend, "default")
-    crawler = GrantsCrawler(table_crawler, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table_crawler, udf)
 
     results = list(crawler.snapshot())
     assert results == [
@@ -384,7 +384,7 @@ def test_grants_returning_error_when_describing() -> None:
     backend = MockBackend(fails_on_first=errors, rows=rows)
     table_crawler = TablesCrawler(backend, "default")
     udf = UdfsCrawler(backend, "default")
-    crawler = GrantsCrawler(table_crawler, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table_crawler, udf)
 
     results = list(crawler.snapshot())
     assert results == [
@@ -423,7 +423,7 @@ def test_udf_grants_returning_error_when_showing_grants() -> None:
     backend = MockBackend(fails_on_first=errors, rows=rows)
     table_crawler = TablesCrawler(backend, "default")
     udf = UdfsCrawler(backend, "default")
-    crawler = GrantsCrawler(table_crawler, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table_crawler, udf)
 
     results = list(crawler.snapshot())
     assert results == [
@@ -459,7 +459,7 @@ def test_udf_grants_returning_error_when_describing() -> None:
     backend = MockBackend(fails_on_first=errors, rows=rows)
     table_crawler = TablesCrawler(backend, "default")
     udf = UdfsCrawler(backend, "default")
-    crawler = GrantsCrawler(table_crawler, udf)
+    crawler = GrantsCrawler(sql_backend, inventory_database, table_crawler, udf)
 
     results = list(crawler.snapshot())
     assert results == [
@@ -504,7 +504,7 @@ def test_crawler_should_filter_databases() -> None:
 
     table = TablesCrawler(sql_backend, "schema", include_databases=["database_one"])
     udf = UdfsCrawler(sql_backend, "schema", include_databases=["database_one"])
-    crawler = GrantsCrawler(table, udf, include_databases=["database_one"])
+    crawler = GrantsCrawler(sql_backend, inventory_database, table, udf, include_databases=["database_one"])
     grants = list(crawler.snapshot())
 
     assert "SHOW DATABASES" not in sql_backend.queries
