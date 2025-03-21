ed964a6081f6f9778eccd31aa5f3d9eb76600619
Pritish Pai <136742693+pritishpai@users.noreply.github.com>
2025-03-21 10:22:38 -0400

    Add links to compute summary widget (#3952)
    ## Changes
    Add links to compute summary widgets
    
    ### Linked issues
    Resolves #3260
    
    
    ### Tests
    - [x] manually tested
    
    
    ![image](https://github.com/user-attachments/assets/8ecb04e1-e64e-48e9-8c29-20971ca94c38)

--- src/databricks/labs/ucx/queries/assessment/main/20_0_clusters.sql
+++ src/databricks/labs/ucx/queries/assessment/main/20_0_clusters.sql
@@ -1,8 +1,21 @@
-/* --title 'Incompatible clusters' --width 3 --height 6 */
+/*
+--title 'Incompatible clusters'
+--width 3
+--height 6
+--overrides '{"spec":{
+    "encodings":{
+      "columns": [
+        {"fieldName": "cluster_id", "booleanValues": ["false", "true"], "linkUrlTemplate": "/compute/clusters/{{ @ }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link", "title": "cluster_id"},
+        {"fieldName": "cluster_name", "booleanValues": ["false", "true"], "linkUrlTemplate": "/compute/clusters/{{ cluster_id }}", "linkTextTemplate": "{{ @ }}", "linkTitleTemplate": "{{ @ }}", "linkOpenInNewTab": true, "type": "string", "displayAs": "link",  "title": "cluster_name"},
+        {"fieldName": "finding", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "finding"},
+        {"fieldName": "creator", "booleanValues": ["false", "true"], "type": "string", "displayAs": "string", "title": "creator"}
+      ]}
+  }}'
+*/
 SELECT
-  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
   cluster_id,
   cluster_name,
+  EXPLODE(FROM_JSON(failures, 'array<string>')) AS finding,
   creator
 FROM inventory.clusters
 WHERE
