f86a6a70e775cf5c4123a9b086adfebd56bbf6ef
hari-selvarajan_data <hari.selvarajan@databricks.com>
2025-03-24 22:27:07 +0000

    review comments

--- src/databricks/labs/ucx/aws/credentials.py
+++ src/databricks/labs/ucx/aws/credentials.py
@@ -60,7 +60,7 @@ class CredentialManager:
     def list_glue(self) -> dict[str, str]:
         # list existed service credentials that are using iam roles, capturing the arns and names
         try:
-            credential_response = self._ws.storage_credentials.list()
+            credential_response = self._ws.credentials.list_credentials()
         except NotFound:
             logger.info('Could not retrieve credentials for Glue access. ')
             return {}
