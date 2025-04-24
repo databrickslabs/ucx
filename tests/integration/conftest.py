c336853029b3a1b392c53bfeb6eb2a500b92094f
pritishpai <pritish.pai@databricks.com>
2025-04-24 09:43:50 -0400

    skip make_dashboard in fixture
    ## query_1 = self.make_query(sql_query="SELECT * from parquet.`dbfs://mnt/foo2/bar2`")
    
    ## self.make_dashboard(query=query_1)

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -631,8 +631,9 @@ class MockRuntimeContext(
         self.make_job(content="spark.table('old.stuff')")
         self.make_job(content="spark.read.parquet('dbfs://mnt/file/')", task_type=SparkPythonTask)
         self.make_job(content="spark.table('some.table')", task_type=SparkPythonTask)
-        query_1 = self.make_query(sql_query="SELECT * from parquet.`dbfs://mnt/foo2/bar2`")
-        self.make_dashboard(query=query_1)
+
+        # TODO: Removed because of deprecation of Legacy Dashboard creation
+
         self.make_lakeview_dashboard(query="SELECT * from my_schema.my_table")
 
     def add_table(self, table: TableInfo):
