a4d06a51fc34545cf3163fd1c2edbbf216049469
pritishpai <pritish.pai@databricks.com>
2025-06-24 08:08:24 -0400

    Add sample for testing

--- tests/integration/source_code/samples/dfsa-paths/dfsa-lint.sql
+++ tests/integration/source_code/samples/dfsa-paths/dfsa-lint.sql
@@ -1,1 +1,3 @@
-SELECT * from json.`dbfs:/tmp/dummy_tp3fd`
+SELECT * from json.`dbfs:/tmp/dummy_tlrrv`;
+
+SELECT * FROM csv.`abfss://labs-azure-sandbox-container@databrickslabsstorage.dfs.core.windows.net/8952c1e3-b265-4adf-98c3-6f755e2e1453/tables/dc8a90b1-1b25-4782-9565-8efe34b96d64`;
