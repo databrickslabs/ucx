a40654b9304e1d414abd438a1860f8459fe6af04
pritishpai <pritish.pai@databricks.com>
2025-08-11 22:17:45 -0400

    Fix retry on

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1466,7 +1466,7 @@ def pytest_ignore_collect(path):
         logger.debug(f"pytest_ignore_collect: error: {err}")
         return False
 
-@retried(on=[KeyError], timeout=timedelta(minutes=2))
+@retried(on=[AssertionError], timeout=timedelta(seconds=30))
 def get_group(group_manager: GroupManager, group_name: str) -> NoReturn:
     """
     # Group deletion is eventually consistent. Although the group manager tries to wait for convergence, parts of the
